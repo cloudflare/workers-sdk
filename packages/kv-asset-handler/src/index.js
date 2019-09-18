d6c5e1fb24c961e5ee05feb2c25c9a32692b7f8f
Victoria <bernard.victoria22@gmail.com>
2019-09-18 18:11:05 -0500

    Add tests and use url string as input

--- packages/kv-asset-handler/src/index.js
+++ packages/kv-asset-handler/src/index.js
@@ -1,35 +1,37 @@
 import mime from 'mime/lite'
 
-const getAssetFromKV = async request => {
-  if (request.method !== 'GET') {
-    throw `this is not a GET request: ${request.method}`
+const defaultKeyModifer = url => {
+  let parsedURL = new URL(url)
+  let pathname = parsedURL.pathname
+  if (pathname.endsWith('/')) {
+    parsedURL += 'index.html'
   }
-  return path.slice(1)
+  return parsedURL.toString()
 }
-const getAssetFromKV = async (path, keyModifer = defaultKeyModifer) => {
+const getAssetFromKV = async (url, keyModifer = defaultKeyModifer) => {
   if (typeof __STATIC_CONTENT === undefined) {
     // TODO: should we say the binding is __STATIC_CONTENT or that they need change wrangler.toml
     throw 'there are no assets defined in KV'
   }
   // TODO: throw if path manifest is undefined
   // TODO: throw if path is not in manifest
-
+  const key = keyModifer(url)
   const cache = caches.default
-  const pathname = new URL(request.url).pathname.slice(1)
 
   // TODO: match cache on manifest
   // Object.assign(request, new Request(manifest[request.url]))
 
-  let response = await cache.match(request)
+  let response = await cache.match(key)
 
   if (!response) {
+    const parsedURL = new URL(url)
+    const pathname = parsedURL.pathname
     const mimeType = mime.getType(pathname)
-    const body = await __STATIC_CONTENT.get(pathname, 'arrayBuffer')
-    if (body === null) {
+    const body = await __STATIC_CONTENT.get(key, 'arrayBuffer')
+    if (!body) {
       // TODO: should we include something about wrangler here
-      throw `could not find ${pathname} in KV`
+      throw `could not find ${key} in KV`
     }
-
     response = new Response(body)
     response.headers.set('Content-Type', mimeType)
 
@@ -42,7 +44,6 @@ const getAssetFromKV = async (path, keyModifer = defaultKeyModifer) => {
     // 	event.waitUntil(cache.put(req, res));
     // }
   }
-
   return response
 }
 
