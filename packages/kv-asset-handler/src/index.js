0ae10300be0f901e0495d95570db93887d9278b2
Avery Harnish <averyharnish@gmail.com>
2019-09-18 10:33:03 -0500

    Remove condition and export a single function

--- packages/kv-asset-handler/src/index.js
+++ packages/kv-asset-handler/src/index.js
@@ -1,44 +1,45 @@
 import mime from "mime/lite";
 
-class AssetWorker {
-  async condition(request) {
-    if (request.method === "GET" && typeof __STATIC_CONTENT !== undefined) {
-      // TODO
-      // and manifest exists
-      // and path is in manifest
-      return true;
-    }
-    return false;
+const getAssetFromKV = async(request) => {
+  if (request.method !== "GET") {
+    throw `this is not a GET request: ${request.method}`
+  }
+  if (typeof __STATIC_CONTENT === undefined) {
+    // TODO: should we say the binding is __STATIC_CONTENT or that they need change wrangler.toml
+    throw "there are no assets defined in KV"
+  }
+  // TODO: throw if path manifest is undefined
+  // TODO: throw if path is not in manifest
+
+  const cache = caches.default;
+  const pathname = new URL(request.url).pathname.slice(1);
+
+  // TODO: match cache on manifest
+  // Object.assign(request, new Request(manifest[request.url]))
+  
+  let response = await cache.match(request);
+
+  if (!response) {
+    const mimeType = mime.getType(pathname);
+    const body = await __STATIC_CONTENT.get(
+      pathname,
+      "arrayBuffer"
+    );
+
+    response = new Response(body);
+    response.headers.set("Content-Type", mimeType);
+
+    // TODO: cache asset
+    // event.waitUntil(cache.put(request, response.clone()));
+
+    // TODO: add browser caching
+    // if (cachePaths.some(cachePath => minimatch(path, cachePath))) {
+    // 	response.headers.set("Cache-Control", "max-age=31536000, immutable");
+    // 	event.waitUntil(cache.put(req, res));
+    // }
   }
 
-  async handler(request) {
-    const cache = caches.default;
-    const pathname = new URL(request.url).pathname.slice(1);
-    // TODO: match cache on manifest
-    let response = await cache.match(request);
-
-    if (!response) {
-      const mimeType = mime.getType(pathname);
-      const body = await __STATIC_CONTENT.get(
-        //assetManifest[pathname],
-        pathname,
-        "arrayBuffer"
-      );
-
-      // TODO: what should we do when body is empty?
-
-      response = new Response(body);
-      response.headers.set("Content-Type", mimeType);
-
-      // TODO: add browser caching
-      // if (cachePaths.some(cachePath => minimatch(path, cachePath))) {
-      // 	response.headers.set("Cache-Control", "max-age=31536000, immutable");
-      // 	event.waitUntil(cache.put(req, res));
-      // }
-    }
-
-    return response;
-  }
+  return response;
 }
 
-export { AssetWorker };
+export { getAssetFromKV };
