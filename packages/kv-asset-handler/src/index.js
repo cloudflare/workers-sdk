c73ac7350f1881e97b722ee0cc634b4ab7ddf0c1
Victoria Bernard <bernard.victoria22@gmail.com>
2019-09-18 18:15:07 -0500

    Merge pull request #3 from cloudflare/victoria/prettier
    formatting

--- packages/kv-asset-handler/src/index.js
+++ packages/kv-asset-handler/src/index.js
@@ -1,10 +1,8 @@
 import mime from 'mime/lite'
 
-const defaultKeyModifer = url => {
-  let parsedURL = new URL(url)
-  let pathname = parsedURL.pathname
-  if (pathname.endsWith('/')) {
-    parsedURL += 'index.html'
+const getAssetFromKV = async request => {
+  if (request.method !== 'GET') {
+    throw `this is not a GET request: ${request.method}`
   }
   return parsedURL.toString()
 }
@@ -15,23 +13,23 @@ const getAssetFromKV = async (url, keyModifer = defaultKeyModifer) => {
   }
   // TODO: throw if path manifest is undefined
   // TODO: throw if path is not in manifest
-  const key = keyModifer(url)
+
   const cache = caches.default
+  const pathname = new URL(request.url).pathname.slice(1)
 
   // TODO: match cache on manifest
   // Object.assign(request, new Request(manifest[request.url]))
 
-  let response = await cache.match(key)
+  let response = await cache.match(request)
 
   if (!response) {
-    const parsedURL = new URL(url)
-    const pathname = parsedURL.pathname
     const mimeType = mime.getType(pathname)
-    const body = await __STATIC_CONTENT.get(key, 'arrayBuffer')
-    if (!body) {
+    const body = await __STATIC_CONTENT.get(pathname, 'arrayBuffer')
+    if (body === null) {
       // TODO: should we include something about wrangler here
       throw `could not find ${key} in KV`
     }
+
     response = new Response(body)
     response.headers.set('Content-Type', mimeType)
 
@@ -44,6 +42,7 @@ const getAssetFromKV = async (url, keyModifer = defaultKeyModifer) => {
     // 	event.waitUntil(cache.put(req, res));
     // }
   }
+
   return response
 }
 
