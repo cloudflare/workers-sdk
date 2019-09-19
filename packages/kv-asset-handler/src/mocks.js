5e6d45f50a93a8990941009b3a29b62e8a8cc56c
Avery Harnish <averyharnish@gmail.com>
2019-09-19 14:03:27 -0500

    Add asset manifest and cache handling logic

--- packages/kv-asset-handler/src/mocks.js
+++ packages/kv-asset-handler/src/mocks.js
@@ -1,22 +1,33 @@
 const makeServiceWorkerEnv = require('service-worker-mock')
 
+const HASH = "123-I-AM-A-HASH-BROWN"
+
 export const mockKV = () => {
   const store = {
-    'https://blah.com/key1.txt': 'val1',
-    'https://blah.com/index.html': 'index.html',
+    `https://blah.com/key1.txt-${HASH}`: 'val1',
+    `https://blah.com/index.html-${HASH}`: 'index.html',
   }
   return {
     get: path => store[path] || null,
   }
 }
+
+export const mockManifest = () => {
+  return {
+    "key1.txt": `key1.txt-${HASH}`,
+    "index.html": `index.html-${HASH}`
+  }
+}
+
 export const mockCaches = () => {
-  const store = { 'https://blah.com/key1.txt': 'val1' }
+  const store = { `https://blah.com/key1.txt-${HASH}`: 'val1' }
   return {
     default: { match: () => null },
   }
 }
 export function mockGlobal() {
   Object.assign(global, makeServiceWorkerEnv())
+  Object.assign(global, { __STATIC_CONTENT_MANIFEST: mockManifest() })
   Object.assign(global, { __STATIC_CONTENT: mockKV() })
   Object.assign(global, { caches: mockCaches() })
 }
