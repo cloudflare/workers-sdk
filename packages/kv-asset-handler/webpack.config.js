c51488ae11ede5d2cbaab04571528194b6000287
Victoria <bernard.victoria22@gmail.com>
2019-11-13 16:44:45 -0600

    Typescript working

---
+++ packages/kv-asset-handler/webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: './dist/bundle.js',
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+}
