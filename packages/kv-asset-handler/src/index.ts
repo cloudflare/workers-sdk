51badae3f9c5ec81b1f8622f92988ae6483e1f6e
Victoria <Bernard.victoria22@gmail.com>
2020-02-19 14:39:03 -0600

    Be explict about ASSET Manifest type

--- packages/kv-asset-handler/src/index.ts
+++ packages/kv-asset-handler/src/index.ts
@@ -2,7 +2,7 @@ import * as mime from 'mime'
 import { Options, CacheControl, MethodNotAllowedError, NotFoundError, InternalError } from './types'
 
 declare global {
-  var __STATIC_CONTENT: any, __STATIC_CONTENT_MANIFEST: any
+  var __STATIC_CONTENT: any, __STATIC_CONTENT_MANIFEST: string
 }
 /**
  * maps the path of incoming request to the request pathKey to look up
@@ -65,7 +65,7 @@ const defaultCacheControl: CacheControl = {
  * @param {{mapRequestToAsset: (string: Request) => Request, cacheControl: {bypassCache:boolean, edgeTTL: number, browserTTL:number}, ASSET_NAMESPACE: any, ASSET_MANIFEST:any}} [options] configurable options
  * @param {CacheControl} [options.cacheControl] determine how to cache on Cloudflare and the browser
  * @param {typeof(options.mapRequestToAsset)} [options.mapRequestToAsset]  maps the path of incoming request to the request pathKey to look up
- * @param {any} [options.ASSET_NAMESPACE] the binding to the namespace that script references
+ * @param {Object | string} [options.ASSET_NAMESPACE] the binding to the namespace that script references
  * @param {any} [options.ASSET_MANIFEST] the map of the key to cache and store in KV
  * */
 const getAssetFromKV = async (event: FetchEvent, options?: Partial<Options>): Promise<Response> => {
@@ -82,7 +82,7 @@ const getAssetFromKV = async (event: FetchEvent, options?: Partial<Options>): Pr
 
   const request = event.request
   const ASSET_NAMESPACE = options.ASSET_NAMESPACE
-  const ASSET_MANIFEST = options.ASSET_MANIFEST
+  const ASSET_MANIFEST = typeof (options.ASSET_MANIFEST) === 'string'
     ? JSON.parse(options.ASSET_MANIFEST)
     : options.ASSET_MANIFEST
 
