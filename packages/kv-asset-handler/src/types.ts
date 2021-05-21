40849b0531437ab39b4873ebbaa597c202235a90
Lucas Boemeke <lucas@blueshift.cc>
2021-05-21 16:18:04 -0300

    make Options.mapRequestToAsset optional

--- packages/kv-asset-handler/src/types.ts
+++ packages/kv-asset-handler/src/types.ts
@@ -7,7 +7,7 @@ export type Options = {
   cacheControl: ((req: Request) => Partial<CacheControl>) | Partial<CacheControl>
   ASSET_NAMESPACE: any
   ASSET_MANIFEST: Object | string
-  mapRequestToAsset: (req: Request) => Request
+  mapRequestToAsset?: (req: Request, options?: Partial<Options>) => Request
   defaultMimeType: string
   defaultDocument: string
 }
