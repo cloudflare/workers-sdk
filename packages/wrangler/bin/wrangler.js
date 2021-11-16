d2ac3e3bc40f6f6c2a0b3acd5de4914294abefeb
Greg Brimble <gbrimble@cloudflare.com>
2021-11-16 00:04:20 +0000

    Cross-compatibility bootstrap script for Windows 10, macOS Big Sur/Monterey & Ubuntu 16/20 (#23)
    * Cross-compatibility bootstrap script for Windows 10, macOS Big Sur/Monterey & Ubuntu 16/20
    
    * Remove nullish-coalescing and use process.exit

--- packages/wrangler/bin/wrangler.js
+++ packages/wrangler/bin/wrangler.js
@@ -1,2 +1,36 @@
-#!/usr/bin/env -S node --no-warnings
-require("../wrangler-dist/index_node.js");
+#!/usr/bin/env node
+const { spawn } = require("child_process");
+const { join } = require("path");
+const semiver = require("semiver");
+
+const MIN_NODE_VERSION = "16.7.0";
+
+async function main() {
+  if (semiver(process.versions.node, MIN_NODE_VERSION) < 0) {
+    // Note Volta and nvm are also recommended in the official docs:
+    // https://developers.cloudflare.com/workers/get-started/guide#2-install-the-workers-cli
+    console.error(
+      `Wrangler requires at least Node.js ${MIN_NODE_VERSION}. 
+You should use the latest Node.js version if possible, as Cloudflare Workers use a very up-to-date version of V8.
+Consider using a Node.js version manager such as https://volta.sh/ or https://github.com/nvm-sh/nvm.`
+    );
+    process.exit(1);
+    return;
+  }
+
+  spawn(
+    process.execPath,
+    [
+      "--no-warnings",
+      "--experimental-vm-modules",
+      ...process.execArgv,
+      join(__dirname, "../wrangler-dist/index_node.js"),
+      ...process.argv.slice(2),
+    ],
+    { stdio: "inherit" }
+  ).on("exit", (code) =>
+    process.exit(code === undefined || code === null ? 0 : code)
+  );
+}
+
+void main();
