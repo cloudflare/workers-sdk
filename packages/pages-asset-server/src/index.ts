a8193b822efe73bfb6860861acfa3fb21392c01c
Greg Brimble <gbrimble@cloudflare.com>
2021-11-29 14:03:39 +0000

    Tidy up scaffolding

--- packages/pages-asset-server/src/index.ts
+++ packages/pages-asset-server/src/index.ts
@@ -5,11 +5,16 @@ import { hideBin } from "yargs/helpers";
 
 yargs(hideBin(process.argv))
   .command(
-    "test",
-    "test",
-    () => {},
-    () => {
-      console.log("Testtt");
+    "serve [directory]",
+    "Serve a directory of static assets.",
+    (yargs) => {
+      return yargs.positional("directory", {
+        type: "string",
+        description: "The directory of static assets to serve.",
+      });
+    },
+    ({ directory }) => {
+      console.log(`Serving ${directory}...`);
     }
   )
   .parse();
