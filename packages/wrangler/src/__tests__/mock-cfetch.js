1055c0e57fd8fb52a2a1791c9ebb472a91774a72
Sunil Pai <threepointone@gmail.com>
2021-11-29 00:13:26 +0000

    proper tests, a better `init`, and a type fix
    - This commit lands a 'proper' test setup. specifically, it mocks `cfetch` so one can "send" whatever responses we want during tests (see kv:namespace tests for examples). I modified the jest config and how we run the script to match.
    -  This commit adds dialogs.tsx, for a couple of common ui patterns. we're starting with `confirm`, and I'll add `select` and `alert` later.
    - finally, this adds an enhancement to `init`, offering to create a package.json and/or a tsconfig if required, including installing `@clouflare/workers-types` into the nearest `package.json`

---
+++ packages/wrangler/src/__tests__/mock-cfetch.js
@@ -0,0 +1,46 @@
+// This file mocks ../cfetch.ts
+// so we can insert whatever responses we want from it
+
+const pathToRegexp = require("path-to-regexp");
+// TODO: add jsdoc style types here
+
+// type MockHandler = (resource: string, init?: RequestInit) => any; // TODO: use a generic here
+
+let mocks = [];
+
+function mockCfetch(resource, init) {
+  for (const { regexp, handler } of mocks) {
+    if (regexp.test(resource)) {
+      return handler(resource, init); // should we have some kind of fallthrough system? we'll see.
+    }
+  }
+  throw new Error(`no mocks found for ${resource}`);
+}
+
+function setMock(resource, handler) {
+  const mock = {
+    resource,
+    handler,
+    regexp: pathToRegexp(resource),
+  };
+  mocks.push(mock);
+  return () => {
+    mocks = mocks.filter((x) => x !== mock);
+  };
+}
+
+function unsetAllMocks() {
+  mocks = [];
+}
+
+const CF_API_BASE_URL =
+  process.env.CF_API_BASE_URL || "https://api.cloudflare.com/client/v4";
+
+Object.assign(module.exports, {
+  __esModule: true,
+  default: mockCfetch,
+  mockCfetch,
+  setMock,
+  unsetAllMocks,
+  CF_API_BASE_URL,
+});
---
+++ packages/wrangler/src/dialogs.tsx
@@ -0,0 +1,85 @@
+import * as React from "react";
+import { useState } from "react";
+import { Box, Text, useInput, render } from "ink";
+import TextInput from "ink-text-input";
+type ConfirmProps = {
+  text: string;
+  onConfirm: (answer: boolean) => void | Promise<void>;
+};
+function Confirm(props: ConfirmProps) {
+  useInput((input: string) => {
+    if (input === "y") {
+      props.onConfirm(true);
+    } else if (input === "n") {
+      props.onConfirm(false);
+    } else {
+      console.log("Unrecognised input");
+    }
+  });
+  return (
+    <Box>
+      <Text>{props.text} (y/n) </Text>
+    </Box>
+  );
+}
+
+export function confirm(text: string): Promise<boolean> {
+  return new Promise((resolve) => {
+    const { unmount } = render(
+      <Confirm
+        text={text}
+        onConfirm={(answer: boolean) => {
+          unmount();
+          resolve(answer);
+        }}
+      />
+    );
+  });
+}
+
+type PromptProps = {
+  text: string;
+  type?: "text" | "password";
+  onSubmit: (text: string) => void;
+};
+
+export function Prompt(props: PromptProps) {
+  const [value, setValue] = useState("");
+
+  return (
+    <Box>
+      <Text>{props.text} </Text>
+      <Box>
+        <TextInput
+          mask={props.type === "password" ? "*" : undefined}
+          value={value}
+          onChange={setValue}
+          onSubmit={props.onSubmit}
+        />
+      </Box>
+    </Box>
+  );
+}
+
+export async function prompt(text: string, type: "text" | "password" = "text") {
+  return new Promise((resolve) => {
+    const { unmount } = render(
+      <Prompt
+        text={text}
+        type={type}
+        onSubmit={(text) => {
+          unmount();
+          resolve(text);
+        }}
+      />
+    );
+  });
+}
+
+// this one feels a little non "standard"
+// export async function select(
+//   title: string,
+//   choices: { label: string; value: string }[]
+// ): Promise<{ label: string; value: string }>{
+
+// }
