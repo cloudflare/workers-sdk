ada913b7f9f9c30e23d745962f0ce7ff876f6099
Sunil Pai <threepointone@gmail.com>
2021-12-15 14:35:29 +0000

    Update packages/wrangler/src/dev.tsx
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>

--- packages/wrangler/src/dev.tsx
+++ packages/wrangler/src/dev.tsx
@@ -374,7 +374,7 @@ function useEsbuild(props: {
         ...(jsxFactory && { jsxFactory }),
         ...(jsxFragment && { jsxFragment }),
         external: ["__STATIC_CONTENT_MANIFEST"],
-        conditions: ["worker", "browser", "module", "imports", "default"],
+        conditions: ["worker", "browser"],
         plugins: [moduleCollector.plugin],
         // TODO: import.meta.url
         watch: {
