cd6c3bffaff675f4386ab6403ebc14a3ef91e93a
Greg Brimble <developer@gregbrimble.com>
2021-12-21 22:52:18 +0000

    esbuild doesn't actually report when complete. Switching to sleep

--- packages/wrangler/src/pages.tsx
+++ packages/wrangler/src/pages.tsx
@@ -726,8 +726,6 @@ export const pages: BuilderCallback<unknown, unknown> = (yargs) => {
 
       let miniflareArgs: MiniflareOptions = {};
 
-      let functionsCompiler: Promise<BuildResult>;
-
       if (usingFunctions) {
         const scriptPath = join(tmpdir(), "./functionsWorker.js");
         const routesModule = join(tmpdir(), "./functionsRoutes.mjs");
@@ -743,7 +741,7 @@ export const pages: BuilderCallback<unknown, unknown> = (yargs) => {
           outfile: routesModule,
         });
 
-        functionsCompiler = buildWorker({
+        buildWorker({
           routesModule,
           outfile: scriptPath,
           minify: false, // TODO: Expose option to enable
