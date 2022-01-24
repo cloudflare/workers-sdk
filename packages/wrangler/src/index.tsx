71b0fab02e4f65342b4b106f9dc3fa6a98db2a19
Pete Bacon Darwin <pete@bacondarwin.com>
2022-01-24 21:52:38 +0000

    fix: warn if the `site.entry-point` configuration is found during publishing
    Also updates the message and adds a test for the error when there is no entry-point specified.
    
    Fixes #282

--- packages/wrangler/src/index.tsx
+++ packages/wrangler/src/index.tsx
@@ -494,6 +494,14 @@ export async function main(argv: string[]): Promise<void> {
         );
       }
 
+      if (config.site?.["entry-point"]) {
+        console.warn(
+          "Deprecation notice: The `site.entry-point` config field is no longer used.\n" +
+            "The entry-point is specified via the command line (e.g. `wrangler dev path/to/script`).\n" +
+            "Please remove the `site.entry-point` field from the `wrangler.toml` file."
+        );
+      }
+
       if (!args.local) {
         // -- snip, extract --
         const loggedIn = await loginOrRefreshIfRequired();
