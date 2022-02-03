8db6755b5b5c8f92ca0bed3bbf6ff3c04e4b4f09
Greg Brimble <gbrimble@cloudflare.com>
2022-02-03 16:02:27 +0000

    Add boot-up test

--- packages/example-remix-cloudflare-pages-app/tests/index.test.ts
+++ packages/example-remix-cloudflare-pages-app/tests/index.test.ts
@@ -1,4 +1,4 @@
-import { ChildProcess, exec, spawn } from "child_process";
+import { ChildProcess, spawn } from "child_process";
 import { fetch } from "undici";
 
 const RUNNING_PROCESSES: ChildProcess[] = [];
@@ -6,16 +6,20 @@ const RUNNING_PROCESSES: ChildProcess[] = [];
 describe("Remix", () => {
   beforeAll(async () => {
     RUNNING_PROCESSES.push(spawn("npm run dev:remix", { shell: true }));
-    RUNNING_PROCESSES.push(spawn("npm run dev:wrangler", { shell: true }));
+    RUNNING_PROCESSES.push(
+      spawn("BROWSER=none npm run dev:wrangler", { shell: true })
+    );
   });
 
   afterAll(() => {
+    console.log("Called");
     RUNNING_PROCESSES.forEach((runningProcess) => {
       runningProcess.kill();
     });
   });
 
   it("renders", async () => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     const response = await fetch("http://localhost:8788/");
     const text = await response.text();
     expect(text).toContain("Welcome to Remix");
