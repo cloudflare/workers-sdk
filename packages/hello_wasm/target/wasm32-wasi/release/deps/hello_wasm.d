0c4ec6075c144465372603b6ae6cd33f52ca9be6
Sunil Pai <spai@cloudflare.com>
2022-02-10 22:10:39 +0000

    add a hello-wasm example package

---
+++ packages/hello_wasm/target/wasm32-wasi/release/deps/hello_wasm.d
@@ -0,0 +1,5 @@
+/Users/sunilpai/code/nu-wrangler/packages/hello_wasm/target/wasm32-wasi/release/deps/hello_wasm.wasm: src/main.rs
+
+/Users/sunilpai/code/nu-wrangler/packages/hello_wasm/target/wasm32-wasi/release/deps/hello_wasm.d: src/main.rs
+
+src/main.rs:
---
+++ packages/hello_wasm/target/wasm32-wasi/release/hello_wasm.d
@@ -0,0 +1,1 @@
+/Users/sunilpai/code/nu-wrangler/packages/hello_wasm/target/wasm32-wasi/release/hello_wasm.wasm: /Users/sunilpai/code/nu-wrangler/packages/hello_wasm/src/main.rs
