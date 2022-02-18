659cc9a4f91c070feeee113674aa8e9115da4077
Pete Bacon Darwin <pete@bacondarwin.com>
2022-02-18 12:39:49 +0000

    ci: further fix to artifact commenting github action

---
+++ .github/extract-pr-and-workflow-id.js
@@ -0,0 +1,18 @@
+const allArtifacts = await github.rest.actions.listWorkflowRunArtifacts({
+  owner: context.repo.owner,
+  repo: context.repo.repo,
+  run_id: context.payload.workflow_run.id,
+});
+
+for (const artifact of allArtifacts.data.artifacts) {
+  // Extract the PR number from the artifact name
+  const match = /^npm-package-wrangler-(\d+)$/.exec(artifact.name);
+  if (match) {
+    fs.appendFileSync(
+      process.env.GITHUB_ENV,
+      `\nWORKFLOW_RUN_PR=${match[1]}` +
+        `\nWORKFLOW_RUN_ID=${context.payload.workflow_run.id}`
+    );
+    break;
+  }
+}
