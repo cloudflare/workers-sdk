cloudflare/workers-sdk

commit 9e89dd7f868e10fcd3e09789f6f6a59dff8ed4e3

feat: zoned worker support for `wrangler dev` (#545)
This implements support for zoned workers in `wrangler dev`. Of note, since we're deprecating `zone_id`, we instead use the domain provided via `--host`/`config.dev.host`/`--routes`/`--route`/`config.routes`/`config.route` and infer the zone id from it.

Fixes https://github.com/cloudflare/wrangler2/issues/544

packages/wrangler/src/api/preview.ts
@@ -61,7 +61,7 @@ async function sessionToken(
 ): Promise<CfPreviewToken> {
   const { accountId } = account;
   const initUrl = ctx.zone
-    ? `/zones/${ctx.zone}/workers/edge-preview`
+    ? `/zones/${ctx.zone.id}/workers/edge-preview`
     : `/accounts/${accountId}/workers/subdomain/edge-preview`;
 
   const { exchange_url } = await fetchResult<{ exchange_url: string }>(initUrl);
@@ -111,7 +111,7 @@ export async function previewToken(
       : `/accounts/${accountId}/workers/scripts/${scriptId}/edge-preview`;
 
   const mode: CfPreviewMode = ctx.zone
-    ? { routes: ["*/*"] }
+    ? { routes: ["*/*"] } // TODO: should we support routes here? how?
     : { workers_dev: true };
 
   const formData = toFormData(worker);
@@ -127,20 +127,18 @@ export async function previewToken(
 
   return {
     value: preview_token,
-    // TODO: verify this works with zoned workers
-    host:
-      worker.name && !ctx.zone
-        ? // hrmm this might need change as well
-          `${
-            worker.name
-            // TODO: this should also probably have the env prefix
-            // but it doesn't appear to work yet, instead giving us the
-            // "There is nothing here yet" screen
-            // ctx.env && !ctx.legacyEnv
-            //   ? `${ctx.env}.${worker.name}`
-            //   : worker.name
-          }.${host.split(".").slice(1).join(".")}`
-        : host,
+    host: ctx.zone
+      ? ctx.zone.host
+      : `${
+          worker.name
+          // TODO: this should also probably have the env prefix
+          // but it doesn't appear to work yet, instead giving us the
+          // "There is nothing here yet" screen
+          // ctx.env && !ctx.legacyEnv
+          //   ? `${ctx.env}.${worker.name}`
+          //   : worker.name
+        }.${host.split(".").slice(1).join(".")}`,
+
     inspectorUrl,
     prewarmUrl,
   };

packages/wrangler/src/api/worker.ts
@@ -133,7 +133,7 @@ export interface CfWorkerInit {
   /**
    * The name of the worker.
    */
-  name: string | undefined;
+  name: string;
   /**
    * The entrypoint module.
    */
@@ -163,7 +163,7 @@ export interface CfWorkerInit {
 export interface CfWorkerContext {
   env: string | undefined;
   legacyEnv: boolean | undefined;
-  zone: string | undefined;
+  zone: { id: string; host: string } | undefined;
 }
 
 /**

packages/wrangler/src/dev.tsx
@@ -56,7 +56,12 @@ export type DevProps = {
   };
   env: string | undefined;
   legacyEnv: boolean;
-  zone: string | undefined;
+  zone:
+    | {
+        id: string;
+        host: string;
+      }
+    | undefined;
 };
 
 function Dev(props: DevProps): JSX.Element {
@@ -128,7 +133,7 @@ function Dev(props: DevProps): JSX.Element {
         />
       ) : (
         <Remote
-          name={props.name}
+          name={scriptName}
           bundle={bundle}
           format={format}
           accountId={props.accountId}
@@ -179,7 +184,7 @@ function useWorkerFormat(props: {
 }
 
 function Remote(props: {
-  name: undefined | string;
+  name: string;
   bundle: EsbuildBundle | undefined;
   format: CfScriptFormat | undefined;
   public: undefined | string;
@@ -194,7 +199,7 @@ function Remote(props: {
   usageModel: undefined | "bundled" | "unbound";
   env: string | undefined;
   legacyEnv: boolean | undefined;
-  zone: string | undefined;
+  zone: { id: string; host: string } | undefined;
 }) {
   assert(props.accountId, "accountId is required");
   assert(props.apiToken, "apiToken is required");
@@ -675,7 +680,7 @@ function useEsbuild({
 }
 
 function useWorker(props: {
-  name: undefined | string;
+  name: string;
   bundle: EsbuildBundle | undefined;
   format: CfScriptFormat | undefined;
   modules: CfModule[];
@@ -689,7 +694,7 @@ function useWorker(props: {
   usageModel: undefined | "bundled" | "unbound";
   env: string | undefined;
   legacyEnv: boolean | undefined;
-  zone: string | undefined;
+  zone: { id: string; host: string } | undefined;
 }): CfPreviewToken | undefined {
   const {
     name,

packages/wrangler/src/__tests__/configuration.test.ts
@@ -21,6 +21,7 @@ describe("normalizeAndValidateConfig()", () => {
         local_protocol: "http",
         port: 8787,
         upstream_protocol: "https",
+        host: undefined,
       },
       durable_objects: {
         bindings: [],

packages/wrangler/src/__tests__/dev.test.tsx
@@ -1,31 +1,23 @@
-import { render } from "ink-testing-library";
+import * as fs from "node:fs";
 import patchConsole from "patch-console";
-import React from "react";
 import Dev from "../dev";
+import { mockAccountId, mockApiToken } from "./helpers/mock-account-id";
+import { setMockResponse, unsetAllMocks } from "./helpers/mock-cfetch";
 import { mockConsoleMethods } from "./helpers/mock-console";
+import { runInTempDir } from "./helpers/run-in-tmp";
 import { runWrangler } from "./helpers/run-wrangler";
 import writeWranglerToml from "./helpers/write-wrangler-toml";
-import type { DevProps } from "../dev";
 
 describe("Dev component", () => {
-  let restoreConsole: ReturnType<typeof patchConsole>;
-  beforeEach(() => (restoreConsole = patchConsole(() => {})));
-  afterEach(() => restoreConsole());
+  beforeEach(() => {});
+  mockAccountId();
+  mockApiToken();
+  runInTempDir();
   const std = mockConsoleMethods();
-
-  // This test needs to be rewritten because the error now throws asynchronously
-  // and the Ink framework does not yet have async testing support.
-  it.skip("should throw if format is service-worker and there is a public directory", () => {
-    const { lastFrame } = renderDev({
-      format: "service-worker",
-      accountId: "some-account-id",
-      public: "some/public/path",
-    });
-    expect(lastFrame()?.split("\n").slice(0, 2).join("\n"))
-      .toMatchInlineSnapshot(`
-      "Something went wrong:
-      Error: You cannot use the service worker format with a \`public\` directory."
-    `);
+  afterEach(() => {
+    (Dev as jest.Mock).mockClear();
+    patchConsole(() => {});
+    unsetAllMocks();
   });
 
   describe("entry-points", () => {
@@ -47,67 +39,210 @@ describe("Dev component", () => {
       `);
     });
   });
+
+  describe("host", () => {
+    it("should resolve a host to its zone", async () => {
+      writeWranglerToml({
+        main: "index.js",
+      });
+      fs.writeFileSync("index.js", `export default {};`);
+      mockGetZones("some-host.com", [{ id: "some-zone-id" }]);
+      await runWrangler("dev --host some-host.com");
+      expect((Dev as jest.Mock).mock.calls[0][0].zone).toEqual({
+        host: "some-host.com",
+        id: "some-zone-id",
+      });
+    });
+
+    it("should read wrangler.toml's dev.host", async () => {
+      writeWranglerToml({
+        main: "index.js",
+        dev: {
+          host: "some-host.com",
+        },
+      });
+      fs.writeFileSync("index.js", `export default {};`);
+      mockGetZones("some-host.com", [{ id: "some-zone-id" }]);
+      await runWrangler("dev");
+      expect((Dev as jest.Mock).mock.calls[0][0].zone.host).toEqual(
+        "some-host.com"
+      );
+    });
+
+    it("should read --route", async () => {
+      writeWranglerToml({
+        main: "index.js",
+      });
+      fs.writeFileSync("index.js", `export default {};`);
+      mockGetZones("some-host.com", [{ id: "some-zone-id" }]);
+      await runWrangler("dev --route http://some-host.com/some/path/*");
+      expect((Dev as jest.Mock).mock.calls[0][0].zone.host).toEqual(
+        "some-host.com"
+      );
+    });
+
+    it("should read wrangler.toml's routes", async () => {
+      writeWranglerToml({
+        main: "index.js",
+        routes: [
+          "http://some-host.com/some/path/*",
+          "http://some-other-host.com/path/*",
+        ],
+      });
+      fs.writeFileSync("index.js", `export default {};`);
+      mockGetZones("some-host.com", [{ id: "some-zone-id" }]);
+      await runWrangler("dev");
+      expect((Dev as jest.Mock).mock.calls[0][0].zone.host).toEqual(
+        "some-host.com"
+      );
+    });
+
+    it("should read wrangler.toml's environment specific routes", async () => {
+      writeWranglerToml({
+        main: "index.js",
+        routes: [
+          "http://a-host.com/some/path/*",
+          "http://another-host.com/path/*",
+        ],
+        env: {
+          staging: {
+            routes: [
+              "http://some-host.com/some/path/*",
+              "http://some-other-host.com/path/*",
+            ],
+          },
+        },
+      });
+      fs.writeFileSync("index.js", `export default {};`);
+      mockGetZones("some-host.com", [{ id: "some-zone-id" }]);
+      await runWrangler("dev --env staging");
+      expect((Dev as jest.Mock).mock.calls[0][0].zone.host).toEqual(
+        "some-host.com"
+      );
+    });
+
+    it("given a long host, it should use the longest subdomain that resolves to a zone", async () => {
+      writeWranglerToml({
+        main: "index.js",
+      });
+      fs.writeFileSync("index.js", `export default {};`);
+      mockGetZones("111.222.333.some-host.com", []);
+      mockGetZones("222.333.some-host.com", []);
+      mockGetZones("333.some-host.com", [{ id: "some-zone-id" }]);
+      await runWrangler("dev --host 111.222.333.some-host.com");
+      expect((Dev as jest.Mock).mock.calls[0][0].zone).toEqual({
+        host: "111.222.333.some-host.com",
+        id: "some-zone-id",
+      });
+    });
+
+    it("should, in order, use args.host/config.dev.host/args.routes/config.route/config.routes", async () => {
+      // This test might seem like it's testing implmentation details, but let's be specific and consider it a spec
+
+      fs.writeFileSync("index.js", `export default {};`);
+
+      // config.routes
+      mockGetZones("5.some-host.com", [{ id: "some-zone-id-5" }]);
+      writeWranglerToml({
+        main: "index.js",
+        routes: ["http://5.some-host.com/some/path/*"],
+      });
+      await runWrangler("dev");
+      expect((Dev as jest.Mock).mock.calls[0][0].zone).toEqual({
+        host: "5.some-host.com",
+        id: "some-zone-id-5",
+      });
+      (Dev as jest.Mock).mockClear();
+
+      // config.route
+      mockGetZones("4.some-host.com", [{ id: "some-zone-id-4" }]);
+      writeWranglerToml({
+        main: "index.js",
+        route: "https://4.some-host.com/some/path/*",
+        routes: ["http://5.some-host.com/some/path/*"],
+      });
+      await runWrangler("dev");
+      expect((Dev as jest.Mock).mock.calls[0][0].zone).toEqual({
+        host: "4.some-host.com",
+        id: "some-zone-id-4",
+      });
+      (Dev as jest.Mock).mockClear();
+
+      // --routes
+      mockGetZones("3.some-host.com", [{ id: "some-zone-id-3" }]);
+      writeWranglerToml({
+        main: "index.js",
+        route: "https://4.some-host.com/some/path/*",
+        routes: ["http://5.some-host.com/some/path/*"],
+      });
+      await runWrangler("dev --routes http://3.some-host.com/some/path/*");
+      expect((Dev as jest.Mock).mock.calls[0][0].zone).toEqual({
+        host: "3.some-host.com",
+        id: "some-zone-id-3",
+      });
+      (Dev as jest.Mock).mockClear();
+
+      // config.dev.host
+      mockGetZones("2.some-host.com", [{ id: "some-zone-id-2" }]);
+      writeWranglerToml({
+        main: "index.js",
+        dev: {
+          host: `2.some-host.com`,
+        },
+        route: "4.some-host.com/some/path/*",
+        routes: ["5.some-host.com/some/path/*"],
+      });
+      await runWrangler("dev --routes http://3.some-host.com/some/path/*");
+      expect((Dev as jest.Mock).mock.calls[0][0].zone).toEqual({
+        host: "2.some-host.com",
+        id: "some-zone-id-2",
+      });
+      (Dev as jest.Mock).mockClear();
+
+      // --host
+      mockGetZones("1.some-host.com", [{ id: "some-zone-id-1" }]);
+      writeWranglerToml({
+        main: "index.js",
+        dev: {
+          host: `2.some-host.com`,
+        },
+        route: "4.some-host.com/some/path/*",
+        routes: ["5.some-host.com/some/path/*"],
+      });
+      await runWrangler(
+        "dev --routes http://3.some-host.com/some/path/* --host 1.some-host.com"
+      );
+      expect((Dev as jest.Mock).mock.calls[0][0].zone).toEqual({
+        host: "1.some-host.com",
+        id: "some-zone-id-1",
+      });
+      (Dev as jest.Mock).mockClear();
+    });
+
+    it("should error if a host can't resolve to a zone", async () => {
+      writeWranglerToml({
+        main: "index.js",
+      });
+      fs.writeFileSync("index.js", `export default {};`);
+      mockGetZones("some-host.com", []);
+      await expect(
+        runWrangler("dev --host some-host.com")
+      ).rejects.toThrowErrorMatchingInlineSnapshot(
+        `"Could not find zone for some-host.com"`
+      );
+    });
+  });
 });
 
-/**
- * Helper function to make it easier to setup and render the `Dev` component.
- *
- * All the `Dev` props are optional here, with sensible defaults for testing.
- */
-function renderDev({
-  name,
-  entry = { file: "some/entry.ts", directory: process.cwd() },
-  port,
-  inspectorPort = 9229,
-  format,
-  accountId,
-  legacyEnv = true,
-  initialMode = "local",
-  jsxFactory,
-  jsxFragment,
-  rules = [],
-  bindings = {
-    kv_namespaces: [],
-    vars: {},
-    durable_objects: { bindings: [] },
-    r2_buckets: [],
-    wasm_modules: {},
-    text_blobs: {},
-    unsafe: [],
-  },
-  public: publicDir,
-  assetPaths,
-  compatibilityDate,
-  compatibilityFlags,
-  usageModel,
-  buildCommand = {},
-  enableLocalPersistence = false,
-  env,
-  zone,
-}: Partial<DevProps>) {
-  return render(
-    <Dev
-      name={name}
-      entry={entry}
-      env={env}
-      rules={rules}
-      port={port}
-      inspectorPort={inspectorPort}
-      legacyEnv={legacyEnv}
-      buildCommand={buildCommand}
-      format={format}
-      initialMode={initialMode}
-      jsxFactory={jsxFactory}
-      jsxFragment={jsxFragment}
-      accountId={accountId}
-      assetPaths={assetPaths}
-      public={publicDir}
-      compatibilityDate={compatibilityDate}
-      compatibilityFlags={compatibilityFlags}
-      usageModel={usageModel}
-      bindings={bindings}
-      enableLocalPersistence={enableLocalPersistence}
-      zone={zone}
-    />
+function mockGetZones(domain: string, zones: { id: string }[] = []) {
+  const removeMock = setMockResponse(
+    "/zones",
+    "GET",
+    (_urlPieces, _init, queryParams) => {
+      expect([...queryParams.entries()]).toEqual([["name", domain]]);
+      // Because the API URL `/zones` is the same for each request, we can get into a situation where earlier mocks get triggered for later requests. So, we simply clear the mock on every trigger.
+      removeMock();
+      return zones;
+    }
   );
 }

packages/wrangler/src/__tests__/jest.setup.ts
@@ -55,3 +55,15 @@ jest.mock("../dialogs");
     "Unexpected call to `prompt()`. You should use `mockPrompt()` to mock calls to `prompt()` with expectations. Search the codebase for `mockPrompt` to learn more."
   );
 });
+
+jest.mock("../dev", () => {
+  const { useApp } = jest.requireActual("ink");
+  const { useEffect } = jest.requireActual("react");
+  return jest.fn().mockImplementation(() => {
+    const { exit } = useApp();
+    useEffect(() => {
+      exit();
+    });
+    return null;
+  });
+});

packages/wrangler/src/config/config.ts
@@ -94,6 +94,11 @@ export interface ConfigFields<Env extends RawEnvironment> {
      * @todo this needs to be implemented
      */
     upstream_protocol?: string;
+
+    /**
+     * Host to forward requests to, defaults to the zone of project
+     */
+    host?: string;
   };
 
   /**

packages/wrangler/src/config/validation.ts
@@ -246,6 +246,7 @@ function normalizeAndValidateDev(
     port = 8787,
     local_protocol = "http",
     upstream_protocol = "https",
+    host,
     ...rest
   } = rawDev;
   validateAdditionalProperties(diagnostics, "dev", Object.keys(rest), []);
@@ -266,7 +267,8 @@ function normalizeAndValidateDev(
     upstream_protocol,
     "string"
   );
-  return { ip, port, local_protocol, upstream_protocol };
+  validateOptionalProperty(diagnostics, "dev", "host", host, "string");
+  return { ip, port, local_protocol, upstream_protocol, host };
 }
 
 /**

commit fbec31e7ce5387a449df1ac22cf32533da1fce57

fix: wrangler dev on unnamed workers in remote mode
With unnamed workers, we use the filename as the name of the worker, which isn't a valid name for workers because of the `.` (This break was introduced in https://github.com/cloudflare/wrangler2/pull/545). The preview service accepts unnamed workers and generates a hash anyway, so the fix is to simply not send it, and use the host that the service provides.

packages/wrangler/src/api/preview.ts
@@ -129,7 +129,8 @@ export async function previewToken(
     value: preview_token,
     host: ctx.zone
       ? ctx.zone.host
-      : `${
+      : worker.name
+      ? `${
           worker.name
           // TODO: this should also probably have the env prefix
           // but it doesn't appear to work yet, instead giving us the
@@ -137,7 +138,8 @@ export async function previewToken(
           // ctx.env && !ctx.legacyEnv
           //   ? `${ctx.env}.${worker.name}`
           //   : worker.name
-        }.${host.split(".").slice(1).join(".")}`,
+        }.${host.split(".").slice(1).join(".")}`
+      : host,
 
     inspectorUrl,
     prewarmUrl,

packages/wrangler/src/api/worker.ts
@@ -133,7 +133,7 @@ export interface CfWorkerInit {
   /**
    * The name of the worker.
    */
-  name: string;
+  name: string | undefined;
   /**
    * The entrypoint module.
    */

packages/wrangler/src/dev.tsx
@@ -114,13 +114,12 @@ function Dev(props: DevProps): JSX.Element {
   );
 
   useTunnel(toggles.tunnel);
-  const scriptName = props.name || path.basename(props.entry.file);
 
   return (
     <>
       {toggles.local ? (
         <Local
-          name={scriptName}
+          name={props.name}
           bundle={bundle}
           format={format}
           bindings={props.bindings}
@@ -133,7 +132,7 @@ function Dev(props: DevProps): JSX.Element {
         />
       ) : (
         <Remote
-          name={scriptName}
+          name={props.name}
           bundle={bundle}
           format={format}
           accountId={props.accountId}
@@ -182,7 +181,7 @@ function useWorkerFormat(props: {
 }
 
 function Remote(props: {
-  name: string;
+  name: string | undefined;
   bundle: EsbuildBundle | undefined;
   format: CfScriptFormat | undefined;
   public: undefined | string;
@@ -678,7 +677,7 @@ function useEsbuild({
 }
 
 function useWorker(props: {
-  name: string;
+  name: string | undefined;
   bundle: EsbuildBundle | undefined;
   format: CfScriptFormat | undefined;
   modules: CfModule[];