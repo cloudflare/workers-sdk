4623302472be9f3141444e3dc3d4ce8006879f64
Adam Janiš <adam.janis@gmail.com>
2022-05-13 12:50:32 +0100

    chore: add prerelease cors (#990)
    * chore: add prerelease cors
    
    * apply review suggestions

---
+++ packages/prerelease-registry/functions/_middleware.ts
@@ -0,0 +1,17 @@
+export const onRequestOptions: PagesFunction = async () => {
+  return new Response(null, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,OPTIONS",
+      "Access-Control-Max-Age": "86400",
+    },
+  });
+};
+
+export const onRequest: PagesFunction = async ({ next }) => {
+  const response = await next();
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "GET,OPTIONS");
+  response.headers.set("Access-Control-Max-Age", "86400");
+  return response;
+};
