7ac673df17a72d8aa0c57cfea4223a6e6c73df64
Adam Janiš <eidam@cloudflare.com>
2022-05-13 16:05:55 +0100

    cors allow all headers for prerelease

--- packages/prerelease-registry/functions/routes/_middleware.ts
+++ packages/prerelease-registry/functions/routes/_middleware.ts
@@ -2,6 +2,7 @@ export const onRequestOptions: PagesFunction = async () => {
   return new Response(null, {
     headers: {
       "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
       "Access-Control-Allow-Methods": "GET,OPTIONS",
       "Access-Control-Max-Age": "86400",
     },
@@ -11,6 +12,7 @@ export const onRequestOptions: PagesFunction = async () => {
 export const onRequest: PagesFunction = async ({ next }) => {
   const response = await next();
   response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Headers", "*");
   response.headers.set("Access-Control-Allow-Methods", "GET,OPTIONS");
   response.headers.set("Access-Control-Max-Age", "86400");
   return response;
