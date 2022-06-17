16bdd37817f6e5f2a767203307ae200eb4790ea7
Glen Maddern <glen83@gmail.com>
2022-06-17 15:42:59 +0100

    Added Wrangler `npm run dev` command (#1279)
    * Moved .npmrc to root of repo
    
    * Added npm run build-watch on Wrangler repo
    
    * Switched to "npm run dev" and added live typechecking
    
    * restored npmrc
    
    * Made concurrently a dev dependency instead
    
    Co-authored-by: Glen Maddern <glen@cloudflare.com>

--- packages/wrangler/scripts/bundle.ts
+++ packages/wrangler/scripts/bundle.ts
@@ -1,10 +1,25 @@
 import path from "node:path";
 import { build } from "esbuild";
+import type { WatchMode } from "esbuild";
 
 // the expectation is that this is being run from the project root
+type BuildFlags = {
+  watch?: boolean;
+};
 
-async function run() {
-  // main cli
+function watchLogger(outputPath: string): WatchMode {
+  return {
+    onRebuild(error, _) {
+      if (error) {
+        console.error(`${outputPath} build failed.\n`, error);
+      } else {
+        console.log(`${outputPath} updated.`);
+      }
+    },
+  };
+}
+
+async function buildMain(flags: BuildFlags = {}) {
   await build({
     entryPoints: ["./src/cli.ts"],
     bundle: true,
@@ -28,9 +43,11 @@ async function run() {
       "import.meta.url": "import_meta_url",
       "process.env.NODE_ENV": '"production"',
     },
+    watch: flags.watch ? watchLogger("./wrangler-dist") : false,
   });
+}
 
-  // custom miniflare cli
+async function buildMiniflareCLI(flags: BuildFlags = {}) {
   await build({
     entryPoints: ["./src/miniflare-cli/index.ts"],
     bundle: true,
@@ -42,7 +59,25 @@ async function run() {
     define: {
       "process.env.NODE_ENV": '"production"',
     },
+    watch: flags.watch ? watchLogger("./miniflare-dist/index.mjs") : false,
   });
+}
+
+async function run() {
+  // main cli
+  await buildMain();
+
+  // custom miniflare cli
+  await buildMiniflareCLI();
+
+  // After built once completely, rerun them both in watch mode
+  if (process.argv.includes("--watch")) {
+    console.log("Built. Watching for changes...");
+    await Promise.all([
+      buildMain({ watch: true }),
+      buildMiniflareCLI({ watch: true }),
+    ]);
+  }
 }
 
 run().catch((e) => {
