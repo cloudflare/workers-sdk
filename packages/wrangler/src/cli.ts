41b52c937841259ac990de70bfa840452f5ae1cd
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2022-06-20 18:15:58 +0200

    feat: what if dev was exported as a function

--- packages/wrangler/src/cli.ts
+++ packages/wrangler/src/cli.ts
@@ -1,13 +1,18 @@
 import "dotenv/config"; // Grab locally specified env params from a `.env` file.
 import process from "process";
 import { hideBin } from "yargs/helpers";
+import { dev } from "./api";
 import { FatalError } from "./errors";
 import { main } from ".";
 
-main(hideBin(process.argv)).catch((e) => {
-  // The logging of any error that was thrown from `main()` is handled in the `yargs.fail()` handler.
-  // Here we just want to ensure that the process exits with a non-zero code.
-  // We don't want to do this inside the `main()` function, since that would kill the process when running our tests.
-  const exitCode = (e instanceof FatalError && e.code) || 1;
-  process.exit(exitCode);
-});
+if (require.main) {
+  main(hideBin(process.argv)).catch((e) => {
+    // The logging of any error that was thrown from `main()` is handled in the `yargs.fail()` handler.
+    // Here we just want to ensure that the process exits with a non-zero code.
+    // We don't want to do this inside the `main()` function, since that would kill the process when running our tests.
+    const exitCode = (e instanceof FatalError && e.code) || 1;
+    process.exit(exitCode);
+  });
+}
+
+module.exports = { dev };
