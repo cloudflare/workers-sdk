bc06badf602bff67fdd69329b17826dce530dc45
Cass Fridkin <cass@cloudflare.com>
2022-06-21 09:20:02 -0500

    check wrangler/package.json instead of wrangler

--- packages/wrangler/bin/wrangler.js
+++ packages/wrangler/bin/wrangler.js
@@ -9,19 +9,11 @@ const MIN_NODE_VERSION = "16.7.0";
 
 let wranglerProcess;
 
-async function main() {
-  if (semiver(process.versions.node, MIN_NODE_VERSION) < 0) {
-    // Note Volta and nvm are also recommended in the official docs:
-    // https://developers.cloudflare.com/workers/get-started/guide#2-install-the-workers-cli
-    console.error(
-      `Wrangler requires at least Node.js v${MIN_NODE_VERSION}. You are using v${process.versions.node}.
-You should use the latest Node.js version if possible, as Cloudflare Workers use a very up-to-date version of V8.
-Consider using a Node.js version manager such as https://volta.sh/ or https://github.com/nvm-sh/nvm.`
-    );
-    process.exitCode = 1;
-    return;
-  }
-
+/**
+ * Executes ../wrangler-dist/cli.js
+ * @returns {ChildProcess} the wrangler process
+ */
+function runWrangler() {
   let pathToCACerts = process.env.NODE_EXTRA_CA_CERTS;
   if (pathToCACerts) {
     // TODO:
@@ -46,17 +38,7 @@ Consider using a Node.js version manager such as https://volta.sh/ or https://gi
     pathToCACerts = certPath;
   }
 
-  let pathToWranglerCli;
-  try {
-    // try to use a local version of wrangler, if there is one.
-    // `require.resolve` throws if it can't find anything.
-    pathToWranglerCli = require.resolve("wrangler", { paths: [process.cwd()] });
-  } catch (e) {
-    // we couldn't find a local install, so just use this (presumably global) version instead.
-    pathToWranglerCli = path.join(__dirname, "../wrangler-dist/cli.js");
-  }
-
-  wranglerProcess = spawn(
+  return spawn(
     process.execPath,
     [
       ...(semiver(process.versions.node, "18.0.0") >= 0
@@ -78,6 +60,72 @@ Consider using a Node.js version manager such as https://volta.sh/ or https://gi
   ).on("exit", (code) =>
     process.exit(code === undefined || code === null ? 0 : code)
   );
+}
+
+/**
+ * Runs a locally-installed version of wrangler, delegating from this version.
+ * @throws {MODULE_NOT_FOUND} if there isn't a locally installed version of wrangler.
+ * @returns {ChildProcess} the delegated wrangler process
+ */
+function runDelegatedWrangler() {
+  const packageJsonPath = require.resolve("wrangler/package.json", {
+    paths: [process.cwd()],
+  });
+  const binaryPath = JSON.parse(packageJsonPath).bin.wrangler;
+  // this call to `spawn` is simpler because the delegated version will do all
+  // of the other work.
+  return spawn(
+    process.execPath,
+    [path.resolve(packageJsonPath, binaryPath), ...process.argv],
+    {
+      stdio: "inherit",
+    }
+  ).on("exit", (code) =>
+    process.exit(code === undefined || code === null ? 0 : code)
+  );
+}
+
+/**
+ * Indicates if this invocation of `wrangler` should delegate
+ * to a locally-installed version.
+ */
+const shouldDelegate = (function () {
+  // IIFE because we need to catch a MODULE_NOT_FOUND error thrown
+  // by `require.resolve`
+  try {
+    // `require.resolve` will throw if it can't find
+    // a locally-installed version of `wrangler`
+    const delegatedPackageJson = require.resolve("wrangler/package.json", {
+      paths: [process.cwd()],
+    });
+    const thisPackageJson = path.resolve(__dirname, "..", "package.json");
+    // if it's the same path, then we're already a local install -- no need to delegate
+    return thisPackageJson !== delegatedPackageJson;
+  } catch (e) {
+    // there's no local version to delegate to -- `require.resolve` threw
+    return false;
+  }
+})();
+
+async function main() {
+  if (shouldDelegate) {
+    wranglerProcess = runDelegatedWrangler();
+    return;
+  }
+
+  if (semiver(process.versions.node, MIN_NODE_VERSION) < 0) {
+    // Note Volta and nvm are also recommended in the official docs:
+    // https://developers.cloudflare.com/workers/get-started/guide#2-install-the-workers-cli
+    console.error(
+      `Wrangler requires at least Node.js v${MIN_NODE_VERSION}. You are using v${process.versions.node}.
+You should use the latest Node.js version if possible, as Cloudflare Workers use a very up-to-date version of V8.
+Consider using a Node.js version manager such as https://volta.sh/ or https://github.com/nvm-sh/nvm.`
+    );
+    process.exitCode = 1;
+    return;
+  }
+
+  wranglerProcess = runWrangler();
 }
 
 process.on("SIGINT", () => {
