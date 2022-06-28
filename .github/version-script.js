cdc0dd9179e9186a15ecd75ef814191c259c73a4
Pete Bacon Darwin <pete@bacondarwin.com>
2022-06-28 10:27:49 +0100

    convert github action helper scripts to use tab indents

--- .github/version-script.js
+++ .github/version-script.js
@@ -2,21 +2,21 @@ const fs = require("fs");
 const { exec } = require("child_process");
 
 try {
-  const package = JSON.parse(
-    fs.readFileSync("./packages/wrangler/package.json")
-  );
-  exec("git rev-parse --short HEAD", (err, stdout) => {
-    if (err) {
-      console.log(err);
-      process.exit(1);
-    }
-    package.version = "0.0.0-" + stdout.trim();
-    fs.writeFileSync(
-      "./packages/wrangler/package.json",
-      JSON.stringify(package, null, "\t") + "\n"
-    );
-  });
+	const package = JSON.parse(
+		fs.readFileSync("./packages/wrangler/package.json")
+	);
+	exec("git rev-parse --short HEAD", (err, stdout) => {
+		if (err) {
+			console.log(err);
+			process.exit(1);
+		}
+		package.version = "0.0.0-" + stdout.trim();
+		fs.writeFileSync(
+			"./packages/wrangler/package.json",
+			JSON.stringify(package, null, "\t") + "\n"
+		);
+	});
 } catch (error) {
-  console.error(error);
-  process.exit(1);
+	console.error(error);
+	process.exit(1);
 }
