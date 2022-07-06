cd9445adecf3979c3a50fb24e740f07e95992d04
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2022-07-06 13:12:46 +0200

    remove redundant undefined

--- packages/wrangler/src/dev.tsx
+++ packages/wrangler/src/dev.tsx
@@ -62,7 +62,7 @@ interface DevArgs {
 	minify?: boolean;
 	"node-compat"?: boolean;
 	"experimental-enable-local-persistence"?: boolean;
-	onReady?: (() => void) | undefined;
+	onReady?: () => void;
 	logLevel?: "none" | "error" | "log" | "warn" | "debug";
 	showInteractiveDevSession?: boolean;
 }
