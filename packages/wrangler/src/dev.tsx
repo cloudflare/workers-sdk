61f39359bcad6850bdd7f9bc30b6889a24914874
Skye <46286597+Skye-31@users.noreply.github.com>
2022-07-08 12:05:15 +0100

    fix wrangler dev bound to services message

--- packages/wrangler/src/dev.tsx
+++ packages/wrangler/src/dev.tsx
@@ -312,7 +312,7 @@ export async function startDev(
 
 		if (config.services && config.services.length > 0) {
 			logger.warn(
-				`This worker is bound to  services: ${config.services
+				`This worker is bound to live services: ${config.services
 					.map(
 						(service) =>
 							`${service.binding} (${service.service}${
