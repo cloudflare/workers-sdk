dd71db66f03c2ce58283a0243779b053173750ca
Sunil Pai <spai@cloudflare.com>
2022-07-15 10:13:24 +0100

    fix: read `process.env.NODE_ENV` correctly when building worker
    We replace `process.env.NODE_ENV` in workers with the value of the environment variable. However, we have a bug where when we make an actual build of wrangler (which has NODE_ENV set as "production"), we were also replacing the expression where we'd replace it in a worker. The result was that all workers would have `process.env.NODE_ENV` set to production, no matter what the user had set. The fix here is to use a "dynamic" value for the expression so that our build system doesn't replace it.
    
    Fixes https://github.com/cloudflare/wrangler2/issues/1477

--- fixtures/local-mode-tests/tests/module.test.ts
+++ fixtures/local-mode-tests/tests/module.test.ts
@@ -1,27 +1,28 @@
 import { spawnWranglerDev } from "./helpers";
 
 it("renders", async () => {
-  const { fetchWhenReady, terminateProcess } = spawnWranglerDev(
-    "src/module.ts",
-    "src/wrangler.module.toml",
-    9001
-  );
+	const { fetchWhenReady, terminateProcess } = spawnWranglerDev(
+		"src/module.ts",
+		"src/wrangler.module.toml",
+		9001
+	);
 
-  try {
-    const response = await fetchWhenReady("http://localhost");
-    const text = await response.text();
-    expect(text).toMatchInlineSnapshot(`
-    "{
-      \\"VAR1\\": \\"value1\\",
-      \\"VAR2\\": 123,
-      \\"VAR3\\": {
-        \\"abc\\": \\"def\\"
-      },
-      \\"text\\": \\"Here be some text\\",
-      \\"data\\": \\"Here be some data\\"
-    }"
-  `);
-  } finally {
-    await terminateProcess();
-  }
+	try {
+		const response = await fetchWhenReady("http://localhost");
+		const text = await response.text();
+		expect(text).toMatchInlineSnapshot(`
+		"{
+		  \\"VAR1\\": \\"value1\\",
+		  \\"VAR2\\": 123,
+		  \\"VAR3\\": {
+		    \\"abc\\": \\"def\\"
+		  },
+		  \\"text\\": \\"Here be some text\\",
+		  \\"data\\": \\"Here be some data\\",
+		  \\"NODE_ENV\\": \\"local-testing\\"
+		}"
+	`);
+	} finally {
+		await terminateProcess();
+	}
 });
