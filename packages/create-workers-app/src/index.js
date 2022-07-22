81e5690249af1b7ca59e2556d02fbdfbaf113ed0
Sunil Pai <spai@cloudflare.com>
2022-07-22 09:51:01 +0100

    chore: remove unused packages (#1517)
    This removes a couple of packages under `packages/` that we're not really working on right now, and can visit in the future.

--- packages/create-workers-app/src/index.js
+++
@@ -1,1 +0,0 @@
-// a cli for initialising a new workers app
--- packages/jest-environment-wrangler/src/index.ts
+++
@@ -1,3 +0,0 @@
-import MiniflareEnvironment from "jest-environment-miniflare";
-
-export default class WranglerEnvironment extends MiniflareEnvironment {}
