0b340af53fbf72c3bb114ea5688c0c2be55c7a6f
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2022-07-27 14:37:05 +0200

    chore: extract devProps parsing into own function

--- packages/wrangler/src/dev/dev.tsx
+++ packages/wrangler/src/dev/dev.tsx
@@ -14,6 +14,7 @@ import { openInspector } from "../inspect";
 import { logger } from "../logger";
 import openInBrowser from "../open-in-browser";
 import { Local } from "./local";
+import { parseDevProps } from "./parse-dev-props";
 import { Remote } from "./remote";
 import { useEsbuild } from "./use-esbuild";
 import type { Config } from "../config";
@@ -68,33 +69,7 @@ export type DevProps = {
 };
 
 export function DevImplementation(props: DevProps): JSX.Element {
-	if (
-		!props.isWorkersSite &&
-		props.assetPaths &&
-		props.entry.format === "service-worker"
-	) {
-		throw new Error(
-			"You cannot use the service-worker format with an `assets` directory yet. For information on how to migrate to the module-worker format, see: https://developers.cloudflare.com/workers/learning/migrating-to-module-workers/"
-		);
-	}
-
-	if (props.bindings.wasm_modules && props.entry.format === "modules") {
-		throw new Error(
-			"You cannot configure [wasm_modules] with an ES module worker. Instead, import the .wasm module directly in your code"
-		);
-	}
-
-	if (props.bindings.text_blobs && props.entry.format === "modules") {
-		throw new Error(
-			"You cannot configure [text_blobs] with an ES module worker. Instead, import the file directly in your code, and optionally configure `[rules]` in your wrangler.toml"
-		);
-	}
-
-	if (props.bindings.data_blobs && props.entry.format === "modules") {
-		throw new Error(
-			"You cannot configure [data_blobs] with an ES module worker. Instead, import the file directly in your code, and optionally configure `[rules]` in your wrangler.toml"
-		);
-	}
+	parseDevProps(props);
 
 	// only load the UI if we're running in a supported environment
 	const { isRawModeSupported } = useStdin();
