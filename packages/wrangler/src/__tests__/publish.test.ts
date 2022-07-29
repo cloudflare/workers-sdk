d05ba5cb0c1b878793d39ddee945fd37e549a969
Glen Maddern <glen@cloudflare.com>
2022-07-29 12:57:12 +0100

    updated snapshots

--- packages/wrangler/src/__tests__/publish.test.ts
+++ packages/wrangler/src/__tests__/publish.test.ts
@@ -108,7 +108,7 @@ describe("publish", () => {
 			Total Upload: 0xx KiB / gzip: 0xx KiB
 			Uploaded test-name (TIMINGS)
 			Published test-name (TIMINGS)
-			          https://test-name.test-sub-domain.workers.dev"
+			  https://test-name.test-sub-domain.workers.dev"
 		`);
 			expect(std.warn).toMatchInlineSnapshot(`""`);
 			expect(std.err).toMatchInlineSnapshot(`""`);
