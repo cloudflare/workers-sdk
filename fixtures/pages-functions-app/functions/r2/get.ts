22ad468e78ce31e85fe6521bd95ff63840569635
Skye <46286597+Skye-31@users.noreply.github.com>
2022-08-03 12:40:52 +0100

    feat: add tests

---
+++ fixtures/pages-functions-app/functions/r2/get.ts
@@ -0,0 +1,8 @@
+type Env = {
+	BUCKET: R2Bucket
+}
+
+export const onRequestGet: PagesFunction<Env> = async ({ env }) => {
+	const object = await env.BUCKET.get('test')
+	return new Response(JSON.stringify(object));
+};
