5209c4e83db9c3f2497945ee37d3836a5d513b82
Skye <46286597+Skye-31@users.noreply.github.com>
2022-08-06 22:58:52 +0100

    chore: add fixtures to prettier

--- fixtures/pages-functions-app/functions/blog/[slug].ts
+++ fixtures/pages-functions-app/functions/blog/[slug].ts
@@ -1,6 +1,6 @@
 export const onRequestGet: PagesFunction<unknown, "slug"> = ({ params }) => {
-  const { slug } = params;
-  return new Response(`<h1>A blog with a slug: ${slug}</h1>`, {
-    headers: { "content-type": "text/html" },
-  });
+	const { slug } = params;
+	return new Response(`<h1>A blog with a slug: ${slug}</h1>`, {
+		headers: { "content-type": "text/html" },
+	});
 };
--- fixtures/remix-pages-app/functions/[[path]].js
+++ fixtures/remix-pages-app/functions/[[path]].js
@@ -4,9 +4,9 @@ import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 import * as build from "../build";
 
 const handleRequest = createPagesFunctionHandler({
-  build,
+	build,
 });
 
 export function onRequest(context) {
-  return handleRequest(context);
+	return handleRequest(context);
 }
