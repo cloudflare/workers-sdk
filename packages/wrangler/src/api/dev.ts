d4804f2343f512fb3351fa189ee784c9769e9cd2
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2022-09-05 18:40:16 +0200

    add apiOptions to jsdoc (#1776)
    * add apiOptions to jsdoc
    
    * add UnstableDev as a type
    
    * explicitly set the port
    
    
    .

--- packages/wrangler/src/api/dev.ts
+++ packages/wrangler/src/api/dev.ts
@@ -55,10 +55,17 @@ interface DevApiOptions {
 	disableExperimentalWarning?: boolean;
 }
 
+interface UnstableDev {
+	stop: () => Promise<void>;
+	fetch: (init?: RequestInit) => Promise<Response | undefined>;
+	waitUntilExit: () => Promise<void>;
+}
 /**
  *  unstable_dev starts a wrangler dev server, and returns a promise that resolves with utility functions to interact with it.
  *  @param {string} script
  *  @param {DevOptions} options
+ *  @param {DevApiOptions} apiOptions
+ * @returns {Promise<UnstableDev>}
  */
 export async function unstable_dev(
 	script: string,
@@ -75,11 +82,7 @@ export async function unstable_dev(
 	//due to Pages adoption of unstable_dev, we can't *just* disable rebuilds and watching. instead, we'll have two versions of startDev, which will converge.
 	if (testMode) {
 		//in testMode, we can run multiple wranglers in parallel, but rebuilds might not work out of the box
-		return new Promise<{
-			stop: () => Promise<void>;
-			fetch: (init?: RequestInit) => Promise<Response | undefined>;
-			waitUntilExit: () => Promise<void>;
-		}>((resolve) => {
+		return new Promise<UnstableDev>((resolve) => {
 			//lmao
 			return new Promise<Awaited<ReturnType<typeof startApiDev>>>((ready) => {
 				// once the devServer is ready for requests, we resolve the inner promise
@@ -110,11 +113,7 @@ export async function unstable_dev(
 		});
 	} else {
 		//outside of test mode, rebuilds work fine, but only one instance of wrangler will work at a time
-		return new Promise<{
-			stop: () => Promise<void>;
-			fetch: (init?: RequestInit) => Promise<Response | undefined>;
-			waitUntilExit: () => Promise<void>;
-		}>((resolve) => {
+		return new Promise<UnstableDev>((resolve) => {
 			//lmao
 			return new Promise<Awaited<ReturnType<typeof startDev>>>((ready) => {
 				const devServer = startDev({
