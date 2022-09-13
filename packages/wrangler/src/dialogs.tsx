3583f313f50d1b0ba703286a44842d1c70b730e9
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2022-09-13 15:48:05 +0200

    fix: wrangler publish for CI after a manual deployment (#1836)
    * fix: wrangler publish for CI after a manual deployment
    
    * Update dialogs.tsx
    
    * Update .changeset/brave-plums-count.md
    
    * add test
    
    * fix test

--- packages/wrangler/src/dialogs.tsx
+++ packages/wrangler/src/dialogs.tsx
@@ -4,7 +4,11 @@ import SelectInput from "ink-select-input";
 import TextInput from "ink-text-input";
 import * as React from "react";
 import { useState } from "react";
+
+import { CI } from "./is-ci";
+import isInteractive from "./is-interactive";
 import { logger } from "./logger";
+
 type ConfirmProps = {
 	text: string;
 	onConfirm: (answer: boolean) => void;
@@ -135,13 +139,16 @@ export function select(
 }
 
 export async function fromDashMessagePrompt(
-	deploySource: string
+	deploySource: "dash" | "wrangler" | "api"
 ): Promise<boolean | void> {
 	if (deploySource === "dash") {
 		logger.warn(
 			`You are about to publish a Workers Service that was last published via the Cloudflare Dashboard.
 		Edits that have been made via the dashboard will be overridden by your local code and config.`
 		);
+
+		if (!isInteractive() || CI.isCI()) return true;
+
 		return await confirm("Would you like to continue?");
 	}
 }
