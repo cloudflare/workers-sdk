5316e2ad6da8e787b982a282d0961843596dd597
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2022-09-13 15:37:41 +0200

    fix test

--- packages/wrangler/src/__tests__/dialogs.test.tsx
+++ packages/wrangler/src/__tests__/dialogs.test.tsx
@@ -1,26 +1,40 @@
+jest.unmock("../dialogs");
 import { fromDashMessagePrompt } from "../dialogs";
 import { CI } from "../is-ci";
-import { useMockIsTTY } from "./helpers/mock-istty";
 
 describe("fromDashMessagePrompt", () => {
-	const { setIsTTY } = useMockIsTTY();
-	beforeEach(() => {
-		setIsTTY(false); // not interactive
-		jest.spyOn(CI, "isCI").mockReturnValue(true); //in CI
+	it("should return undefined in CI when last deployed from api", async () => {
+		//in CI
+		jest.spyOn(CI, "isCI").mockReturnValue(true);
+		const result = await fromDashMessagePrompt("api");
+		expect(result).toBe(undefined);
+	});
+
+	it("should return undefined in CI when last deployed from wrangler", async () => {
+		//in CI
+		jest.spyOn(CI, "isCI").mockReturnValue(true);
+		const result = await fromDashMessagePrompt("wrangler");
+		expect(result).toBe(undefined);
+	});
+
+	it("should return true in CI when last deployed from dash", async () => {
+		//in CI
+		jest.spyOn(CI, "isCI").mockReturnValue(true);
+		const result = await fromDashMessagePrompt("dash");
+		expect(result).toBe(true);
 	});
 
 	it("should return undefined when last deployed from api", async () => {
+		//not in CI
+		jest.spyOn(CI, "isCI").mockReturnValue(false);
 		const result = await fromDashMessagePrompt("api");
 		expect(result).toBe(undefined);
 	});
 
 	it("should return undefined when last deployed from wrangler", async () => {
+		//not in CI
+		jest.spyOn(CI, "isCI").mockReturnValue(false);
 		const result = await fromDashMessagePrompt("wrangler");
 		expect(result).toBe(undefined);
-	});
-
-	it("should return true when last deployed from dash", async () => {
-		const result = await fromDashMessagePrompt("dash");
-		expect(result).toBe(true);
 	});
 });
