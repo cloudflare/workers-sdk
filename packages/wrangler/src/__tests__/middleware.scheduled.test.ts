eb8a357275e459b97325c479b2484b15bd50e517
Cameron Robey <cameron@cameronrobey.co.uk>
2022-09-14 14:59:40 +0100

    more tests

--- packages/wrangler/src/__tests__/middleware.scheduled.test.ts
+++ packages/wrangler/src/__tests__/middleware.scheduled.test.ts
@@ -1,24 +1,14 @@
 import * as fs from "node:fs";
 import { unstable_dev } from "../api";
 import { unsetAllMocks } from "./helpers/mock-cfetch";
-import { useMockIsTTY } from "./helpers/mock-istty";
 import { runInTempDir } from "./helpers/run-in-tmp";
 
 jest.unmock("undici");
 
 describe("run scheduled events with middleware (module workers)", () => {
 	runInTempDir();
-	const { setIsTTY } = useMockIsTTY();
 
 	beforeEach(() => {
-		setIsTTY(true);
-	});
-
-	afterEach(() => {
-		unsetAllMocks();
-	});
-
-	it("should not intercept when middleware is not enabled", async () => {
 		const scriptContent = `
     export default {
       fetch(request, env, ctx) {
@@ -34,7 +24,13 @@ describe("run scheduled events with middleware (module workers)", () => {
     };
     `;
 		fs.writeFileSync("index.js", scriptContent);
+	});
 
+	afterEach(() => {
+		unsetAllMocks();
+	});
+
+	it("should not intercept when middleware is not enabled", async () => {
 		const worker = await unstable_dev(
 			"index.js",
 			{},
@@ -49,22 +45,6 @@ describe("run scheduled events with middleware (module workers)", () => {
 	});
 
 	it("should intercept when middleware is enabled", async () => {
-		const scriptContent = `
-    export default {
-      fetch(request, env, ctx) {
-        const url = new URL(request.url);
-        if (url.pathname === "/__scheduled") {
-          return new Response("Fetch triggered at /__scheduled");
-        }
-        return new Response("Hello world!");
-      },
-      scheduled(controller, env, ctx) {
-        console.log("Doing something scheduled in modules...");
-      },
-    };
-    `;
-		fs.writeFileSync("index.js", scriptContent);
-
 		const worker = await unstable_dev(
 			"index.js",
 			{ testScheduled: true },
@@ -75,6 +55,88 @@ describe("run scheduled events with middleware (module workers)", () => {
 		let text;
 		if (resp) text = await resp.text();
 		expect(text).toMatchInlineSnapshot(`"Ran scheduled event"`);
+		await worker.stop();
+	});
+
+	it("should not trigger scheduled event on wrong route", async () => {
+		const worker = await unstable_dev(
+			"index.js",
+			{ testScheduled: true },
+			{ disableExperimentalWarning: true }
+		);
+
+		const resp = await worker.fetch("/test");
+		let text;
+		if (resp) text = await resp.text();
+		expect(text).toMatchInlineSnapshot(`"Hello world!"`);
+		await worker.stop();
+	});
+});
+
+describe("run scheduled events with middleware (service worker)", () => {
+	runInTempDir();
+
+	beforeEach(() => {
+		const scriptContent = `
+		addEventListener("scheduled", (event) => {
+			console.log("Doing something scheduled in service worker...");
+		});
+
+		addEventListener("fetch", (event) => {
+			const url = new URL(event.request.url);
+			if (url.pathname === "/__scheduled") {
+				event.respondWith(new Response("Fetch triggered at /__scheduled"));
+			} else {
+				event.respondWith(new Response("Hello world!"));
+			}
+		});
+		`;
+		fs.writeFileSync("index.js", scriptContent);
+	});
+
+	afterEach(() => {
+		unsetAllMocks();
+	});
+
+	it("should not intercept when middleware is not enabled", async () => {
+		const worker = await unstable_dev(
+			"index.js",
+			{},
+			{ disableExperimentalWarning: true }
+		);
+
+		const resp = await worker.fetch("/__scheduled");
+		let text;
+		if (resp) text = await resp.text();
+		expect(text).toMatchInlineSnapshot(`"Fetch triggered at /__scheduled"`);
+		await worker.stop();
+	});
+
+	it("should intercept when middleware is enabled", async () => {
+		const worker = await unstable_dev(
+			"index.js",
+			{ testScheduled: true },
+			{ disableExperimentalWarning: true }
+		);
+
+		const resp = await worker.fetch("/__scheduled");
+		let text;
+		if (resp) text = await resp.text();
+		expect(text).toMatchInlineSnapshot(`"Ran scheduled event"`);
+		await worker.stop();
+	});
+
+	it("should not trigger scheduled event on wrong route", async () => {
+		const worker = await unstable_dev(
+			"index.js",
+			{ testScheduled: true },
+			{ disableExperimentalWarning: true }
+		);
+
+		const resp = await worker.fetch("/test");
+		let text;
+		if (resp) text = await resp.text();
+		expect(text).toMatchInlineSnapshot(`"Hello world!"`);
 		await worker.stop();
 	});
 });
