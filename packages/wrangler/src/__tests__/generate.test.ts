e963c8a24ec288c2ba6eb28856501ea393dd964a
Cass Fridkin <cass@cloudflare.com>
2022-09-29 13:50:18 -0500

    refactor tests

--- packages/wrangler/src/__tests__/generate.test.ts
+++ packages/wrangler/src/__tests__/generate.test.ts
@@ -6,48 +6,42 @@ import { runWrangler } from "./helpers/run-wrangler";
 
 describe("generate", () => {
 	runInTempDir();
-	const std = mockConsoleMethods();
 
-	afterEach(() => {
-		clearConfirmMocks();
-	});
+	describe("cli functionality", () => {
+		const std = mockConsoleMethods();
 
-	it("defers to `wrangler init` when no template is given", async () => {
-		mockConfirm(
-			{
-				text: "Would you like to use git to manage this Worker?",
-				result: false,
-			},
-			{
-				text: "No package.json found. Would you like to create one?",
-				result: false,
-			}
-		);
-		await runWrangler("generate no-template");
-		expect(std.out).toMatchInlineSnapshot(
-			`"✨ Created no-template/wrangler.toml"`
-		);
-	});
+		afterEach(() => {
+			clearConfirmMocks();
+		});
 
-	it("clones a given template", async () => {
-		await expect(
-			runWrangler("generate my-worker worker-typescript")
-		).resolves.toBeUndefined();
+		it("defers to `wrangler init` when no template is given", async () => {
+			mockConfirm(
+				{
+					text: "Would you like to use git to manage this Worker?",
+					result: false,
+				},
+				{
+					text: "No package.json found. Would you like to create one?",
+					result: false,
+				}
+			);
+			await runWrangler("generate no-template");
+			expect(std.out).toMatchInlineSnapshot(
+				`"✨ Created no-template/wrangler.toml"`
+			);
+		});
 
-		expect(fs.existsSync("my-worker")).toBe(true);
-	});
+		it("complains when given the --type argument", async () => {
+			await expect(
+				runWrangler("generate worker-name worker-template --type rust")
+			).rejects.toThrowErrorMatchingInlineSnapshot(
+				`"The --type option is no longer supported."`
+			);
+		});
 
-	it("complains when given the --type argument", async () => {
-		await expect(
-			runWrangler("generate worker-name worker-template --type rust")
-		).rejects.toThrowErrorMatchingInlineSnapshot(
-			`"The --type option is no longer supported."`
-		);
-	});
-
-	it("complains when given the --site argument", async () => {
-		await expect(runWrangler("generate worker-name worker-template --site"))
-			.rejects.toThrowErrorMatchingInlineSnapshot(`
+		it("complains when given the --site argument", async () => {
+			await expect(runWrangler("generate worker-name worker-template --site"))
+				.rejects.toThrowErrorMatchingInlineSnapshot(`
 					"The --site option is no longer supported.
 					If you wish to create a brand new Worker Sites project then clone the \`worker-sites-template\` starter repository:
 
@@ -59,21 +53,34 @@ describe("generate", () => {
 					Find out more about how to create and maintain Sites projects at https://developers.cloudflare.com/workers/platform/sites.
 					Have you considered using Cloudflare Pages instead? See https://pages.cloudflare.com/."
 				`);
+		});
+
+		it("auto-increments the worker directory name", async () => {
+			fs.mkdirSync("my-worker");
+
+			await expect(
+				runWrangler("generate my-worker worker-typescript")
+			).resolves.toBeUndefined();
+
+			expect(fs.existsSync("my-worker-1")).toBe(true);
+
+			await expect(
+				runWrangler("generate my-worker worker-typescript")
+			).resolves.toBeUndefined();
+
+			expect(fs.existsSync("my-worker-2")).toBe(true);
+		});
 	});
 
-	it("auto-increments the worker directory name", async () => {
-		fs.mkdirSync("my-worker");
+	describe("cloning", () => {
+		it("clones a given template", async () => {
+			await expect(
+				runWrangler("generate my-worker worker-typescript")
+			).resolves.toBeUndefined();
 
-		await expect(
-			runWrangler("generate my-worker worker-typescript")
-		).resolves.toBeUndefined();
+			expect(fs.existsSync("my-worker")).toBe(true);
+		});
 
-		expect(fs.existsSync("my-worker-1")).toBe(true);
-
-		await expect(
-			runWrangler("generate my-worker worker-typescript")
-		).resolves.toBeUndefined();
-
-		expect(fs.existsSync("my-worker-2")).toBe(true);
+		// TODO: more tests around cloning behavior
 	});
 });
