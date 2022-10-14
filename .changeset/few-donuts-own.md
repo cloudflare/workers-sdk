d6660ce3e26d44b4db39b149868cb850e47763f0
Cass Fridkin <cass@cloudflare.com>
2022-10-14 06:40:15 -0500

    Display a more helpful error when publishing to a route in use by another worker (#2023)
    Previously, when trying to publish a worker to a route that was in use by another worker,
    there would be a really unhelpful message about a failed API call. Now, there's a much
    nicer message that tells you what worker is running on that route, and gives you a link
    to the workers overview page so you can unassign it if you want.
    
    Closes #1849

---
+++ .changeset/few-donuts-own.md
@@ -0,0 +1,28 @@
+---
+"wrangler": patch
+---
+
+Display a more helpful error when trying to publish to a route in use by another worker.
+
+Previously, when trying to publish a worker to a route that was in use by another worker,
+there would be a really unhelpful message about a failed API call. Now, there's a much
+nicer message that tells you what worker is running on that route, and gives you a link
+to the workers overview page so you can unassign it if you want.
+
+```text
+ ⛅️ wrangler 2.1.11
+--------------------
+Total Upload: 0.20 KiB / gzip: 0.17 KiB
+
+✘ [ERROR] Can't publish a worker to routes that are assigned to another worker.
+
+  "test-custom-routes-redeploy" is already assigned to route
+  test-custom-worker.swag.lgbt
+
+  Unassign other workers from the routes you want to publish to, and then try again.
+  Visit
+  https://dash.cloudflare.com/<account_id>/workers/overview
+  to unassign a worker from a route.
+```
+
+Closes #1849
--- packages/wrangler/src/publish/publish.ts
+++ packages/wrangler/src/publish/publish.ts
@@ -2,6 +2,7 @@ import assert from "node:assert";
 import { mkdirSync, readFileSync, writeFileSync } from "node:fs";
 import path from "node:path";
 import { URLSearchParams } from "node:url";
+import chalk from "chalk";
 import tmp from "tmp-promise";
 import { bundleWorker } from "../bundle";
 import { printBundleSize } from "../bundle-reporter";
@@ -348,7 +349,7 @@ See https://developers.cloudflare.com/workers/platform/compatibility-dates for m
 		? `/accounts/${accountId}/workers/services/${scriptName}/environments/${envName}`
 		: `/accounts/${accountId}/workers/scripts/${scriptName}`;
 
-	let available_on_subdomain; // we'll set this later
+	let available_on_subdomain: boolean | undefined = undefined; // we'll set this later
 
 	const { format } = props.entry;
 
@@ -648,6 +649,61 @@ See https://developers.cloudflare.com/workers/platform/compatibility-dates for m
 					"Content-Type": "application/json",
 				},
 			});
+		} else if (routes.length !== 0) {
+			// if you get to this point it's because
+			// you're trying to deploy a worker to a custom
+			// domain that's already bound to another worker.
+			// so this thing is about finding workers that have
+			// bindings to the routes you're trying to deploy to.
+			//
+			// the logic is kinda similar (read: duplicated) from publishRoutesFallback,
+			// except here we know we have a good API token or whatever so we don't need
+			// to bother with all the error handling tomfoolery.
+			const routesWithOtherBindings: Record<string, string[]> = {};
+			for (const route of routes) {
+				const zone = await getZoneForRoute(route);
+				if (!zone) {
+					continue;
+				}
+
+				const routePattern = typeof route === "string" ? route : route.pattern;
+				const routesInZone = await fetchListResult<{
+					pattern: string;
+					script: string;
+				}>(`/zones/${zone.id}/workers/routes`);
+
+				routesInZone.forEach(({ script, pattern }) => {
+					if (pattern === routePattern && script !== scriptName) {
+						if (!(script in routesWithOtherBindings)) {
+							routesWithOtherBindings[script] = [];
+						}
+
+						routesWithOtherBindings[script].push(pattern);
+					}
+				});
+			}
+
+			if (Object.keys(routesWithOtherBindings).length > 0) {
+				let errorMessage =
+					"Can't publish a worker to routes that are assigned to another worker.\n";
+
+				for (const worker in routesWithOtherBindings) {
+					const assignedRoutes = routesWithOtherBindings[worker];
+					errorMessage += `"${worker}" is already assigned to routes:\n${assignedRoutes.map(
+						(r) => `  - ${chalk.underline(r)}\n`
+					)}`;
+				}
+
+				const resolution =
+					"Unassign other workers from the routes you want to publish to, and then try again.";
+				const dashLink = `Visit ${chalk.blue(
+					chalk.underline(
+						`https://dash.cloudflare.com/${accountId}/workers/overview`
+					)
+				)} to unassign a worker from a route.`;
+
+				throw new Error(`${errorMessage}\n${resolution}\n${dashLink}`);
+			}
 		}
 	}
 
