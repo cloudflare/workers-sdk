5337e9453910f916979bd998eea05f6f07de056f
Jacob M-G Evans <27247160+JacobMGEvans@users.noreply.github.com>
2022-11-06 12:25:40 -0600

    chore: Update naming conventions (#2119)
    * Update naming conventions
    Naming conventions are being reworked and will be standardized at Worker Org level
    this is part of this effort.
    
    * Change naming conventions

--- packages/wrangler/src/index.tsx
+++ packages/wrangler/src/index.tsx
@@ -544,14 +544,14 @@ export function createCLIParser(argv: string[]) {
 			);
 
 			const scriptTag = scriptMetadata.default_environment.script.tag;
-			const deployments = await fetchResult<DeploymentListRes>(
+			const { items: deployments } = await fetchResult<DeploymentListRes>(
 				`/accounts/${accountId}/workers/versions/by-script/${scriptTag}`
 			);
 
-			const versionMessages = deployments.versions.map(
+			const versionMessages = deployments.map(
 				(versions, index) =>
-					`\nVersion ID: ${versions.version_id}\nVersion number: ${
-						versions.version_number
+					`\nVersion ID: ${versions.id}\nVersion number: ${
+						versions.number
 					}\nCreated on: ${versions.metadata.created_on}\nAuthor email: ${
 						versions.metadata.author_email
 					}\nLatest deploy: ${index === 0}\n`
