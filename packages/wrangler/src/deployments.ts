9f00916def0e43dc033fc2f4e00db938e2cac2dc
jjohnson <jjohnson@cloudflare.com>
2022-11-08 15:41:48 -0600

    Move deployments code to file

--- packages/wrangler/src/deployments.ts
+++ packages/wrangler/src/deployments.ts
@@ -1,3 +1,7 @@
+import { fetchResult } from "./cfetch";
+import { logger } from "./logger";
+import type { ServiceMetadataRes } from "./init";
+
 export type DeploymentListRes = {
 	latest: {
 		id: string;
@@ -26,3 +30,27 @@ export type DeploymentListRes = {
 		};
 	}[];
 };
+
+export async function deployments(
+	accountId: string,
+	scriptName: string | undefined
+) {
+	const scriptMetadata = await fetchResult<ServiceMetadataRes>(
+		`/accounts/${accountId}/workers/services/${scriptName}`
+	);
+
+	const scriptTag = scriptMetadata.default_environment.script.tag;
+	const { items: deploys } = await fetchResult<DeploymentListRes>(
+		`/accounts/${accountId}/workers/versions/by-script/${scriptTag}`
+	);
+
+	const versionMessages = deploys.map(
+		(versions, index) =>
+			`\nVersion ID: ${versions.id}\nVersion number: ${
+				versions.number
+			}\nCreated on: ${versions.metadata.created_on}\nAuthor email: ${
+				versions.metadata.author_email
+			}\nLatest deploy: ${index === 0}\n`
+	);
+	logger.log(...versionMessages);
+}
