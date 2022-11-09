892a3dfc91e45e4f36d9cbdd996220e9879ed2f9
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2022-11-09 16:16:44 +1100

    Update execute.tsx

--- packages/wrangler/src/d1/execute.tsx
+++ packages/wrangler/src/d1/execute.tsx
@@ -104,7 +104,7 @@ export async function executeSql(
 
 	if (!sql) throw new Error(`Error: must provide --command or --file.`);
 	if (file && sql) {
-		console.log("sql: ", sql);
+		console.log("sql.toString(): ", sql.toString());
 		if ("SQLite format 3" === String(sql).slice(0, 16)) {
 			console.log("it's an sqlite file?");
 		}
