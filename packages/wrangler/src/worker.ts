67b694028fdb44b05bc2147b575cfd98f5bc54a9
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2022-11-15 18:34:23 +0100

    Update worker.ts

--- packages/wrangler/src/worker.ts
+++ packages/wrangler/src/worker.ts
@@ -126,6 +126,7 @@ export interface CfR2Bucket {
 export const D1_BETA_PREFIX = `__D1_BETA__` as const;
 export type D1PrefixedBinding = `${typeof D1_BETA_PREFIX}${string}`;
 
+// TODO: figure out if this is duplicated in packages/wrangler/src/config/environment.ts
 export interface CfD1Database {
 	// For now, all D1 bindings are alpha
 	binding: D1PrefixedBinding;
