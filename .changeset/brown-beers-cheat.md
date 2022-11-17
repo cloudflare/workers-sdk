d0f237d9965f782ae8415fe9ff02e83e6e86b9af
Jacob M-G Evans <27247160+JacobMGEvans@users.noreply.github.com>
2022-11-17 10:32:10 -0600

    XDG compliant metrics directory (#2209)
    * XDG compliant metrics directory
    When metrics was implemented the XDG compliant pathing was done in parrelel,
    this bring metrics directory into XDG compliance with other Wrangler root config.
    
    resolves #2075

---
+++ .changeset/brown-beers-cheat.md
@@ -0,0 +1,7 @@
+---
+"wrangler": patch
+---
+
+This change makes the metrics directory XDG compliant
+
+resolves #2075
--- packages/wrangler/src/metrics/metrics-config.ts
+++ packages/wrangler/src/metrics/metrics-config.ts
@@ -1,11 +1,11 @@
 import { randomUUID } from "node:crypto";
 import { readFileSync, mkdirSync, writeFileSync } from "node:fs";
-import os from "node:os";
 import path from "node:path";
 import { fetchResult } from "../cfetch";
 import { getConfigCache, saveToConfigCache } from "../config-cache";
 import { confirm } from "../dialogs";
 import { getEnvironmentVariableFactory } from "../environment-variables";
+import { getGlobalWranglerConfigPath } from "../global-wrangler-config-path";
 import { CI } from "../is-ci";
 import isInteractive from "../is-interactive";
 import { logger } from "../logger";
@@ -167,7 +167,7 @@ export function readMetricsConfig(): MetricsConfigFile {
  * Get the path to the metrics config file.
  */
 function getMetricsConfigPath(): string {
-	return path.resolve(os.homedir(), ".wrangler/config/metrics.json");
+	return path.resolve(getGlobalWranglerConfigPath(), "metrics.json");
 }
 
 /**
