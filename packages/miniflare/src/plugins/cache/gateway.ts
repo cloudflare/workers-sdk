cde71d5e024bee0118a59befa6fec2469eb7b5f1
MrBBot <bcoll@cloudflare.com>
2022-11-30 09:49:17 +0000

    Cache API tests (#447)
    * Respect `cf.cacheKey` in Cache API
    
    * Fix caching of `ReadableStream`-bodied `Response`s
    
    * Propagate `clock` through `GatewayFactory` for tests
    
    * Catch loopback handler errors and return 500 response
    
    * Add tests for `_getRangeResponse` and `_parseRanges` helpers
    
    * Add `Miniflare` integration test helper
    
    * Bump `undici` to `5.13.0` and add `@cloudflare/workers-types`
    
    * Add Cache API tests

--- packages/miniflare/src/plugins/cache/gateway.ts
+++ packages/miniflare/src/plugins/cache/gateway.ts
@@ -1,3 +1,4 @@
+import assert from "assert";
 import crypto from "crypto";
 import http from "http";
 import { AddressInfo } from "net";
@@ -174,12 +175,13 @@ class HttpParser {
 		});
 	}
 	private listen(request: http.IncomingMessage, response: http.ServerResponse) {
-		if (request.url) {
-			response?.socket?.write(
-				this.responses.get(request.url) ?? new Uint8Array()
-			);
-		}
-		response.end();
+		assert(request.url !== undefined);
+		assert(response.socket !== null);
+		const array = this.responses.get(request.url);
+		assert(array !== undefined);
+		// Write response to parse directly to underlying socket
+		response.socket.write(array);
+		response.socket.end();
 	}
 	public async parse(response: Uint8Array): Promise<ParsedHttpResponse> {
 		await this.connected;
@@ -210,11 +212,12 @@ export class CacheGateway {
 		private readonly clock: Clock
 	) {}
 
-	async match(request: Request): Promise<Response> {
+	async match(request: Request, cacheKey?: string): Promise<Response> {
 		// Never cache Workers Sites requests, so we always return on-disk files
 		if (isSitesRequest(request)) throw new CacheMiss();
 
-		const cached = await this.storage.get<CacheMetadata>(request.url);
+		cacheKey ??= request.url;
+		const cached = await this.storage.get<CacheMetadata>(cacheKey);
 		if (cached?.metadata === undefined) throw new CacheMiss();
 
 		const response = new CacheResponse(
@@ -231,11 +234,15 @@ export class CacheGateway {
 		);
 	}
 
-	async put(request: Request, value: ArrayBuffer): Promise<Response> {
+	async put(
+		request: Request,
+		value: Uint8Array,
+		cacheKey?: string
+	): Promise<Response> {
 		// Never cache Workers Sites requests, so we always return on-disk files
 		if (isSitesRequest(request)) return new Response(null, { status: 204 });
 
-		const response = await HttpParser.get().parse(new Uint8Array(value));
+		const response = await HttpParser.get().parse(value);
 
 		const { storable, expiration, headers } = getExpiration(
 			this.clock,
@@ -249,7 +256,8 @@ export class CacheGateway {
 			throw new StorageFailure();
 		}
 
-		await this.storage.put<CacheMetadata>(request.url, {
+		cacheKey ??= request.url;
+		await this.storage.put<CacheMetadata>(cacheKey, {
 			value: response.body,
 			expiration: millisToSeconds(this.clock() + expiration),
 			metadata: {
@@ -260,8 +268,9 @@ export class CacheGateway {
 		return new Response(null, { status: 204 });
 	}
 
-	async delete(request: Request): Promise<Response> {
-		const deleted = await this.storage.delete(request.url);
+	async delete(request: Request, cacheKey?: string): Promise<Response> {
+		cacheKey ??= request.url;
+		const deleted = await this.storage.delete(cacheKey);
 		// This is an extremely vague error, but it fits with what the cache API in workerd expects
 		if (!deleted) throw new PurgeFailure();
 		return new Response(null);
