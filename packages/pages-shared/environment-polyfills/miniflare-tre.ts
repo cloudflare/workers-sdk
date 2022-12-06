f6821189110e5b6301fe77509a6bb9a8652bbc1b
Greg Brimble <gbrimble@cloudflare.com>
2022-12-06 12:16:06 +0000

    Fix `wrangler dev --local` and its lazy imports of `@miniflare/tre` (#2339)

--- packages/pages-shared/environment-polyfills/miniflare-tre.ts
+++ packages/pages-shared/environment-polyfills/miniflare-tre.ts
@@ -1,14 +1,12 @@
-import {
-	fetch as miniflareFetch,
-	Headers as MiniflareHeaders,
-	Request as MiniflareRequest,
-	Response as MiniflareResponse,
-} from "@miniflare/tre";
 import { polyfill } from ".";
 
-polyfill({
-	fetch: miniflareFetch,
-	Headers: MiniflareHeaders,
-	Request: MiniflareRequest,
-	Response: MiniflareResponse,
-});
+export default async () => {
+	const mf = await import("@miniflare/tre");
+
+	polyfill({
+		fetch: mf.fetch,
+		Headers: mf.Headers,
+		Request: mf.Request,
+		Response: mf.Response,
+	});
+};
