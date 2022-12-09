99ed3f3b0e1afd6aa655e8b6ed06b90f3bda425d
James Ross <james@jross.me>
2022-12-09 18:12:31 +0000

    chore: use tabs for indentation

--- packages/kv-asset-handler/src/test/mapRequestToAsset.ts
+++ packages/kv-asset-handler/src/test/mapRequestToAsset.ts
@@ -5,33 +5,33 @@ mockGlobalScope()
 import { mapRequestToAsset } from '../index'
 
 test('mapRequestToAsset() correctly changes /about -> /about/index.html', async (t) => {
-  mockRequestScope()
-  let path = '/about'
-  let request = new Request(`https://foo.com${path}`)
-  let newRequest = mapRequestToAsset(request)
-  t.is(newRequest.url, request.url + '/index.html')
+	mockRequestScope()
+	let path = '/about'
+	let request = new Request(`https://foo.com${path}`)
+	let newRequest = mapRequestToAsset(request)
+	t.is(newRequest.url, request.url + '/index.html')
 })
 
 test('mapRequestToAsset() correctly changes /about/ -> /about/index.html', async (t) => {
-  mockRequestScope()
-  let path = '/about/'
-  let request = new Request(`https://foo.com${path}`)
-  let newRequest = mapRequestToAsset(request)
-  t.is(newRequest.url, request.url + 'index.html')
+	mockRequestScope()
+	let path = '/about/'
+	let request = new Request(`https://foo.com${path}`)
+	let newRequest = mapRequestToAsset(request)
+	t.is(newRequest.url, request.url + 'index.html')
 })
 
 test('mapRequestToAsset() correctly changes /about.me/ -> /about.me/index.html', async (t) => {
-  mockRequestScope()
-  let path = '/about.me/'
-  let request = new Request(`https://foo.com${path}`)
-  let newRequest = mapRequestToAsset(request)
-  t.is(newRequest.url, request.url + 'index.html')
+	mockRequestScope()
+	let path = '/about.me/'
+	let request = new Request(`https://foo.com${path}`)
+	let newRequest = mapRequestToAsset(request)
+	t.is(newRequest.url, request.url + 'index.html')
 })
 
 test('mapRequestToAsset() correctly changes /about -> /about/default.html', async (t) => {
-  mockRequestScope()
-  let path = '/about'
-  let request = new Request(`https://foo.com${path}`)
-  let newRequest = mapRequestToAsset(request, { defaultDocument: 'default.html' })
-  t.is(newRequest.url, request.url + '/default.html')
+	mockRequestScope()
+	let path = '/about'
+	let request = new Request(`https://foo.com${path}`)
+	let newRequest = mapRequestToAsset(request, { defaultDocument: 'default.html' })
+	t.is(newRequest.url, request.url + '/default.html')
 })
