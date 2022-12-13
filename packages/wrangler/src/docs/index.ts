b805a9ac12f41c3fd750127571b6b0e6f03182cd
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2022-12-13 15:10:26 +0100

    chore: refactor static lookup for a proper search index

--- packages/wrangler/src/docs/index.ts
+++ packages/wrangler/src/docs/index.ts
@@ -10,80 +10,40 @@ import type {
 } from "../yargs-types";
 import type { ArgumentsCamelCase, Argv } from "yargs";
 
-const argToUrlHash = {
-	docs: "docs",
-	init: "init",
-	generate: "generate",
-	dev: "dev",
-	publish: "publish",
-	delete: "delete",
-	"kv:namespace": "kvnamespace",
-	"kv:key": "kvkey",
-	"kv:bulk": "kvbulk",
-	"r2 bucket": "r2-bucket",
-	"r2 object": "r2-object",
-	secret: "secret",
-	"secret:bulk": "secretbulk",
-	tail: "tail",
-	pages: "pages",
-	login: "login",
-	logout: "logout",
-	whoami: "whoami",
-	types: "types",
-	deployments: "deployments",
-};
-
 export function docsOptions(yargs: Argv<CommonYargsOptions>) {
 	return yargs.positional("command", {
 		describe: "Enter the wrangler command you want to know more about",
 		type: "string",
 		// requiresArg: true,
-		choices: [
-			"docs",
-			"init",
-			"generate",
-			"dev",
-			"publish",
-			"delete",
-			"tail",
-			"secret",
-			"secret:bulk",
-			"kv:namespace",
-			"kv:key",
-			"kv:bulk",
-			"pages",
-			// "queues", //TODO: Undocumented
-			"r2 object",
-			"r2 bucket",
-			// "dispatch-namespace", // TODO: Undocumented - Workers for Platforms
-			// "d1", //TODO: Undocumented
-			// "pubsub", //TODO: Undocumented
-			"login",
-			"logout",
-			"whoami",
-			"types",
-			"deployments",
-			"api",
-		],
 	});
 }
 
 type DocsArgs = YargsOptionsToInterface<typeof docsOptions>;
 
-function isValidParam(k: string): k is keyof typeof argToUrlHash {
-	return k in argToUrlHash;
-}
-
 export async function docsHandler(args: ArgumentsCamelCase<DocsArgs>) {
 	let urlToOpen =
 		"https://developers.cloudflare.com/workers/wrangler/commands/";
 
-	if (args.command === "api") {
-		//if api, take them to the API docs
-		urlToOpen = "https://developers.cloudflare.com/workers/wrangler/api/";
-	} else if (args.command && isValidParam(args.command)) {
-		//otherwise, they get the wrangler commands page
-		urlToOpen += `#${argToUrlHash[args.command]}`;
+	if (args.command) {
+		const searchResp = await fetch(
+			`https://${process.env.ALGOLIA_APPLICATION_ID}-dsn.algolia.net/1/indexes/developers-cloudflare-wrangler/query`,
+			{
+				method: "POST",
+				body: JSON.stringify({
+					params: `query=${args.command}&hitsPerPage=1&getRankingInfo=0`,
+				}),
+				headers: {
+					"X-Algolia-API-Key": process.env.ALGOLIA_API_KEY as string,
+					"X-Algolia-Application-Id": process.env
+						.ALGOLIA_APPLICATION_ID as string,
+				},
+			}
+		);
+		const searchData = (await searchResp.json()) as { hits: { url: string }[] };
+		logger.log("searchData: ", searchData);
+		if (searchData.hits[0]) {
+			urlToOpen = searchData.hits[0].url;
+		}
 	}
 
 	await printWranglerBanner();
