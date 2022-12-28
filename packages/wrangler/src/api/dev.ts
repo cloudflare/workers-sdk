20dc1f00e1bba0990ebc2d8012f35379ff938679
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2022-12-28 17:13:06 +0100

    don't throw, just log

--- packages/wrangler/src/api/dev.ts
+++ packages/wrangler/src/api/dev.ts
@@ -80,7 +80,7 @@ export async function unstable_dev(
 	const { testMode = true, disableExperimentalWarning = false } =
 		options?.experimental ?? {};
 	if (apiOptions) {
-		throw logger.error(
+		logger.error(
 			"unstable_dev's third argument (apiOptions) has been deprecated in favor of an `experimental` property within the second argument (options).\nPlease update your code from:\n`await unstable_dev('...', {...}, {...});`\nto:\n`await unstable_dev('...', {..., experimental: {...}});`"
 		);
 	}
