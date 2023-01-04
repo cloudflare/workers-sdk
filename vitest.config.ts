276091e81f1732161fe2d13129974fcc03ffa4fc
Greg Brimble <gbrimble@cloudflare.com>
2023-01-04 15:14:03 +0000

    Windows hack v2

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,3 @@
-import { join } from "path";
 import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
@@ -7,10 +6,7 @@ export default defineConfig({
 			__dirname === process.cwd()
 				? ["fixtures/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"]
 				: configDefaults.include,
-		exclude: [
-			...configDefaults.exclude,
-			join(__dirname, "fixtures", "local-mode-tests", "**", "*"),
-		],
+		exclude: [...configDefaults.exclude, "fixtures/local-mode-tests/**/*"],
 		root: __dirname,
 	},
 });
