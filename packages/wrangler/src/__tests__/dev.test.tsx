056ff34de9cd5708f8b0d2a26582b1d3f4370cd0
Samuel Macleod <smacleod@cloudflare.com>
2023-01-05 14:12:42 +0000

    Snapshots

--- packages/wrangler/src/__tests__/dev.test.tsx
+++ packages/wrangler/src/__tests__/dev.test.tsx
@@ -1218,10 +1218,11 @@ describe("wrangler dev", () => {
 			  script  The path to an entry point for your worker  [string]
 
 			Flags:
-			  -c, --config   Path to .toml configuration file  [string]
-			  -e, --env      Environment to use for operations and .env files  [string]
-			  -h, --help     Show help  [boolean]
-			  -v, --version  Show version number  [boolean]
+			  -j, --json-config  [33mExperimental[39m Support wrangler.json  [boolean]
+			  -c, --config       Path to .toml configuration file  [string]
+			  -e, --env          Environment to use for operations and .env files  [string]
+			  -h, --help         Show help  [boolean]
+			  -v, --version      Show version number  [boolean]
 
 			Options:
 			      --name                                       Name of the worker  [string]
