505d39e50b8d27b8822cc642d70a69264b4b9abc
Samuel Macleod <smacleod@cloudflare.com>
2023-01-23 22:03:59 +0000

    Add action to publish JSON schema for Wrangler

---
+++ packages/json-schema-worker/src/index.ts
@@ -0,0 +1,12 @@
+export default {
+	async fetch(request: Request, env: Env): Promise<Response> {
+		const url = new URL(request.url);
+		const matches = url.pathname.match(/\/(.*?).json/);
+		if (matches) {
+			const branch = decodeURIComponent(matches[1]);
+			return Response.json(await env.Schema.get(branch, "json"));
+		} else {
+			return new Response(null, { status: 404 });
+		}
+	},
+};
