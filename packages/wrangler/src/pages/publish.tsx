a3b36f351c31e0750eb77fc4f8941828e8d48f4a
Carmen Popoviciu <cpopoviciu@cloudflare.com>
2023-01-25 18:14:28 +0100

    feat: Add wasm support in `wrangler pages publish`
    Currently it is not possible to import `wasm` modules in either Pages
    Functions or Pages Advanced Mode projects.
    
    This commit caries out work to address the aforementioned issue by
    enabling `wasm` module imports in `wrangler pages publish`. As a result,
    Pages users can now import their `wasm` modules withing their Functions
    or `_worker.js` files, and `wrangler pages publish` will correctly
    bundle everything and serve these "external" modules.
    
    ```
    import hello from "./hello.wasm"
    
    export async function onRequest() {
    	const module = await WebAssembly.instantiate(hello);
    	return new Response(module.exports.hello);
    }
    ```

--- packages/wrangler/src/pages/publish.tsx
+++ packages/wrangler/src/pages/publish.tsx
@@ -66,6 +66,13 @@ export function Options(yargs: CommonYargsArgv) {
 				default: true,
 				description: "Whether to run bundling on `_worker.js` before deploying",
 			},
+			"experimental-worker-bundle": {
+				type: "boolean",
+				default: false,
+				hidden: true,
+				description:
+					"Whether to process non-JS module imports or not, such as wasm/text/binary, when we run bundling on `functions` or `_worker.js`",
+			},
 			config: {
 				describe: "Pages does not support wrangler.toml",
 				type: "string",
@@ -85,6 +92,7 @@ export const Handler = async ({
 	skipCaching,
 	bundle,
 	noBundle,
+	experimentalWorkerBundle,
 	config: wranglerConfig,
 }: PublishArgs) => {
 	if (wranglerConfig) {
@@ -253,6 +261,7 @@ export const Handler = async ({
 		// TODO: Here lies a known bug. If you specify both `--bundle` and `--no-bundle`, this behavior is undefined and you will get unexpected results.
 		// There is no sane way to get the true value out of yargs, so here we are.
 		bundle: bundle ?? !noBundle,
+		experimentalWorkerBundle,
 	});
 
 	saveToConfigCache<PagesConfigCache>(PAGES_CONFIG_CACHE_FILENAME, {
