9778b33eb27f721fb743a970fb1520782ab0d573
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2023-01-26 05:05:18 +1100

    fix: cleans up `d1 list` output for json (#2622)
    * fix: cleans up `d1 list` output for json
    
    * add changeset, move tests
    
    * address PR feedback
    
    * Update rare-laws-sing.md
    
    * Update rare-laws-sing.md

--- packages/wrangler/src/__tests__/d1/d1.test.ts
+++ packages/wrangler/src/__tests__/d1/d1.test.ts
@@ -1,9 +1,6 @@
-import { cwd } from "process";
 import { mockConsoleMethods } from "../helpers/mock-console";
-import { useMockIsTTY } from "../helpers/mock-istty";
 import { runInTempDir } from "../helpers/run-in-tmp";
 import { runWrangler } from "../helpers/run-wrangler";
-import writeWranglerToml from "../helpers/write-wrangler-toml";
 
 function endEventLoop() {
 	return new Promise((resolve) => setImmediate(resolve));
@@ -12,7 +9,6 @@ function endEventLoop() {
 describe("d1", () => {
 	const std = mockConsoleMethods();
 	runInTempDir();
-	const { setIsTTY } = useMockIsTTY();
 
 	it("should show help when no argument is passed", async () => {
 		await runWrangler("d1");
@@ -85,45 +81,5 @@ describe("d1", () => {
 		🚧 To give feedback, visit https://discord.gg/cloudflaredev
 		--------------------"
 	`);
-	});
-
-	describe("migrate", () => {
-		describe("apply", () => {
-			it("should not attempt to login in local mode", async () => {
-				setIsTTY(false);
-				writeWranglerToml({
-					d1_databases: [
-						{ binding: "DATABASE", database_name: "db", database_id: "xxxx" },
-					],
-				});
-				// If we get to the point where we are checking for migrations then we have not been asked to log in.
-				await expect(
-					runWrangler("d1 migrations apply --local DATABASE")
-				).rejects.toThrowError(
-					`No migrations present at ${cwd().replaceAll("\\", "/")}/migrations.`
-				);
-			});
-
-			it("should try to read D1 config from wrangler.toml", async () => {
-				setIsTTY(false);
-				writeWranglerToml();
-				await expect(
-					runWrangler("d1 migrations apply DATABASE")
-				).rejects.toThrowError(
-					"Can't find a DB with name/binding 'DATABASE' in local config. Check info in wrangler.toml..."
-				);
-			});
-
-			it("should not try to read wrangler.toml in local mode", async () => {
-				setIsTTY(false);
-				writeWranglerToml();
-				// If we get to the point where we are checking for migrations then we have not checked wrangler.toml.
-				await expect(
-					runWrangler("d1 migrations apply --local DATABASE")
-				).rejects.toThrowError(
-					`No migrations present at ${cwd().replaceAll("\\", "/")}/migrations.`
-				);
-			});
-		});
 	});
 });
