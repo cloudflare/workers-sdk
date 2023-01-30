2efd4537cb141e88fe9a674c2fd093b40a3c9d63
Mark Ladyshau <47859603+mrkldshv@users.noreply.github.com>
2023-01-30 23:22:22 +0100

    fix: change `jest` to `vitest` types in tsconfig (#2652)
    Co-authored-by: Jacob M-G Evans <27247160+JacobMGEvans@users.noreply.github.com>

--- packages/wrangler/src/__tests__/init.test.ts
+++ packages/wrangler/src/__tests__/init.test.ts
@@ -1392,7 +1392,7 @@ describe("init", () => {
 						contents: {
 							config: {
 								compilerOptions: expect.objectContaining({
-									types: ["@cloudflare/workers-types", "jest"],
+									types: ["@cloudflare/workers-types", "vitest"],
 								}),
 							},
 							error: undefined,
