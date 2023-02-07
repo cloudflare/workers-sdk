520eb2df43a7402df1ac0549feeb114b5216f064
John Fawcett <jfawcett@cloudflare.com>
2023-02-07 15:53:40 -0600

    fix: Pages direct upload failing with expired JWT error
    For projects which are slow to upload - either because of client bandwidth or large numbers of files and sizes - It's possible for the JWT to expire multiple times. Since our network request concurrency is set to 3, it's possible that each time the JWT expires we get 3 failed attempts. This can quickly exhaust our upload attempt count and cause the entire process to bail.
    
    This change makes it such that jwt refreshes do not count as a failed upload attempt.

--- packages/wrangler/src/pages/upload.tsx
+++ packages/wrangler/src/pages/upload.tsx
@@ -317,12 +317,15 @@ export const upload = async (
 					console.debug("failed:", e, "retrying...");
 					// Exponential backoff, 1 second first time, then 2 second, then 4 second etc.
 					await new Promise((resolvePromise) =>
-						setTimeout(resolvePromise, Math.pow(2, attempts++) * 1000)
+						setTimeout(resolvePromise, Math.pow(2, attempts) * 1000)
 					);
 
 					if ((e as { code: number }).code === 8000013 || isJwtExpired(jwt)) {
 						// Looks like the JWT expired, fetch another one
 						jwt = await fetchJwt();
+					} else {
+						// Only count as a failed attempt if the error _wasn't_ an expired JWT
+						attempts++;
 					}
 					return doUpload();
 				} else {
