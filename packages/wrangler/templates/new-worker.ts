d1a76686d2655da027d1c70c08297cd4ed0a87a0
Violet McKinney <opensource@viomck.com>
2023-02-13 03:40:16 -0800

    docs(templates): add a service binding example to new-worker.ts (#2702)
    with the release of service bindings, it's helpful to have an immediate idea of how service bindings work.  especially in typescript land, where the type is useful info.  in my case, i found `Fetcher` to be the most accurate type, but feel free to correct me if this is invalid.

--- packages/wrangler/templates/new-worker.ts
+++ packages/wrangler/templates/new-worker.ts
@@ -17,6 +17,9 @@ export interface Env {
 	//
 	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
 	// MY_BUCKET: R2Bucket;
+	//
+	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
+	// MY_SERVICE: Fetcher;
 }
 
 export default {
