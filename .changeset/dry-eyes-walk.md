084b2c58ba051811afe4adf1518cab033ba62872
Evan Simons <evan.simons@gmail.com>
2023-04-03 10:17:27 -0400

    Respect querystring params passed to `.fetch` exposed by `unstable_dev` (#2957)
    * do not strip querystring in parseRequestInput
    
    * add changeset
    
    * prettierfix changeset
    
    * don't mutate inputs

---
+++ .changeset/dry-eyes-walk.md
@@ -0,0 +1,27 @@
+---
+"wrangler": patch
+---
+
+fix: Respect querystring params when calling `.fetch` on a worker instantiated with `unstable_dev`
+
+Previously, querystring params would be stripped, causing issues for test cases that depended on them. For example, given the following worker script:
+
+```js
+export default {
+	fetch(req) {
+		const url = new URL(req.url);
+		const name = url.searchParams.get("name");
+		return new Response("Hello, " + name);
+	},
+};
+```
+
+would fail the following test case:
+
+```js
+const worker = await unstable_dev("script.js");
+const res = await worker.fetch("http://worker?name=Walshy");
+const text = await res.text();
+// Following fails, as returned text is 'Hello, null'
+expect(text).toBe("Hello, Walshy");
+```
--- packages/wrangler/src/__tests__/api.test.ts
+++ packages/wrangler/src/__tests__/api.test.ts
@@ -5,7 +5,7 @@ describe("parseRequestInput for fetch on unstable dev", () => {
 	it("should allow no input to be passed in", () => {
 		const [input, _] = parseRequestInput("0.0.0.0", 8080);
 
-		expect(input).toMatchInlineSnapshot(`"http://0.0.0.0:8080"`);
+		expect(input).toMatchInlineSnapshot(`"http://0.0.0.0:8080/"`);
 	});
 
 	it("should allow string of pathname to be passed in", () => {
@@ -14,14 +14,24 @@ describe("parseRequestInput for fetch on unstable dev", () => {
 		expect(input).toMatchInlineSnapshot(`"http://0.0.0.0:8080/test"`);
 	});
 
+	it("should allow string of pathname and querystring to be passed in", () => {
+		const [input, _] = parseRequestInput("0.0.0.0", 8080, "/test?q=testparam");
+
+		expect(input).toMatchInlineSnapshot(
+			`"http://0.0.0.0:8080/test?q=testparam"`
+		);
+	});
+
 	it("should allow full url to be passed in as string and stripped", () => {
 		const [input, _] = parseRequestInput(
 			"0.0.0.0",
 			8080,
-			"http://cloudflare.com/test"
+			"http://cloudflare.com/test?q=testparam"
 		);
 
-		expect(input).toMatchInlineSnapshot(`"http://0.0.0.0:8080/test"`);
+		expect(input).toMatchInlineSnapshot(
+			`"http://0.0.0.0:8080/test?q=testparam"`
+		);
 	});
 
 	it("should allow URL object without pathname to be passed in and stripped", () => {
@@ -44,18 +54,30 @@ describe("parseRequestInput for fetch on unstable dev", () => {
 		expect(input).toMatchInlineSnapshot(`"http://0.0.0.0:8080/test"`);
 	});
 
+	it("should allow URL object with pathname and querystring to be passed in and stripped", () => {
+		const [input, _] = parseRequestInput(
+			"0.0.0.0",
+			8080,
+			new URL("http://cloudflare.com/test?q=testparam")
+		);
+
+		expect(input).toMatchInlineSnapshot(
+			`"http://0.0.0.0:8080/test?q=testparam"`
+		);
+	});
+
 	it("should allow request object to be passed in", () => {
 		const [input, init] = parseRequestInput(
 			"0.0.0.0",
 			8080,
-			new Request("http://cloudflare.com/test", { method: "POST" })
+			new Request("http://cloudflare.com/test?q=testparam", { method: "POST" })
 		);
 
 		expect(init).toBeUndefined();
 		expect(input).toBeInstanceOf(Request);
 		// We don't expect the request to be modified
 		expect((input as Request).url).toMatchInlineSnapshot(
-			`"http://cloudflare.com/test"`
+			`"http://cloudflare.com/test?q=testparam"`
 		);
 		expect((input as Request).method).toMatchInlineSnapshot(`"POST"`);
 	});
