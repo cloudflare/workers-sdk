43558c66bedd773e31a60ceeb1f2b27a7ea5e107
Samuel Macleod <smacleod@cloudflare.com>
2023-04-03 14:24:45 +0100

    fixup! types and linting

--- packages/cloudflare-logic/src/extension.ts
+++ packages/cloudflare-logic/src/extension.ts
@@ -1,31 +1,31 @@
 import * as vscode from "vscode";
 import { CFS } from "./cfs";
-import {
-	Channel,
-	FromQuickEditMessage,
-	ToQuickEditMessage,
-	WorkerLoadedMessage,
-} from "./ipc";
+import { Channel } from "./ipc";
+import type { FromQuickEditMessage, ToQuickEditMessage } from "./ipc";
 
 export function activate(context: vscode.ExtensionContext) {
-	const channel = Channel<FromQuickEditMessage, ToQuickEditMessage>(
-		context.messagePassingProtocol!
-	);
-	const cfs = new CFS(channel);
-	context.subscriptions.push(cfs);
-	vscode.commands.executeCommand("workbench.action.closeAllEditors");
+	if (context.messagePassingProtocol) {
+		const channel = Channel<FromQuickEditMessage, ToQuickEditMessage>(
+			context.messagePassingProtocol
+		);
+		const cfs = new CFS(channel);
+		context.subscriptions.push(cfs);
+		void vscode.commands.executeCommand("workbench.action.closeAllEditors");
 
-	channel.onMessage(async (data) => {
-		if (data.type === "WorkerLoaded") {
-			console.log("WorkerLoaded", data.body);
-			await cfs.seed(data.body);
-			vscode.commands.executeCommand(
-				"vscode.open",
-				vscode.Uri.parse(`cfs:/${data.body.name}/${data.body.entrypoint}`),
-				{ preview: false }
-			);
-		}
-	});
+		channel.onMessage(async (data) => {
+			if (data.type === "WorkerLoaded") {
+				console.log("WorkerLoaded", data.body);
+				await cfs.seed(data.body);
+				void vscode.commands.executeCommand(
+					"vscode.open",
+					vscode.Uri.parse(`cfs:/${data.body.name}/${data.body.entrypoint}`),
+					{ preview: false }
+				);
+			}
+		});
+	} else {
+		throw new Error("MessagePort required");
+	}
 }
 
 export function deactivate() {}
