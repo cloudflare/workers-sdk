e311bbbf64343badd4bba7eb017b796a89eaf9fe
MrBBot <bcoll@cloudflare.com>
2023-04-04 15:26:43 +0200

    [wrangler] Make `WRANGLER_LOG` more lenient and fallback to `log` (#2840)
    * Make `WRANGLER_LOG` more lenient and fallback to `log`
    
    Previously, levels set via the `WRANGLER_LOG` environment-variable
    were case-sensitive. If an unexpected level was set, Wrangler would
    fallback to `none`, hiding all logs. The fallback has now been
    switched to `log`, and lenient case-insensitive matching is used when
    setting the level.
    
    * fixup! Make `WRANGLER_LOG` more lenient and fallback to `log`

--- packages/wrangler/src/__tests__/logger.test.ts
+++ packages/wrangler/src/__tests__/logger.test.ts
@@ -1,10 +1,11 @@
-import { logger } from "../logger";
+import { Logger } from "../logger";
 import { mockConsoleMethods } from "./helpers/mock-console";
 
 describe("logger", () => {
 	const std = mockConsoleMethods();
 
 	it("should add colored markers to error and warning messages", () => {
+		const logger = new Logger();
 		logger.loggerLevel = "debug";
 		logger.debug("This is a debug message");
 		logger.log("This is a log message");
@@ -27,6 +28,7 @@ describe("logger", () => {
 
 	describe("loggerLevel=debug", () => {
 		it("should render messages that are at or above the log level set in the logger", () => {
+			const logger = new Logger();
 			logger.loggerLevel = "debug";
 			logger.debug("This is a debug message");
 			logger.log("This is a log message");
@@ -50,6 +52,7 @@ describe("logger", () => {
 
 	describe("loggerLevel=log", () => {
 		it("should render messages that are at or above the log level set in the logger", () => {
+			const logger = new Logger();
 			logger.loggerLevel = "log";
 			logger.debug("This is a debug message");
 			logger.log("This is a log message");
@@ -73,6 +76,7 @@ describe("logger", () => {
 
 	describe("loggerLevel=warn", () => {
 		it("should render messages that are at or above the log level set in the logger", () => {
+			const logger = new Logger();
 			logger.loggerLevel = "warn";
 			logger.debug("This is a debug message");
 			logger.log("This is a log message");
@@ -96,6 +100,7 @@ describe("logger", () => {
 
 	describe("loggerLevel=error", () => {
 		it("should render messages that are at or above the log level set in the logger", () => {
+			const logger = new Logger();
 			logger.loggerLevel = "error";
 			logger.debug("This is a debug message");
 			logger.log("This is a log message");
@@ -122,7 +127,7 @@ describe("logger", () => {
 		});
 
 		it("should render messages that are at or above the log level set in the env var", () => {
-			logger.loggerLevel = "error";
+			const logger = new Logger();
 			logger.debug("This is a debug message");
 			logger.log("This is a log message");
 			logger.warn("This is a warn message");
@@ -131,6 +136,67 @@ describe("logger", () => {
 			expect(std.debug).toMatchInlineSnapshot(`""`);
 			expect(std.out).toMatchInlineSnapshot(`""`);
 			expect(std.warn).toMatchInlineSnapshot(`""`);
+			expect(std.err).toMatchInlineSnapshot(`
+        "[31mX [41;31m[[41;97mERROR[41;31m][0m [1mThis is a error message[0m
+
+        "
+      `);
+		});
+	});
+
+	describe("loggerLevelFromEnvVar case-insensitive", () => {
+		beforeEach(() => {
+			process.env.WRANGLER_LOG = "wARn";
+		});
+		afterEach(() => {
+			process.env.WRANGLER_LOG = undefined;
+		});
+
+		it("should render messages that are at or above the log level set in the env var", () => {
+			const logger = new Logger();
+			logger.debug("This is a debug message");
+			logger.log("This is a log message");
+			logger.warn("This is a warn message");
+			logger.error("This is a error message");
+
+			expect(std.debug).toMatchInlineSnapshot(`""`);
+			expect(std.out).toMatchInlineSnapshot(`""`);
+			expect(std.warn).toMatchInlineSnapshot(`
+        "[33m▲ [43;33m[[43;30mWARNING[43;33m][0m [1mThis is a warn message[0m
+
+        "
+      `);
+			expect(std.err).toMatchInlineSnapshot(`
+        "[31mX [41;31m[[41;97mERROR[41;31m][0m [1mThis is a error message[0m
+
+        "
+      `);
+		});
+	});
+
+	describe("loggerLevelFromEnvVar falls back to log on invalid level", () => {
+		beforeEach(() => {
+			process.env.WRANGLER_LOG = "everything";
+		});
+		afterEach(() => {
+			process.env.WRANGLER_LOG = undefined;
+		});
+
+		it("should render messages that are at or above the log level set in the env var", () => {
+			const logger = new Logger();
+			logger.debug("This is a debug message");
+			logger.log("This is a log message");
+			logger.warn("This is a warn message");
+			logger.error("This is a error message");
+
+			expect(std.debug).toMatchInlineSnapshot(`""`);
+			expect(std.out).toMatchInlineSnapshot(`"This is a log message"`);
+			expect(std.warn).toMatchInlineSnapshot(`
+        "Unrecognised WRANGLER_LOG value \\"everything\\", expected \\"none\\" | \\"error\\" | \\"warn\\" | \\"info\\" | \\"log\\" | \\"debug\\", defaulting to \\"log\\"...
+        [33m▲ [43;33m[[43;30mWARNING[43;33m][0m [1mThis is a warn message[0m
+
+        "
+      `);
 			expect(std.err).toMatchInlineSnapshot(`
         "[31mX [41;31m[[41;97mERROR[41;31m][0m [1mThis is a error message[0m
 
