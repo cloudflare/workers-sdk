62ced62fd51df9464087e96acf74ed01329e7454
Somhairle MacLeòid <smacleod@cloudflare.com>
2023-04-05 02:11:17 +0100

    Add `format-errors` worker (#2977)
    * Add format-errors worker
    
    * fixup! add env & ctx
    
    * fixup! linting
    
    * fixup! linting
    
    * Add more details on fork to README
    
    * Address PR feedback
    
    * address comments

---
+++ packages/format-errors/src/Stacktracey.js
@@ -0,0 +1,330 @@
+"use strict";
+/**
+ * Adapted from Stacktracey (https://github.com/xpl/stacktracey).
+ *
+ * For the original license, see https://github.com/xpl/stacktracey/blob/master/LICENSE
+ *
+ */
+import partition from "./impl/partition";
+const O = Object,
+	isBrowser = true,
+	nodeRequire = null,
+	lastOf = (x) => x[x.length - 1],
+	getSource = null,
+	nixSlashes = (x) => x.replace(/\\/g, "/"),
+	pathRoot = "/";
+
+/*  ------------------------------------------------------------------------ */
+
+export default class StackTracey {
+	constructor(input, offset) {
+		const originalInput = input,
+			isParseableSyntaxError =
+				input && input instanceof SyntaxError && !isBrowser;
+
+		/*  new StackTracey ()            */
+
+		if (!input) {
+			input = new Error();
+			offset = offset === undefined ? 1 : offset;
+		}
+
+		/*  new StackTracey (Error)      */
+
+		if (input instanceof Error) {
+			input = input.stack || "";
+		}
+
+		/*  new StackTracey (string)     */
+
+		if (typeof input === "string") {
+			input = this.rawParse(input)
+				.slice(offset)
+				.map((x) => this.extractEntryMetadata(x));
+		}
+
+		/*  new StackTracey (array)      */
+
+		if (Array.isArray(input)) {
+			if (isParseableSyntaxError) {
+				const rawLines = nodeRequire("util").inspect(originalInput).split("\n"),
+					fileLine = rawLines[0].split(":"),
+					line = fileLine.pop(),
+					file = fileLine.join(":");
+
+				if (file) {
+					input.unshift({
+						file: nixSlashes(file),
+						line: line,
+						column: (rawLines[2] || "").indexOf("^") + 1,
+						sourceLine: rawLines[1],
+						callee: "(syntax error)",
+						syntaxError: true,
+					});
+				}
+			}
+
+			this.items = input;
+		} else {
+			this.items = [];
+		}
+	}
+
+	extractEntryMetadata(e) {
+		const decomposedPath = this.decomposePath(e.file || "");
+		const fileRelative = decomposedPath[0];
+		const externalDomain = decomposedPath[1];
+
+		return O.assign(e, {
+			calleeShort: e.calleeShort || lastOf((e.callee || "").split(".")),
+			fileRelative: fileRelative,
+			fileShort: this.shortenPath(fileRelative),
+			fileName: lastOf((e.file || "").split("/")),
+			thirdParty: this.isThirdParty(fileRelative, externalDomain) && !e.index,
+			externalDomain: externalDomain,
+		});
+	}
+
+	shortenPath(relativePath) {
+		return relativePath
+			.replace(/^node_modules\//, "")
+			.replace(/^webpack\/bootstrap\//, "")
+			.replace(/^__parcel_source_root\//, "");
+	}
+
+	decomposePath(fullPath) {
+		let result = fullPath;
+
+		if (isBrowser) result = result.replace(pathRoot, "");
+
+		const externalDomainMatch = result.match(
+			/^(http|https)\:\/\/?([^\/]+)\/(.*)/
+		);
+		const externalDomain = externalDomainMatch
+			? externalDomainMatch[2]
+			: undefined;
+		result = externalDomainMatch ? externalDomainMatch[3] : result;
+
+		if (!isBrowser) result = nodeRequire("path").relative(pathRoot, result);
+
+		return [
+			nixSlashes(result).replace(/^.*\:\/\/?\/?/, ""), // cut webpack:/// and webpack:/ things
+			externalDomain,
+		];
+	}
+
+	isThirdParty(relativePath, externalDomain) {
+		return (
+			externalDomain ||
+			relativePath[0] === "~" || // webpack-specific heuristic
+			relativePath[0] === "/" || // external source
+			relativePath.indexOf("node_modules") === 0 ||
+			relativePath.indexOf("webpack/bootstrap") === 0
+		);
+	}
+
+	rawParse(str) {
+		const lines = (str || "").split("\n");
+
+		const entries = lines.map((line) => {
+			line = line.trim();
+
+			let callee,
+				fileLineColumn = [],
+				native,
+				planA,
+				planB;
+
+			if (
+				(planA = line.match(/at (.+) \(eval at .+ \((.+)\), .+\)/)) || // eval calls
+				(planA = line.match(/at (.+) \((.+)\)/)) ||
+				(line.slice(0, 3) !== "at " && (planA = line.match(/(.*)@(.*)/)))
+			) {
+				callee = planA[1];
+				native = planA[2] === "native";
+				fileLineColumn = (
+					planA[2].match(/(.*):(\d+):(\d+)/) ||
+					planA[2].match(/(.*):(\d+)/) ||
+					[]
+				).slice(1);
+			} else if ((planB = line.match(/^(at\s+)*(.+):(\d+):(\d+)/))) {
+				fileLineColumn = planB.slice(2);
+			} else {
+				return undefined;
+			}
+
+			/*  Detect things like Array.reduce
+            TODO: detect more built-in types            */
+
+			if (callee && !fileLineColumn[0]) {
+				const type = callee.split(".")[0];
+				if (type === "Array") {
+					native = true;
+				}
+			}
+
+			return {
+				beforeParse: line,
+				callee: callee || "",
+				index: isBrowser && fileLineColumn[0] === "/",
+				native: native || false,
+				file: nixSlashes(fileLineColumn[0] || ""),
+				line: parseInt(fileLineColumn[1] || "", 10) || undefined,
+				column: parseInt(fileLineColumn[2] || "", 10) || undefined,
+			};
+		});
+
+		return entries.filter((x) => x !== undefined);
+	}
+
+	withSourceAt(i) {
+		return this.items[i] && this.withSource(this.items[i]);
+	}
+
+	withSourceAsyncAt(i) {
+		return this.items[i] && this.withSourceAsync(this.items[i]);
+	}
+
+	withSource(loc) {
+		if (this.shouldSkipResolving(loc)) {
+			return loc;
+		} else {
+			let resolved = getSource(loc.file || "").resolve(loc);
+
+			if (!resolved.sourceFile) {
+				return loc;
+			}
+
+			return this.withSourceResolved(loc, resolved);
+		}
+	}
+
+	withSourceAsync(loc) {
+		if (this.shouldSkipResolving(loc)) {
+			return Promise.resolve(loc);
+		} else {
+			return getSource
+				.async(loc.file || "")
+				.then((x) => x.resolve(loc))
+				.then((resolved) => this.withSourceResolved(loc, resolved))
+				.catch((e) =>
+					this.withSourceResolved(loc, { error: e, sourceLine: "" })
+				);
+		}
+	}
+
+	shouldSkipResolving(loc) {
+		return (
+			loc.sourceFile || loc.error || (loc.file && loc.file.indexOf("<") >= 0)
+		); // skip things like <anonymous> and stuff that was already fetched
+	}
+
+	withSourceResolved(loc, resolved) {
+		if (resolved.sourceFile && !resolved.sourceFile.error) {
+			resolved.file = nixSlashes(resolved.sourceFile.path);
+			resolved = this.extractEntryMetadata(resolved);
+		}
+
+		if (resolved.sourceLine.includes("// @hide")) {
+			resolved.sourceLine = resolved.sourceLine.replace("// @hide", "");
+			resolved.hide = true;
+		}
+
+		if (
+			resolved.sourceLine.includes("__webpack_require__") || // webpack-specific heuristics
+			resolved.sourceLine.includes("/******/ ({")
+		) {
+			resolved.thirdParty = true;
+		}
+
+		return O.assign({ sourceLine: "" }, loc, resolved);
+	}
+
+	withSources() {
+		return this.map((x) => this.withSource(x));
+	}
+
+	withSourcesAsync() {
+		return Promise.all(this.items.map((x) => this.withSourceAsync(x))).then(
+			(items) => new StackTracey(items)
+		);
+	}
+
+	mergeRepeatedLines() {
+		return new StackTracey(
+			partition(this.items, (e) => e.file + e.line).map((group) => {
+				return group.items.slice(1).reduce((memo, entry) => {
+					memo.callee =
+						(memo.callee || "<anonymous>") +
+						" → " +
+						(entry.callee || "<anonymous>");
+					memo.calleeShort =
+						(memo.calleeShort || "<anonymous>") +
+						" → " +
+						(entry.calleeShort || "<anonymous>");
+					return memo;
+				}, O.assign({}, group.items[0]));
+			})
+		);
+	}
+
+	clean() {
+		const s = this.withSources().mergeRepeatedLines();
+		return s.filter(s.isClean.bind(s));
+	}
+
+	cleanAsync() {
+		return this.withSourcesAsync().then((s) => {
+			s = s.mergeRepeatedLines();
+			return s.filter(s.isClean.bind(s));
+		});
+	}
+
+	isClean(entry, index) {
+		return index === 0 || !(entry.thirdParty || entry.hide || entry.native);
+	}
+
+	at(i) {
+		return O.assign(
+			{
+				beforeParse: "",
+				callee: "<???>",
+				index: false,
+				native: false,
+				file: "<???>",
+				line: 0,
+				column: 0,
+			},
+			this.items[i]
+		);
+	}
+
+	maxColumnWidths() {
+		return {
+			callee: 30,
+			file: 60,
+			sourceLine: 80,
+		};
+	}
+
+	static resetCache() {
+		getSource.resetCache();
+		getSource.async.resetCache();
+	}
+
+	static locationsEqual(a, b) {
+		return a.file === b.file && a.line === b.line && a.column === b.column;
+	}
+}
+
+/*  Array methods
+    ------------------------------------------------------------------------ */
+
+["map", "filter", "slice", "concat"].forEach((method) => {
+	StackTracey.prototype[method] = function (/*...args */) {
+		// no support for ...args in Node v4 :(
+		return new StackTracey(this.items[method].apply(this.items, arguments));
+	};
+});
+
+/*  ------------------------------------------------------------------------ */
---
+++ packages/format-errors/src/index.ts
@@ -0,0 +1,163 @@
+import { Toucan } from "toucan-js";
+import { z } from "zod";
+import Youch from "./Youch";
+export interface Env {
+	SENTRY_ACCESS_CLIENT_SECRET: string;
+	SENTRY_ACCESS_CLIENT_ID: string;
+	SENTRY_DSN: string;
+}
+export interface JsonError {
+	message?: string;
+	name?: string;
+	stack?: string;
+	cause?: JsonError;
+}
+export interface Payload {
+	url?: string;
+	method?: string;
+	headers?: Record<string, string>;
+	error?: JsonError;
+}
+export const JsonErrorSchema: z.ZodType<JsonError> = z.lazy(() =>
+	z.object({
+		message: z.string().optional(),
+		name: z.string().optional(),
+		stack: z.string().optional(),
+		cause: JsonErrorSchema.optional(),
+	})
+);
+
+export const PayloadSchema = z.object({
+	url: z.string().optional(),
+	method: z.string().optional(),
+	headers: z.record(z.string()),
+	error: JsonErrorSchema.optional(),
+});
+
+interface StandardErrorConstructor {
+	new (message?: string, options?: { cause?: Error }): Error;
+}
+const ALLOWED_ERROR_SUBCLASS_CONSTRUCTORS: StandardErrorConstructor[] = [
+	EvalError,
+	RangeError,
+	ReferenceError,
+	SyntaxError,
+	TypeError,
+	URIError,
+];
+export function reviveError(jsonError: JsonError): Error {
+	// At a high level, this function takes a JSON-serialisable representation of
+	// an `Error`, and converts it to an `Error`. `Error`s may have `cause`s, so
+	// we need to do this recursively.
+	let cause: Error | undefined;
+	if (jsonError.cause !== undefined) {
+		cause = reviveError(jsonError.cause);
+	}
+
+	// If this is one of the built-in error types, construct an instance of that.
+	// For example, if we threw a `TypeError` in the Worker, we'd like to
+	// construct a `TypeError` here, so it looks like the error has been thrown
+	// through a regular function call, not an HTTP request (i.e. we want
+	// `instanceof TypeError` to pass in Node for `TypeError`s thrown in Workers).
+	let ctor: StandardErrorConstructor = Error;
+	if (jsonError.name !== undefined && jsonError.name in globalThis) {
+		const maybeCtor = (globalThis as Record<string, unknown>)[
+			jsonError.name
+		] as StandardErrorConstructor;
+		if (ALLOWED_ERROR_SUBCLASS_CONSTRUCTORS.includes(maybeCtor)) {
+			ctor = maybeCtor;
+		}
+	}
+
+	// Construct the error, copying over the correct name and stack trace.
+	// Because constructing an `Error` captures the stack trace at point of
+	// construction, we override the stack trace to the one from the Worker in the
+	// JSON-serialised error.
+	const error = new ctor(jsonError.message, { cause });
+	if (jsonError.name !== undefined) error.name = jsonError.name;
+	error.stack = jsonError.stack;
+
+	return error;
+}
+
+export async function handlePrettyErrorRequest({
+	error: jsonError,
+	url,
+	method,
+	headers,
+}: Payload): Promise<Response> {
+	// Parse and validate the error we've been given from user code
+	const caught = JsonErrorSchema.parse(jsonError);
+
+	// Convert the error into a regular `Error` object and try to source-map it.
+	// We need to give `name`, `message` and `stack` to Youch, but StackTracy,
+	// Youch's dependency for parsing `stack`s, will only extract `stack` from
+	// an object if it's an `instanceof Error`.
+	const error = reviveError(caught);
+
+	// Log source-mapped error to console if logging enabled
+
+	// Lazily import `youch` when required
+	// `cause` is usually more useful than the error itself, display that instead
+	// TODO(someday): would be nice if we could display both
+	const youch = new Youch(error.cause ?? error, {
+		url,
+		method,
+		headers,
+	});
+	youch.addLink(() => {
+		return [
+			'<a href="https://developers.cloudflare.com/workers/" target="_blank" style="text-decoration:none">📚 Workers Docs</a>',
+			'<a href="https://discord.gg/cloudflaredev" target="_blank" style="text-decoration:none">💬 Workers Discord</a>',
+		].join("");
+	});
+	return new Response(await youch.toHTML(), {
+		headers: { "Content-Type": "text/html;charset=utf-8" },
+	});
+}
+
+export default {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<Response> {
+		const sentry = new Toucan({
+			dsn: env.SENTRY_DSN,
+			context: ctx,
+			request,
+			requestDataOptions: {
+				allowedHeaders: [
+					"user-agent",
+					"accept-encoding",
+					"accept-language",
+					"cf-ray",
+					"content-length",
+					"content-type",
+					"host",
+				],
+			},
+			transportOptions: {
+				headers: {
+					"CF-Access-Client-ID": env.SENTRY_ACCESS_CLIENT_ID,
+					"CF-Access-Client-Secret": env.SENTRY_ACCESS_CLIENT_SECRET,
+				},
+			},
+		});
+		try {
+			const payload = PayloadSchema.parse(await request.json());
+			return handlePrettyErrorRequest(payload);
+		} catch (e) {
+			sentry.captureException(e);
+			return Response.json(
+				{
+					error: "UnexpectedError",
+					message: "Something went wrong",
+				},
+				{
+					status: 500,
+				}
+			);
+		}
+	},
+};
