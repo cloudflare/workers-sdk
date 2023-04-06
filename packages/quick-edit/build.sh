94154275ec9d79fc3a6f95ae7b26eecd1a55900a
Samuel Macleod <smacleod@cloudflare.com>
2023-04-06 14:42:34 +0100

    fix package-lock & build

--- packages/quick-edit/build.sh
+++ packages/quick-edit/build.sh
@@ -3,9 +3,22 @@ set -eu
 # Cleanup development symlink to vscode
 rm -f web/vscode
 
+# See https://unix.stackexchange.com/questions/30091/fix-or-alternative-for-mktemp-in-os-x
+BUILD_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'vscode-build')
+DIR=$(pwd)
+
+# Isolate the from the workers-sdk environment to ensure a clean build
+cp -r vscode $BUILD_DIR/vscode
+
+cd $BUILD_DIR/vscode
+yarn
+
 # Build vscode
-yarn --cwd vscode gulp vscode-web-min
-mv vscode-web web/assets
+yarn gulp vscode-web-min
+
+cd $DIR
+
+mv $BUILD_DIR/vscode-web web/assets
 # Pages doesn't support uploading folders called node_modules
 mv web/assets/node_modules web/assets/modules
 
