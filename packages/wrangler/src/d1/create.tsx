6563216daa920efa645adb09054824035b503554
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2023-04-06 15:43:13 +0200

    [wrangler] make flag name consistent with R2 (#3000)
    * [wrangler] make flag name consistent with R2
    
    * [wrangler] missed a few types

--- packages/wrangler/src/d1/create.tsx
+++ packages/wrangler/src/d1/create.tsx
@@ -19,9 +19,9 @@ export function Options(yargs: CommonYargsArgv) {
 			type: "string",
 			demandOption: true,
 		})
-		.option("primary-location-hint", {
+		.option("location", {
 			describe:
-				"A hint for the main location of the new DB. Options:\nweur: Europe (west)\neeur: Europe (east)\napac: Asia Pacific\nwnam: North America (west)\nenam: North America (east) \n",
+				"A hint for the primary location of the new DB. Options:\nweur: Europe (west)\neeur: Europe (east)\napac: Asia Pacific\nwnam: North America (west)\nenam: North America (east) \n",
 			type: "string",
 			choices: ["weur", "eeur", "apac", "wnam", "enam"],
 		})
@@ -30,7 +30,7 @@ export function Options(yargs: CommonYargsArgv) {
 
 export async function Handler({
 	name,
-	primaryLocationHint,
+	location,
 }: StrictYargsOptionsToInterface<typeof Options>): Promise<void> {
 		const accountId = await requireAuth(config);
 
@@ -45,8 +45,8 @@ export async function Handler({
 			},
 			body: JSON.stringify({
 				name,
-				...(primaryLocationHint && {
-					primary_location_hint: primaryLocationHint,
+				...(location && {
+					primary_location_hint: location,
 				}),
 			}),
 		});
@@ -82,9 +82,7 @@ export async function Handler({
 			<Box flexDirection="column">
 				<Text>
 					✅ Successfully created DB &apos;{db.name}&apos;
-					{primaryLocationHint
-						? ` using location hint ${primaryLocationHint}`
-						: ``}
+					{location ? ` using primary location hint ${location}` : ``}
 				</Text>
 				<Text>&nbsp;</Text>
 				<Text>
