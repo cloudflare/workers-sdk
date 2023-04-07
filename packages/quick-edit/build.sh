9301c6100356b4126b88f04bc1d397424c032589
Samuel Macleod <smacleod@cloudflare.com>
2023-04-07 02:16:33 +0100

    fix types, i hope

--- packages/quick-edit/build.sh
+++ packages/quick-edit/build.sh
@@ -3,22 +3,12 @@ set -eu
 # Cleanup development symlink to vscode
 rm -f web/vscode
 
-# See https://unix.stackexchange.com/questions/30091/fix-or-alternative-for-mktemp-in-os-x
-BUILD_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'vscode-build')
-DIR=$(pwd)
-
-# Isolate the from the workers-sdk environment to ensure a clean build
-cp -r vscode $BUILD_DIR/vscode
-
-cd $BUILD_DIR/vscode
-yarn
+yarn --cwd ../../vendor/vscode
 
 # Build vscode
-yarn gulp vscode-web-min
+yarn --cwd ../../vendor/vscode gulp vscode-web-min
 
-cd $DIR
-
-mv $BUILD_DIR/vscode-web web/assets
+mv ../../vendor/vscode-web web/assets
 # Pages doesn't support uploading folders called node_modules
 mv web/assets/node_modules web/assets/modules
 
--- packages/quick-edit/setup.sh
+++ packages/quick-edit/setup.sh
@@ -3,19 +3,24 @@ set -eu
 VERSION=1.76.0
 
 rm -rf web
+rm -rf ../../vendor/vscode
 mkdir -p web
 cd ..
 ln -s $PWD/quick-edit-extension $PWD/quick-edit/web/quick-edit-extension
 
-cd quick-edit
-git clone --depth 1 --branch "$VERSION" https://github.com/microsoft/vscode
+cd quick-edit-extension
+npx vscode-dts dev 1.76.0
+npx vscode-dts 1.76.0
 
-cd vscode
+cd ../quick-edit
+git clone --depth 1 --branch "$VERSION" https://github.com/microsoft/vscode ../../vendor/vscode
+
+cd ../../vendor/vscode
 git checkout -b base
 git checkout -b cloudflare
 git config user.email "workers-devprod@cloudflare.com"
 git config user.name "Workers DevProd"
 
-git am ../patches/*.patch
-cd ..
-ln -s $PWD/vscode $PWD/web/vscode
+git am ../../packages/quick-edit/patches/*.patch
+yarn
+cd ../../packages/quick-edit
