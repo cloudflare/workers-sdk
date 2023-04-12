423627237360d70e84fc045219d9500e5f7b5c6d
Jacob M-G Evans <27247160+JacobMGEvans@users.noreply.github.com>
2023-04-12 12:31:47 -0500

    Entrypoint & Theme (#3028)
    * Entrypoint & Theme
    Create an extension command and palette command for changing the file entrypoint and sending the event to the frontend through the messagePort.
    Dynamic Theme for light and dark mode on VSCode setup.

--- packages/quick-edit/functions/_middleware.ts
+++ packages/quick-edit/functions/_middleware.ts
@@ -4,9 +4,14 @@ export const onRequest = async ({
 	next,
 }: Parameters<PagesFunction>[0]) => {
 	const url = new URL(request.url);
+
 	const values = {
 		WORKBENCH_WEB_CONFIGURATION: JSON.stringify({
 			configurationDefaults: {
+				"workbench.colorTheme":
+					url.searchParams.get("theme") === "dark"
+						? "Solarflare Dark"
+						: "Solarflare Light",
 				"workbench.startupEditor": "none",
 				"editor.minimap.autohide": true,
 			},
