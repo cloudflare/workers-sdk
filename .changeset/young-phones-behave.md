63987316d8728573b0a6931623b8755afd0e1e2d
Jacob Hands <jacob@gogit.io>
2023-05-03 14:49:09 -0500

    fix: Remove global flag in pages-shared regex

---
+++ .changeset/young-phones-behave.md
@@ -0,0 +1,5 @@
+---
+"@cloudflare/pages-shared": patch
+---
+
+fix: Remove global flag in pages-shared regex
--- packages/pages-shared/__tests__/asset-server/responses.test.ts
+++ packages/pages-shared/__tests__/asset-server/responses.test.ts
@@ -1,7 +1,7 @@
 import { stripLeadingDoubleSlashes } from "../../asset-server/responses";
 
 describe("stripLeadingDoubleSlashes", () => {
-	it("strips extra leading `/`, `%2F` and `%2f`s", () => {
+	it("strips extra leading `/`, `%2F`, `%2f`s, spaces, and tabs", () => {
 		expect(stripLeadingDoubleSlashes("/")).toMatchInlineSnapshot(`"/"`);
 		expect(stripLeadingDoubleSlashes("/%2Ffoo")).toMatchInlineSnapshot(
 			`"/foo"`
@@ -46,5 +46,16 @@ describe("stripLeadingDoubleSlashes", () => {
 		expect(stripLeadingDoubleSlashes("/%09foo")).toMatchInlineSnapshot(
 			`"/foo"`
 		);
+		expect(stripLeadingDoubleSlashes("/%09/foo")).toMatchInlineSnapshot(
+			`"/foo"`
+		);
+		// Unencoded space / tab
+		expect(stripLeadingDoubleSlashes("/%09/foo/%09/ /	/")).toMatchInlineSnapshot(
+			`"/foo/%09/ /	/"`
+		);
+		// Unencoded space
+		expect(stripLeadingDoubleSlashes("/ /foo")).toMatchInlineSnapshot(`"/foo"`);
+		// Unencoded tab
+		expect(stripLeadingDoubleSlashes("/	/foo")).toMatchInlineSnapshot(`"/foo"`);
 	});
 });
