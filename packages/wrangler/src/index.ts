d49b2757574942e1f436f1123df2fb3393e493ec
MrBBot <bcoll@cloudflare.com>
2023-05-12 13:26:04 +0100

    feature: warn when a new major version is available (#3189)
    This change logs a much more noticeable warning when a new major
    version is available, with instructions for update.
    
    Closes DEVX-624

--- packages/wrangler/src/index.ts
+++ packages/wrangler/src/index.ts
@@ -98,7 +98,11 @@ export async function printWranglerBanner() {
 		return;
 	}
 
-	const text = ` ⛅️ wrangler ${wranglerVersion} ${await updateCheck()}`;
+	let text = ` ⛅️ wrangler ${wranglerVersion}`;
+	const maybeNewVersion = await updateCheck();
+	if (maybeNewVersion !== undefined) {
+		text += ` (update available ${chalk.green(maybeNewVersion)})`;
+	}
 
 	logger.log(
 		text +
@@ -107,6 +111,20 @@ export async function printWranglerBanner() {
 				? chalk.hex("#FF8800")("-".repeat(text.length))
 				: "-".repeat(text.length))
 	);
+
+	// Log a slightly more noticeable message if this is a major bump
+	if (maybeNewVersion !== undefined) {
+		const currentMajor = parseInt(wranglerVersion.split(".")[0]);
+		const newMajor = parseInt(maybeNewVersion.split(".")[0]);
+		if (newMajor > currentMajor) {
+			logger.warn(
+				`The version of Wrangler you are using is now out-of-date.
+Please update to the latest version to prevent critical errors.
+Run \`npm install --save-dev wrangler@${newMajor}\` to update to the latest version.
+After installation, run Wrangler with \`npx wrangler\`.`
+			);
+		}
+	}
 }
 
 export function isLegacyEnv(config: Config): boolean {
