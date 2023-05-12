02a672ed39921a75f32d6d2665d5a0080a71e34f
MrBBot <bcoll@cloudflare.com>
2023-05-12 15:29:46 +0100

    [wrangler] feature: enable persistent storage in local mode by default (#3188)
    * feature: enable persistent storage in local mode by default
    
    Wrangler will now persist local KV, R2, D1, Cache and Durable Object
    data in the `.wrangler` folder, by default, between reloads. This
    persistence directory can be customised with the `--persist-to` flag.
    The `--persist` flag has been removed, as this is now the default
    behaviour.
    
    * fixup! feature: enable persistent storage in local mode by default
    
    Make change for Pages too

--- packages/wrangler/src/dev.tsx
+++ packages/wrangler/src/dev.tsx
@@ -240,14 +240,9 @@ export function devOptions(yargs: CommonYargsArgv) {
 				deprecated: true,
 				hidden: true,
 			})
-			.option("persist", {
-				describe:
-					"Enable persistence for local mode, using default path: .wrangler/state",
-				type: "boolean",
-			})
 			.option("persist-to", {
 				describe:
-					"Specify directory to use for local persistence (implies --persist)",
+					"Specify directory to use for local persistence (defaults to .wrangler/state)",
 				type: "string",
 				requiresArg: true,
 			})
@@ -792,7 +787,6 @@ async function validateDevServerSettings(
 
 	const localPersistencePath = getLocalPersistencePath(
 		args.persistTo,
-		Boolean(args.persist),
 		config.configPath
 	);
 
