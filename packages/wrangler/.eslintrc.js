459d91347b19f9b1350b23abf2e14875d8874acd
Jacob M-G Evans <jacobmgevans@gmail.com>
2023-05-19 14:28:00 -0500

    ESLint

--- packages/wrangler/.eslintrc.js
+++ packages/wrangler/.eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
 	root: true,
 	extends: ["eslint-worker-config"],
+	ignorePatterns: [
+		"vendor",
+		"*-dist",
+		"pages/functions/template-worker.ts",
+		"templates",
+		"emitted-types",
+	],
 };
