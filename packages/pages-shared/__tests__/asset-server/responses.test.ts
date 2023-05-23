d3e69aefe9547d98254fbd27171f2214a7b27178
Jacob M-G Evans <jacobmgevans@gmail.com>
2023-05-23 10:07:28 -0500

    add back test files

---
+++ packages/pages-shared/__tests__/asset-server/responses.test.ts
@@ -0,0 +1,61 @@
+import { stripLeadingDoubleSlashes } from "../../asset-server/responses";
+
+describe("stripLeadingDoubleSlashes", () => {
+	it("strips extra leading `/`, `%2F`, `%2f`s, spaces, and tabs", () => {
+		expect(stripLeadingDoubleSlashes("/")).toMatchInlineSnapshot(`"/"`);
+		expect(stripLeadingDoubleSlashes("/%2Ffoo")).toMatchInlineSnapshot(
+			`"/foo"`
+		);
+		expect(stripLeadingDoubleSlashes("/%2ffoo")).toMatchInlineSnapshot(
+			`"/foo"`
+		);
+		expect(stripLeadingDoubleSlashes("/%2F%2f/foo")).toMatchInlineSnapshot(
+			`"/foo"`
+		);
+		expect(stripLeadingDoubleSlashes("/%5Cfoo")).toMatchInlineSnapshot(
+			`"/foo"`
+		);
+		expect(stripLeadingDoubleSlashes("/%5cfoo")).toMatchInlineSnapshot(
+			`"/foo"`
+		);
+		expect(stripLeadingDoubleSlashes("/%5C%5c/foo")).toMatchInlineSnapshot(
+			`"/foo"`
+		);
+		expect(stripLeadingDoubleSlashes("/%2f%5c/foo")).toMatchInlineSnapshot(
+			`"/foo"`
+		);
+		expect(stripLeadingDoubleSlashes("/\\/foo")).toMatchInlineSnapshot(
+			`"/foo"`
+		);
+		expect(stripLeadingDoubleSlashes("%2ffoo")).toMatchInlineSnapshot(`"/foo"`);
+		expect(stripLeadingDoubleSlashes("/foo/%2f")).toMatchInlineSnapshot(
+			`"/foo/%2f"`
+		);
+		expect(stripLeadingDoubleSlashes("/foo/%2F")).toMatchInlineSnapshot(
+			`"/foo/%2F"`
+		);
+		expect(stripLeadingDoubleSlashes("/foo//")).toMatchInlineSnapshot(
+			`"/foo//"`
+		);
+		expect(stripLeadingDoubleSlashes("/foo//bar")).toMatchInlineSnapshot(
+			`"/foo//bar"`
+		);
+		expect(stripLeadingDoubleSlashes("/foo/\\/bar")).toMatchInlineSnapshot(
+			`"/foo/\\\\/bar"`
+		);
+		expect(stripLeadingDoubleSlashes("/%09foo")).toMatchInlineSnapshot(
+			`"/foo"`
+		);
+		expect(stripLeadingDoubleSlashes("/%09/foo")).toMatchInlineSnapshot(
+			`"/foo"`
+		);
+		// Unencoded space / tab
+		expect(stripLeadingDoubleSlashes("/%09/foo/%09/ /	/")).toMatchInlineSnapshot(
+			`"/foo/%09/ /	/"`
+		);
+		// Unencoded space
+		expect(stripLeadingDoubleSlashes("/ /foo")).toMatchInlineSnapshot(`"/foo"`);
+		// Unencoded tab
+		expect(stripLeadingDoubleSlashes("/	/foo")).toMatchInlineSnapshot(`"/foo"`);
+	});
+});
