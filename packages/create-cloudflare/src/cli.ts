01bd661899fa67edf4f0ac6123d2eb0ce36aede3
Matt Silverlock <matt@eatsleeprepeat.net>
2023-06-09 12:58:24 -0400

    c3: clearer language during init flow

--- packages/create-cloudflare/src/cli.ts
+++ packages/create-cloudflare/src/cli.ts
@@ -126,7 +126,7 @@ const templateMap: Record<string, TemplateConfig> = {
 		handler: runWorkersGenerator,
 	},
 	chatgptPlugin: {
-		label: `ChatGPT plugin (Typescript)`,
+		label: `ChatGPT plugin (TypeScript)`,
 		handler: (args) =>
 			runWorkersGenerator({
 				...args,
