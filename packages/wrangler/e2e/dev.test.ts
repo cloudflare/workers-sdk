c69d270301c1183d8d444399584d5a07ecd0528f
Pete Bacon Darwin <pete@bacondarwin.com>
2023-06-15 10:47:40 +0100

    e2e clean up for c3 integration testing

--- packages/wrangler/e2e/dev.test.ts
+++ packages/wrangler/e2e/dev.test.ts
@@ -1,47 +1,43 @@
 import crypto from "node:crypto";
 import path from "node:path";
 import getPort from "get-port";
+import shellac from "shellac";
 import { fetch } from "undici";
-import { beforeEach, describe, expect, it } from "vitest";
+import { beforeAll, beforeEach, describe, expect, it } from "vitest";
 import { retry } from "./helpers/retry";
-import { RUN, runInBg } from "./helpers/run";
 import { dedent, makeRoot, seed } from "./helpers/setup";
+import { WRANGLER } from "./helpers/wrangler-command";
 
 async function runDevSession(
-	workerName: string,
 	workerPath: string,
 	flags: string,
 	session: (port: number) => Promise<void>
 ) {
 	let pid;
-	let promise;
 	try {
 		const port = await getPort();
-		const bg = await runInBg(workerPath, {
-			[workerName]: "smoke-test-worker",
-			[`http://127.0.0.1:${port}`]: "http://127.0.0.1:PORT",
-		})`
-	in ${workerPath} {
-		exits {
-			$$ ${RUN} dev ${flags} --port ${port}
-		}
-	}
-`;
+		const bg = await shellac.in(workerPath).env(process.env).bg`
+			exits {
+				$$ ${WRANGLER} dev ${flags} --port ${port}
+			}
+		`;
 		pid = bg.pid;
-		promise = bg.promise;
 		await session(port);
+		return bg.promise;
 	} finally {
 		if (pid) process.kill(pid);
 	}
-	return await promise;
 }
 
-describe("basic dev tests", async () => {
-	const root = await makeRoot();
-	const workerName = `smoke-test-worker-${crypto
-		.randomBytes(4)
-		.toString("hex")}`;
-	const workerPath = path.join(root, workerName);
+describe("basic dev tests", () => {
+	let workerName: string;
+	let workerPath: string;
+
+	beforeAll(async () => {
+		const root = await makeRoot();
+		workerName = `smoke-test-worker-${crypto.randomBytes(4).toString("hex")}`;
+		workerPath = path.join(root, workerName);
+	});
 
 	beforeEach(async () => {
 		await seed(workerPath, {
@@ -65,8 +61,9 @@ describe("basic dev tests", async () => {
 					`,
 		});
 	});
+
 	it("can modify worker during dev session (local)", async () => {
-		await runDevSession(workerName, workerPath, "", async (port) => {
+		await runDevSession(workerPath, "", async (port) => {
 			await retry(() =>
 				expect(
 					fetch(`http://127.0.0.1:${port}`).then((r) => r.text())
@@ -88,31 +85,27 @@ describe("basic dev tests", async () => {
 			);
 		});
 	});
+
 	it("can modify worker during dev session (remote)", async () => {
-		await runDevSession(
-			workerName,
-			workerPath,
-			"--remote --ip 127.0.0.1",
-			async (port) => {
-				await retry(() =>
-					expect(
-						fetch(`http://127.0.0.1:${port}`).then((r) => r.text())
-					).resolves.toMatchInlineSnapshot('"Hello World!"')
-				);
-				await seed(workerPath, {
-					"src/index.ts": dedent`
+		await runDevSession(workerPath, "--remote --ip 127.0.0.1", async (port) => {
+			await retry(() =>
+				expect(
+					fetch(`http://127.0.0.1:${port}`).then((r) => r.text())
+				).resolves.toMatchInlineSnapshot('"Hello World!"')
+			);
+			await seed(workerPath, {
+				"src/index.ts": dedent`
 						export default {
 							fetch(request) {
 								return new Response("Updated Worker!")
 							}
 						}`,
-				});
-				await retry(() =>
-					expect(
-						fetch(`http://127.0.0.1:${port}`).then((r) => r.text())
-					).resolves.toMatchInlineSnapshot('"Updated Worker!"')
-				);
-			}
-		);
+			});
+			await retry(() =>
+				expect(
+					fetch(`http://127.0.0.1:${port}`).then((r) => r.text())
+				).resolves.toMatchInlineSnapshot('"Updated Worker!"')
+			);
+		});
 	});
 });
