f5c4fe95ac37f6a6cf020a153c18d1a85065fe39
Jacob M-G Evans <jacobmgevans@gmail.com>
2023-06-15 16:51:01 -0500

    Still TS issues

--- packages/wrangler/src/__tests__/vitest.config.ts
+++ packages/wrangler/src/__tests__/vitest.config.ts
@@ -1,24 +1,17 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    name: "wrangler-tests",
-    globals: true,
-    reporters: [
-      "json",
-      "html",
-      "text",
-      "cobertura"
-    ],
-    alias: {
-      "clipboardy": "<rootDir>/src/__tests__/helpers/clipboardy-mock.js",
-      "miniflare/cli": "<rootDir>/../../node_modules/miniflare/dist/src/cli.js"
-    },
-    restoreMocks: true,
-    setupFiles: [
-      "./src/__tests__/vitest.setup.ts"
-    ],
-    testNamePattern: ".*\\.(test|spec)\\.[jt]sx?$",
-    testTimeout: 30000
-  }
-})
+	test: {
+		name: "wrangler-tests",
+		globals: true,
+		reporters: ["json", "html", "text", "cobertura"],
+		alias: {
+			clipboardy: "<rootDir>/src/__tests__/helpers/clipboardy-mock.js",
+			"miniflare/cli": "<rootDir>/../../node_modules/miniflare/dist/src/cli.js",
+		},
+		restoreMocks: true,
+		setupFiles: ["./src/__tests__/vitest.setup.ts"],
+		testNamePattern: "./**/*.test.[jt]sx?",
+		testTimeout: 30000,
+	},
+});
