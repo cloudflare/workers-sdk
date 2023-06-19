d5fb91a325bfaec78cc4b1cdea02ab3e670d1038
Jacob M-G Evans <jacobmgevans@gmail.com>
2023-06-19 15:59:56 -0500

    no vitest tools for global stubbing

--- packages/pages-shared/__tests__/vitest.setup.ts
+++ packages/pages-shared/__tests__/vitest.setup.ts
@@ -1,4 +1,3 @@
 import makeServiceWorkerEnv from "service-worker-mock";
-import { vi } from "vitest";
 
-vi.stubGlobal('globalThis', makeServiceWorkerEnv())
+Object.assign("globalThis", makeServiceWorkerEnv());
