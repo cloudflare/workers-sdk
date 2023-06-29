eada9762b8f13946ac4d1737305d1e7b0c11d247
Jacob M-G Evans <jacobmgevans@gmail.com>
2023-06-29 13:22:36 -0500

    Formatting

--- packages/pages-shared/vite.config.ts
+++ packages/pages-shared/vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    include: [
-      '**/__tests__/**/*.{test,spec}.{ts,js,tsx,jsx}',
-    ],
-    testTimeout: 30000,
-    setupFiles: "./vite.setup.ts"
-  }
-})
+	test: {
+		include: ["**/__tests__/**/*.{test,spec}.{ts,js,tsx,jsx}"],
+		testTimeout: 30000,
+		setupFiles: "./vite.setup.ts",
+	},
+});
