b6b3433e119459677046be71281fb4eae0a58226
Dario Piotrowicz <dario@cloudflare.com>
2023-07-13 11:34:46 +0100

    C3: offer eslint-plugin-next-on-pages
    Add the option to add the eslint-plugin-next-on-pages eslint plugin
    to developers creating a new Next.js app with eslint enabled

--- packages/create-cloudflare/src/frameworks/next/index.ts
+++ packages/create-cloudflare/src/frameworks/next/index.ts
@@ -1,8 +1,9 @@
-import { mkdirSync } from "fs";
+import { existsSync, mkdirSync, writeFileSync } from "fs";
 import { updateStatus } from "helpers/cli";
 import { brandColor, dim } from "helpers/colors";
 import { installPackages, runFrameworkGenerator } from "helpers/command";
 import { probePaths, usesTypescript, writeFile } from "helpers/files";
+import { processArgument } from "helpers/interactive";
 import { detectPackageManager } from "helpers/packages";
 import { getFrameworkVersion } from "../index";
 import {
@@ -11,7 +12,7 @@ import {
 	apiPagesDirHelloJs,
 	apiPagesDirHelloTs,
 } from "./templates";
-import type { PagesGeneratorContext, FrameworkConfig } from "types";
+import type { PagesGeneratorContext, FrameworkConfig, C3Args } from "types";
 
 const { npm, npx, dlx } = detectPackageManager();
 
@@ -72,14 +73,63 @@ const configure = async (ctx: PagesGeneratorContext) => {
 	writeFile(handlerPath, handlerFile);
 	updateStatus("Created an example API route handler");
 
+	const installEslintPlugin = await shouldInstallNextOnPagesEslintPlugin(ctx);
+
+	if (installEslintPlugin) {
+		await writeEslintrc(ctx);
+		updateStatus("eslint-plugin-next-on-pages added to eslintrc");
+	}
+
 	// Add some dev dependencies
 	process.chdir(projectName);
-	const packages = ["@cloudflare/next-on-pages@1", "vercel"];
+	const packages = [
+		"@cloudflare/next-on-pages@1",
+		"vercel",
+		...(installEslintPlugin ? ["eslint-plugin-next-on-pages"] : []),
+	];
 	await installPackages(packages, {
 		dev: true,
 		startText: "Adding the Cloudflare Pages adapter",
 		doneText: `${brandColor(`installed`)} ${dim(packages.join(", "))}`,
 	});
+};
+
+// Note: this isn't a generic helper since it's Next specific, this can be
+//       made into a generic helper by checking all possible types of configs
+//       (including a field in the package.json)
+//       (and potentially returning what type of config is used)
+export const usesEslint = (projectRoot = ".") => {
+	return existsSync(`${projectRoot}/.eslintrc.json`);
+};
+
+export const shouldInstallNextOnPagesEslintPlugin = async (
+	ctx: PagesGeneratorContext
+): Promise<boolean> => {
+	if (!usesEslint(ctx.project.name)) return false;
+
+	return await processArgument(ctx.args, "eslint-plugin" as keyof C3Args, {
+		type: "confirm",
+		question: "Do you want to use the next-on-pages eslint-plugin?",
+		label: "",
+		defaultValue: true,
+	});
+};
+
+export const writeEslintrc = async (
+	ctx: PagesGeneratorContext
+): Promise<void> => {
+	const eslintConfig = {
+		plugins: ["eslint-plugin-next-on-pages"],
+		extends: [
+			"next/core-web-vitals",
+			"plugin:eslint-plugin-next-on-pages/recommended",
+		],
+	};
+
+	writeFileSync(
+		`${ctx.project.name}/.eslintrc.json`,
+		JSON.stringify(eslintConfig, null, "  ") + "\n"
+	);
 };
 
 const config: FrameworkConfig = {
