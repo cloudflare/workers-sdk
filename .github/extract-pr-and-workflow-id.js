081fa5f30748c47d938742e00dd24a43eb1ec59a
Pete Bacon Darwin <pete@bacondarwin.com>
2023-07-13 11:10:42 +0100

    ci: implement PR previews for C3

--- .github/extract-pr-and-workflow-id.js
+++ .github/extract-pr-and-workflow-id.js
@@ -1,3 +1,6 @@
+// This file is not used directly.
+// Instead its contents are used in of `.github/workflows/write-prerelease-comment.yml`
+// Any changes here should be copied into the CI step there.
 const allArtifacts = await github.rest.actions.listWorkflowRunArtifacts({
 	owner: context.repo.owner,
 	repo: context.repo.repo,
@@ -6,12 +9,13 @@ const allArtifacts = await github.rest.actions.listWorkflowRunArtifacts({
 
 for (const artifact of allArtifacts.data.artifacts) {
 	// Extract the PR number from the artifact name
-	const match = /^npm-package-wrangler-(\d+)$/.exec(artifact.name);
+	const match = /^npm-package-(.+)-(\d+)$/.exec(artifact.name);
 	if (match) {
+		const packageName = match[1].toUpperCase();
 		fs.appendFileSync(
 			process.env.GITHUB_ENV,
-			`\nWORKFLOW_RUN_PR=${match[1]}` +
-				`\nWORKFLOW_RUN_ID=${context.payload.workflow_run.id}`
+			`\nWORKFLOW_RUN_PR_FOR_${packageName}=${match[2]}` +
+				`\nWORKFLOW_RUN_ID_FOR_${packageName}=${context.payload.workflow_run.id}`
 		);
 		break;
 	}
