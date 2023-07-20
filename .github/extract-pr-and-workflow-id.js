37c15594146104645314576760d7aa21501d4900
Peter Bacon Darwin <pete@bacondarwin.com>
2023-07-20 21:02:46 +0100

    ci: ensure that `fs` has been imported in the prerelease comment job.

--- .github/extract-pr-and-workflow-id.js
+++ .github/extract-pr-and-workflow-id.js
@@ -12,7 +12,7 @@ for (const artifact of allArtifacts.data.artifacts) {
 	const match = /^npm-package-(.+)-(\d+)$/.exec(artifact.name);
 	if (match) {
 		const packageName = match[1].toUpperCase();
-		fs.appendFileSync(
+		require("fs").appendFileSync(
 			process.env.GITHUB_ENV,
 			`\nWORKFLOW_RUN_PR_FOR_${packageName}=${match[2]}` +
 				`\nWORKFLOW_RUN_ID_FOR_${packageName}=${context.payload.workflow_run.id}`
