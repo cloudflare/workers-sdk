a05185635a2b689e7b22037f1af1882eea61e3e7
lrapoport-cf <lrapoport@cloudflare.com>
2023-08-10 17:29:11 -0400

    Change emoji on secret subcommands

--- packages/wrangler/src/secret/index.ts
+++ packages/wrangler/src/secret/index.ts
@@ -28,7 +28,7 @@ export const secret = (secretYargs: CommonYargsArgv) => {
 		})
 		.command(
 			"put <key>",
-			"Create or update a secret variable for a Worker",
+			"🔸Create or update a secret variable for a Worker",
 			(yargs) => {
 				return yargs
 					.positional("key", {
@@ -166,7 +166,7 @@ export const secret = (secretYargs: CommonYargsArgv) => {
 		)
 		.command(
 			"delete <key>",
-			"Delete a secret variable from a Worker",
+			"🔸Delete a secret variable from a Worker",
 			async (yargs) => {
 				await printWranglerBanner();
 				return yargs
@@ -222,7 +222,7 @@ export const secret = (secretYargs: CommonYargsArgv) => {
 		)
 		.command(
 			"list",
-			"List all secrets for a Worker",
+			"🔸List all secrets for a Worker",
 			(yargs) => {
 				return yargs.option("name", {
 					describe: "Name of the Worker",
