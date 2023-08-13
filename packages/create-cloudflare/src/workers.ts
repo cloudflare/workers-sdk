c51d8361b4e7fea538f5f4679ea6e702cfe96160
Rahul Sethi <5822355+RamIdeas@users.noreply.github.com>
2023-08-13 20:23:03 +0100

    c3: rename .gitignore to an intermediate name that npm pack will not exclude

--- packages/create-cloudflare/src/workers.ts
+++ packages/create-cloudflare/src/workers.ts
@@ -1,4 +1,12 @@
-import { readFile, writeFile, mkdtemp, cp, rm, readdir } from "fs/promises";
+import {
+	readFile,
+	writeFile,
+	mkdtemp,
+	cp,
+	rm,
+	readdir,
+	rename,
+} from "fs/promises";
 import { tmpdir } from "os";
 import { resolve, join } from "path";
 import { chdir } from "process";
@@ -81,6 +89,9 @@ async function copyFiles(ctx: Context) {
 	// copy template files
 	updateStatus(`Copying files from "${template}" template`);
 	await cp(srcdir, destdir, { recursive: true });
+
+	// reverse renaming from build step
+	await rename(join(destdir, "__dot__gitignore"), join(destdir, ".gitignore"));
 }
 
 async function copyExistingWorkerFiles(ctx: Context) {
