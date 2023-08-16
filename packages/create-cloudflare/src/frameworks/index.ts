cb95d529975137b9f2b9cc9ee1ce1cde1533e5f1
Dario Piotrowicz <dario@cloudflare.com>
2023-08-16 16:40:38 +0100

    convert cliMap.json to package.json so that we can leverage dependabot

--- packages/create-cloudflare/src/frameworks/index.ts
+++ packages/create-cloudflare/src/frameworks/index.ts
@@ -1,12 +1,12 @@
 import { crash } from "helpers/cli";
 import angular from "./angular";
 import astro from "./astro";
-import cliMap from "./cliMap.json";
 import docusaurus from "./docusaurus";
 import gatsby from "./gatsby";
 import hono from "./hono";
 import next from "./next";
 import nuxt from "./nuxt";
+import clisPackageJson from "./package.json";
 import qwik from "./qwik";
 import react from "./react";
 import remix from "./remix";
@@ -35,15 +35,6 @@ export const supportedFramework = (framework: string) => {
 	return Object.keys(FrameworkMap).includes(framework);
 };
 
-export const getFrameworkVersion = (ctx: PagesGeneratorContext) => {
-	if (!ctx.framework) {
-		return crash("Framework not specified.");
-	}
-
-	const framework = ctx.framework.name as keyof typeof cliMap;
-	return cliMap[framework].version;
-};
-
 export const getFrameworkCli = (
 	ctx: PagesGeneratorContext,
 	withVersion = true
@@ -52,7 +43,11 @@ export const getFrameworkCli = (
 		return crash("Framework not specified.");
 	}
 
-	const framework = ctx.framework.name as keyof typeof cliMap;
-	const { cli, version } = cliMap[framework];
-	return withVersion ? `${cli}@${version}` : cli;
+	const framework = ctx.framework
+		.name as keyof typeof clisPackageJson.frameworkCliMap;
+	const frameworkCli = clisPackageJson.frameworkCliMap[
+		framework
+	] as keyof typeof clisPackageJson.dependencies;
+	const version = clisPackageJson.dependencies[frameworkCli];
+	return withVersion ? `${frameworkCli}@${version}` : frameworkCli;
 };
