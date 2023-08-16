b30bacb6ff178653e87c56334975177d37fa23bb
Dario Piotrowicz <dario@cloudflare.com>
2023-08-16 13:55:42 +0100

    make commit header imperative

--- packages/create-cloudflare/src/common.ts
+++ packages/create-cloudflare/src/common.ts
@@ -271,7 +271,7 @@ export const gitCommit = async (ctx: PagesGeneratorContext) => {
 const generateFrameworkCommitMessage = (ctx: PagesGeneratorContext) => {
 	if (!ctx.framework) return;
 
-	const header = "Web application initialized by Create-Cloudflare CLI";
+	const header = "Initialize Web application via Create-Cloudflare CLI";
 
 	const packageManager = detectPackageManager();
 
