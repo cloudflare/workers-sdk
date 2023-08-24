694e914fd7b2dd641c718226212d069e03d4b0e8
Dario Piotrowicz <dario@cloudflare.com>
2023-08-24 17:18:44 +0100

    run prettify

--- .github/generate-c3-dependabot-pr-changeset.mjs
+++ .github/generate-c3-dependabot-pr-changeset.mjs
@@ -1,50 +1,57 @@
-import { execSync } from 'child_process';
-import { writeFileSync } from 'fs';
+import { execSync } from "child_process";
+import { writeFileSync } from "fs";
 
-const diff = execSync(`git diff HEAD~1 packages/create-cloudflare/src/frameworks/package.json`).toString();
+const diff = execSync(
+	`git diff HEAD~1 packages/create-cloudflare/src/frameworks/package.json`
+).toString();
 
-const changedPackages = diff.match(/-\s*".*":\s".*",?/g)?.map(
-    match => match.match(/-\s*"(.*)":/)?.[1]
-).filter(Boolean) ?? [];
+const changedPackages =
+	diff
+		.match(/-\s*".*":\s".*",?/g)
+		?.map((match) => match.match(/-\s*"(.*)":/)?.[1])
+		.filter(Boolean) ?? [];
 
-const changes = changedPackages.map(pkg => {
-    const getPackageChangeRegex = (addition) =>
-        new RegExp(`${ addition ? '\\+' : '-' }\\s*"${pkg}":\\s"(.*)",?`);
+const changes = changedPackages.map((pkg) => {
+	const getPackageChangeRegex = (addition) =>
+		new RegExp(`${addition ? "\\+" : "-"}\\s*"${pkg}":\\s"(.*)",?`);
 
-    const from = diff.match(getPackageChangeRegex(false))?.[1];
-    const to = diff.match(getPackageChangeRegex(true))?.[1];
+	const from = diff.match(getPackageChangeRegex(false))?.[1];
+	const to = diff.match(getPackageChangeRegex(true))?.[1];
 
-    if(!from || !to) {
-        throw new Error(`Unexpected changes for package ${pkg} (could not determine upgrade)`);
-    }
+	if (!from || !to) {
+		throw new Error(
+			`Unexpected changes for package ${pkg} (could not determine upgrade)`
+		);
+	}
 
-    return {
-        package: pkg,
-        from,
-        to,
-    }
+	return {
+		package: pkg,
+		from,
+		to,
+	};
 });
 
-if(!changes.length) {
-    console.warn('No changes detected!');
+if (!changes.length) {
+	console.warn("No changes detected!");
 } else {
-
-    writeFileSync(`.changeset/changeset-${Math.round(Math.random() * 100000)}.md`,
-`---
+	writeFileSync(
+		`.changeset/changeset-${Math.round(Math.random() * 100000)}.md`,
+		`---
 "dependabot-testing": patch
 ---
 
 Updated dependencies
-${
-    changes.map(({package: pkg, from, to}) =>
-        ` - \`${pkg}\` from \`${from}\` to \`${to}\``
-    ).join('\n')
-}
+${changes
+	.map(
+		({ package: pkg, from, to }) =>
+			` - \`${pkg}\` from \`${from}\` to \`${to}\``
+	)
+	.join("\n")}
 
-`);
+`
+	);
 
-execSync("git add .changeset");
-execSync("git commit --amend -m '[C3] Update frameworks cli dependencies'");
-execSync("git push -f");
-
+	execSync("git add .changeset");
+	execSync("git commit --amend -m '[C3] Update frameworks cli dependencies'");
+	execSync("git push -f");
 }
