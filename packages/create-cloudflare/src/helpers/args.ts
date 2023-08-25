4fc39fa17e87cf417317593898e09d3e4d0a156a
Dario Piotrowicz <dario@cloudflare.com>
2023-08-25 16:04:35 +0100

    make yargs strict

--- packages/create-cloudflare/src/helpers/args.ts
+++ packages/create-cloudflare/src/helpers/args.ts
@@ -41,6 +41,7 @@ export const parseArgs = async (argv: string[]): Promise<Partial<C3Args>> => {
 		})
 		.option("wrangler-defaults", { type: "boolean", hidden: true })
 		.version(version)
+		.strict()
 		.alias('h', 'help')
 		.help().argv;
 
