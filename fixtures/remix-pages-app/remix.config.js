75afdf1bb4e20ef935d71649fd3fe1d067121950
Michaël De Boey <info@michaeldeboey.be>
2023-08-30 15:11:53 +0200

    chore: update Remix fixture

--- fixtures/remix-pages-app/remix.config.js
+++ fixtures/remix-pages-app/remix.config.js
@@ -1,11 +1,23 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-	serverBuildTarget: "cloudflare-pages",
-	server: "./server.js",
-	devServerBroadcastDelay: 1000,
+export default {
 	ignoredRouteFiles: ["**/.*"],
+	server: "./server.ts",
+	serverBuildPath: "functions/[[path]].js",
+	serverConditions: ["workerd", "worker", "browser"],
+	serverDependenciesToBundle: "all",
+	serverMainFields: ["browser", "module", "main"],
+	serverMinify: true,
+	serverModuleFormat: "esm",
+	serverPlatform: "neutral",
 	// appDirectory: "app",
 	// assetsBuildDirectory: "public/build",
-	// serverBuildPath: "functions/[[path]].js",
 	// publicPath: "/build/",
+	future: {
+		v2_dev: true,
+		v2_errorBoundary: true,
+		v2_headers: true,
+		v2_meta: true,
+		v2_normalizeFormMethod: true,
+		v2_routeConvention: true,
+	},
 };
