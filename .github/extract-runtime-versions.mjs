aa8b5217b18846ac15d938452777e04e1a8c5b2f
Samuel Macleod <smacleod@cloudflare.com>
2023-09-05 13:42:19 +0100

    Restore version table and add `workerd` binary version

--- .github/extract-runtime-versions.mjs
+++ .github/extract-runtime-versions.mjs
@@ -32,13 +32,11 @@ const wranglerVersion = wranglerPackage.version;
 const miniflareVersionConstraint = wranglerPackage.dependencies.miniflare;
 
 // 2. Load `miniflare` `package.json`, getting `miniflare` version and `workerd` version constraint
-const miniflareMainPath = path.resolve(
-	__dirname,
-	"../packages/wrangler/node_modules/miniflare"
-);
+// (`createRequire()` just needs to be passed a file in the `wrangler` directory)
+const wranglerRequire = module.createRequire(wranglerPackageJsonPath);
+const miniflareMainPath = wranglerRequire.resolve("miniflare");
 const miniflarePackageJsonPath = findClosestPackageJson(miniflareMainPath);
 assert(miniflarePackageJsonPath !== undefined);
-const miniflarePackagePath = path.dirname(miniflarePackageJsonPath);
 const miniflarePackageJson = await fs.readFile(
 	miniflarePackageJsonPath,
 	"utf8"
@@ -48,12 +46,17 @@ const miniflareVersion = miniflarePackage.version;
 const workerdVersionConstraint = miniflarePackage.dependencies.workerd;
 
 // 3. Load `workerd` `package.json`, getting `workerd` version
-const workerdMainPath = path.resolve(
-	__dirname,
-	"../packages/wrangler/node_modules/miniflare/node_modules/.bin/workerd"
-);
+const miniflareRequire = module.createRequire(miniflarePackageJsonPath);
+const workerdMainPath = miniflareRequire.resolve("workerd");
+const workerdPackageJsonPath = findClosestPackageJson(workerdMainPath);
+assert(workerdPackageJsonPath !== undefined);
+const workerdPackageJson = await fs.readFile(workerdPackageJsonPath, "utf8");
+const workerdPackage = JSON.parse(workerdPackageJson);
+const workerdVersion = workerdPackage.version;
 
-const workerdVersion = execSync(workerdMainPath + " --version")
+const workerdBinary = path.resolve(workerdPackageJsonPath, "../bin/workerd");
+
+const workerdBinaryVersion = execSync(workerdBinary + " --version")
 	.toString()
 	.split(" ")[1];
 
@@ -65,6 +68,7 @@ const report = [
 	"|-------|----------|--------|",
 	`|\`miniflare\`|${miniflareVersionConstraint}|${miniflareVersion}|`,
 	`|\`workerd\`|${workerdVersionConstraint}|${workerdVersion}|`,
+	`|\`workerd --version\`|${workerdVersion}|${workerdBinaryVersion}|`,
 	"",
 	"Please ensure constraints are pinned, and `miniflare`/`workerd` minor versions match.",
 	"",
