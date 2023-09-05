d4303ed2ed077539b1c3e52740dc3535a634e60f
James <jculveyhouse@cloudflare.com>
2023-09-05 15:30:31 -0500

    Fixing test:e2e:npm task

--- packages/create-cloudflare/src/helpers/packages.ts
+++ packages/create-cloudflare/src/helpers/packages.ts
@@ -24,6 +24,10 @@ export const detectPackageManager = () => {
 				name = "yarn";
 				version = devDependencies["yarn"].replace("^", "");
 				break;
+			case "npm":
+				name = "npm";
+				version = "0.0.0";
+				break;
 		}
 	}
 
