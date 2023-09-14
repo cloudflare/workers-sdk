d414ca9f60b29e1be78c4f4d34dd35e818d9a3b9
bcoll <bcoll@cloudflare.com>
2023-09-14 15:30:16 +0100

    Source map `Runtime.exceptionThrown` events with multiple sources
    When `Runtime.exceptionThrown` events are dispatched by V8, Wrangler
    previously only used the source map produced by `esbuild` to source
    map the stack trace. In `--no-bundle` mode, it's possible there might
    be multiple user provided source maps needed to source map the stack
    trace.
    
    This change switches to using the `source-map-support` package to
    source map stack traces. Miniflare uses this already, so we're not
    adding a new dependency. This relies on file URLs in stack traces,
    something we now have from adding `//# sourceURL` comments.

--- packages/wrangler/src/inspect.ts
+++ packages/wrangler/src/inspect.ts
@@ -1,5 +1,4 @@
 import { readFileSync } from "fs";
-import { readFile } from "fs/promises";
 import assert from "node:assert";
 import crypto from "node:crypto";
 import { createServer } from "node:http";
@@ -8,11 +7,11 @@ import path from "node:path";
 import { URL, fileURLToPath, pathToFileURL } from "node:url";
 import open from "open";
 import { useEffect, useRef, useState } from "react";
-import { SourceMapConsumer } from "source-map";
 import WebSocket, { WebSocketServer } from "ws";
 import { version } from "../package.json";
 import { logger } from "./logger";
 import { waitForPortToBeAvailable } from "./proxy";
+import { getSourceMappedStack } from "./sourcemap";
 import { getAccessToken } from "./user/access";
 import type Protocol from "devtools-protocol";
 import type { IncomingMessage, Server, ServerResponse } from "node:http";
@@ -371,88 +370,8 @@ export default function useInspector(props: InspectorProps) {
 					const evt = JSON.parse(event.data);
 					if (evt.method === "Runtime.exceptionThrown") {
 						const params = evt.params as Protocol.Runtime.ExceptionThrownEvent;
-
-						// Parse stack trace with source map.
-						if (props.sourceMapPath) {
-							// Parse in the sourcemap
-							const mapContent = JSON.parse(
-								await readFile(props.sourceMapPath, "utf-8")
-							);
-
-							// Create the lines for the exception details log
-							const exceptionLines = [
-								params.exceptionDetails.exception?.description?.split("\n")[0],
-							];
-
-							await SourceMapConsumer.with(
-								mapContent,
-								null,
-								async (consumer) => {
-									// Pass each of the callframes into the consumer, and format the error
-									const stack = params.exceptionDetails.stackTrace?.callFrames;
-
-									stack?.forEach(
-										({ functionName, lineNumber, columnNumber }, i) => {
-											try {
-												if (lineNumber) {
-													// The line and column numbers in the stackTrace are zero indexed,
-													// whereas the sourcemap consumer indexes from one.
-													const pos = consumer.originalPositionFor({
-														line: lineNumber + 1,
-														column: columnNumber + 1,
-													});
-
-													// Print out line which caused error:
-													if (i === 0 && pos.source && pos.line) {
-														const fileSource = consumer.sourceContentFor(
-															pos.source
-														);
-														const fileSourceLine =
-															fileSource?.split("\n")[pos.line - 1] || "";
-														exceptionLines.push(fileSourceLine.trim());
-
-														// If we have a column, we can mark the position underneath
-														if (pos.column) {
-															exceptionLines.push(
-																`${" ".repeat(
-																	pos.column - fileSourceLine.search(/\S/)
-																)}^`
-															);
-														}
-													}
-
-													// From the way esbuild implements the "names" field:
-													// > To save space, the original name is only recorded when it's different from the final name.
-													// however, source-map consumer does not handle this
-													if (pos && pos.line != null) {
-														const convertedFnName =
-															pos.name || functionName || "";
-														exceptionLines.push(
-															`    at ${convertedFnName} (${pos.source}:${pos.line}:${pos.column})`
-														);
-													}
-												}
-											} catch {
-												// Line failed to parse through the sourcemap consumer
-												// We should handle this better
-											}
-										}
-									);
-								}
-							);
-
-							// Log the parsed stacktrace
-							logger.error(
-								params.exceptionDetails.text,
-								exceptionLines.join("\n")
-							);
-						} else {
-							// We log the stacktrace to the terminal
-							logger.error(
-								params.exceptionDetails.text,
-								params.exceptionDetails.exception?.description ?? ""
-							);
-						}
+						const stack = getSourceMappedStack(params.exceptionDetails);
+						logger.error(params.exceptionDetails.text, stack);
 					}
 					if (evt.method === "Runtime.consoleAPICalled") {
 						logConsoleMessage(
