fb38292a84843c46b2978d8c1fd27d442dc78caf
Jacob M-G Evans <jacobmgevans@gmail.com>
2023-09-21 11:31:33 -0500

    TurboRepo Gen Templates & improvements Additional turborepo generation functionality, including adding code gen for templates & improving existing code gen for packages.

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,4 +1,25 @@
 import type { PlopTypes } from "@turbo/gen";
+import { spawnSync } from "node:child_process";
+
+type Answers = {
+	name: string;
+	typescript: boolean;
+	worker: boolean;
+};
+
+const ThinkOfAName: PlopTypes.CustomActionFunction = async (answers) => {
+	const typedAnswers = answers as Answers;
+
+	// if (typedAnswers.worker) {
+	spawnSync(
+		`cd templates/${typedAnswers.name} && pnpm create cloudflare@latest`
+	);
+	// }	else {
+	// 		await execAsync()
+	// 	}
+
+	return "Finished install dependencies";
+};
 
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
 	plop.setGenerator("package", {
@@ -9,28 +30,73 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
 				name: "name",
 				message: "What is your package name?",
 			},
-			{
-				type: "input",
-				name: "version",
-				message: "What is your package version?",
-			},
 		],
 		actions: [
 			{
 				type: "add",
-				path: "packages/{{name}}/package.json",
-				templateFile: "templates/package.json.hbs",
+				path: "packages/{{name}}/src/",
 			},
+			// {
+			// 	type: "add",
+			// 	path: "packages/{{name}}/package.json",
+			// 	templateFile: "templates/package.json.hbs",
+			// },
+			// {
+			// 	type: "add",
+			// 	path: "packages/{{name}}/.eslintrc.js",
+			// 	templateFile: "templates/.eslintrc.js.hbs",
+			// },
+			// {
+			// 	type: "add",
+			// 	path: "packages/{{name}}/tsconfig.json",
+			// 	templateFile: "templates/tsconfig.json.hbs",
+			// },
+			ThinkOfAName,
+		],
+	});
+
+	plop.setGenerator("template", {
+		description: "Generator description",
+		prompts: [
+			{
+				type: "input",
+				name: "name",
+				message: "What is your package name?",
+			},
+			// {
+			// 	type: "confirm",
+			// 	name: "typescript",
+			// 	message: "Is this template using typescript?",
+			// },
+			// {
+			// 	type: "confirm",
+			// 	name: "worker",
+			// 	message: "Is this a worker template?",
+			// },
+		],
+		actions: [
+			// {
+			// 	type: "add",
+			// 	path: "templates/{{name}}/package.json",
+			// 	templateFile: "templates/package.json.hbs",
+			// },
 			{
 				type: "add",
-				path: "packages/{{name}}/.eslintrc.js",
+				path: "templates/{{name}}/.eslintrc.js",
 				templateFile: "templates/.eslintrc.js.hbs",
 			},
-			{
-				type: "add",
-				path: "packages/{{name}}/tsconfig.json",
-				templateFile: "templates/tsconfig.json.hbs",
-			},
+			// {
+			// 	type: "add",
+			// 	skip: (answers: Record<string, any>) => {
+			// 		if (answers.typescript === false) {
+			// 			return "Skipping TypeScript setup";
+			// 		}
+			// 		return false;
+			// 	},
+			// 	path: "templates/{{name}}/tsconfig.json",
+			// 	templateFile: "templates/tsconfig.json.hbs",
+			// },
+			async (...args) => await ThinkOfAName(...args),
 		],
 	});
 }
