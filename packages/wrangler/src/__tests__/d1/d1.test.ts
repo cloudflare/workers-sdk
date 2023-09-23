d54207b98927b5bc778727e42963fd00ea482346
Matt Silverlock <matt@eatsleeprepeat.net>
2023-09-23 15:57:04 -0400

    vectorize: npm run bundle

--- packages/wrangler/src/__tests__/d1/d1.test.ts
+++ packages/wrangler/src/__tests__/d1/d1.test.ts
@@ -31,7 +31,12 @@ describe("d1", () => {
 		  -c, --config                    Path to .toml configuration file  [string]
 		  -e, --env                       Environment to use for operations and .env files  [string]
 		  -h, --help                      Show help  [boolean]
-		  -v, --version                   Show version number  [boolean]"
+		  -v, --version                   Show version number  [boolean]
+
+		--------------------
+		🚧 D1 is currently in open beta
+		🚧 Please report any bugs to https://github.com/cloudflare/workers-sdk/issues/new/choose
+		--------------------"
 	`);
 	});
 
@@ -66,7 +71,12 @@ describe("d1", () => {
 		  -c, --config                    Path to .toml configuration file  [string]
 		  -e, --env                       Environment to use for operations and .env files  [string]
 		  -h, --help                      Show help  [boolean]
-		  -v, --version                   Show version number  [boolean]"
+		  -v, --version                   Show version number  [boolean]
+
+		--------------------
+		🚧 D1 is currently in open beta
+		🚧 Please report any bugs to https://github.com/cloudflare/workers-sdk/issues/new/choose
+		--------------------"
 	`);
 	});
 
@@ -96,7 +106,12 @@ describe("d1", () => {
 		  -c, --config                    Path to .toml configuration file  [string]
 		  -e, --env                       Environment to use for operations and .env files  [string]
 		  -h, --help                      Show help  [boolean]
-		  -v, --version                   Show version number  [boolean]"
+		  -v, --version                   Show version number  [boolean]
+
+		--------------------
+		🚧 D1 is currently in open beta
+		🚧 Please report any bugs to https://github.com/cloudflare/workers-sdk/issues/new/choose
+		--------------------"
 	`);
 	});
 
