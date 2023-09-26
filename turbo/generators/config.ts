88f4892151e80e32af743ad49a02706323ab225d
Jacob M-G Evans <jacobmgevans@gmail.com>
2023-09-26 14:11:44 -0500

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.3.1
      project name = TEST
      framework = hono
      framework cli = create-hono@0.2.6
      package manager = pnpm@8.7.1
      wrangler = wrangler@3.9.1
      git = 2.42.0

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -19,16 +19,15 @@ const webFramework = [
 
 type Answers = {
 	name: string;
-	typescript: boolean;
-	worker: boolean;
-	webframework: typeof webFramework;
+	folder: "package" | "template";
 };
 
 const ThinkOfAName: PlopTypes.CustomActionFunction = async (answers) => {
 	const typedAnswers = answers as Answers;
 
+	console.dir(answers, { depth: Infinity });
 	execSync(
-		`cd templates/ && pnpm create cloudflare@latest ${typedAnswers.name} --no-deploy --no-git`,
+		`cd ${typedAnswers.folder}/ && pnpm create cloudflare@latest ${typedAnswers.name} --no-deploy --no-git`,
 		{
 			stdio: "inherit",
 		}
@@ -37,22 +36,19 @@ const ThinkOfAName: PlopTypes.CustomActionFunction = async (answers) => {
 	return "Finished C3 Setup & Installing Deps";
 };
 
-const skipWebFramework = (answers: Record<string, any>) => {
-	if (answers.worker === false) {
-		console.log("Skipping Web Framework selection");
-		return true;
-	}
-	return false;
-};
-
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
-	plop.setGenerator("package", {
+	plop.setGenerator("repo", {
 		description: "Generator description",
 		prompts: [
 			{
 				type: "input",
 				name: "name",
-				message: "What is your package name?",
+				message: "What is your package/template name?",
+			},
+			{
+				type: "list",
+				name: "folder",
+				choices: ["packages", "templates"],
 			},
 		],
 		actions: [
@@ -63,37 +59,10 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
 				templateFile: "templates/.eslintrc.js.hbs",
 			},
 			{
-				type: "add",
-				path: "packages/{{name}}/tsconfig.json",
-				templateFile: "templates/tsconfig.json.hbs",
-			},
-		],
-	});
-
-	plop.setGenerator("template", {
-		description: "Generator description",
-		prompts: [
-			{
-				type: "input",
-				name: "name",
-				message: "What is your template name?",
-			},
-		],
-		actions: [
-			ThinkOfAName,
-			{
-				type: "add",
-				path: "templates/{{name}}/.eslintrc.js",
-				templateFile: "templates/.eslintrc.js.hbs",
-			},
-			{
 				type: "modify",
 				path: "packages/{{name}}/tsconfig.json",
-				// templateFile: "templates/tsconfig.json.hbs",
-				transform(template, data, cfg) {
-					console.log({ template, data, cfg });
-					return template;
-				},
+				pattern: /^\{/g,
+				template: "Hello {{name}}",
 			},
 		],
 	});
