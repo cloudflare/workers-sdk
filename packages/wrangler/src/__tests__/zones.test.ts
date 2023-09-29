4a483b3b72c3180fc85f3320399041f3877a2f76
Rahul Sethi <5822355+RamIdeas@users.noreply.github.com>
2023-09-29 10:58:49 +0100

    make test examples more readable

--- packages/wrangler/src/__tests__/zones.test.ts
+++ packages/wrangler/src/__tests__/zones.test.ts
@@ -1,33 +1,35 @@
 import { getHostFromUrl } from "../zones";
 
 describe("Zones", () => {
-	test.each`
-		pattern                              | host
-		${"example.com"}                     | ${"example.com"}
-		${"*.example.com"}                   | ${"example.com"}
-		${"*example.com"}                    | ${"example.com"}
-		${"example.com/path/name"}           | ${"example.com"}
-		${"*.example.com/path/name"}         | ${"example.com"}
-		${"*example.com/path/name"}          | ${"example.com"}
-		${"*example.com"}                    | ${"example.com"}
-		${"*/path/name"}                     | ${undefined}
-		${"http://example.com"}              | ${"example.com"}
-		${"http://*.example.com"}            | ${"example.com"}
-		${"http://*example.com"}             | ${"example.com"}
-		${"http://example.com/path/name"}    | ${"example.com"}
-		${"http://*.example.com/path/name"}  | ${"example.com"}
-		${"http://*example.com/path/name"}   | ${"example.com"}
-		${"http://*example.com"}             | ${"example.com"}
-		${"http://*/path/name"}              | ${undefined}
-		${"https://example.com"}             | ${"example.com"}
-		${"https://*.example.com"}           | ${"example.com"}
-		${"https://*example.com"}            | ${"example.com"}
-		${"https://example.com/path/name"}   | ${"example.com"}
-		${"https://*.example.com/path/name"} | ${"example.com"}
-		${"https://*example.com/path/name"}  | ${"example.com"}
-		${"https://*example.com"}            | ${"example.com"}
-		${"https://*/path/name"}             | ${undefined}
-	`('getHostFromUrl("$pattern") === "$host"', ({ pattern, host }) => {
-		expect(getHostFromUrl(pattern)).toBe(host);
+	describe("getHostFromUrl", () => {
+		test.each`
+			pattern                                             | host
+			${"rootdomain.com"}                                 | ${"rootdomain.com"}
+			${"*.subdomain.com"}                                | ${"subdomain.com"}
+			${"*rootdomain-or-subdomain.com"}                   | ${"rootdomain-or-subdomain.com"}
+			${"rootdomain.com/path/name"}                       | ${"rootdomain.com"}
+			${"*.subdomain.com/path/name"}                      | ${"subdomain.com"}
+			${"*rootdomain-or-subdomain.com/path/name"}         | ${"rootdomain-or-subdomain.com"}
+			${"*rootdomain-or-subdomain.com"}                   | ${"rootdomain-or-subdomain.com"}
+			${"*/path/name"}                                    | ${undefined}
+			${"http://rootdomain.com"}                          | ${"rootdomain.com"}
+			${"http://*.subdomain.com"}                         | ${"subdomain.com"}
+			${"http://*rootdomain-or-subdomain.com"}            | ${"rootdomain-or-subdomain.com"}
+			${"http://rootdomain.com/path/name"}                | ${"rootdomain.com"}
+			${"http://*.subdomain.com/path/name"}               | ${"subdomain.com"}
+			${"http://*rootdomain-or-subdomain.com/path/name"}  | ${"rootdomain-or-subdomain.com"}
+			${"http://*rootdomain-or-subdomain.com"}            | ${"rootdomain-or-subdomain.com"}
+			${"http://*/path/name"}                             | ${undefined}
+			${"https://rootdomain.com"}                         | ${"rootdomain.com"}
+			${"https://*.subdomain.com"}                        | ${"subdomain.com"}
+			${"https://*rootdomain-or-subdomain.com"}           | ${"rootdomain-or-subdomain.com"}
+			${"https://rootdomain.com/path/name"}               | ${"rootdomain.com"}
+			${"https://*.subdomain.com/path/name"}              | ${"subdomain.com"}
+			${"https://*rootdomain-or-subdomain.com/path/name"} | ${"rootdomain-or-subdomain.com"}
+			${"https://*rootdomain-or-subdomain.com"}           | ${"rootdomain-or-subdomain.com"}
+			${"https://*/path/name"}                            | ${undefined}
+		`("$pattern --> $host", ({ pattern, host }) => {
+			expect(getHostFromUrl(pattern)).toBe(host);
+		});
 	});
 });
