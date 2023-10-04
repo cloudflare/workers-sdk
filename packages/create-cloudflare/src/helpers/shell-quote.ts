5fc7a8839851cb1181ef907c199b9c1e1e3766fd
Rahul Sethi <5822355+RamIdeas@users.noreply.github.com>
2023-10-04 23:20:08 +0100

    fix usage of shell-quote.parse on windows (#4105)

--- packages/create-cloudflare/src/helpers/shell-quote.ts
+++ packages/create-cloudflare/src/helpers/shell-quote.ts
@@ -3,6 +3,15 @@ import shellquote from "shell-quote";
 export const quote = shellquote.quote;
 
 export function parse(cmd: string, env?: Record<string, string>): string[] {
+	// This is a workaround for a bug in shell-quote on Windows
+	// It was fixed and, because it was a breaking change, then reverted
+	// in https://github.com/ljharb/shell-quote/commit/144e1c2#diff-e727e4b
+	// We can remove this once we upgrade to a version that includes the fix
+	// tracked by https://github.com/ljharb/shell-quote/issues/10
+	if (process.platform === "win32") {
+		cmd = cmd.replaceAll("\\", "\\\\");
+	}
+
 	const entries = shellquote.parse(cmd, env);
 	const argv: string[] = [];
 
