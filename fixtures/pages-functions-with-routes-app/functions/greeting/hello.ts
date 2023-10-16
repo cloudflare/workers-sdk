0faa6f8b43c9b264a5559d4c5281d735076f3c3e
Carmen Popoviciu <cpopoviciu@cloudflare.com>
2023-10-16 14:55:54 +0200

    pages: Fix "worker called response.clone()" warnings in `wrangler pages dev`
    Fixes #3259

--- fixtures/pages-functions-with-routes-app/functions/greeting/hello.ts
+++ fixtures/pages-functions-with-routes-app/functions/greeting/hello.ts
@@ -1,3 +1,13 @@
-export async function onRequest() {
-	return new Response("[/functions/greeting/hello]: Bonjour le monde!");
+export async function onRequest(context) {
+	const url = new URL(context.request.url);
+
+	const response = await fetch(`${url.origin}/api/greet`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ greeting: "Bonjour le monde!" }),
+	});
+
+	return new Response(`[/functions/greeting/hello]: ${await response.text()}`);
 }
