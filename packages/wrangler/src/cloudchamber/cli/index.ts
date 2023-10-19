26d2819e7965f90a8016d6299424dc00172d03ad
Gabi Villalonga Simon <gvillalongasimon@cloudflare.com>
2023-10-19 15:52:41 +0100

    Wrangler Cloudchamber: Add cloudchamber create

---
+++ packages/wrangler/src/cloudchamber/cli/index.ts
@@ -0,0 +1,326 @@
+import {
+	error,
+	updateStatus,
+	log,
+	endSection,
+	logRaw,
+	status,
+} from "@cloudflare/cli";
+import { brandColor, dim } from "@cloudflare/cli/colors";
+import { spinner } from "@cloudflare/cli/interactive";
+import {
+	DeploymentsService,
+	PlacementsService,
+	SshPublicKeysService,
+} from "../client";
+import { wrap } from "../helpers/wrap";
+import { idToLocationName } from "../locations";
+import type {
+	Deployment,
+	PlacementWithEvents,
+	PlacementEvent,
+	ListSSHPublicKeys,
+} from "../client";
+import type { EventName, Status } from "../enums";
+
+export function pollSSHKeysUntilCondition(
+	onSSHKeys: (sshKeys: ListSSHPublicKeys) => boolean
+): Promise<ListSSHPublicKeys> {
+	return new Promise<ListSSHPublicKeys>((res, rej) => {
+		let errCount = 0;
+		const poll = () => {
+			SshPublicKeysService.listSshPublicKeys()
+				.then((sshKeys) => {
+					try {
+						if (!onSSHKeys(sshKeys)) setTimeout(() => poll(), 500);
+						else res(sshKeys);
+					} catch (err) {
+						rej(err);
+					}
+				})
+				.catch((err) => {
+					errCount++;
+					// if there are too many errors, throw it
+					if (errCount > 3) {
+						rej(err);
+						return;
+					}
+
+					poll();
+				});
+		};
+
+		poll();
+	});
+}
+
+async function pollDeploymentUntilCondition(
+	deploymentId: string,
+	onDeployment: (d: Deployment) => boolean
+): Promise<Deployment> {
+	return new Promise<Deployment>((res, rej) => {
+		let errCount = 0;
+		const poll = () => {
+			DeploymentsService.getDeployment(deploymentId)
+				.then((deployment) => {
+					errCount = 0;
+					try {
+						if (!onDeployment(deployment)) setTimeout(() => poll(), 500);
+						else res(deployment);
+					} catch (err) {
+						rej(err);
+					}
+				})
+				.catch((err) => {
+					errCount++;
+					// if there are too many errors, throw it
+					if (errCount > 3) {
+						rej(err);
+					}
+				});
+		};
+
+		poll();
+	});
+}
+
+async function pollDeploymentUntilConditionWithPlacement(
+	placementId: string,
+	onPlacement: (p: PlacementWithEvents) => boolean
+): Promise<PlacementWithEvents> {
+	return new Promise((res, rej) => {
+		let errCount = 0;
+		const poll = () => {
+			PlacementsService.getPlacement(placementId)
+				.then((placement) => {
+					errCount = 0;
+					if (!onPlacement(placement)) setTimeout(() => poll(), 500);
+					else res(placement);
+				})
+				.catch((err) => {
+					errCount++;
+					// if there are too many errors, throw it
+					if (errCount > 3) {
+						rej(err);
+					}
+				});
+		};
+
+		poll();
+	});
+}
+
+function unexpectedLastEvent(placement: PlacementWithEvents) {
+	throw new WaitForAnotherPlacement(
+		`There has been an unknown error creating the container. (${
+			placement.events[placement.events.length - 1].name
+		})`
+	);
+}
+
+export class WaitForAnotherPlacement extends Error {
+	constructor(message: string) {
+		super(message);
+	}
+}
+
+async function waitForEvent(
+	deployment: Deployment,
+	...eventName: EventName[]
+): Promise<{ event?: PlacementEvent; placement: PlacementWithEvents }> {
+	if (!deployment.current_placement)
+		throw new Error("unexpected null current placement");
+	let foundEvent: PlacementEvent | undefined;
+	const placement = await pollDeploymentUntilConditionWithPlacement(
+		deployment.current_placement.id,
+		(p) => {
+			const event = p.events.find((e) =>
+				eventName.includes(e.name as EventName)
+			);
+			if (!event) {
+				if ((p.status["health"] as Status) === "failed") {
+					return true;
+				}
+
+				if ((p.status["health"] as Status) == "stopped") {
+					return true;
+				}
+
+				return false;
+			}
+
+			foundEvent = event;
+			return true;
+		}
+	);
+
+	return { event: foundEvent, placement };
+}
+
+async function waitForImagePull(deployment: Deployment) {
+	const s = spinner();
+	s.start("Pulling your image");
+	const [eventPlacement, err] = await wrap(
+		waitForEvent(deployment, "ImagePulled", "ImagePullError")
+	);
+	s.stop();
+	if (err) {
+		error(err.message);
+		return;
+	}
+
+	if (
+		eventPlacement.event == undefined ||
+		(eventPlacement.event.name === "ImagePullError" &&
+			eventPlacement.event.type !== "UserError")
+	) {
+		checkPlacementStatus(eventPlacement.placement);
+		return;
+	}
+
+	if (eventPlacement.event.name == "ImagePullError") {
+		error(
+			"Your container image couldn't be pulled, (404 not found). Did you specify the correct URL?",
+			`Run ${brandColor(
+				process.argv0 + " containers modify " + deployment.id
+			)} to change the deployment image`
+		);
+		return;
+	}
+
+	updateStatus("Pulled your image");
+	log(
+		`${brandColor("pulled image")} ${dim(
+			`in ${eventPlacement.event.details["duration"]}`
+		)}\n`
+	);
+}
+
+/**
+ * Will check the placement object and throw an error with the correct message
+ */
+function checkPlacementStatus(placement: PlacementWithEvents) {
+	if (placement.status["health"] === "stopped") {
+		throw new WaitForAnotherPlacement(
+			`The container stopped while waiting for the deployment to be created.\nEither the deployment has been modified, changed location, or your container is in a reboot loop!`
+		);
+	}
+
+	unexpectedLastEvent(placement);
+}
+
+async function waitForVMToStart(deployment: Deployment) {
+	const s = spinner();
+	s.start("Creating your container");
+	const [eventPlacement, err] = await wrap(
+		waitForEvent(deployment, "VMStarted")
+	);
+	s.stop();
+	if (err) {
+		error(err.message);
+		return;
+	}
+
+	if (!eventPlacement.event) {
+		checkPlacementStatus(eventPlacement.placement);
+	}
+
+	updateStatus(status.success + " Created your container");
+	log(
+		`${brandColor("assigned ipv6 address")} ${dim(
+			eventPlacement.placement.status["ipv6Address"]
+		)}\n`
+	);
+	endSection("Your container is up and running");
+	logRaw(
+		`Run ${brandColor(
+			`wrangler containers list ${deployment.id.slice(0, 6)}`
+		)} to check its status`
+	);
+}
+
+async function waitForPlacementInstance(deployment: Deployment) {
+	const s = spinner();
+	s.start("Assigning a placement in " + idToLocationName(deployment.location));
+	const [d, err] = await wrap(
+		pollDeploymentUntilCondition(deployment.id, (newDeployment) => {
+			if (newDeployment.current_placement && !deployment.current_placement) {
+				return true;
+			}
+
+			if (!newDeployment.current_placement) return false;
+
+			if (newDeployment.version > deployment.version) {
+				s.stop();
+				throw new WaitForAnotherPlacement(
+					`There is a new version of the deployment modified in another wrangler session, new version is ${newDeployment.version}`
+				);
+			}
+
+			if (
+				newDeployment.current_placement.deployment_version < deployment.version
+			) {
+				return false;
+			}
+
+			return (
+				newDeployment.current_placement.id !== deployment.current_placement!.id
+			);
+		})
+	);
+	s.stop();
+	if (err instanceof WaitForAnotherPlacement) {
+		throw err;
+	}
+
+	if (err) {
+		error(err.message);
+		return;
+	}
+
+	updateStatus(
+		"Assigned placement in " + idToLocationName(deployment.location)
+	);
+	log(
+		`${brandColor("assigned placement")} ${dim(
+			`version ${d.current_placement!.deployment_version}`
+		)}\n`
+	);
+	deployment.current_placement = d.current_placement;
+}
+
+export async function waitForPlacement(deployment: Deployment) {
+	let currentDeployment = { ...deployment };
+	let keepIterating = true;
+	while (keepIterating) {
+		try {
+			await waitForPlacementInstance(currentDeployment);
+			await waitForImagePull(currentDeployment);
+			await waitForVMToStart(currentDeployment);
+			keepIterating = false;
+		} catch (err) {
+			if (err instanceof WaitForAnotherPlacement) {
+				updateStatus(status.error + " " + err.message);
+				log(
+					"We will retry by waiting for another placement. You can see more details about your deployment with \n" +
+						brandColor("wrangler containers list " + currentDeployment.id) +
+						"\n"
+				);
+				const [newDeployment, getDeploymentError] = await wrap(
+					DeploymentsService.getDeployment(deployment.id)
+				);
+				if (getDeploymentError) {
+					error(
+						"Couldn't retrieve a new deployment: " + getDeploymentError.message
+					);
+					return;
+				}
+
+				currentDeployment = newDeployment;
+				continue;
+			}
+
+			throw err;
+		}
+	}
+}
---
+++ packages/wrangler/src/cloudchamber/common.ts
@@ -0,0 +1,466 @@
+import { mkdir } from "fs/promises";
+import { exit } from "process";
+import { error, space, updateStatus } from "@cloudflare/cli";
+import { dim, brandColor } from "@cloudflare/cli/colors";
+import { inputPrompt, spinner } from "@cloudflare/cli/interactive";
+import { version as wranglerVersion } from "../../package.json";
+import { readConfig } from "../config";
+import { getConfigCache, purgeConfigCaches } from "../config-cache";
+import { getCloudflareApiBaseUrl } from "../environment-variables/misc-variables";
+import { CI } from "../is-ci";
+import isInteractive from "../is-interactive";
+import {
+	getAPIToken,
+	getAccountId,
+	requireAuth,
+	getAuthFromEnv,
+	reinitialiseAuthTokens,
+} from "../user";
+import { ApiError, DeploymentMutationError, OpenAPI } from "./client";
+import { wrap } from "./helpers/wrap";
+import { idToLocationName, loadAccount } from "./locations";
+import type { Config } from "../config";
+import type { CommonYargsOptions } from "../yargs-types";
+import type { EnvironmentVariable, CompleteAccountCustomer } from "./client";
+import type { Arg } from "@cloudflare/cli/interactive";
+import type {
+	ArgumentsCamelCase as ArgumentsCamelCaseRaw,
+	CamelCaseKey,
+	Argv,
+} from "yargs";
+
+export type CommonCloudchamberConfiguration = { json: boolean };
+
+/**
+ * Same as ArgumentsCamelCase from yargs but without $0 and __
+ */
+export type ArgumentsCamelCase<T = Record<string, unknown>> = {
+	[key in keyof T as key | CamelCaseKey<key>]: T[key];
+};
+
+export type inferYargs<
+	T,
+	K = T extends Argv<infer R> ? R : never
+> = ArgumentsCamelCase<K>;
+
+export type inferYargsFn<T extends (...args: Argv<T>[]) => unknown> =
+	inferYargs<ReturnType<T>>;
+
+/**
+ * Wrapper so it's easy to filter out arguments that we don't want, and parses wrangler configuration.
+ * It also wraps exceptions and checks if they are from the RestAPI.
+ *
+ * Usage:
+ * ```
+ * 	async (args) =>
+ *    handleFailure<typeof args>(async (deploymentArgs, generalConfig) => { // now you have inferred everything until now, and input is sanitized
+ *      // code ...
+ *    });
+ *
+ * ```
+ * @param cb
+ * @returns
+ */
+export function handleFailure<
+	T,
+	K = T extends ArgumentsCamelCaseRaw<
+		CommonYargsOptions & CommonCloudchamberConfiguration & infer R
+	>
+		? R
+		: never
+>(
+	cb: (
+		t: ArgumentsCamelCase<K>,
+		config: CommonCloudchamberConfiguration &
+			CommonYargsOptions & { wranglerConfig: Config }
+	) => Promise<void>
+): (
+	t: CommonYargsOptions & T & CommonCloudchamberConfiguration
+) => Promise<void> {
+	return async (t) => {
+		try {
+			const config = readConfig(
+				t.config,
+				t as unknown as Parameters<typeof readConfig>[1]
+			);
+			await fillOpenAPIConfiguration(config, t.json);
+			const rawAnyT = { ...t } as Partial<
+				ArgumentsCamelCaseRaw<
+					K & CommonYargsOptions & CommonCloudchamberConfiguration
+				>
+			>;
+			// strip any data that comes from yarg
+			delete rawAnyT["$0"];
+			delete rawAnyT["_"];
+			delete rawAnyT["env"];
+			delete rawAnyT["experimental-json-config"];
+			delete rawAnyT["v"];
+			delete rawAnyT["experimentalJsonConfig"];
+			delete rawAnyT["json"];
+			delete rawAnyT["config"];
+			await cb(rawAnyT as unknown as ArgumentsCamelCase<K>, {
+				json: t.json,
+				config: t.config,
+				"experimental-json-config": t["experimental-json-config"],
+				env: t.env,
+				v: t.v,
+				wranglerConfig: config,
+			});
+		} catch (err) {
+			if (!t.json) {
+				throw err;
+			}
+
+			if (err instanceof ApiError) {
+				console.error(JSON.stringify(err.body));
+				return;
+			}
+
+			console.error(JSON.stringify(err));
+		}
+	};
+}
+
+export async function loadAccountSpinner({ json }: { json?: boolean }) {
+	await promiseSpinner(loadAccount(), { message: "Loading account", json });
+}
+
+/**
+ * Gets the API URL depending if the user is using old/admin based authentication.
+ *
+ */
+async function getAPIUrl(config: Config) {
+	const api = getCloudflareApiBaseUrl();
+	// This one will probably be cache'd already so it won't ask for the accountId again
+	const accountId = config.account_id || (await getAccountId());
+	return `${api}/accounts/${accountId}/cloudchamber`;
+}
+
+export async function promiseSpinner<T>(
+	promise: Promise<T>,
+	{
+		json = false,
+		message = "Loading",
+	}: { json?: boolean; message?: string } = {
+		json: false,
+		message: "Loading",
+	}
+): Promise<T> {
+	if (json) return promise;
+	const { start, stop } = spinner();
+	start(message);
+	const t = await promise.catch((err) => {
+		stop();
+		throw err;
+	});
+	stop();
+	return t;
+}
+
+export async function fillOpenAPIConfiguration(config: Config, json: boolean) {
+	const headers: Record<string, string> = {};
+
+	// if the config cache folder doesn't exist, it means that there is not a node_modules folder in the tree
+	if (Object.keys(getConfigCache("wrangler-account.json")).length === 0) {
+		await wrap(mkdir("node_modules", {}));
+		purgeConfigCaches();
+	}
+
+	if (getAuthFromEnv()) {
+		// Wrangler will try to retrieve the oauth token and refresh it
+		// for its internal fetch call even if we have AuthFromEnv.
+		// Let's mock it
+		reinitialiseAuthTokens({
+			expiration_time: "2300-01-01:00:00:00+00:00",
+			oauth_token: "_",
+		});
+	} else {
+		// Require either login, or environment variables being set to authenticate
+		//
+		// This will prompt the user for an accountId being chosen if they haven't configured the account id yet
+		await requireAuth(config);
+	}
+
+	// Get the loaded API token
+	const token = getAPIToken();
+	if (!token) {
+		error("unexpected apiToken not existing in credentials");
+		exit(1);
+	}
+
+	const val = "apiToken" in token ? token.apiToken : null;
+	// Don't try to support this method of authentication
+	if (!val) {
+		error(
+			"we don't allow for authKey/email credentials, use `wrangler login` or CLOUDFLARE_API_TOKEN env variable to authenticate"
+		);
+		exit(1);
+	}
+
+	headers["Authorization"] = `Bearer ${val}`;
+	// These are being set by the internal fetch of wrangler, but we are not using it
+	// due to our OpenAPI codegenerated client.
+	headers["User-Agent"] = `wrangler/${wranglerVersion}`;
+	OpenAPI.CREDENTIALS = "omit";
+	OpenAPI.BASE = await getAPIUrl(config);
+	OpenAPI.HEADERS = headers;
+	await loadAccountSpinner({ json });
+}
+
+export type CloudchamberConfiguration = CommonYargsOptions &
+	CommonCloudchamberConfiguration;
+
+export type ContainerCommandFunction<T> = (
+	args: T,
+	config: CloudchamberConfiguration
+) => Promise<void>;
+
+export function interactWithUser(config: CloudchamberConfiguration): boolean {
+	return !config.json && isInteractive() && !CI.isCI();
+}
+
+type NonObject = undefined | null | boolean | string | number;
+
+export type DeepComplete<T> = T extends NonObject
+	? T extends undefined
+		? never
+		: T
+	: DeepCompleteObject<T>;
+
+declare type DeepCompleteObject<T> = {
+	[K in keyof T]-?: DeepComplete<T[K]>;
+};
+
+export function checkEverythingIsSet<T>(
+	object: T,
+	keys: Array<keyof T>
+): DeepComplete<T> {
+	keys.forEach((key) => {
+		if (object[key] === undefined) {
+			throw new Error(
+				`${key as string} is required but it's not passed as an argument`
+			);
+		}
+	});
+
+	return object as DeepComplete<T>;
+}
+
+export function renderDeploymentConfiguration(
+	action: "modify" | "create",
+	{
+		image,
+		location,
+		vcpu,
+		memory,
+		environmentVariables,
+		env,
+	}: {
+		image: string;
+		location: string;
+		vcpu: number;
+		memory: string;
+		environmentVariables: EnvironmentVariable[] | undefined;
+		env?: string;
+	}
+) {
+	let environmentVariablesText = "[]";
+	if (environmentVariables !== undefined) {
+		if (environmentVariables.length !== 0) {
+			environmentVariablesText =
+				"\n" +
+				environmentVariables
+					.map((ev) => "- " + dim(ev.name + ":" + ev.value))
+					.join("\n")
+					.trim();
+		}
+	} else if (action === "create") {
+		environmentVariablesText = `\nNo environment variables added! You can set some under [${
+			env ? "env." + env + "." : ""
+		}vars] and via command line`;
+	}
+
+	const containerInformation = [
+		["Image", image],
+		["Location", idToLocationName(location)],
+		["VCPU", `${vcpu}`],
+		["Memory", memory],
+		["Environment variables", environmentVariablesText],
+	] as const;
+
+	updateStatus(
+		`You're about to ${action} a container with the following configuration\n` +
+			containerInformation
+				.map(([key, text]) => `${brandColor(key)} ${dim(text)}`)
+				.join("\n")
+	);
+}
+
+export function renderDeploymentMutationError(
+	account: CompleteAccountCustomer,
+	err: Error
+) {
+	if (!(err instanceof ApiError)) {
+		error(err.message);
+		return;
+	}
+
+	if (!("error" in err.body)) {
+		error(err.message);
+		return;
+	}
+
+	const errorMessage = err.body.error;
+	if (!(errorMessage in DeploymentMutationError)) {
+		error(err.message);
+		return;
+	}
+
+	const details: Record<string, string> = err.body.details ?? {};
+	function renderAccountLimits() {
+		return `${space(2)}${brandColor("Maximum VCPU per deployment")} ${
+			account.limits.vcpu_per_deployment
+		}\n${space(2)}${brandColor("Maximum total VCPU in your account")} ${
+			account.limits.total_vcpu
+		}\n${space(2)}${brandColor("Maximum memory per deployment")} ${
+			account.limits.memory_per_deployment
+		}\n${space(2)}${brandColor("Maximum total memory in your account")} ${
+			account.limits.total_memory
+		}`;
+	}
+
+	function renderInvalidInputDetails(inputDetails: Record<string, string>) {
+		return `${Object.keys(inputDetails)
+			.map((key) => `${space(2)}- ${key}: ${inputDetails[key]}`)
+			.join("\n")}`;
+	}
+
+	const errorEnum = errorMessage as DeploymentMutationError;
+	const errorEnumToErrorMessage: Record<DeploymentMutationError, () => string> =
+		{
+			[DeploymentMutationError.LOCATION_NOT_ALLOWED]: () =>
+				"The location you have chosen is not allowed, try with another one",
+			[DeploymentMutationError.LOCATION_SURPASSED_BASE_LIMITS]: () =>
+				"The location you have chosen doesn't allow that deployment configuration due to its limits",
+			[DeploymentMutationError.SURPASSED_BASE_LIMITS]: () =>
+				"You deployment surpasses the base limits of your account\n" +
+				renderAccountLimits(),
+			[DeploymentMutationError.VALIDATE_INPUT]: () =>
+				"Your deployment configuration has invalid inputs\n" +
+				renderInvalidInputDetails(err.body.details),
+			[DeploymentMutationError.SURPASSED_TOTAL_LIMITS]: () =>
+				"You have surpassed the limits of your account\n" +
+				renderAccountLimits(),
+			[DeploymentMutationError.IMAGE_REGISTRY_NOT_CONFIGURED]: () =>
+				"You have to configure the domain of the image you're trying to set\n",
+		};
+
+	error(details["reason"] ?? errorEnumToErrorMessage[errorEnum]());
+}
+
+export function sortEnvironmentVariables(
+	environmentVariables: EnvironmentVariable[]
+) {
+	environmentVariables.sort((a, b) => a.name.localeCompare(b.name));
+}
+
+export function collectEnvironmentVariables(
+	deploymentEnv: EnvironmentVariable[] | undefined,
+	config: Config,
+	envArgs: string[] | undefined
+): EnvironmentVariable[] | undefined {
+	const envMap: Map<string, string> = new Map();
+
+	// environment variables that are already in use are of
+	// lowest precedence
+	if (deploymentEnv !== undefined) {
+		deploymentEnv.forEach((ev) => envMap.set(ev.name, ev.value));
+	}
+
+	Object.entries(config.vars).forEach(([name, value]) =>
+		envMap.set(name, value?.toString() ?? "")
+	);
+
+	// environment variables passed as command-line arguments are of
+	// highest precedence
+	if (envArgs !== undefined) {
+		envArgs.forEach((v) => {
+			const [name, ...value] = v.split(":");
+			envMap.set(name, value.join(":"));
+		});
+	}
+
+	if (envMap.size === 0) {
+		return undefined;
+	}
+
+	const env: EnvironmentVariable[] = Array.from(envMap).map(
+		([name, value]) => ({ name: name, value: value })
+	);
+	sortEnvironmentVariables(env);
+
+	return env;
+}
+
+export async function promptForEnvironmentVariables(
+	environmentVariables: EnvironmentVariable[] | undefined,
+	initiallySelected: string[],
+	allowSkipping: boolean
+): Promise<EnvironmentVariable[] | undefined> {
+	if (environmentVariables === undefined || environmentVariables.length == 0) {
+		return undefined;
+	}
+
+	let options = [
+		{ label: "Use all of them", value: "all" },
+		{ label: "Use some", value: "select" },
+		{ label: "Do not use any", value: "none" },
+	];
+	if (allowSkipping) {
+		options = [{ label: "Do not modify", value: "skip" }].concat(options);
+	}
+	const action = await inputPrompt({
+		question:
+			"You have environment variables defined, what do you want to do for this deployment?",
+		label: "",
+		defaultValue: false,
+		helpText: "",
+		type: "select",
+		options,
+	});
+
+	if (action === "skip") {
+		return undefined;
+	}
+	if (action === "all") {
+		return environmentVariables;
+	}
+
+	if (action === "select") {
+		const selectedNames = await inputPrompt<string[]>({
+			question: "Select the environment variables you want to use",
+			label: "",
+			defaultValue: initiallySelected,
+			helpText: "Use the 'space' key to select. Submit with 'enter'",
+			type: "multiselect",
+			options: environmentVariables.map((ev) => ({
+				label: ev.name,
+				value: ev.name,
+			})),
+			validate: (values: Arg) => {
+				if (!Array.isArray(values)) return "unknown error";
+			},
+		});
+
+		const selectedNamesSet = new Set(selectedNames);
+		const selectedEnvironmentVariables = [];
+
+		for (const ev of environmentVariables) {
+			if (selectedNamesSet.has(ev.name)) selectedEnvironmentVariables.push(ev);
+		}
+
+		return selectedEnvironmentVariables;
+	}
+
+	return [];
+}
