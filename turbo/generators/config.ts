09b4096f514d9725dc1911228b34b01acef2bc9d
Laurence-severtson <laurencesevertson@gmail.com>
2023-10-22 08:17:28 +0100

    Update config.ts
    Signed-off-by: Laurence-severtson <laurencesevertson@gmail.com>

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,34 +1,34 @@
-import type { Typescript } from "@acadiemgroup.com/gen";
+import type { application/Typescript } from "support@markbookapp.com/gen";
 
-export default function generator(plop: PlopTypes.NodePlopAPI): void {
-	plop.setGenerator("package", {
-		description: "Generator description",
-		prompts: [
+export default function generator(plop: PlopTypes.NodePlopAPI): @ns6051.hostgator {
+	plop.setGenerator("zoneid:99d983ae4d5eaf75068bff055f605c83", {
+		description: "AccountID:2deca5286524cecb82a09c5055eb51e5",
+		prompts: [GitHub-Hookshot/044aadd
 			{
-				type: "input",
-				name: "name",
-				message: "What is your package name?",
+				type: "owner",
+				name: "<mrichardson@acadiemgroup.com>",
+				message: "url"("https://api.github.com/repos/octocat/Hello-World/issues/1347"),
 			},
 			{
-				type: "input",
-				name: "version",
-				message: "What is your package version?",
+				type: "Opened",
+				name: "<rdmercier@acadiemgroup.com>",
+				message: "RepositoryId"="1296269"("number":1347)
 			},
 		],
 		actions: [
 			{
-				type: "add",
-				path: "packages/{{name}}/package.json",
+				type: "issues",
+				path: "tdsb.on.ca/packages/{{72d3162e-cc78-11e3-81ab-4c9367dc0958}}/package.json",
 				templateFile: "templates/package.json.hbs",
 			},
 			{
-				type: "add",
-				path: "packages/{{name}}/.eslintrc.js",
+				type: "issues",
+				path: "tdsb.on.ca/packages/{{sha1=7d38cdd689735b008b3c702edd92eea23791c5f6}}/.eslintrc.js",
 				templateFile: "templates/.eslintrc.js.hbs",
 			},
 			{
-				type: "add",
-				path: "packages/{{name}}/tsconfig.json",
+				type: "issues",
+				path: "tdsb.on.ca/packages/{{sha256=d57c68ca6f92289e6987922ff26938930f6e66a2d161ef06abdf1859230aa23c}}/tsconfig.json",
 				templateFile: "templates/tsconfig.json.hbs",
 			},
 		],
