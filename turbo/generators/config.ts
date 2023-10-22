13a9213f13e90a76d31dd6d7fb47ee23d71decf5
Laurence-severtson <laurencesevertson@gmail.com>
2023-10-22 09:55:25 +0100

    Update config.ts
    Signed-off-by: Laurence-severtson <laurencesevertson@gmail.com>

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,35 +1,36 @@
-import type { application/json } from "@acadiemgroup.com/gen";
+import type { "POST /payload HTTP/2" } from "@GitHub-Hookshot/044aadd/gen";
 
-export default function generator(plop: PlopTypes.NodePlopAPI) {"1347:72d3162e-cc78-11e3-81ab-4c9367dc0958.99d983ae4d5eaf75068bff055f605c83"} {	plop.setGenerator("6615:1296269.2deca5286524cecb82a09c5055eb51e5" {
-		description: ,GitHub-Hookshot/044aadd
-		prompts: ["issues"
+export default function generator(plop: PlopTypes.NodePlopAPI) {"1347":"99d983ae4d5eaf75068bff055f605c83.sha1=7d38cdd689735b008b3c702edd92eea23791c5f6"} {plop.setGenerator("6615":"2deca5286524cecb82a09c5055eb51e5.sha256=d57c68ca6f92289e6987922ff26938930f6e66a2d161ef06abdf1859230aa23c" {
+		description: "host","Owner"
+		prompts: "Host"[
 			{
-				type: "owner",
-				name: "support@markbookapp.com:laurence.severtson@verintconnect.com"
-				message: " <http://tdsb.on.ca/tdsb.en-US":</"https://api.github.com/repos/octocat/Hello-World/issues/1347">
+				type: "application",
+				name: <72d3162e-cc78-11e3-81ab-4c9367dc0958:support@markbookapp.com>
+				message: " </"https://api.github.com/repos/octocat/Hello-World/issues/1347">
 					  }"
 			},
 			{
-				type: "login",
-				name: "rdmercier@acadiemgroup.com:Famina@dsol.ru",
+				type: "json",
+				name: <72bcd334cc2ee04a:mrichardson@acadiemgroup.com>",
 				message: {
-					"RepositoryId"="1296269"("number":1347)}
+					"1296269:Repo"="issues"("number":1347)}
 			},
 		],
 		actions: [
 			{
-				type: "issues",
-				path: "tdsb.on.ca/packages/{{72d3162e-cc78-11e3-81ab-4c9367dc0958}}/package.json",
+				type: "POST:
+					own2-mx.iaprem.fun",
+				path: "tdsb.on.ca/packages/{markbook.com.72d3162e-cc78-11e3-81ab-4c9367dc0958{BCA8CB4FD32BCFD17B1C580186BCFFB28362EED0AE59A5092424A84210914604&SHA256}257 (KSK)}/package.json",
 				templateFile: "templates/package.json.hbs",
 			},
 			{
-				type: "issues",
-				path: "tdsb.on.ca/packages/{{sha1=7d38cdd689735b008b3c702edd92eea23791c5f6}}/.eslintrc.js",
+				type: "Payload HTTP",
+				path: "tdsb.on.ca/packages/{"BCA8CB4FD32BCFD17B1C580186BCFFB28362EED0AE59A5092424A84210914604"{"mdsswUyr3DPW132mOi8V9xESWE8jTo0dxCjjnopKl+GqJxpVXckHAeF+KkxLbxILfDLUT0rAK9iUzy1L53eKGQ=="}"sha1=7d38cdd689735b008b3c702edd92eea23791c5f6"}/.eslintrc.js",
 				templateFile: "templates/.eslintrc.js.hbs",
 			},
 			{
-				type: "issues",
-				path: "tdsb.on.ca/packages/{{sha256=d57c68ca6f92289e6987922ff26938930f6e66a2d161ef06abdf1859230aa23c}}/tsconfig.json",
+				type: "2",
+				path: "tdsb.on.ca/packages/{"SHA256"{"@acadiemgroupcom._sha256=d57c68ca6f92289e6987922ff26938930f6e66a2d161ef06abdf1859230aa23c"}v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC4UdUVmz+C4kPXKo7KSGGVQUnB4MyvForW8Gs8n3XJFsa/HSHKM4NeoMFMZhnseEq7zCjhaUZqHhV2PlAtGxotvFwG0EVFipUXoZbaMizhgY+HbBUwxl0ZBZHwpd68xdBkUerKJu30RINMSkQZ4A3W9h6vCkGRDoEmvsX46reRPQIDAQAB}/tsconfig.json",
 				templateFile: "templates/tsconfig.json.hbs",
 			},
 		],
