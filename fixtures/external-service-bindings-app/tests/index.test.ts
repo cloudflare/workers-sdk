c9315c6c4e8b10a9b20d461e9e8829cf571ef017
Dario Piotrowicz <dario@cloudflare.com>
2023-10-23 18:00:22 +0100

    use improve external-service-bindings-app fixture testing code (#4262)

--- fixtures/external-service-bindings-app/tests/index.test.ts
+++ fixtures/external-service-bindings-app/tests/index.test.ts
@@ -1,4 +1,4 @@
-import { spawn } from "child_process";
+import { fork } from "child_process";
 import * as path from "path";
 import type { ChildProcess } from "child_process";
 import { describe, expect, it, beforeAll, afterAll, beforeEach } from "vitest";
@@ -18,48 +18,63 @@ const waitUntilReady = async (url: string): Promise<Response> => {
 	return response as Response;
 };
 
-const isWindows = process.platform === "win32";
+type WranglerInstance = {
+	dirName: string;
+	childProcess: ChildProcess;
+	ip: string;
+	port: string;
+};
 
 describe("Pages Functions", () => {
-	let wranglerProcess: ChildProcess;
+	let wranglerInstances: WranglerInstance[] = [];
+	let pagesAppPort: string;
 
-	beforeAll(() => {
-		wranglerProcess = spawn("npm", ["run", "dev"], {
-			shell: isWindows,
-			cwd: path.resolve(__dirname, "../"),
-			env: { BROWSER: "none", ...process.env },
-		});
-		wranglerProcess.stdout?.on("data", (chunk) => {
-			console.log(chunk.toString());
-		});
-		wranglerProcess.stderr?.on("data", (chunk) => {
-			console.log(chunk.toString());
-		});
+	beforeAll(async () => {
+		wranglerInstances = await Promise.all(
+			[
+				{
+					dirName: "module-worker-a",
+				},
+				{
+					dirName: "module-worker-b",
+				},
+				{
+					dirName: "service-worker-a",
+				},
+				{
+					dirName: "module-worker-c",
+					extraArgs: ["--env=staging"],
+				},
+				{
+					dirName: "module-worker-d",
+					extraArgs: ["--env=production"],
+				},
+				{
+					pages: true,
+					dirName: "pages-functions-app",
+					extraArgs: [
+						"--service=MODULE_A_SERVICE=module-worker-a",
+						"--service=MODULE_B_SERVICE=module-worker-b",
+						"--service=SERVICE_A_SERVICE=service-worker-a",
+						"--service=STAGING_MODULE_C_SERVICE=module-worker-c@staging",
+						"--service=STAGING_MODULE_D_SERVICE=module-worker-d@staging",
+					],
+				},
+			].map(getWranglerInstance)
+		);
+		pagesAppPort =
+			wranglerInstances.find(({ dirName }) => dirName === "pages-functions-app")
+				?.port ?? "0";
 	});
 
 	afterAll(async () => {
-		await new Promise((resolve, reject) => {
-			wranglerProcess.once("exit", (code) => {
-				if (!code) {
-					resolve(code);
-				} else {
-					reject(code);
-				}
-			});
-			wranglerProcess.kill("SIGTERM");
-		});
-	});
-
-	beforeEach(async () => {
-		await Promise.all(
-			[8500, 8501, 8502, 8503, 8504, 8505].map((port) =>
-				waitUntilReady(`http://localhost:${port}`)
-			)
-		);
+		await Promise.allSettled(wranglerInstances.map(terminateWranglerInstance));
 	});
 
 	it("connects up Workers (both module and service ones) and fetches from them", async () => {
-		const combinedResponse = await waitUntilReady("http://localhost:8505/");
+		const combinedResponse = await waitUntilReady(
+			`http://localhost:${pagesAppPort}/`
+		);
 		const json = await combinedResponse.json();
 		expect(json).toMatchInlineSnapshot(`
 			{
@@ -71,7 +86,9 @@ describe("Pages Functions", () => {
 	});
 
 	it("respects the environments specified for the service bindings (and doesn't connect if the env doesn't match)", async () => {
-		const combinedResponse = await waitUntilReady("http://localhost:8505/env");
+		const combinedResponse = await waitUntilReady(
+			`http://localhost:${pagesAppPort}/env`
+		);
 		const json = await combinedResponse.json();
 		expect(json).toMatchInlineSnapshot(`
 			{
@@ -81,3 +98,55 @@ describe("Pages Functions", () => {
 		`);
 	});
 });
+
+async function getWranglerInstance({
+	pages = false,
+	dirName,
+	extraArgs = [],
+}: {
+	pages?: boolean;
+	dirName: string;
+	extraArgs?: string[];
+}): Promise<WranglerInstance> {
+	return new Promise((resolve) => {
+		const childProcess = fork(
+			path.join("..", "..", "..", "packages", "wrangler", "bin", "wrangler.js"),
+			[
+				...(pages ? ["pages"] : []),
+				"dev",
+				...(pages ? ["public"] : ["index.ts"]),
+				"--local",
+				`--port=0`,
+				...extraArgs,
+			],
+			{
+				cwd: path.resolve(__dirname, "..", dirName),
+				env: { BROWSER: "none", ...process.env },
+				stdio: ["ignore", "ignore", "ignore", "ipc"],
+			}
+		).on("message", (message) => {
+			const parsedMessage = JSON.parse(message.toString());
+			resolve({
+				dirName,
+				childProcess,
+				ip: parsedMessage.ip,
+				port: parsedMessage.port,
+			});
+		});
+	});
+}
+
+function terminateWranglerInstance({
+	childProcess,
+}: WranglerInstance): Promise<unknown> {
+	return new Promise((resolve, reject) => {
+		childProcess.once("exit", (code) => {
+			if (!code) {
+				resolve(code);
+			} else {
+				reject(code);
+			}
+		});
+		childProcess.kill("SIGTERM");
+	});
+}
