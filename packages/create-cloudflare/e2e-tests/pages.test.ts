5e0c26a54cc834e76d9d7240ee468071dcb48db1
Dario Piotrowicz <dario@cloudflare.com>
2023-10-27 00:13:55 +0100

    quarantine astro in C3 e2es

--- packages/create-cloudflare/e2e-tests/pages.test.ts
+++ packages/create-cloudflare/e2e-tests/pages.test.ts
@@ -36,6 +36,7 @@ describe.concurrent(`E2E: Web frameworks`, () => {
 	// These are ordered based on speed and reliability for ease of debugging
 	const frameworkTests: Record<string, FrameworkTestConfig> = {
 		astro: {
+			quarantine: true,
 			expectResponseToContain: "Hello, Astronaut!",
 			testCommitMessage: true,
 		},
