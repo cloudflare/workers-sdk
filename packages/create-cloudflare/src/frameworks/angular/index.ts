7de8795c324971dee54b14ba3abee5ec1a084729
Alan Agius <alanagius@google.com>
2023-10-30 10:46:19 +0000

    feat: update Angular template to version 17
    This commits updates the Angular template to use Angular 17.

--- packages/create-cloudflare/src/frameworks/angular/index.ts
+++ packages/create-cloudflare/src/frameworks/angular/index.ts
@@ -1,40 +1,29 @@
-import { cp, rm } from "node:fs/promises";
+import { cp } from "node:fs/promises";
 import { resolve } from "node:path";
 import { logRaw } from "@cloudflare/cli";
 import { brandColor, dim } from "@cloudflare/cli/colors";
 import { spinner } from "@cloudflare/cli/interactive";
-import {
-	installPackages,
-	runCommand,
-	runFrameworkGenerator,
-} from "helpers/command";
+import { installPackages, runFrameworkGenerator } from "helpers/command";
 import { compatDateFlag, readFile, readJSON, writeFile } from "helpers/files";
 import { detectPackageManager } from "helpers/packages";
 import { getFrameworkCli } from "../index";
 import type { FrameworkConfig, PagesGeneratorContext } from "types";
 
-const { dlx, npx, npm } = detectPackageManager();
+const { dlx, npm } = detectPackageManager();
 
 const generate = async (ctx: PagesGeneratorContext) => {
 	const cli = getFrameworkCli(ctx);
 
-	await runFrameworkGenerator(
-		ctx,
-		`${dlx} ${cli} new ${ctx.project.name} --standalone`
-	);
+	await runFrameworkGenerator(ctx, `${dlx} ${cli} ${ctx.project.name} --ssr`);
 
 	logRaw("");
 };
 
 const configure = async (ctx: PagesGeneratorContext) => {
 	process.chdir(ctx.project.path);
-	await runCommand(`${npx} ng analytics disable`, {
-		silent: true,
-	});
-	await addSSRAdapter();
-	await installCFWorker(ctx);
-	await updateAppCode();
 	updateAngularJson(ctx);
+	await updateAppCode();
+	await installCFWorker(ctx);
 };
 
 const config: FrameworkConfig = {
@@ -42,15 +31,14 @@ const config: FrameworkConfig = {
 	configure,
 	displayName: "Angular",
 	getPackageScripts: async () => ({
-		process:
-			"node ./tools/copy-worker-files.mjs && node ./tools/copy-client-files.mjs && node ./tools/bundle.mjs",
-		"pages:build": `${npm} run build:ssr && ${npm} run process`,
 		start: `${npm} run pages:build && wrangler pages dev dist/cloudflare ${await compatDateFlag()} --experimental-local`,
+		process: "node ./tools/copy-files.mjs && node ./tools/alter-polyfills.mjs",
+		"pages:build": `ng build && ${npm} run process`,
 		deploy: `${npm} run pages:build && wrangler pages deploy dist/cloudflare`,
 	}),
 	deployCommand: "deploy",
 	devCommand: "start",
-	testFlags: ["--routing", "--style", "sass"],
+	testFlags: ["--ssr", "--style", "sass"],
 };
 export default config;
 
@@ -66,15 +54,7 @@ async function installCFWorker(ctx: PagesGeneratorContext) {
 	s.stop(`${brandColor("copied")} ${dim("adapter code")}`);
 
 	await installPackages(
-		[
-			"@cloudflare/workers-types",
-			"@esbuild-plugins/node-globals-polyfill",
-			"@esbuild-plugins/node-modules-polyfill",
-			"@miniflare/tre@next",
-			"esbuild",
-			"fast-glob",
-			"wrangler@beta",
-		],
+		["@cloudflare/workers-types", "@miniflare/tre@next", "wrangler@beta"],
 		{
 			dev: true,
 			startText: "Installing adapter dependencies",
@@ -82,51 +62,43 @@ async function installCFWorker(ctx: PagesGeneratorContext) {
 		}
 	);
 }
-
-async function addSSRAdapter() {
-	const cmd = `${npx} ng add @nguniversal/express-engine`;
-
-	await runCommand(`${cmd} --skip-confirmation`, {
-		silent: true,
-		startText: "Installing Angular SSR",
-		doneText: `${brandColor("installed")} ${dim(`via \`${cmd}\``)}`,
-	});
-}
-
 async function updateAppCode() {
 	const s = spinner();
 	s.start(`Updating application code`);
 
 	// Update an app config file to:
-	// - add the `provideClientHydration()` provider to enable hydration
 	// - add the `provideHttpClient(withFetch())` call to enable `fetch` usage in `HttpClient`
 	const appConfigPath = "src/app/app.config.ts";
 	const appConfig = readFile(resolve(appConfigPath));
 	const newAppConfig =
-		"import { provideClientHydration } from '@angular/platform-browser';\n" +
 		"import { provideHttpClient, withFetch } from '@angular/common/http';\n" +
 		appConfig.replace(
 			"providers: [",
-			"providers: [provideHttpClient(withFetch()), provideClientHydration(), "
+			"providers: [provideHttpClient(withFetch()), "
 		);
 	writeFile(resolve(appConfigPath), newAppConfig);
-
-	// Remove the unwanted node.js server entry-point
-	await rm(resolve("server.ts"));
-
 	s.stop(`${brandColor(`updated`)} ${dim(appConfigPath)}`);
+
+	// Remove unwanted dependencies
+	s.start(`Updating package.json`);
+	const packageJsonPath = resolve("package.json");
+	const packageManifest = readJSON(packageJsonPath);
+
+	delete packageManifest["dependencies"]["@angular/ssr"];
+	delete packageManifest["dependencies"]["express"];
+
+	writeFile(packageJsonPath, JSON.stringify(packageManifest, null, 2));
+	s.stop(`${brandColor(`updated`)} ${dim(`\`package.json\``)}`);
 }
 
 function updateAngularJson(ctx: PagesGeneratorContext) {
 	const s = spinner();
 	s.start(`Updating angular.json config`);
 	const angularJson = readJSON(resolve("angular.json"));
+	// Update builder
 	const architectSection = angularJson.projects[ctx.project.name].architect;
-	architectSection.build.options.outputPath = "dist/browser";
+	architectSection.build.options.outputPath = "dist";
 	architectSection.build.options.assets.push("src/_routes.json");
-	architectSection.server.options.outputPath = "dist/server";
-	architectSection.server.options.main = "src/main.server.ts";
-	delete architectSection["serve-ssr"];
 
 	writeFile(resolve("angular.json"), JSON.stringify(angularJson, null, 2));
 	s.stop(`${brandColor(`updated`)} ${dim(`\`angular.json\``)}`);
--- packages/create-cloudflare/src/frameworks/angular/templates/server.ts
+++ packages/create-cloudflare/src/frameworks/angular/templates/server.ts
@@ -1,11 +1,5 @@
-import "zone.js/dist/zone-node";
-import "@angular/platform-server/init";
-
-import { bootstrapApplication } from "@angular/platform-browser";
 import { renderApplication } from "@angular/platform-server";
-
-import { AppComponent } from "./app/app.component";
-import { config } from "./app/app.config.server";
+import bootstrap from "./src/main.server";
 
 interface Env {
 	ASSETS: { fetch: typeof fetch };
@@ -14,10 +8,7 @@ interface Env {
 // We attach the Cloudflare `fetch()` handler to the global scope
 // so that we can export it when we process the Angular output.
 // See tools/bundle.mjs
-(globalThis as any).__workerFetchHandler = async function fetch(
-	request: Request,
-	env: Env
-) {
+async function workerFetchHandler(request: Request, env: Env) {
 	const url = new URL(request.url);
 	console.log("render SSR", url.href);
 
@@ -26,10 +17,18 @@ interface Env {
 	const indexResponse = await env.ASSETS.fetch(new Request(indexUrl));
 	const document = await indexResponse.text();
 
-	const content = await renderApplication(
-		() => bootstrapApplication(AppComponent, config),
-		{ document, url: url.pathname }
-	);
+	const content = await renderApplication(bootstrap, {
+		document,
+		url: url.pathname,
+	});
+
 	// console.log("rendered SSR", content);
 	return new Response(content, indexResponse);
+}
+
+export default {
+	fetch: (request: Request, env: Env) =>
+		(globalThis as any)["__zone_symbol__Promise"].resolve(
+			workerFetchHandler(request, env)
+		),
 };
