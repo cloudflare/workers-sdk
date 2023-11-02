1bc339f9f0311dff36e772c51bb302b5c765e9ed
Samuel Macleod <smacleod@cloudflare.com>
2023-11-02 03:10:16 +0000

    more time for additional modules

--- fixtures/additional-modules/vitest.config.ts
+++ fixtures/additional-modules/vitest.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		testTimeout: 5_000,
-		hookTimeout: 5_000,
-		teardownTimeout: 5_000,
+		testTimeout: 15_000,
+		hookTimeout: 15_000,
+		teardownTimeout: 15_000,
 		useAtomics: true,
 	},
 });
