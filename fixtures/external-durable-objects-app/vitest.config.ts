72d0b76717814aef79e3c0fad83d42d8f8ba7ab5
Samuel Macleod <smacleod@cloudflare.com>
2023-11-03 01:18:18 +0000

    Give durable objects a bit more time

--- fixtures/external-durable-objects-app/vitest.config.ts
+++ fixtures/external-durable-objects-app/vitest.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		testTimeout: 5_000,
-		hookTimeout: 5_000,
-		teardownTimeout: 5_000,
+		testTimeout: 25_000,
+		hookTimeout: 25_000,
+		teardownTimeout: 25_000,
 		useAtomics: true,
 	},
 });
