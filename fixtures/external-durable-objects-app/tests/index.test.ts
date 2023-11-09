60e14132ba82f848ff95ab24aa9d4b372763bf52
Samuel Macleod <smacleod@cloudflare.com>
2023-11-09 15:40:41 +0000

    Use unstable_dev

--- fixtures/external-durable-objects-app/tests/index.test.ts
+++ fixtures/external-durable-objects-app/tests/index.test.ts
@@ -3,29 +3,13 @@ import * as path from "path";
 import { fetch } from "undici";
 import { describe, expect, it, beforeAll, afterAll } from "vitest";
 import type { ChildProcess } from "child_process";
+import { type UnstableDevWorker, unstable_dev } from "wrangler";
 
 describe("Pages Functions", () => {
-	let aWranglerProcess: ChildProcess;
-	let aIP: string;
-	let aPort: number;
-	let aResolveReadyPromise: (value: unknown) => void;
-	const aReadyPromise = new Promise((resolve) => {
-		aResolveReadyPromise = resolve;
-	});
-	let bWranglerProcess: ChildProcess;
-	let bIP: string;
-	let bPort: number;
-	let bResolveReadyPromise: (value: unknown) => void;
-	const bReadyPromise = new Promise((resolve) => {
-		bResolveReadyPromise = resolve;
-	});
-	let cWranglerProcess: ChildProcess;
-	let cIP: string;
-	let cPort: number;
-	let cResolveReadyPromise: (value: unknown) => void;
-	const cReadyPromise = new Promise((resolve) => {
-		cResolveReadyPromise = resolve;
-	});
+	let a: UnstableDevWorker;
+	let b: UnstableDevWorker;
+	let c: UnstableDevWorker;
+
 	let dWranglerProcess: ChildProcess;
 	let dIP: string;
 	let dPort: number;
@@ -34,46 +18,18 @@ describe("Pages Functions", () => {
 		dResolveReadyPromise = resolve;
 	});
 
-	beforeAll(() => {
-		aWranglerProcess = fork(
-			path.join("..", "..", "..", "packages", "wrangler", "bin", "wrangler.js"),
-			["dev", "index.ts", "--local", "--port=0"],
-			{
-				stdio: ["ignore", "ignore", "ignore", "ipc"],
-				cwd: path.resolve(__dirname, "..", "a"),
-			}
-		).on("message", (message) => {
-			const parsedMessage = JSON.parse(message.toString());
-			aIP = parsedMessage.ip;
-			aPort = parsedMessage.port;
-			aResolveReadyPromise(undefined);
+	beforeAll(async () => {
+		a = await unstable_dev(path.join(__dirname, "../a/index.ts"), {
+			config: path.join(__dirname, "../a/wrangler.toml"),
 		});
-		bWranglerProcess = fork(
-			path.join("..", "..", "..", "packages", "wrangler", "bin", "wrangler.js"),
-			["dev", "index.ts", "--local", "--port=0"],
-			{
-				stdio: ["ignore", "ignore", "ignore", "ipc"],
-				cwd: path.resolve(__dirname, "..", "b"),
-			}
-		).on("message", (message) => {
-			const parsedMessage = JSON.parse(message.toString());
-			bIP = parsedMessage.ip;
-			bPort = parsedMessage.port;
-			bResolveReadyPromise(undefined);
+		b = await unstable_dev(path.join(__dirname, "../b/index.ts"), {
+			config: path.join(__dirname, "../b/wrangler.toml"),
 		});
-		cWranglerProcess = fork(
-			path.join("..", "..", "..", "packages", "wrangler", "bin", "wrangler.js"),
-			["dev", "index.ts", "--local", "--port=0"],
-			{
-				stdio: ["ignore", "ignore", "ignore", "ipc"],
-				cwd: path.resolve(__dirname, "..", "c"),
-			}
-		).on("message", (message) => {
-			const parsedMessage = JSON.parse(message.toString());
-			cIP = parsedMessage.ip;
-			cPort = parsedMessage.port;
-			cResolveReadyPromise(undefined);
+
+		c = await unstable_dev(path.join(__dirname, "../c/index.ts"), {
+			config: path.join(__dirname, "../c/wrangler.toml"),
 		});
+
 		dWranglerProcess = fork(
 			path.join("..", "..", "..", "packages", "wrangler", "bin", "wrangler.js"),
 			[
@@ -96,36 +52,12 @@ describe("Pages Functions", () => {
 	});
 
 	afterAll(async () => {
-		await aReadyPromise;
-		await bReadyPromise;
-		await cReadyPromise;
 		await dReadyPromise;
+		await a.stop();
+		await b.stop();
+		await c.stop();
 
 		await new Promise((resolve, reject) => {
-			aWranglerProcess.once("exit", (code) => {
-				if (!code) {
-					resolve(code);
-				} else {
-					reject(code);
-				}
-			});
-			aWranglerProcess.kill("SIGTERM");
-			bWranglerProcess.once("exit", (code) => {
-				if (!code) {
-					resolve(code);
-				} else {
-					reject(code);
-				}
-			});
-			bWranglerProcess.kill("SIGTERM");
-			cWranglerProcess.once("exit", (code) => {
-				if (!code) {
-					resolve(code);
-				} else {
-					reject(code);
-				}
-			});
-			cWranglerProcess.kill("SIGTERM");
 			dWranglerProcess.once("exit", (code) => {
 				if (!code) {
 					resolve(code);
@@ -138,28 +70,29 @@ describe("Pages Functions", () => {
 	});
 
 	it("connects up Durable Objects and keeps state across wrangler instances", async () => {
-		await aReadyPromise;
-		await bReadyPromise;
-		await cReadyPromise;
 		await dReadyPromise;
 
 		// Service registry is polled every 300ms,
 		// so let's give all the Workers a little time to find each other
 		await new Promise((resolve) => setTimeout(resolve, 700));
 
-		const responseA = await fetch(`http://${aIP}:${aPort}/`);
+		const responseA = await a.fetch(`/`, {
+			headers: {
+				"X-Reset-Count": "true",
+			},
+		});
 		const dataA = (await responseA.json()) as { count: number; id: string };
 		expect(dataA.count).toEqual(1);
-		const responseB = await fetch(`http://${bIP}:${bPort}/`);
+		const responseB = await b.fetch(`/`);
 		const dataB = (await responseB.json()) as { count: number; id: string };
 		expect(dataB.count).toEqual(2);
-		const responseC = await fetch(`http://${cIP}:${cPort}/`);
+		const responseC = await c.fetch(`/`);
 		const dataC = (await responseC.json()) as { count: number; id: string };
 		expect(dataC.count).toEqual(3);
 		const responseD = await fetch(`http://${dIP}:${dPort}/`);
 		const dataD = (await responseD.json()) as { count: number; id: string };
 		expect(dataD.count).toEqual(4);
-		const responseA2 = await fetch(`http://${aIP}:${aPort}/`);
+		const responseA2 = await a.fetch(`/`);
 		const dataA2 = (await responseA2.json()) as { count: number; id: string };
 		expect(dataA2.count).toEqual(5);
 		expect(dataA.id).toEqual(dataB.id);
