a81882dcb6316e1c8c7c22a5595f355471b2be63
Rahul Sethi <5822355+RamIdeas@users.noreply.github.com>
2023-11-13 15:29:19 +0000

    change debug log default directory

--- packages/wrangler/src/utils/debug-log-file.ts
+++ packages/wrangler/src/utils/debug-log-file.ts
@@ -1,10 +1,9 @@
-import { appendFile } from "node:fs/promises";
+import { appendFile, mkdir, readFile } from "node:fs/promises";
 import path from "node:path";
-import { format } from "node:util";
 import { Mutex } from "miniflare";
 import { getEnvironmentVariableFactory } from "../environment-variables/factory";
-import { logger } from "../logger";
 import { getBasePath } from "../paths";
+import type { LoggerLevel } from "../logger";
 
 const getDebugFilepath = getEnvironmentVariableFactory({
 	variableName: "WRANGLER_DEBUG_LOG",
@@ -18,37 +17,61 @@ const getDebugFilepath = getEnvironmentVariableFactory({
 		const absoluteFilepath = path.join(
 			getBasePath(),
 			".wrangler",
+			"debug-logs",
 			`wrangler-debug-${date}.log`
 		);
-		const relativeFilepath = path.relative(process.cwd(), absoluteFilepath);
 
-		return relativeFilepath;
+		return absoluteFilepath;
 	},
 });
 
-const debugLogFilepath = getDebugFilepath();
+async function ensureDirectoryExists(filepath: string) {
+	const dirpath = path.dirname(filepath);
+
+	await mkdir(dirpath, { recursive: true });
+}
+
+export const debugLogFilepath = getDebugFilepath();
 const mutex = new Mutex();
 
 let hasLoggedLocation = false;
 let hasLoggedError = false;
 
-export async function appendToDebugLogFile(...args: unknown[]) {
+/**
+ * Appends a message to the log file after waiting for pending writes to complete
+ */
+export async function appendToDebugLogFile(
+	messageLevel: LoggerLevel,
+	message: string
+) {
 	const entry = `
---- ${new Date().toISOString()}
-${format(...args)}
+--- ${new Date().toISOString()} ${messageLevel}
+${message}
 ---
 `;
 
-	await mutex.runWith(() =>
-		appendFile(debugLogFilepath, entry).catch((err) => {
+	await mutex.runWith(async () => {
+		try {
+			await ensureDirectoryExists(debugLogFilepath);
+			await appendFile(debugLogFilepath, entry);
+		} catch (err) {
 			if (hasLoggedError) return;
 			hasLoggedError = true;
-			logger.error(`Failed to write to debug log file`, err);
-		})
-	);
+			console.error(`Failed to write to debug log file`, err);
+			console.error(`Would have written:`, entry);
+		}
+	});
 
 	if (hasLoggedLocation) return;
 	hasLoggedLocation = true;
-	logger.info(`🐛 Writing debug logs to "${debugLogFilepath}"`);
+	const relativeFilepath = path.relative(process.cwd(), debugLogFilepath);
+	console.info(`🐛 Writing debug logs to "${relativeFilepath}"`);
 	// TODO: move this into an exit hook so it isn't the first thing logged
+}
+
+/**
+ * Reads the current log file after waiting for all pending writes
+ */
+export function readDebugLogFile(): Promise<string> {
+	return mutex.runWith(() => readFile(debugLogFilepath, "utf-8"));
 }
