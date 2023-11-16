7d849057dd0596dbc4362724246be1ed0d6d7fe0
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2023-11-16 13:44:08 +0100

    use NO_HYPERDRIVE_WARNING

--- packages/wrangler/src/hyperdrive/utils.ts
+++ packages/wrangler/src/hyperdrive/utils.ts
@@ -4,5 +4,6 @@ export const getHyperdriveWarningFromEnv = getEnvironmentVariableFactory({
 	variableName: "NO_HYPERDRIVE_WARNING",
 });
 
-export const hyperdriveBetaWarning =
-	"--------------------\n📣 Hyperdrive is currently in open beta\n📣 Please report any bugs to https://github.com/cloudflare/workers-sdk/issues/new/choose\n📣 To give feedback, visit https://discord.gg/cloudflaredev\n--------------------\n";
+export const hyperdriveBetaWarning = process.env.NO_HYPERDRIVE_WARNING
+	? ""
+	: "--------------------\n📣 Hyperdrive is currently in open beta\n📣 Please report any bugs to https://github.com/cloudflare/workers-sdk/issues/new/choose\n📣 To give feedback, visit https://discord.gg/cloudflaredev\n--------------------\n";
