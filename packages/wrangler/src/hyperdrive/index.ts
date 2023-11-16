f180e4976f438779e780c52b6b58ae39cc25e1f5
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2023-11-16 13:56:36 +0100

    make linter happy

--- packages/wrangler/src/hyperdrive/index.ts
+++ packages/wrangler/src/hyperdrive/index.ts
@@ -1,9 +1,9 @@
-import { hyperdriveBetaWarning } from "./utils";
 import { options as createOptions, handler as createHandler } from "./create";
 import { options as deleteOptions, handler as deleteHandler } from "./delete";
 import { options as getOptions, handler as getHandler } from "./get";
 import { options as listOptions, handler as listHandler } from "./list";
 import { options as updateOptions, handler as updateHandler } from "./update";
+import { hyperdriveBetaWarning } from "./utils";
 import type { CommonYargsArgv } from "../yargs-types";
 
 export function hyperdrive(yargs: CommonYargsArgv) {
