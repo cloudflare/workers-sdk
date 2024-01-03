dd2bfa5b011d84c305f1bc6b82d8841061de7b01
Dario Piotrowicz <dario@cloudflare.com>
2024-01-03 12:52:28 +0100

    improve the c3 dependabot workflow
    improve the c3 dependabot testing workflow by
     - externalizing js code inlined in yml files
     - not relying on dependabot PR descriptions which can
       potentially create issues and be unreliability

---
+++ .github/get-c3-dependabot-bumped-framework.cjs
@@ -0,0 +1,26 @@
+const { execSync } = require("child_process");
+
+module.exports = function () {
+	const diff = execSync(
+		"git diff HEAD~2 packages/create-cloudflare/src/frameworks/package.json"
+	).toString();
+
+	const changedPackages = diff
+		.match(/-\s*".*?":\s".*?",?/g)
+		.map((match) => match.match(/-\s*"(.*)":/)?.[1])
+		.filter(Boolean);
+
+	if (changedPackages.length === 0) {
+		console.warn("No changes detected!");
+		return null;
+	} else if (changedPackages.length > 1) {
+		console.warn(
+			`More then one package has changed (${changedPackages.join(
+				", "
+			)}), that's not currently supported`
+		);
+		throw new Error("More than one package bump detected");
+	} else {
+		return changedPackages[0];
+	}
+};
