adad76507b5c0a41efc2b172c75c7b50ce946fcf
Dario Piotrowicz <dario@cloudflare.com>
2024-01-15 17:40:11 +0000

    update package.json scripts

--- packages/create-cloudflare/src/frameworks/next/index.ts
+++ packages/create-cloudflare/src/frameworks/next/index.ts
@@ -166,12 +166,13 @@ const config: FrameworkConfig = {
 		const nextOnPagesScope = isNpmOrBun ? "@cloudflare/" : "";
 		const nextOnPagesCommand = `${nextOnPagesScope}next-on-pages`;
 		const pmCommand = isNpmOrBun ? npx : npm;
-		const pagesDeployCommand = isNpm ? "npm run" : isBun ? "bun" : pmCommand;
+		const pagesBuildRunCommand = `${
+			isNpm ? "npm run" : isBun ? "bun" : pmCommand
+		} pages:build`;
 		return {
 			"pages:build": `${pmCommand} ${nextOnPagesCommand}`,
-			"pages:deploy": `${pagesDeployCommand} pages:build && wrangler pages deploy .vercel/output/static`,
-			"pages:watch": `${pmCommand} ${nextOnPagesCommand} --watch`,
-			"pages:dev": `${pmCommand} wrangler pages dev .vercel/output/static ${await compatDateFlag()} --compatibility-flag=nodejs_compat`,
+			"pages:preview": `${pagesBuildRunCommand} && wrangler pages dev .vercel/output/static ${await compatDateFlag()} --compatibility-flag=nodejs_compat`,
+			"pages:deploy": `${pagesBuildRunCommand} && wrangler pages deploy .vercel/output/static`,
 		};
 	},
 	testFlags: [
