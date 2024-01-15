dec045c9ece6c9aec428e9207047fb1da09af269
Dario Piotrowicz <dario@cloudflare.com>
2024-01-15 14:39:19 +0000

    add link comment referencing next-dev readme

--- packages/create-cloudflare/src/frameworks/next/templates.ts
+++ packages/create-cloudflare/src/frameworks/next/templates.ts
@@ -191,12 +191,15 @@ const nextConfig = {}
 
 module.exports = nextConfig
 
+// Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
+// (when running the application with \`next dev\`), for more information see:
+// https://github.com/dario-piotrowicz/next-on-pages/blob/8e93067/internal-packages/next-dev/README.md
 if (process.env.NODE_ENV === 'development') {
   import('@cloudflare/next-on-pages/next-dev').then(({ setupDevBindings }) => {
       setupDevBindings({
           bindings: {
-              // Add here the Cloudflare Bindings you want to have available during local development (with \`next dev\`)
-              // (for more details on Bindings see: https://developers.cloudflare.com/pages/functions/bindings/)
+              // Add here the Cloudflare Bindings you want to have available during local development,
+              // for more details on Bindings see: https://developers.cloudflare.com/pages/functions/bindings/)
               //
               // KV Example
               // MY_KV: {
