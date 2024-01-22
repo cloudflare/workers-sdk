0a25576d6a1bcce92be96ff4e4940f35725e8a24
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2024-01-22 11:54:53 +0000

    test: avoid mocking undici for kv and r2 local tests
    These tests exercise Miniflare to access the local data stores via requests, which involve Undici.
    Undici and jest-mock-fetch do not work well together,
    resulting in confusing error that are hard to debug.
    
    For example:
    
    ```
    TypeError: Unable to parse input as string or Request
    
          89 |      }
          90 |
        > 91 |      const response = await baseFetch(request, {
             |                             ^
          92 |              dispatcher: requestInit?.dispatcher,
          93 |      });
          94 |      return new Response(response.body, response);
    
          at normalizeRequest (../../node_modules/.pnpm/jest-fetch-mock@3.0.3/node_modules/jest-fetch-mock/src/index.js:129:11)
          at fetch (../miniflare/src/http/fetch.ts:91:25)
    ```
    
    In the long run we should move away from Jest and jest-mock-fetch altogether to make our tests more maintainable.

---
+++ packages/wrangler/src/__tests__/r2.local.test.ts
@@ -0,0 +1,122 @@
+import * as fs from "node:fs";
+import { mockConsoleMethods } from "./helpers/mock-console";
+import { runInTempDir } from "./helpers/run-in-tmp";
+import { runWrangler } from "./helpers/run-wrangler";
+// Miniflare's use of undici doesn't play well with jest-mock-fetch
+// and it is not needed here anyway.
+jest.unmock("undici");
+
+describe("r2", () => {
+	const std = mockConsoleMethods();
+	runInTempDir();
+
+	describe("r2 object", () => {
+		describe("local", () => {
+			it("should put R2 object from local bucket", async () => {
+				await expect(() =>
+					runWrangler(
+						`r2 object get bucketName-object-test/wormhole-img.png --file ./wormhole-img.png --local`
+					)
+				).rejects.toThrowErrorMatchingInlineSnapshot(
+					`"The specified key does not exist."`
+				);
+
+				fs.writeFileSync("wormhole-img.png", "passageway");
+				await runWrangler(
+					`r2 object put bucketName-object-test/wormhole-img.png --file ./wormhole-img.png --local`
+				);
+				expect(std.out).toMatchInlineSnapshot(`
+			"Downloading \\"wormhole-img.png\\" from \\"bucketName-object-test\\".
+
+			[32mIf you think this is a bug then please create an issue at https://github.com/cloudflare/workers-sdk/issues/new/choose[0m
+			Creating object \\"wormhole-img.png\\" in bucket \\"bucketName-object-test\\".
+			Upload complete."
+		`);
+
+				await runWrangler(
+					`r2 object get bucketName-object-test/wormhole-img.png --file ./wormhole-img.png --local`
+				);
+				expect(std.out).toMatchInlineSnapshot(`
+			"Downloading \\"wormhole-img.png\\" from \\"bucketName-object-test\\".
+
+			[32mIf you think this is a bug then please create an issue at https://github.com/cloudflare/workers-sdk/issues/new/choose[0m
+			Creating object \\"wormhole-img.png\\" in bucket \\"bucketName-object-test\\".
+			Upload complete.
+			Downloading \\"wormhole-img.png\\" from \\"bucketName-object-test\\".
+			Download complete."
+		`);
+			});
+
+			it("should delete R2 object from local bucket", async () => {
+				fs.writeFileSync("wormhole-img.png", "passageway");
+				await runWrangler(
+					`r2 object put bucketName-object-test/wormhole-img.png --file ./wormhole-img.png --local`
+				);
+
+				await runWrangler(
+					`r2 object get bucketName-object-test/wormhole-img.png --file ./wormhole-img.png --local`
+				);
+				expect(std.out).toMatchInlineSnapshot(`
+			"Creating object \\"wormhole-img.png\\" in bucket \\"bucketName-object-test\\".
+			Upload complete.
+			Downloading \\"wormhole-img.png\\" from \\"bucketName-object-test\\".
+			Download complete."
+		`);
+
+				await runWrangler(
+					`r2 object delete bucketName-object-test/wormhole-img.png --local`
+				);
+				expect(std.out).toMatchInlineSnapshot(`
+			"Creating object \\"wormhole-img.png\\" in bucket \\"bucketName-object-test\\".
+			Upload complete.
+			Downloading \\"wormhole-img.png\\" from \\"bucketName-object-test\\".
+			Download complete.
+			Deleting object \\"wormhole-img.png\\" from bucket \\"bucketName-object-test\\".
+			Delete complete."
+		`);
+
+				await expect(() =>
+					runWrangler(
+						`r2 object get bucketName-object-test/wormhole-img.png --file ./wormhole-img.png --local`
+					)
+				).rejects.toThrowErrorMatchingInlineSnapshot(
+					`"The specified key does not exist."`
+				);
+			});
+
+			it("should follow persist-to for object bucket", async () => {
+				fs.writeFileSync("wormhole-img.png", "passageway");
+				await runWrangler(
+					`r2 object put bucketName-object-test/file-one --file ./wormhole-img.png --local`
+				);
+
+				await runWrangler(
+					`r2 object put bucketName-object-test/file-two --file ./wormhole-img.png --local --persist-to ./different-dir`
+				);
+
+				await expect(() =>
+					runWrangler(
+						`r2 object get bucketName-object-test/file-one --file ./wormhole-img.png --local --persist-to ./different-dir`
+					)
+				).rejects.toThrowErrorMatchingInlineSnapshot(
+					`"The specified key does not exist."`
+				);
+
+				await runWrangler(
+					`r2 object get bucketName-object-test/file-two --file ./wormhole-img.png --local --persist-to ./different-dir`
+				);
+				expect(std.out).toMatchInlineSnapshot(`
+			"Creating object \\"file-one\\" in bucket \\"bucketName-object-test\\".
+			Upload complete.
+			Creating object \\"file-two\\" in bucket \\"bucketName-object-test\\".
+			Upload complete.
+			Downloading \\"file-one\\" from \\"bucketName-object-test\\".
+
+			[32mIf you think this is a bug then please create an issue at https://github.com/cloudflare/workers-sdk/issues/new/choose[0m
+			Downloading \\"file-two\\" from \\"bucketName-object-test\\".
+			Download complete."
+		`);
+			});
+		});
+	});
+});
