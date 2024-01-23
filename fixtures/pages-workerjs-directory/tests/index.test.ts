033759673a4d88331777c4814243631c2e0a81f5
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2024-01-23 17:05:04 +0000

    test: ensure that temp dirs used in tests do not collide

--- fixtures/pages-workerjs-directory/tests/index.test.ts
+++ fixtures/pages-workerjs-directory/tests/index.test.ts
@@ -1,5 +1,5 @@
 import { execSync } from "node:child_process";
-import { existsSync, readFileSync } from "node:fs";
+import { existsSync, mkdtempSync, readFileSync, realpathSync } from "node:fs";
 import { tmpdir } from "node:os";
 import path, { join, resolve } from "node:path";
 import { fetch } from "undici";
@@ -8,7 +8,9 @@ import { runWranglerPagesDev } from "../../shared/src/run-wrangler-long-lived";
 
 describe("Pages _worker.js/ directory", () => {
 	it("should support non-bundling with 'dev'", async ({ expect }) => {
-		const tmpDir = join(tmpdir(), Math.random().toString(36).slice(2));
+		const tmpDir = realpathSync(
+			mkdtempSync(join(tmpdir(), "worker-directory-tests"))
+		);
 
 		const { ip, port, stop } = await runWranglerPagesDev(
 			resolve(__dirname, ".."),
@@ -67,8 +69,10 @@ describe("Pages _worker.js/ directory", () => {
 	});
 
 	it("should bundle", async ({ expect }) => {
-		const dir = tmpdir();
-		const file = join(dir, "_worker.bundle");
+		const tempDir = realpathSync(
+			mkdtempSync(join(tmpdir(), "worker-bundle-tests"))
+		);
+		const file = join(tempDir, "_worker.bundle");
 
 		execSync(
 			`npx wrangler pages functions build --build-output-directory public --outfile ${file} --bindings="{\\"d1_databases\\":{\\"D1\\":{}}}"`,
