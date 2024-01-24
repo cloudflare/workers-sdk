996cf3555bd794fb9868a6637148cab90ff60989
Phillip Jones <pjones@cloudflare.com>
2024-01-23 17:00:35 -0800

    Added handling for empty/invalid prompt values

--- packages/wrangler/src/r2/sippy.ts
+++ packages/wrangler/src/r2/sippy.ts
@@ -11,6 +11,9 @@ import type {
 } from "../yargs-types";
 import type { SippyPutConfig } from "./helpers";
 
+const NO_SUCH_OBJECT_KEY = 10007;
+const SIPPY_PROVIDER_CHOICES = ["AWS", "GCS"];
+
 export function EnableOptions(yargs: CommonYargsArgv) {
 	return yargs
 		.positional("name", {
@@ -25,7 +28,7 @@ export function EnableOptions(yargs: CommonYargsArgv) {
 			type: "string",
 		})
 		.option("provider", {
-			choices: ["AWS", "GCS"],
+			choices: SIPPY_PROVIDER_CHOICES,
 		})
 		.option("bucket", {
 			description: "The name of the upstream bucket",
@@ -81,9 +84,18 @@ export async function EnableHandler(
 		args.provider ??= await prompt(
 			"Enter the cloud storage provider of your bucket (AWS or GCS):"
 		);
+		if (!args.provider) {
+			throw new UserError("Must specify a cloud storage provider.");
+		}
+		if (!SIPPY_PROVIDER_CHOICES.includes(args.provider)) {
+			throw new UserError("Cloud storage provider must be: AWS or GCS");
+		}
 		args.bucket ??= await prompt(
 			`Enter the name of your ${args.provider} bucket:`
 		);
+		if (!args.bucket) {
+			throw new UserError(`Must specify ${args.provider} bucket name.`);
+		}
 
 		if (args.provider == "AWS") {
 			args.region ??= await prompt(
@@ -92,9 +104,15 @@ export async function EnableHandler(
 			args.keyId ??= await prompt(
 				"Enter your AWS Access Key ID (requires read and list access):"
 			);
+			if (!args.keyId) {
+				throw new UserError("Must specify an AWS Access Key ID.");
+			}
 			args.secretAccessKey ??= await prompt(
 				"Enter your AWS Secret Access Key:"
 			);
+			if (!args.secretAccessKey) {
+				throw new UserError("Must specify an AWS Secret Access Key.");
+			}
 		} else if (args.provider == "GCS") {
 			if (
 				!(args.clientEmail && args.privateKey) &&
@@ -103,12 +121,24 @@ export async function EnableHandler(
 				args.serviceAccountKeyFile = await prompt(
 					"Enter the path to your Google Cloud service account key JSON file:"
 				);
+				if (!args.serviceAccountKeyFile) {
+					throw new UserError(
+						"Must specify the path to a service account key JSON file."
+					);
+				}
 			}
 		}
+
 		args.r2KeyId ??= await prompt(
 			"Enter your R2 Access Key ID (requires read and write access):"
 		);
+		if (!args.r2KeyId) {
+			throw new UserError("Must specify an R2 Access Key ID.");
+		}
 		args.r2SecretAccessKey ??= await prompt("Enter your R2 Secret Access Key:");
+		if (!args.r2SecretAccessKey) {
+			throw new UserError("Must specify an R2 Secret Access Key.");
+		}
 	}
 
 	let sippyConfig = {
@@ -174,7 +204,6 @@ export function GetOptions(yargs: CommonYargsArgv) {
 		});
 }
 
-const NO_SUCH_OBJECT_KEY = 10007;
 export async function GetHandler(
 	args: StrictYargsOptionsToInterface<typeof GetOptions>
 ) {
