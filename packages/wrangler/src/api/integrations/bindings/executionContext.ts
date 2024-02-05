37013025582124818e4b67b25b8c10b76055d3bd
Dario Piotrowicz <dario@cloudflare.com>
2024-02-05 10:11:34 +0000

    add ctx to getBindingsProxy

---
+++ packages/wrangler/src/api/integrations/bindings/executionContext.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// waitUntil: (promise: Promise<any>) => void;
+// passThroughOnException: () => void;
+// };
+
+export class ExecutionContext {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any, unused-imports/no-unused-vars
+	waitUntil(promise: Promise<any>): void {}
+	passThroughOnException(): void {}
+}
