06eee310624ad6cd129c2b68623ec6898a49f283
Dario Piotrowicz <dario@cloudflare.com>
2024-02-05 13:41:02 +0000

    Update packages/wrangler/src/api/integrations/bindings/executionContext.ts
    Co-authored-by: MrBBot <bcoll@cloudflare.com>

--- packages/wrangler/src/api/integrations/bindings/executionContext.ts
+++ packages/wrangler/src/api/integrations/bindings/executionContext.ts
@@ -1,7 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-// waitUntil: (promise: Promise<any>) => void;
-// passThroughOnException: () => void;
-// };
 
 export class ExecutionContext {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any, unused-imports/no-unused-vars
