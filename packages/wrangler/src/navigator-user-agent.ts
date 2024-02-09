f92a8fbb2257c8ee0b034202a3d4a11d8d4cdf98
bcoll <bcoll@cloudflare.com>
2024-02-09 10:35:40 +0000

    fix: ensure conflicting compatibility flags reported as user error

--- packages/wrangler/src/navigator-user-agent.ts
+++ packages/wrangler/src/navigator-user-agent.ts
@@ -1,16 +1,16 @@
-import assert from "node:assert";
+import { UserError } from "./errors";
 
 export function isNavigatorDefined(
 	compatibility_date: string | undefined,
 	compatibility_flags: string[] = []
 ) {
-	assert(
-		!(
-			compatibility_flags.includes("global_navigator") &&
-			compatibility_flags.includes("no_global_navigator")
-		),
-		"Can't both enable and disable a flag"
-	);
+	if (
+		compatibility_flags.includes("global_navigator") &&
+		compatibility_flags.includes("no_global_navigator")
+	) {
+		throw new UserError("Can't both enable and disable a flag");
+	}
+
 	if (compatibility_flags.includes("global_navigator")) {
 		return true;
 	}
