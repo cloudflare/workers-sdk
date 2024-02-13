d00d60f001416728bf2c1bccd75d858d586d0fb8
Dario Piotrowicz <dario@cloudflare.com>
2024-02-13 14:53:48 +0000

    update env.d.ts

--- packages/create-cloudflare/templates/next/env.d.ts
+++ packages/create-cloudflare/templates/next/env.d.ts
@@ -1,13 +1,7 @@
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      // Add here the Cloudflare Bindings you want to have available in your application
-      // (for more details on Bindings see: https://developers.cloudflare.com/pages/functions/bindings/)
-      //
-      // KV Example:
-      // MY_KV: KVNamespace
-    }
-  }
+interface CloudflareEnv extends Env {
+  // Add here the Cloudflare Bindings you want to have available in your application
+  // (for more details on Bindings see: https://developers.cloudflare.com/pages/functions/bindings/)
+  //
+  // KV Example:
+  // MY_KV: KVNamespace
 }
-
-export {}
