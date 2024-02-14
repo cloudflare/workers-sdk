1f0f2276cbfda5cde912c72a2d8784fd55aa959d
Dario Piotrowicz <dario@cloudflare.com>
2024-02-14 20:57:36 +0000

    fixup! update env.d.ts

--- packages/create-cloudflare/templates/next/env.d.ts
+++ packages/create-cloudflare/templates/next/env.d.ts
@@ -1,4 +1,4 @@
-interface CloudflareEnv extends Env {
+interface CloudflareEnv {
   // Add here the Cloudflare Bindings you want to have available in your application
   // (for more details on Bindings see: https://developers.cloudflare.com/pages/functions/bindings/)
   //
