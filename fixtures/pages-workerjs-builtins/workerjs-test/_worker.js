a20df383645845f12fd2df047e91721df826cb93
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2024-02-19 20:28:56 +0000

    fixup! Avoid blocking built-in node modules when using --no-bundle flag

--- fixtures/pages-workerjs-builtins/workerjs-test/_worker.js
+++
@@ -1,9 +0,0 @@
-import { Buffer } from "node:buffer";
-import { EmailMessage } from "cloudflare:email";
-
-export default {
-	async fetch() {
-		console.log("EmailMessage", EmailMessage);
-		return new Response(Buffer.from("test", "utf8"));
-	},
-};
