034db8855947c668e3e566c97d1a7814b4a63f8c
Samuel Macleod <smacleod@cloudflare.com>
2024-02-20 11:36:26 +0000

    Format

--- packages/workers-playground/functions/playground/api/[[route]].ts
+++ packages/workers-playground/functions/playground/api/[[route]].ts
@@ -1,4 +1,10 @@
 export function onRequest({ request }) {
-  const url = new URL(request.url)
-  return fetch(new URL(url.pathname.split("/playground")[1], `https://playground-testing.devprod.cloudflare.dev`), request)
+	const url = new URL(request.url);
+	return fetch(
+		new URL(
+			url.pathname.split("/playground")[1],
+			`https://playground-testing.devprod.cloudflare.dev`
+		),
+		request
+	);
 }
