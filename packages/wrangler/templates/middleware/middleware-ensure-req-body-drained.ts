abe0d4579a3fce8f4c563c8a45e6d5802062f7e5
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2024-02-28 12:38:14 +0000

    fixup! ensure request.body is drained

--- packages/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts
+++ packages/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts
@@ -4,9 +4,13 @@ const drainBody: Middleware = async (request, env, _ctx, middlewareCtx) => {
 	try {
 		return await middlewareCtx.next(request, env);
 	} finally {
-		if (request.body !== null && !request.bodyUsed) {
-			const reader = request.body.getReader();
-			while (!(await reader.read()).done) {}
+		try {
+			if (request.body !== null && !request.bodyUsed) {
+				const reader = request.body.getReader();
+				while (!(await reader.read()).done) {}
+			}
+		} catch (e) {
+			console.error("Failed to drain the unused request body.", e);
 		}
 	}
 };
