95154918beace911620294180dafa425c66cf5a2
Samuel Macleod <smacleod@cloudflare.com>
2024-02-29 12:44:51 +0000

    Add explanatory comments to pages functions

--- packages/workers-playground/functions/playground/api/[[route]].ts
+++ packages/workers-playground/functions/playground/api/[[route]].ts
@@ -1,3 +1,11 @@
+/**
+ * These functions are purely for local development, and when running a Pages preview. In production, requests go through the following pipeline:
+
+ * eyeball -> playground-preview-worker -> workers-playground
+ * However, locally, and in a Pages preview, requests go through this pipeline:
+
+ * eyeball ->  workers-playground (local or preview) -> playground-preview-worker (staging)
+ */
 export function onRequest({ request }) {
 	const url = new URL(request.url);
 	return fetch(
