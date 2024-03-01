4c7dabbec9d97de1cca0cc4158ec89a131a34777
Dario Piotrowicz <dario@cloudflare.com>
2024-03-01 12:12:43 +0000

    [c3] fix: update solid template to write app.config.(js/ts)

---
+++ packages/create-cloudflare/templates/solid/js/app.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "@solidjs/start/config";
+
+export default defineConfig({
+    server: {
+        preset: "cloudflare-pages",
+        rollupConfig: {
+            external: ["node:async_hooks"]
+        },
+    }
+});
