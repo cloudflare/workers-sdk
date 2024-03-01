fb7ff7062000d0e1ed2b0dffe983a7dfa2ad8c90
Dario Piotrowicz <dario@cloudflare.com>
2024-03-01 12:34:01 +0000

    use codemod instead of overriding the file

--- packages/create-cloudflare/templates/solid/c3.ts
+++ packages/create-cloudflare/templates/solid/c3.ts
@@ -1,7 +1,10 @@
-import { logRaw } from "@cloudflare/cli";
+import { logRaw, updateStatus } from "@cloudflare/cli";
+import { blue } from "@cloudflare/cli/colors";
+import { transformFile } from "helpers/codemod";
 import { runFrameworkGenerator } from "helpers/command";
-import { compatDateFlag } from "helpers/files";
+import { compatDateFlag, usesTypescript } from "helpers/files";
 import { detectPackageManager } from "helpers/packages";
+import * as recast from "recast";
 import type { TemplateConfig } from "../../src/templates";
 import type { C3Context } from "types";
 
@@ -14,18 +17,55 @@ const generate = async (ctx: C3Context) => {
 	logRaw("");
 };
 
+const configure = async (ctx: C3Context) => {
+	usesTypescript(ctx);
+	const filePath = `app.config.${usesTypescript(ctx) ? "ts" : "js"}`;
+
+	updateStatus(`Updating configuration in ${blue(filePath)}`);
+
+	transformFile(filePath, {
+		visitCallExpression: function (n) {
+			const callee = n.node.callee as recast.types.namedTypes.Identifier;
+			if (callee.name !== "defineConfig") {
+				return this.traverse(n);
+			}
+
+			const b = recast.types.builders;
+			n.node.arguments = [
+				b.objectExpression([
+					b.objectProperty(
+						b.identifier("server"),
+						b.objectExpression([
+							b.objectProperty(
+								b.identifier("preset"),
+								b.stringLiteral("cloudflare-pages")
+							),
+							b.objectProperty(
+								b.identifier("rollupConfig"),
+								b.objectExpression([
+									b.objectProperty(
+										b.identifier("external"),
+										b.arrayExpression([b.stringLiteral("node:async_hooks")])
+									),
+								])
+							),
+						])
+					),
+				]),
+			];
+
+			return false;
+		},
+	});
+};
+
 const config: TemplateConfig = {
 	configVersion: 1,
 	id: "solid",
 	displayName: "Solid",
 	platform: "pages",
-	copyFiles: {
-		variants: {
-			js: { path: "./js" },
-			ts: { path: "./ts" },
-		},
-	},
 	generate,
+	configure,
 	transformPackageJson: async () => ({
 		scripts: {
 			"pages:preview": `${npm} run build && npx wrangler pages dev dist ${await compatDateFlag()} --compatibility-flag nodejs_compat`,
