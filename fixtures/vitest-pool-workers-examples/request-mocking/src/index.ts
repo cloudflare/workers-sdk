1a8a5fb065d0d1f3ac6348ac8633fcab11932970
bcoll <bcoll@cloudflare.com>
2024-03-04 15:13:20 +0000

    test: add request mocking `vitest-pool-workers` example

---
+++ fixtures/vitest-pool-workers-examples/request-mocking/src/index.ts
@@ -0,0 +1,11 @@
+export default <ExportedHandler>{
+	async fetch(request, env, ctx) {
+		const url = new URL(request.url);
+		url.host = "cloudflare.com";
+		try {
+			return await fetch(url, request);
+		} catch (e) {
+			return new Response(String(e), { status: 500 });
+		}
+	},
+};
