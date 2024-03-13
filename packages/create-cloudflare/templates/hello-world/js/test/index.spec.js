c721a9a202b39e95e5c7bfd784478790c906b4ae
Adam Murray <amurray@cloudflare.com>
2024-03-13 16:37:52 -0500

    update tests with comments

--- packages/create-cloudflare/templates/hello-world/js/test/index.spec.js
+++ packages/create-cloudflare/templates/hello-world/js/test/index.spec.js
@@ -3,15 +3,17 @@ import { describe, it, expect } from "vitest";
 import worker from "../src";
 
 describe("Hello World worker", () => {
-  it("displays Hello World! (unit style)", async () => {
+  it("responds with Hello World! (unit style)", async () => {
     const request = new Request("http://example.com");
+    //create an empty context to pass to `worker.fetch()`.
     const ctx = createExecutionContext();
     const response = await worker.fetch(request, env, ctx);
+    // wait for all Promises passed to ctx.waitUntil() to settle before running test assertions
     await waitOnExecutionContext(ctx);
     expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
   });
 
-  it("displays Hello World! (integration style)", async () => {
+  it("responds with Hello World! (integration style)", async () => {
    const response = await SELF.fetch(request, env, ctx);
    expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
  });
--- packages/create-cloudflare/templates/hello-world/ts/test/index.spec.ts
+++ packages/create-cloudflare/templates/hello-world/ts/test/index.spec.ts
@@ -8,15 +8,17 @@ import worker from "../src/index";
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
 describe("Hello World worker", () => {
-  it("displays Hello World! (unit style)", async () => {
+  it("responds with Hello World! (unit style)", async () => {
     const request = new IncomingRequest("http://example.com");
+    //create an empty context to pass to `worker.fetch()`.
     const ctx = createExecutionContext();
     const response = await worker.fetch(request, env, ctx);
+    // wait for all Promises passed to ctx.waitUntil() to settle before running test assertions
     await waitOnExecutionContext(ctx);
     expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
   });
 
-  it("displays Hello World! (integration style)", async () => {
+  it("responds with Hello World! (integration style)", async () => {
    const response = await SELF.fetch("https://example.com");
    expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
  });
