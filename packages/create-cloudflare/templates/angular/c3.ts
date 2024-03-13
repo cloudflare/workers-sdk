2f1d353abac2d6a867726f64aa435eb7b2db7ef5
Nuno Arruda <nuno@nunoarruda.com>
2024-03-13 18:45:50 +0000

    Delete @types/express as well

--- packages/create-cloudflare/templates/angular/c3.ts
+++ packages/create-cloudflare/templates/angular/c3.ts
@@ -55,6 +55,7 @@ async function updateAppCode() {
 
 	delete packageManifest["dependencies"]["@angular/ssr"];
 	delete packageManifest["dependencies"]["express"];
+	delete packageManifest["devDependencies"]["@types/express"];
 
 	writeFile(packageJsonPath, JSON.stringify(packageManifest, null, 2));
 	s.stop(`${brandColor(`updated`)} ${dim(`\`package.json\``)}`);
