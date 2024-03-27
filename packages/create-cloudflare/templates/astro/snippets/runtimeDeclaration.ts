1a9f55e82d0956d8046ca042dad176745290439f
Dario Piotrowicz <dario@cloudflare.com>
2024-03-27 17:05:36 +0100

    rename AdvancedRuntime to just Runtime

--- packages/create-cloudflare/templates/astro/snippets/runtimeDeclaration.ts
+++ packages/create-cloudflare/templates/astro/snippets/runtimeDeclaration.ts
@@ -1,4 +1,4 @@
-type Runtime = import("@astrojs/cloudflare").AdvancedRuntime<Env>;
+type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 
 declare namespace App {
 	interface Locals extends Runtime {}
