bab6f124fc8db6be70945e1679531e6ee320619c
bcoll <bcoll@cloudflare.com>
2024-03-31 19:04:29 +0100

    refactor: improve debuging for a couple internal errors

--- packages/vitest-pool-workers/src/worker/lib/node/module.ts
+++ packages/vitest-pool-workers/src/worker/lib/node/module.ts
@@ -2,9 +2,17 @@
 export const builtinModules: string[] = [];
 
 // https://nodejs.org/api/module.html#modulecreaterequirefilename
-export function createRequire(_filename: string) {
-	return () => {
-		throw new Error("require() is not yet implemented in Workers");
+export function createRequire(filename: string) {
+	return (specifier: string) => {
+		const quotedSpecifier = JSON.stringify(specifier);
+		const quotedFilename = JSON.stringify(filename);
+		const message = `Attempted to \`require(${quotedSpecifier})\` from ${quotedFilename}`;
+		// Vitest can swallow stack traces making this error tricky to debug.
+		// To get around this, include a stack trace in the error message itself.
+		const stack = (new Error(message).stack ?? "").replace("Error: ", "");
+		throw new Error(
+			`\`require()\` is not yet implemented in Workers.\n${stack}`
+		);
 	};
 }
 
