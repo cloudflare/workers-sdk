c1c888e72cd4e06cb9f21b34d31b0377953e8304
James Culveyhouse <jculveyhouse@cloudflare.com>
2024-04-04 10:24:49 -0500

    Add codemod for server handler

--- packages/create-cloudflare/templates/analog/c3.ts
+++ packages/create-cloudflare/templates/analog/c3.ts
@@ -1,4 +1,4 @@
-import { logRaw } from "@cloudflare/cli";
+import { crash, logRaw } from "@cloudflare/cli";
 import { brandColor, dim } from "@cloudflare/cli/colors";
 import { spinner } from "@cloudflare/cli/interactive";
 import { runFrameworkGenerator } from "frameworks/index";
@@ -18,20 +18,28 @@ const generate = async (ctx: C3Context) => {
 };
 
 const configure = async () => {
-	const packages = ["nitro-cloudflare-dev"];
+	// const packages = ["nitro-cloudflare-dev"];
 
 	// When using pnpm, explicitly add h3 package so the H3Event type declaration can be updated.
 	// Package managers other than pnpm will hoist the dependency, as will pnpm with `--shamefully-hoist`
 	if (pm === "pnpm") {
-		packages.push("h3");
+		// packages.push("h3");
+
+		await installPackages(["h3"], {
+			dev: true,
+			startText: "Installing `h3`",
+			doneText: `${brandColor("installed")} ${dim(`via \`${npm} install\``)}`,
+		});
 	}
 
-	await installPackages(packages, {
-		dev: true,
-		startText: "Installing nitro module `nitro-cloudflare-dev`",
-		doneText: `${brandColor("installed")} ${dim(`via \`${npm} install\``)}`,
-	});
+	// await installPackages(packages, {
+	// 	dev: true,
+	// 	startText: "Installing nitro module `nitro-cloudflare-dev`",
+	// 	doneText: `${brandColor("installed")} ${dim(`via \`${npm} install\``)}`,
+	// });
+
 	updateViteConfig();
+	updateMainServer();
 };
 
 const updateViteConfig = () => {
@@ -65,15 +73,135 @@ const updateViteConfig = () => {
 								),
 							])
 						),
-						b.objectProperty(
-							b.identifier("modules"),
-							b.arrayExpression([b.stringLiteral("nitro-cloudflare-dev")])
-						),
+						// b.objectProperty(
+						// 	b.identifier("modules"),
+						// 	b.arrayExpression([b.stringLiteral("nitro-cloudflare-dev")])
+						// ),
 					])
 				);
 
 				n.node.arguments = [b.objectExpression([pluginArguments])];
 			}
+
+			return this.traverse(n);
+		},
+	});
+
+	s.stop(`${brandColor(`updated`)} ${dim(`\`${configFile}\``)}`);
+};
+
+const updateMainServer = () => {
+	const b = recast.types.builders;
+	const s = spinner();
+
+	const configFile = "src/main.server.ts";
+	s.start(`Updating \`${configFile}\``);
+	transformFile(configFile, {
+		visitProgram(n) {
+			const baseUrlImport = b.importDeclaration(
+				[b.importSpecifier(b.identifier("APP_BASE_HREF"))],
+				b.stringLiteral("@angular/common"),
+				"value"
+			);
+
+			const lastImportIndex = n.node.body.findLastIndex(
+				(t) => t.type === "ImportDeclaration"
+			);
+			const lastImport = n.get("body", lastImportIndex);
+			lastImport.insertAfter(baseUrlImport);
+
+			return this.traverse(n);
+		},
+		visitFunctionDeclaration(n) {
+			if (n.node.id?.name !== "render") {
+				return this.traverse(n);
+			}
+
+			const baseHrefDeclaration = b.variableDeclaration("const", [
+				b.variableDeclarator(
+					b.identifier("baseHref"),
+					b.logicalExpression(
+						"??",
+						b.memberExpression(
+							b.memberExpression(b.identifier("process"), b.identifier("env")),
+							b.stringLiteral("CF_PAGES_URL"),
+							true
+						),
+						b.stringLiteral("http://localhost:8788")
+					)
+				),
+			]);
+			baseHrefDeclaration.comments = [b.commentLine("set the base href")];
+
+			n.get("body", "body").unshift(baseHrefDeclaration);
+
+			return this.traverse(n);
+		},
+		visitCallExpression(n) {
+			const name = n.node.callee;
+			// Only modify the call to `renderApplication`
+			if (name.type !== "Identifier" || name.name !== "renderApplication") {
+				return this.traverse(n);
+			}
+
+			// The second argument to `renderApplication` is a config object we need to update
+			const objectArg = n.node.arguments[1];
+			if (objectArg.type !== "ObjectExpression") {
+				crash(
+					`Found unexpected argument type when modifying ${configFile}. Expected an object as second parameter to "renderApplication"`
+				);
+			}
+
+			// Add a comment
+			n.parent.parent.parent.node.comments = [
+				b.commentLine("Use the full URL and provide the APP_BASE_HREF"),
+			];
+
+			// Add a "platformProviders" entry to the config object
+			objectArg.properties.push(
+				b.objectProperty(
+					b.identifier("platformProviders"),
+					b.arrayExpression([
+						b.objectExpression([
+							b.objectProperty(
+								b.identifier("provide"),
+								b.identifier("APP_BASE_HREF")
+							),
+							b.objectProperty(
+								b.identifier("useValue"),
+								b.identifier("baseHref")
+							),
+						]),
+					])
+				)
+			);
+
+			// Lookup the url property in the object. Crash if not found
+			const urlIndex = objectArg.properties.findIndex((prop) => {
+				if (prop.type !== "ObjectProperty" || prop.key.type !== "Identifier") {
+					return false;
+				}
+				return prop.key.name === "url";
+			});
+
+			if (!urlIndex) {
+				crash("Failed to update `url` property of `renderApplication`");
+			}
+			const urlProp = n.get("arguments", 1, "properties", urlIndex);
+
+			urlProp.replace(
+				b.objectProperty(
+					b.identifier("url"),
+					b.templateLiteral(
+						[
+							b.templateElement({ raw: "", cooked: "" }, false),
+							b.templateElement({ raw: "", cooked: "" }, false),
+							b.templateElement({ raw: "", cooked: "" }, true),
+						],
+						[b.identifier("baseHref"), b.identifier("url")]
+					)
+				)
+			);
 
 			return this.traverse(n);
 		},
