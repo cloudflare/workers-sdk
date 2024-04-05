5247e1429b4b5be7c8cdefb3d96b2b75cc925a8a
Samuel Macleod <smacleod@cloudflare.com>
2024-04-05 13:49:19 +0100

    Write build-env to file

--- packages/wrangler/src/__tests__/pages/pages-build-env.test.ts
+++ packages/wrangler/src/__tests__/pages/pages-build-env.test.ts
@@ -1,5 +1,5 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
-import { writeFileSync } from "node:fs";
+import { readFileSync, writeFileSync } from "node:fs";
 import { mockConsoleMethods } from "../helpers/mock-console";
 import { runInTempDir } from "../helpers/run-in-tmp";
 import { runWrangler } from "../helpers/run-wrangler";
@@ -22,24 +22,31 @@ describe("pages build env", () => {
 			pages_build_output_dir: "./dist",
 			vars: {},
 		});
-		await runWrangler("pages functions build-env .");
-		expect(std).toMatchInlineSnapshot(`
-		Object {
-		  "debug": "",
-		  "err": "",
-		  "info": "",
-		  "out": "{\\"vars\\":{},\\"pages_build_output_dir\\":\\"dist\\"}",
-		  "warn": "",
-		}
-	`);
+		await runWrangler("pages functions build-env . --outfile data.json");
+		expect(std.out).toMatchInlineSnapshot(`""`);
+		expect(readFileSync("data.json", "utf8")).toMatchInlineSnapshot(
+			`"{\\"vars\\":{},\\"pages_build_output_dir\\":\\"dist\\"}"`
+		);
 	});
 
 	it("should fail with no config file", async () => {
 		await expect(
-			runWrangler("pages functions build-env .")
+			runWrangler("pages functions build-env . --outfile data.json")
 		).rejects.toThrowErrorMatchingInlineSnapshot(
 			`"No Pages config file found"`
 		);
+	});
+	it("should fail with no project dir", async () => {
+		await expect(
+			runWrangler("pages functions build-env")
+		).rejects.toThrowErrorMatchingInlineSnapshot(
+			`"No Pages project location specified"`
+		);
+	});
+	it("should fail with no outfile", async () => {
+		await expect(
+			runWrangler("pages functions build-env .")
+		).rejects.toThrowErrorMatchingInlineSnapshot(`"No outfile specified"`);
 	});
 	it("should fail correctly with a non-pages config file", async () => {
 		writeWranglerToml({
@@ -69,7 +76,7 @@ describe("pages build env", () => {
 		});
 		// This error is specifically handled by the caller of build-env
 		await expect(
-			runWrangler("pages functions build-env .")
+			runWrangler("pages functions build-env . --outfile data.json")
 		).rejects.toThrowErrorMatchingInlineSnapshot(
 			`"Your wrangler.toml is not a valid Pages config file"`
 		);
@@ -78,7 +85,7 @@ describe("pages build env", () => {
 		writeFileSync("./wrangler.toml", 'INVALID "FILE');
 		// This error is specifically handled by the caller of build-env
 		await expect(
-			runWrangler("pages functions build-env .")
+			runWrangler("pages functions build-env . --outfile data.json")
 		).rejects.toThrowErrorMatchingInlineSnapshot(
 			`"Your wrangler.toml is not a valid Pages config file"`
 		);
@@ -112,7 +119,7 @@ describe("pages build env", () => {
 		});
 		// This error is specifically handled by the caller of build-env
 		await expect(
-			runWrangler("pages functions build-env .")
+			runWrangler("pages functions build-env . --outfile data.json")
 		).rejects.toThrowErrorMatchingInlineSnapshot(
 			`"Your wrangler.toml is not a valid Pages config file"`
 		);
@@ -146,16 +153,11 @@ describe("pages build env", () => {
 				},
 			},
 		});
-		await runWrangler("pages functions build-env .");
-		expect(std).toMatchInlineSnapshot(`
-		Object {
-		  "debug": "",
-		  "err": "",
-		  "info": "",
-		  "out": "{\\"vars\\":{\\"VAR1\\":\\"VALUE1\\",\\"VAR2\\":\\"VALUE2\\"},\\"pages_build_output_dir\\":\\"dist\\"}",
-		  "warn": "",
-		}
-	`);
+		await runWrangler("pages functions build-env . --outfile data.json");
+		expect(std.out).toMatchInlineSnapshot(`""`);
+		expect(readFileSync("data.json", "utf8")).toMatchInlineSnapshot(
+			`"{\\"vars\\":{\\"VAR1\\":\\"VALUE1\\",\\"VAR2\\":\\"VALUE2\\"},\\"pages_build_output_dir\\":\\"dist\\"}"`
+		);
 	});
 	it("should return production", async () => {
 		process.env.PAGES_ENVIRONMENT = "production";
@@ -185,16 +187,11 @@ describe("pages build env", () => {
 				},
 			},
 		});
-		await runWrangler("pages functions build-env .");
-		expect(std).toMatchInlineSnapshot(`
-		Object {
-		  "debug": "",
-		  "err": "",
-		  "info": "",
-		  "out": "{\\"vars\\":{\\"VAR1\\":\\"PROD_VALUE1\\",\\"VAR2\\":\\"PROD_VALUE2\\",\\"PROD_VAR3\\":\\"PROD_VALUE3\\"},\\"pages_build_output_dir\\":\\"dist\\"}",
-		  "warn": "",
-		}
-	`);
+		await runWrangler("pages functions build-env . --outfile data.json");
+		expect(std.out).toMatchInlineSnapshot(`""`);
+		expect(readFileSync("data.json", "utf8")).toMatchInlineSnapshot(
+			`"{\\"vars\\":{\\"VAR1\\":\\"PROD_VALUE1\\",\\"VAR2\\":\\"PROD_VALUE2\\",\\"PROD_VAR3\\":\\"PROD_VALUE3\\"},\\"pages_build_output_dir\\":\\"dist\\"}"`
+		);
 	});
 
 	it("should return preview", async () => {
@@ -225,15 +222,10 @@ describe("pages build env", () => {
 				},
 			},
 		});
-		await runWrangler("pages functions build-env .");
-		expect(std).toMatchInlineSnapshot(`
-		Object {
-		  "debug": "",
-		  "err": "",
-		  "info": "",
-		  "out": "{\\"vars\\":{\\"VAR1\\":\\"PREVIEW_VALUE1\\",\\"VAR2\\":\\"PREVIEW_VALUE2\\",\\"PREVIEW_VAR3\\":\\"PREVIEW_VALUE3\\"},\\"pages_build_output_dir\\":\\"dist\\"}",
-		  "warn": "",
-		}
-	`);
+		await runWrangler("pages functions build-env . --outfile data.json");
+		expect(std.out).toMatchInlineSnapshot(`""`);
+		expect(readFileSync("data.json", "utf8")).toMatchInlineSnapshot(
+			`"{\\"vars\\":{\\"VAR1\\":\\"PREVIEW_VALUE1\\",\\"VAR2\\":\\"PREVIEW_VALUE2\\",\\"PREVIEW_VAR3\\":\\"PREVIEW_VALUE3\\"},\\"pages_build_output_dir\\":\\"dist\\"}"`
+		);
 	});
 });
--- packages/wrangler/src/pages/build-env.ts
+++ packages/wrangler/src/pages/build-env.ts
@@ -1,8 +1,7 @@
-import { existsSync } from "node:fs";
+import { existsSync, writeFileSync } from "node:fs";
 import path from "node:path";
 import { readConfig } from "../config";
 import { FatalError } from "../errors";
-import { logger } from "../logger";
 import { EXIT_CODE_NO_CONFIG_FOUND } from "./errors";
 import type {
 	CommonYargsArgv,
@@ -12,16 +11,27 @@ import type {
 export type PagesBuildEnvArgs = StrictYargsOptionsToInterface<typeof Options>;
 
 export function Options(yargs: CommonYargsArgv) {
-	return yargs.positional("projectDir", {
-		type: "string",
-		description: "The location of the Pages project",
-	});
+	return yargs
+		.positional("projectDir", {
+			type: "string",
+			description: "The location of the Pages project",
+		})
+		.options({
+			outfile: {
+				type: "string",
+				description: "The location to write the build environment file",
+			},
+		});
 }
 
 export const Handler = async (args: PagesBuildEnvArgs) => {
 	if (!args.projectDir) {
 		throw new FatalError("No Pages project location specified");
 	}
+	if (!args.outfile) {
+		throw new FatalError("No outfile specified");
+	}
+
 	const configPath = path.resolve(args.projectDir, "wrangler.toml");
 	// Fail early if the config file doesn't exist
 	if (!existsSync(configPath)) {
@@ -46,7 +56,8 @@ export const Handler = async (args: PagesBuildEnvArgs) => {
 		Object.entries(config.vars).filter(([_, v]) => typeof v === "string")
 	);
 
-	logger.log(
+	writeFileSync(
+		args.outfile,
 		JSON.stringify({
 			vars: textVars,
 			pages_build_output_dir: path.relative(
