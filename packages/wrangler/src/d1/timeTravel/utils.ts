eda58688bbc0337ae81677cc6a03f8fc51f98220
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2024-04-08 10:20:53 +0200

    chore: refactor time-travel compatibility check

--- packages/wrangler/src/d1/timeTravel/utils.ts
+++ packages/wrangler/src/d1/timeTravel/utils.ts
@@ -37,9 +37,9 @@ export const checkIfDatabaseIsExperimental = async (
 	databaseId: string
 ): Promise<void> => {
 	const dbInfo = await getDatabaseInfoFromId(accountId, databaseId);
-	if (dbInfo.version !== "beta") {
+	if (dbInfo.version === "alpha") {
 		throw new UserError(
-			"Time travel is only available for D1 databases created with the --experimental-backend flag"
+			"Time travel is not available for alpha D1 databases. You will need to migrate to a new database for access to this feature."
 		);
 	}
 };
