586f4defded133e35be45a93f77e8d15ce2f5d54
James Culveyhouse <jculveyhouse@cloudflare.com>
2024-04-12 08:46:53 -0500

    Update to analog@1.2.0

--- packages/create-cloudflare/templates/analog/c3.ts
+++ packages/create-cloudflare/templates/analog/c3.ts
@@ -1,4 +1,4 @@
-import { crash, logRaw } from "@cloudflare/cli";
+import { logRaw } from "@cloudflare/cli";
 import { brandColor, dim } from "@cloudflare/cli/colors";
 import { spinner } from "@cloudflare/cli/interactive";
 import { runFrameworkGenerator } from "frameworks/index";
@@ -40,7 +40,6 @@ const configure = async (ctx: C3Context) => {
 	}
 
 	updateViteConfig(ctx);
-	updateMainServer();
 	updateEnvTypes(ctx);
 };
 
@@ -95,19 +94,6 @@ const updateViteConfig = (ctx: C3Context) => {
 							b.stringLiteral("cloudflare-pages")
 						),
 						b.objectProperty(
-							b.identifier("output"),
-							b.objectExpression([
-								b.objectProperty(
-									b.identifier("dir"),
-									b.stringLiteral("./dist/analog/public")
-								),
-								b.objectProperty(
-									b.identifier("serverDir"),
-									b.stringLiteral("./dist/analog/public/_worker.js")
-								),
-							])
-						),
-						b.objectProperty(
 							b.identifier("modules"),
 							b.arrayExpression([b.identifier("devBindingsModule")])
 						),
@@ -124,126 +110,6 @@ const updateViteConfig = (ctx: C3Context) => {
 	s.stop(`${brandColor(`updated`)} ${dim(`\`${configFile}\``)}`);
 };
 
-const updateMainServer = () => {
-	const b = recast.types.builders;
-	const s = spinner();
-
-	const serverFile = "src/main.server.ts";
-	s.start(`Updating \`${serverFile}\``);
-	transformFile(serverFile, {
-		visitProgram(n) {
-			const baseUrlImport = b.importDeclaration(
-				[b.importSpecifier(b.identifier("APP_BASE_HREF"))],
-				b.stringLiteral("@angular/common"),
-				"value"
-			);
-
-			const lastImportIndex = n.node.body.findLastIndex(
-				(t) => t.type === "ImportDeclaration"
-			);
-			const lastImport = n.get("body", lastImportIndex);
-			lastImport.insertAfter(baseUrlImport);
-
-			return this.traverse(n);
-		},
-		visitFunctionDeclaration(n) {
-			if (n.node.id?.name !== "render") {
-				return this.traverse(n);
-			}
-
-			const baseHrefDeclaration = b.variableDeclaration("const", [
-				b.variableDeclarator(
-					b.identifier("baseHref"),
-					b.logicalExpression(
-						"??",
-						b.memberExpression(
-							b.memberExpression(b.identifier("process"), b.identifier("env")),
-							b.stringLiteral("CF_PAGES_URL"),
-							true
-						),
-						b.stringLiteral("http://localhost:8788")
-					)
-				),
-			]);
-			baseHrefDeclaration.comments = [b.commentLine("set the base href")];
-
-			n.get("body", "body").unshift(baseHrefDeclaration);
-
-			return this.traverse(n);
-		},
-		visitCallExpression(n) {
-			const name = n.node.callee;
-			// Only modify the call to `renderApplication`
-			if (name.type !== "Identifier" || name.name !== "renderApplication") {
-				return this.traverse(n);
-			}
-
-			// The second argument to `renderApplication` is a config object we need to update
-			const objectArg = n.node.arguments[1];
-			if (objectArg.type !== "ObjectExpression") {
-				crash(
-					`Found unexpected argument type when modifying ${serverFile}. Expected an object as second parameter to "renderApplication"`
-				);
-			}
-
-			// Add a comment
-			n.parent.parent.parent.node.comments = [
-				b.commentLine("Use the full URL and provide the APP_BASE_HREF"),
-			];
-
-			// Add a "platformProviders" entry to the config object
-			objectArg.properties.push(
-				b.objectProperty(
-					b.identifier("platformProviders"),
-					b.arrayExpression([
-						b.objectExpression([
-							b.objectProperty(
-								b.identifier("provide"),
-								b.identifier("APP_BASE_HREF")
-							),
-							b.objectProperty(
-								b.identifier("useValue"),
-								b.identifier("baseHref")
-							),
-						]),
-					])
-				)
-			);
-
-			// Lookup the url property in the object. Crash if not found
-			const urlIndex = objectArg.properties.findIndex((prop) => {
-				if (prop.type !== "ObjectProperty" || prop.key.type !== "Identifier") {
-					return false;
-				}
-				return prop.key.name === "url";
-			});
-
-			if (!urlIndex) {
-				crash("Failed to update `url` property of `renderApplication`");
-			}
-			const urlProp = n.get("arguments", 1, "properties", urlIndex);
-
-			urlProp.replace(
-				b.objectProperty(
-					b.identifier("url"),
-					b.templateLiteral(
-						[
-							b.templateElement({ raw: "", cooked: "" }, false),
-							b.templateElement({ raw: "", cooked: "" }, false),
-							b.templateElement({ raw: "", cooked: "" }, true),
-						],
-						[b.identifier("baseHref"), b.identifier("url")]
-					)
-				)
-			);
-
-			return this.traverse(n);
-		},
-	});
-
-	s.stop(`${brandColor(`updated`)} ${dim(`\`${serverFile}\``)}`);
-};
-
 const config: TemplateConfig = {
 	configVersion: 1,
 	id: "analog",
@@ -258,7 +124,7 @@ const config: TemplateConfig = {
 		scripts: {
 			preview: `${npm} run build && wrangler pages dev`,
 			deploy: `${npm} run build && wrangler pages deploy`,
-			"build-cf-types": `wrangler types`,
+			"cf-typegen": `wrangler types`,
 		},
 	}),
 	devScript: "dev",
