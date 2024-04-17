87ae31dd161661e696fa3e536397684edc41ba04
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2024-04-17 15:16:32 +0100

    fix: display user-friendly message when Pages function route param names are invalid.
    Param names can only contain alphanumeric and underscore characters. Previously the user would see a confusing error message similar to:
    
    ```
     TypeError: Unexpected MODIFIER at 8, expected END
    ```
    
    Now the user is given an error similar to:
    
    ```
    Invalid Pages function route parameter - "[hyphen-not-allowed]". Parameter names must only contain alphanumeric and underscore characters.
    ```
    
    Fixes #5540

--- packages/wrangler/src/pages/functions/filepath-routing.ts
+++ packages/wrangler/src/pages/functions/filepath-routing.ts
@@ -1,6 +1,7 @@
 import fs from "node:fs/promises";
 import path from "node:path";
 import { build } from "esbuild";
+import { FatalError } from "../../errors";
 import { toUrlPath } from "../../paths";
 import { FunctionsBuildError } from "../errors";
 import type { UrlPath } from "../../paths";
@@ -72,10 +73,10 @@ export async function generateConfigFromFileTree({
 					routePath = `${baseURL}/${routePath}`;
 					mountPath = `${baseURL}/${mountPath}`;
 
-					routePath = routePath.replace(/\[\[([^\]]+)\]\]/g, ":$1*"); // transform [[id]] => :id*
-					routePath = routePath.replaceAll(/\[([^\]]+)\]/g, ":$1"); // transform [id] => :id
-					mountPath = mountPath.replace(/\[\[([^\]]+)\]\]/g, ":$1*"); // transform [[id]] => :id*
-					mountPath = mountPath.replaceAll(/\[([^\]]+)\]/g, ":$1"); // transform [id] => :id
+					routePath = convertCatchallParams(routePath);
+					routePath = convertSimpleParams(routePath);
+					mountPath = convertCatchallParams(mountPath);
+					mountPath = convertSimpleParams(mountPath);
 
 					// These are used as module specifiers so UrlPaths are okay to use even on Windows
 					const modulePath = toUrlPath(path.relative(baseDir, filepath));
@@ -189,4 +190,39 @@ interface NonEmptyArray<T> extends Array<T> {
 }
 function isNotEmpty<T>(array: T[]): array is NonEmptyArray<T> {
 	return array.length > 0;
+}
+
+/**
+ * See https://github.com/pillarjs/path-to-regexp?tab=readme-ov-file#named-parameters
+ */
+const validParamNameRegExp = /^[A-Za-z0-9_]+$/;
+
+/**
+ * Transform all [[id]] => :id*
+ */
+function convertCatchallParams(routePath: string): string {
+	return routePath.replace(/\[\[([^\]]+)\]\]/g, (_, param) => {
+		if (validParamNameRegExp.test(param)) {
+			return `:${param}*`;
+		} else {
+			throw new FatalError(
+				`Invalid Pages function route parameter - "[[${param}]]". Parameters names must only contain alphanumeric and underscore characters.`
+			);
+		}
+	});
+}
+
+/**
+ * Transform all [id] => :id
+ */
+function convertSimpleParams(routePath: string): string {
+	return routePath.replace(/\[([^\]]+)\]/g, (_, param) => {
+		if (validParamNameRegExp.test(param)) {
+			return `:${param}`;
+		} else {
+			throw new FatalError(
+				`Invalid Pages function route parameter - "[${param}]". Parameter names must only contain alphanumeric and underscore characters.`
+			);
+		}
+	});
 }
