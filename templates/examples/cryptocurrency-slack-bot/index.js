1de34cb6fea2e0d26202a07dca0f573c2adb6c06
Gift Egwuenu <laurandidi21@gmail.com>
2024-04-19 17:06:11 +0100

    Convert Templates and Examples to use Module Workers Format (#2928)
    * modify templates and examples to use module workers format
    
    * Update templates/examples/edge-cache-html/edge-cache-html.js
    
    Co-authored-by: MrBBot <bcoll@cloudflare.com>
    
    * Update templates/examples/edge-cache-html/edge-cache-html.js
    
    Co-authored-by: MrBBot <bcoll@cloudflare.com>
    
    * Update templates/examples/edge-cache-html/edge-cache-html.js
    
    Co-authored-by: MrBBot <bcoll@cloudflare.com>
    
    * Update templates/examples/edge-cache-html/edge-cache-html.js
    
    Co-authored-by: MrBBot <bcoll@cloudflare.com>
    
    * Update templates/worker-aws/index.js
    
    Co-authored-by: MrBBot <bcoll@cloudflare.com>
    
    * Update templates/worker-aws/index.js
    
    Co-authored-by: MrBBot <bcoll@cloudflare.com>
    
    * Update templates/examples/third-party-scripts/third-party-scripts.js
    
    Co-authored-by: MrBBot <bcoll@cloudflare.com>
    
    * Update templates/examples/edge-cache-html/edge-cache-html.js
    
    Co-authored-by: MrBBot <bcoll@cloudflare.com>
    
    * Update templates/examples/third-party-scripts/third-party-scripts.js
    
    Co-authored-by: MrBBot <bcoll@cloudflare.com>
    
    * Fix: add correct params for KVAssetsHandler
    
    * fix linting errors
    
    * address failing test
    
    * Update templates/examples/edge-cache-html/edge-cache-html.js
    
    Co-authored-by: MrBBot <bcoll@cloudflare.com>
    
    * Update templates/examples/edge-cache-html/edge-cache-html.js
    
    Co-authored-by: MrBBot <bcoll@cloudflare.com>
    
    * Update templates/worker-aws/src/index.js
    
    Co-authored-by: MrBBot <bcoll@cloudflare.com>
    
    * Update templates/examples/fast-google-fonts/fast-google-fonts.js
    
    Co-authored-by: MrBBot <bcoll@cloudflare.com>
    
    * Update templates/examples/fast-google-fonts/fast-google-fonts.js
    
    Co-authored-by: MrBBot <bcoll@cloudflare.com>
    
    * fix linting errors
    
    * Update templates/examples/edge-cache-html/edge-cache-html.js
    
    Co-authored-by: Cina Saffary <cina@cloudflare.com>
    
    * Update templates/examples/fast-google-fonts/fast-google-fonts.js
    
    Co-authored-by: Cina Saffary <cina@cloudflare.com>
    
    * refactor crypto example to use secret env var
    
    * add env params where neccessary
    
    ---------
    
    Co-authored-by: MrBBot <bcoll@cloudflare.com>
    Co-authored-by: Cina Saffary <cina@cloudflare.com>

--- templates/examples/cryptocurrency-slack-bot/index.js
+++ templates/examples/cryptocurrency-slack-bot/index.js
@@ -1,10 +1,3 @@
-addEventListener("fetch", (event) => {
-	event.respondWith(slackWebhookHandler(event.request));
-});
-
-// SLACK_TOKEN is used to authenticate requests are from Slack.
-// Keep this value secret.
-let SLACK_TOKEN = "PUTYOURTOKENHERE";
 let BOT_NAME = "Crypto-bot 🤖";
 let REPO_URL =
 	"https://github.com/cloudflare/workers-sdk/tree/main/templates/examples/cryptocurrency-slack-bot";
@@ -251,53 +244,55 @@ async function currencyRequest(currency, display) {
 	}
 }
 
-/**
- * slackWebhookHandler handles an incoming Slack
- * webhook and generates a response.
- * @param {Request} request
- */
-async function slackWebhookHandler(request) {
-	// As per: https://api.slack.com/slash-commands
-	// - Slash commands are outgoing webhooks (POST requests)
-	// - Slack authenticates via a verification token.
-	// - The webhook payload is provided as POST form data
+export default {
+	/**
+	 * slackWebhookHandler handles an incoming Slack
+	 * webhook and generates a response.
+	 * @param {Request} request
+	 */
+	async fetch(request, env) {
+		// As per: https://api.slack.com/slash-commands
+		// - Slash commands are outgoing webhooks (POST requests)
+		// - Slack authenticates via a verification token.
+		// - The webhook payload is provided as POST form data
 
-	if (request.method != "POST") {
-		return simpleResponse(
-			200,
-			`Hi, I'm ${BOT_NAME}, a Slack bot for fetching the latest crypto-currenncy prices. Find my source code at ${REPO_URL}`
-		);
-	}
-
-	let formData;
-	try {
-		formData = await request.formData();
-		if (formData.get("token").toString() !== SLACK_TOKEN) {
-			return simpleResponse(403, "invalid Slack verification token");
-		}
-	} catch (e) {
-		return simpleResponse(400, "could not decode POST form data");
-	}
-
-	try {
-		let parsed = parseMessage(formData);
-		if (parsed === null) {
-			throw new Error("could not parse your message");
+		if (request.method != "POST") {
+			return simpleResponse(
+				200,
+				`Hi, I'm ${BOT_NAME}, a Slack bot for fetching the latest crypto-currenncy prices. Find my source code at ${REPO_URL}`
+			);
 		}
 
-		let reply = await currencyRequest(parsed.currency, parsed.display);
+		let formData;
+		try {
+			formData = await request.formData();
+			if (formData.get("token").toString() !== env.SLACK_TOKEN) {
+				return simpleResponse(403, "invalid Slack verification token");
+			}
+		} catch (e) {
+			return simpleResponse(400, "could not decode POST form data");
+		}
 
-		return slackResponse(
-			`Current price (${reply.currency}): 💵 $USD${reply.USD}`,
-			[
-				`1h Δ: ${reply.percent_change_1h} · 24h Δ: ${reply.percent_change_24h} · 7d Δ: ${reply.percent_change_7d}`,
-				`Updated: ${reply.updated} | ${reply.cached}`,
-			]
-		);
-	} catch (e) {
-		return simpleResponse(
-			200,
-			`Sorry, I had an issue retrieving anything for that currency: ${e}`
-		);
-	}
-}
+		try {
+			let parsed = parseMessage(formData);
+			if (parsed === null) {
+				throw new Error("could not parse your message");
+			}
+
+			let reply = await currencyRequest(parsed.currency, parsed.display);
+
+			return slackResponse(
+				`Current price (${reply.currency}): 💵 $USD${reply.USD}`,
+				[
+					`1h Δ: ${reply.percent_change_1h} · 24h Δ: ${reply.percent_change_24h} · 7d Δ: ${reply.percent_change_7d}`,
+					`Updated: ${reply.updated} | ${reply.cached}`,
+				]
+			);
+		} catch (e) {
+			return simpleResponse(
+				200,
+				`Sorry, I had an issue retrieving anything for that currency: ${e}`
+			);
+		}
+	},
+};
