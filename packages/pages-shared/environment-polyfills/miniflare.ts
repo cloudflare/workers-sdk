c74ecd27ed00341fbaa13be34de4aca021de366a
Jacob Hands <jacob@gogit.io>
2024-04-22 16:10:18 -0500

    fix(pages-shared): Only apply early hints to html assets
    - fixed cache logic where we were still writing to cache despite cache hits.
    - Increased cache ttl from 2 hours (default) to 30 days.
    - This change required removing service-worker-mock due to missing arrayBuffer() implementation

--- packages/pages-shared/environment-polyfills/miniflare.ts
+++ packages/pages-shared/environment-polyfills/miniflare.ts
@@ -1,11 +1,13 @@
 import { polyfill } from ".";
 
 export default async () => {
+	const { HTMLRewriter } = await import("@miniflare/html-rewriter");
 	const mf = await import("miniflare");
 	polyfill({
 		fetch: mf.fetch,
 		Headers: mf.Headers,
 		Request: mf.Request,
 		Response: mf.Response,
+		HTMLRewriter: HTMLRewriter,
 	});
 };
