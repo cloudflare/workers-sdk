bc112649aefe86d420ab7f07f302c1bb5b48d6d0
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2024-04-22 08:46:55 +0100

    fix: provide a simple fetch handler to the scheduled template to avoid confusing error messages.

--- packages/create-cloudflare/templates/scheduled/js/src/index.js
+++ packages/create-cloudflare/templates/scheduled/js/src/index.js
@@ -13,6 +13,13 @@
  */
 
 export default {
+	async fetch(req) {
+		const url = new URL(req.url)
+		url.pathname = "/__scheduled";
+		url.searchParams.append("cron", "* * * * *");
+		return new Response(`To test the scheduled handler, ensure you have used the "--test-scheduled" then try running "curl ${url.href}".`);
+	},
+
 	// The scheduled handler is invoked at the interval set in our wrangler.toml's
 	// [[triggers]] configuration.
 	async scheduled(event, env, ctx) {
