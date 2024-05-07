13eb42346c8b5e121559e6b0ae420c9afb7c2ccb
aaronlisman <83968625+aaronlisman@users.noreply.github.com>
2024-05-07 10:39:15 -0500

    Update packages/pages-shared/asset-server/handler.ts
    Co-authored-by: Daniel Walsh <walshydev@gmail.com>

--- packages/pages-shared/asset-server/handler.ts
+++ packages/pages-shared/asset-server/handler.ts
@@ -448,7 +448,7 @@ export async function generateHandler<
 		);
 
 		// If the response was an internal error from the asset handler, skip applying header rules.
-		// This avoids a case where we can cache 500s for a long time, depending on headers set by a user.
+		// This avoids a cases where we can cache undesirable files for a long time, depending on headers set by a user.
 		const assetError = response.status >= 400 && response.status <= 599
 		const matches = assetError ? [] : headersMatcher({ request });
 
