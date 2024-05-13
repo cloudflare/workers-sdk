d854f9e030192ff075bf55efee59b8b56b74837c
Thomas Gauvin <tgauvin@cloudflare.com>
2024-05-13 11:13:02 -0400

    Remove Hyperdrive warning log indicating that Hyperdrive local binding does not work
    Description Hyperdrive is supported locally (see https://github.com/cloudflare/workerd/pull/1266) and this warning is not longer accurate. It was added while the support was in progress to avoid confusion (https://github.com/cloudflare/workers-sdk/commit/54800f6f2dc52b921e7dd1d9a57bb437e2094bb0), but now that local support for Hyperdrive bindings has been added, this log is incorrect and should be removed

--- packages/wrangler/src/dev/dev.tsx
+++ packages/wrangler/src/dev/dev.tsx
@@ -433,12 +433,6 @@ function DevSession(props: DevSessionProps) {
 		);
 	}
 
-	if (props.local && props.bindings.hyperdrive?.length) {
-		logger.warn(
-			"Hyperdrive does not currently support 'wrangler dev' in local mode at this stage of the beta. Use the '--remote' flag to test a Hyperdrive configuration before deploying."
-		);
-	}
-
 	const announceAndOnReady: typeof props.onReady = async (
 		finalIp,
 		finalPort,
