40075103b7c1b99513b5438c318de8dd05af7f64
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2024-05-23 12:29:30 +0100

    skip annoying windws failures

--- packages/vitest-pool-workers/test/console.test.ts
+++ packages/vitest-pool-workers/test/console.test.ts
@@ -1,14 +1,12 @@
 import dedent from "ts-dedent";
 import { minimalVitestConfig, test, waitFor } from "./helpers";
 
-test("console.log()s include correct source-mapped locations", async ({
-	expect,
-	seed,
-	vitestDev,
-}) => {
-	await seed({
-		"vitest.config.ts": minimalVitestConfig,
-		"index.test.ts": dedent`
+test.skipIf(process.platform === "win32")(
+	"console.log()s include correct source-mapped locations",
+	async ({ expect, seed, vitestDev }) => {
+		await seed({
+			"vitest.config.ts": minimalVitestConfig,
+			"index.test.ts": dedent`
 			import { describe, it } from "vitest";
 			console.log("global");
 			describe("thing", () => {
@@ -18,21 +16,21 @@ test("console.log()s include correct source-mapped locations", async ({
 				});
 			});
 		`,
-	});
-	const result = vitestDev();
-	await waitFor(() => {
-		expect(result.stdout).toMatch("stdout | index.test.ts:2:9\nglobal");
-		expect(result.stdout).toMatch("stdout | index.test.ts:4:10\ndescribe");
-		expect(result.stdout).toMatch(
-			"stdout | index.test.ts > thing > does something\ntest"
-		);
-	});
+		});
+		const result = vitestDev();
+		await waitFor(() => {
+			expect(result.stdout).toMatch("stdout | index.test.ts:2:9\nglobal");
+			expect(result.stdout).toMatch("stdout | index.test.ts:4:10\ndescribe");
+			expect(result.stdout).toMatch(
+				"stdout | index.test.ts > thing > does something\ntest"
+			);
+		});
 
-	// Check still have correct locations on reload
-	// TODO(soon): when issue with source map cache not being flushed between reloads,
-	//  add a test here to ensure that changing line numbers changes output
-	await seed({
-		"index.test.ts": dedent`
+		// Check still have correct locations on reload
+		// TODO(soon): when issue with source map cache not being flushed between reloads,
+		//  add a test here to ensure that changing line numbers changes output
+		await seed({
+			"index.test.ts": dedent`
 			import { describe, it } from "vitest";
 			console.log("new global");
 			describe("new thing", () => {
@@ -42,12 +40,15 @@ test("console.log()s include correct source-mapped locations", async ({
 				});
 			});
 		`,
-	});
-	await waitFor(() => {
-		expect(result.stdout).toMatch("stdout | index.test.ts:2:9\nnew global");
-		expect(result.stdout).toMatch("stdout | index.test.ts:4:10\nnew describe");
-		expect(result.stdout).toMatch(
-			"stdout | index.test.ts > new thing > does something else\nnew test"
-		);
-	});
-});
+		});
+		await waitFor(() => {
+			expect(result.stdout).toMatch("stdout | index.test.ts:2:9\nnew global");
+			expect(result.stdout).toMatch(
+				"stdout | index.test.ts:4:10\nnew describe"
+			);
+			expect(result.stdout).toMatch(
+				"stdout | index.test.ts > new thing > does something else\nnew test"
+			);
+		});
+	}
+);
