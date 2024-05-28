e61f26d5e0d36ccd0447478d41dfdd28cdd37dbe
Daniel Benhamou <dbenhamou@cloudflare.com>
2024-05-28 14:59:40 +0100

    Update create KV namespace binding message

--- packages/wrangler/src/kv/index.ts
+++ packages/wrangler/src/kv/index.ts
@@ -82,12 +82,17 @@ export function kvNamespace(kvYargs: CommonYargsArgv) {
 					`Add the following to your configuration file in your kv_namespaces array${envString}:`
 				);
 				logger.log(
-					`{ binding = "${getValidBindingName(
+					`[[kv_namespaces]]`
+				);
+				logger.log(
+					`binding = "${getValidBindingName(
 						args.namespace,
 						"KV"
-					)}", ${previewString}id = "${namespaceId}" }`
+					)}"`
 				);
-
+				logger.log(
+					`id = "${previewString}id = "${namespaceId}"`
+				);
 				// TODO: automatically write this block to the wrangler.toml config file??
 			}
 		)
