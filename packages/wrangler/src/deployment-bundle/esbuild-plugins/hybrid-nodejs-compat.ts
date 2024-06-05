55f3f14a36e2b8acb1b0145d9324e566c65e4ca3
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2024-06-05 21:30:21 +0100

    Separate out the require call conversion from the alias import handling in the hybrid plugin

--- packages/wrangler/src/deployment-bundle/esbuild-plugins/hybrid-nodejs-compat.ts
+++ packages/wrangler/src/deployment-bundle/esbuild-plugins/hybrid-nodejs-compat.ts
@@ -1,96 +1,157 @@
+import { builtinModules } from "node:module";
 import nodePath from "node:path";
 import { cloudflare, env, nodeless } from "unenv";
 import { getBasePath } from "../../paths";
-import type { Plugin } from "esbuild";
+import type { Plugin, PluginBuild } from "esbuild";
 
 const REQUIRED_NODE_BUILT_IN_NAMESPACE = "node-built-in-modules";
 
 export const nodejsHybridPlugin: () => Plugin = () => {
 	const { alias, inject, external } = env(nodeless, cloudflare);
-
 	return {
 		name: "unenv-cloudflare",
 		setup(build) {
-			const UNENV_ALIAS_RE = new RegExp(`^(${Object.keys(alias).join("|")})$`);
+			handleRequireCallsToNodeJSBuiltins(build);
+			handleAliasedNodeJSPackages(build, alias, external);
+			handleNodeJSGlobals(build, inject);
+		},
+	};
+};
 
-			// esbuild expects alias paths to be absolute
-			const aliasAbsolute = Object.fromEntries(
-				Object.entries(alias)
-					.map(([key, value]) => {
-						let resolvedAliasPath;
-						try {
-							resolvedAliasPath = require.resolve(value);
-						} catch (e) {
-							// this is an alias for package that is not installed in the current app => ignore
-							resolvedAliasPath = "";
-						}
+/**
+ * We must convert `require()` calls for Node.js to a virtual ES Module that can be imported avoiding the require calls.
+ * We do this by creating a special virtual ES module that re-exports the library in an onLoad handler.
+ * The onLoad handler is triggered by matching the "namespace" added to the resolve.
+ */
+function handleRequireCallsToNodeJSBuiltins(build: PluginBuild) {
+	const NODEJS_MODULES_RE = new RegExp(
+		`^(node:)?(${builtinModules.join("|")})$`
+	);
+	build.onResolve({ filter: NODEJS_MODULES_RE }, (args) => {
+		if (args.kind === "require-call") {
+			return {
+				path: args.path,
+				namespace: REQUIRED_NODE_BUILT_IN_NAMESPACE,
+			};
+		}
+	});
+	build.onLoad(
+		{ filter: /.*/, namespace: REQUIRED_NODE_BUILT_IN_NAMESPACE },
+		({ path }) => {
+			return {
+				contents: `export * from '${path}'`,
+				loader: "js",
+			};
+		}
+	);
+}
 
-						return [key, resolvedAliasPath.replace(/\.cjs$/, ".mjs")];
-					})
-					.filter((entry) => entry[1] !== "")
-			);
+function handleAliasedNodeJSPackages(
+	build: PluginBuild,
+	alias: Record<string, string>,
+	external: string[]
+) {
+	const UNENV_ALIAS_RE = new RegExp(`^(${Object.keys(alias).join("|")})$`);
 
-			build.onResolve(
-				{
-					filter: UNENV_ALIAS_RE,
-				},
-				(args) => {
-					const result = aliasAbsolute[args.path];
-					if (result.startsWith("node:") && args.kind === "require-call") {
-						// we must convert `require("node:*")` to ESM imports
-						// Tag with a namespace that can be specially handled in onLoad
-						return {
-							path: result,
-							namespace: REQUIRED_NODE_BUILT_IN_NAMESPACE,
-						};
-					} else if (result) {
-						// If we match an alias then return that and potentially mark it as external
-						return { path: result, external: external.includes(result) };
-					} else {
-						// Otherwise continue
-						return undefined;
-					}
+	// esbuild expects alias paths to be absolute
+	const aliasAbsolute = Object.fromEntries(
+		Object.entries(alias)
+			.map(([key, value]) => {
+				let resolvedAliasPath;
+				try {
+					resolvedAliasPath = require.resolve(value);
+				} catch (e) {
+					// this is an alias for package that is not installed in the current app => ignore
+					resolvedAliasPath = "";
 				}
-			);
 
-			// Requires of node-built-in-modules are converted to embed a file that does an ESM import
-			build.onLoad(
-				{ filter: /.*/, namespace: REQUIRED_NODE_BUILT_IN_NAMESPACE },
-				({ path }) => {
-					return {
-						contents: `export * from '${path}'`,
-						loader: "js",
-					};
-				}
-			);
+				return [key, resolvedAliasPath.replace(/\.cjs$/, ".mjs")];
+			})
+			.filter((entry) => entry[1] !== "")
+	);
 
-			// Inject node globals defined in unenv's `inject` config via virtual modules
-			const UNENV_GLOBALS_RE = /_virtual_unenv_global_polyfill-([^.]+)\.js$/;
+	build.onResolve({ filter: UNENV_ALIAS_RE }, (args) => {
+		// Resolve the alias to its absolute path and potentially mark it as external
+		return {
+			path: aliasAbsolute[args.path],
+			external: external.includes(args.path),
+		};
+	});
+}
 
-			build.initialOptions.inject = [
-				...(build.initialOptions.inject ?? []),
-				//convert unenv's inject keys to absolute specifiers of custom virtual modules that will be provided via a custom onLoad
-				...Object.keys(inject).map((globalName) =>
-					nodePath.resolve(
-						getBasePath(),
-						`_virtual_unenv_global_polyfill-${globalName}.js`
-					)
-				),
-			];
+/**
+ * Inject node globals defined in unenv's `inject` config via virtual modules
+ */
+function handleNodeJSGlobals(
+	build: PluginBuild,
+	inject: Record<string, string | string[]>
+) {
+	const UNENV_GLOBALS_RE = /_virtual_unenv_global_polyfill-([^.]+)\.js$/;
 
-			build.onResolve({ filter: UNENV_GLOBALS_RE }, ({ path }) => ({ path }));
+	build.initialOptions.inject = [
+		...(build.initialOptions.inject ?? []),
+		//convert unenv's inject keys to absolute specifiers of custom virtual modules that will be provided via a custom onLoad
+		...Object.keys(inject).map((globalName) =>
+			nodePath.resolve(
+				getBasePath(),
+				`_virtual_unenv_global_polyfill-${globalName}.js`
+			)
+		),
+	];
 
-			build.onLoad({ filter: UNENV_GLOBALS_RE }, ({ path }) => {
-				// eslint-disable-next-line  @typescript-eslint/no-non-null-assertion
-				const globalName = path.match(UNENV_GLOBALS_RE)![1];
-				const globalMapping = inject[globalName];
+	build.onResolve({ filter: UNENV_GLOBALS_RE }, ({ path }) => ({ path }));
 
-				if (typeof globalMapping === "string") {
-					const globalPolyfillSpecifier = globalMapping;
+	build.onLoad({ filter: UNENV_GLOBALS_RE }, ({ path }) => {
+		// eslint-disable-next-line  @typescript-eslint/no-non-null-assertion
+		const globalName = path.match(UNENV_GLOBALS_RE)![1];
+		const globalMapping = inject[globalName];
 
-					return {
-						contents: `
-							import globalVar from "${globalPolyfillSpecifier}";
+		if (typeof globalMapping === "string") {
+			const globalPolyfillSpecifier = globalMapping;
+
+			return {
+				contents: `
+								import globalVar from "${globalPolyfillSpecifier}";
+
+								${
+									/*
+								// ESBuild's inject doesn't actually touch globalThis, so let's do it ourselves
+								// by creating an exportable so that we can preserve the globalThis assignment if
+								// the ${globalName} was found in the app, or tree-shake it, if it wasn't
+								// see https://esbuild.github.io/api/#inject
+								*/ ""
+								}
+								const exportable =
+									${
+										/*
+									// mark this as a PURE call so it can be ignored and tree-shaken by ESBuild,
+									// when we don't detect 'process', 'global.process', or 'globalThis.process'
+									// in the app code
+									// see https://esbuild.github.io/api/#tree-shaking-and-side-effects
+									*/ ""
+									}
+									/* @__PURE__ */ (() => {
+										${
+											/*
+										// TODO: should we try to preserve globalThis.${globalName} if it exists?
+										*/ ""
+										}
+										return globalThis.${globalName} = globalVar;
+									})();
+
+								export {
+									exportable as '${globalName}',
+									exportable as 'globalThis.${globalName}',
+								}
+							`,
+			};
+		}
+
+		const [moduleName, exportName] = inject[globalName];
+
+		return {
+			contents: `
+							import { ${exportName} } from "${moduleName}";
 
 							${
 								/*
@@ -115,57 +176,15 @@ export const nodejsHybridPlugin: () => Plugin = () => {
 									// TODO: should we try to preserve globalThis.${globalName} if it exists?
 									*/ ""
 									}
-									return globalThis.${globalName} = globalVar;
-								})();
+									return globalThis.${globalName} = ${exportName};
+							})();
 
 							export {
 								exportable as '${globalName}',
-								exportable as 'globalThis.${globalName}',
+								exportable as 'global.${globalName}',
+								exportable as 'globalThis.${globalName}'
 							}
 						`,
-					};
-				}
-
-				const [moduleName, exportName] = inject[globalName];
-
-				return {
-					contents: `
-						import { ${exportName} } from "${moduleName}";
-
-						${
-							/*
-						// ESBuild's inject doesn't actually touch globalThis, so let's do it ourselves
-						// by creating an exportable so that we can preserve the globalThis assignment if
-						// the ${globalName} was found in the app, or tree-shake it, if it wasn't
-						// see https://esbuild.github.io/api/#inject
-						*/ ""
-						}
-						const exportable =
-							${
-								/*
-							// mark this as a PURE call so it can be ignored and tree-shaken by ESBuild,
-							// when we don't detect 'process', 'global.process', or 'globalThis.process'
-							// in the app code
-							// see https://esbuild.github.io/api/#tree-shaking-and-side-effects
-							*/ ""
-							}
-							/* @__PURE__ */ (() => {
-								${
-									/*
-								// TODO: should we try to preserve globalThis.${globalName} if it exists?
-								*/ ""
-								}
-								return globalThis.${globalName} = ${exportName};
-						})();
-
-						export {
-							exportable as '${globalName}',
-							exportable as 'global.${globalName}',
-							exportable as 'globalThis.${globalName}'
-						}
-					`,
-				};
-			});
-		},
-	};
-};
+		};
+	});
+}
