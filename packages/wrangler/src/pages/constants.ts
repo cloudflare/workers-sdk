ae9d837d3e23991457dcc0490f1b71ab0b0e9434
Rahul Sethi <5822355+RamIdeas@users.noreply.github.com>
2024-06-11 16:25:42 +0100

    reduce pages assets MAX_BUCKET_FILE_COUNT on windows

--- packages/wrangler/src/pages/constants.ts
+++ packages/wrangler/src/pages/constants.ts
@@ -1,10 +1,12 @@
 import { version as wranglerVersion } from "../../package.json";
 
+const isWindows = process.platform === "win32";
+
 export const MAX_ASSET_COUNT = 20_000;
 export const MAX_ASSET_SIZE = 25 * 1024 * 1024;
 export const PAGES_CONFIG_CACHE_FILENAME = "pages.json";
 export const MAX_BUCKET_SIZE = 40 * 1024 * 1024;
-export const MAX_BUCKET_FILE_COUNT = 2000;
+export const MAX_BUCKET_FILE_COUNT = isWindows ? 1000 : 2000;
 export const BULK_UPLOAD_CONCURRENCY = 3;
 export const MAX_UPLOAD_ATTEMPTS = 5;
 export const MAX_UPLOAD_GATEWAY_ERRORS = 5;
