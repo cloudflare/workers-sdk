7fc64f0ec529394d94f9b5c79754abe4ecc4b952
Sunil Pai <spai@cloudflare.com>
2024-06-15 17:40:49 +0100

    run react eslint rules only where needed
    Our default eslint config includes react linting, and was applied to every package. However that meant we kept seeing warnings about react not being detected etc. This patch splits out the react part, and applies it only where required.

---
+++ packages/eslint-config-worker/react.js
@@ -0,0 +1,11 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+	extends: ["turbo", "@cloudflare/eslint-config-worker"],
+	plugins: ["eslint-plugin-react", "eslint-plugin-react-hooks"],
+	overrides: [
+		{
+			files: ["*.ts", "*.tsx"],
+			extends: ["plugin:react/recommended", "plugin:react-hooks/recommended"],
+		},
+	],
+};
---
+++ packages/workers-playground/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	root: true,
+	extends: ["@cloudflare/eslint-config-worker/react"],
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
+};
