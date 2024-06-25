b9d6ab3c4f230d048b5e14605a73dd1646582df8
Rahul Sethi <5822355+RamIdeas@users.noreply.github.com>
2024-06-25 23:07:03 +0100

    implement higher-level interface for hotkeys automatically compute instructions

--- packages/wrangler/src/cli-hotkeys.ts
+++ packages/wrangler/src/cli-hotkeys.ts
@@ -1,11 +1,66 @@
 import readline from "readline";
+import { unwrapHook } from "./api/startDevWorker/utils";
+import type { Hook } from "./api";
+import type { Logger } from "./logger";
 
-type KeypressEvent = {
-	name: string;
-	ctrl: boolean;
-};
+type KeypressEvent = { name: string; ctrl: boolean };
 
-export function hotkeys(
+export default function ({
+	options,
+	logger,
+	stdin,
+}: {
+	options: Array<{
+		keys: string[];
+		label: Hook<string>;
+		handler: (helpers: {
+			printIntructions: () => void;
+		}) => void | Promise<void>;
+	}>;
+	logger: Logger;
+	stdin?: typeof process.stdin;
+}) {
+	/**
+	 * Prints all options, comma-separated, prefixed by the first key in square brackets.
+	 *
+	 * Example output:
+	 *  ╭─────────────────────────────────────────────────────────╮
+	 *  │  [a] first option, [b] second option, [c] third option  │
+	 *  ╰─────────────────────────────────────────────────────────╯
+	 *
+	 * Limitations:
+	 *  - doesn't break nicely across lines
+	 */
+	function printIntructions() {
+		const instructions = options
+			.map(({ keys, label }) => `[${keys[0]}] ${unwrapHook(label)}`)
+			.join(", ");
+
+		logger.log(
+			`╭──${"─".repeat(instructions.length)}──╮\n` +
+				`│  ${instructions}  │\n` +
+				`╰──${"─".repeat(instructions.length)}──╯\n`
+		);
+	}
+
+	printIntructions();
+
+	return onKeyPress(async (key) => {
+		key = key.toLowerCase();
+
+		for (const { keys, handler } of options) {
+			if (keys.includes(key)) {
+				try {
+					await handler({ printIntructions });
+				} catch (error) {
+					logger.error(`Error while handling hotkey [${key}]\n`, error);
+				}
+			}
+		}
+	}, stdin);
+}
+
+export function onKeyPress(
 	callback: (key: string) => void,
 	stdin = process.stdin
 ) {
@@ -14,7 +69,7 @@ export function hotkeys(
 		stdin.setRawMode(true);
 	}
 
-	const onKeyPress = (char: string, key: KeypressEvent) => {
+	const handler = async (char: string, key: KeypressEvent) => {
 		if (key && key.ctrl && key.name == "c") {
 			char = "CTRL+C";
 		}
@@ -24,7 +79,7 @@ export function hotkeys(
 		}
 	};
 
-	stdin.on("keypress", onKeyPress);
+	stdin.on("keypress", handler);
 
-	return () => stdin.off("keypress", onKeyPress);
+	return () => stdin.off("keypress", handler);
 }
