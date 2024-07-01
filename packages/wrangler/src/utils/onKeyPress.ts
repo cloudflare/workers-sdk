d003e81168411cc6dda1dfaf8b98ddb31b46a96d
Rahul Sethi <5822355+RamIdeas@users.noreply.github.com>
2024-07-01 14:35:06 +0100

    fix: onKeyPress keeps the process alive after unregistering
    Listening for events on process.stdin (eg .on('keypress')) causes it to go into 'old mode'
    which keeps this nodejs process alive even after calling .off('keypress')
    
    WORKAROUND: piping stdin via a transform stream allows us to call stream.destroy()
    which then allows this nodejs process to close cleanly

--- packages/wrangler/src/utils/onKeyPress.ts
+++ packages/wrangler/src/utils/onKeyPress.ts
@@ -1,14 +1,23 @@
 import readline from "readline";
+import { Transform } from "stream";
 
 type KeypressEvent = { name: string; ctrl: boolean };
 
-export function onKeyPress(
-	callback: (key: string) => void,
-	stdin = process.stdin
-) {
-	if (stdin.isTTY) {
-		readline.emitKeypressEvents(stdin);
-		stdin.setRawMode(true);
+export function onKeyPress(callback: (key: string) => void) {
+	// Listening for events on process.stdin (eg .on('keypress')) causes it to go into 'old mode'
+	// which keeps this nodejs process alive even after calling .off('keypress')
+	// WORKAROUND: piping stdin via a transform stream allows us to call stream.destroy()
+	// which then allows this nodejs process to close cleanly
+	const stream = new Transform({
+		transform(chunk, encoding, cb) {
+			cb(null, chunk);
+		},
+	});
+	process.stdin.pipe(stream);
+
+	if (process.stdin.isTTY) {
+		readline.emitKeypressEvents(stream);
+		process.stdin.setRawMode(true);
 	}
 
 	const handler = async (char: string, key: KeypressEvent) => {
@@ -21,9 +30,11 @@ export function onKeyPress(
 		}
 	};
 
-	stdin.on("keypress", handler);
+	stream.on("keypress", handler);
 
 	return () => {
-		stdin.off("keypress", handler);
+		process.stdin.setRawMode(false);
+		stream.off("keypress", handler);
+		stream.destroy();
 	};
 }
