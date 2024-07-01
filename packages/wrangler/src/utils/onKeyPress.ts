2e280616491435b6924c6cdd1ed8699570d28a3d
Rahul Sethi <5822355+RamIdeas@users.noreply.github.com>
2024-07-01 18:30:21 +0100

    add docs link to comment

--- packages/wrangler/src/utils/onKeyPress.ts
+++ packages/wrangler/src/utils/onKeyPress.ts
@@ -8,6 +8,7 @@ export function onKeyPress(callback: (key: string) => void) {
 	// which keeps this nodejs process alive even after calling .off('keypress')
 	// WORKAROUND: piping stdin via a transform stream allows us to call stream.destroy()
 	// which then allows this nodejs process to close cleanly
+	// https://nodejs.org/api/process.html#signal-events:~:text=be%20used%20in-,%22old%22%20mode,-that%20is%20compatible
 	const stream = new Transform({
 		transform(chunk, encoding, cb) {
 			cb(null, chunk);
