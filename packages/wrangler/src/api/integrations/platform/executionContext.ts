8f46fb7527661ce98f174f225fc963dd9a06916a
Dario Piotrowicz <dario@cloudflare.com>
2024-07-04 14:55:34 +0100

    [wrangler] fix: make sure `getPlatformProxy`'s `ctx` method throw illegal invocation errors like workerd

--- packages/wrangler/src/api/integrations/platform/executionContext.ts
+++ packages/wrangler/src/api/integrations/platform/executionContext.ts
@@ -1,5 +1,13 @@
 export class ExecutionContext {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any, unused-imports/no-unused-vars
-	waitUntil(promise: Promise<any>): void {}
-	passThroughOnException(): void {}
+	waitUntil(promise: Promise<any>): void {
+		if (!(this instanceof ExecutionContext)) {
+			throw new Error("Illegal invocation");
+		}
+	}
+	passThroughOnException(): void {
+		if (!(this instanceof ExecutionContext)) {
+			throw new Error("Illegal invocation");
+		}
+	}
 }
