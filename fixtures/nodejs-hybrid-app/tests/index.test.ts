a4420a3c281714c6ac0979a7836d37ccbf81a9f0
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2024-07-10 16:11:23 +0100

    disable nodejs-hybrid-app fixture test
    This test hits a public database that has started
    failing with
    
    > too many connections for role 'reader'
    
    We need to find an alternative (ideally local) DB to test against.

--- fixtures/nodejs-hybrid-app/tests/index.test.ts
+++ fixtures/nodejs-hybrid-app/tests/index.test.ts
@@ -1,5 +1,5 @@
 import { resolve } from "node:path";
-import { fetch } from "undici";
+// import { fetch } from "undici";
 import { describe, it } from "vitest";
 import { runWranglerDev } from "../../shared/src/run-wrangler-long-lived";
 
@@ -12,9 +12,13 @@ describe("nodejs compat", () => {
 			["--port=0", "--inspector-port=0"]
 		);
 		try {
-			const response = await fetch(`http://${ip}:${port}`);
-			const result = (await response.json()) as { id: string };
-			expect(result.id).toEqual("1");
+			// Disabling actually querying the database since we are getting this error:
+			// > too many connections for role 'reader'
+			// const response = await fetch(`http://${ip}:${port}`);
+			// const body = await response.text();
+			// console.log(body);
+			// const result = JSON.parse(body) as { id: string };
+			// expect(result.id).toEqual("1");
 		} finally {
 			await stop();
 		}
