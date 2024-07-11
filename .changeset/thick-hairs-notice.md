a7a4f392e0c4bd249a39b6bbca3069d7955befaa
Garvit Gupta <garvit@cloudflare.com>
2024-07-11 13:25:45 -0500

    VS-197: Add Vectorize V2 operations to Wrangler, Deprecate V1

---
+++ .changeset/thick-hairs-notice.md
@@ -0,0 +1,5 @@
+---
+"wrangler": minor
+---
+
+feat: Enable Wrangler to operate on Vectorize V2 indexes
---
+++ packages/wrangler/src/vectorize/deleteByIds.ts
@@ -0,0 +1,45 @@
+import { readConfig } from "../config";
+import { logger } from "../logger";
+import { deleteByIds } from "./client";
+import { vectorizeBetaWarning } from "./common";
+import type {
+	CommonYargsArgv,
+	StrictYargsOptionsToInterface,
+} from "../yargs-types";
+
+export function options(yargs: CommonYargsArgv) {
+	return yargs
+		.positional("name", {
+			type: "string",
+			demandOption: true,
+			description: "The name of the Vectorize index.",
+		})
+		.options({
+			ids: {
+				type: "array",
+				demandOption: true,
+				describe:
+					"Vector identifiers to be deleted from the Vectorize Index.  Example: `--ids a 'b' 1 '2'`",
+				coerce: (arg: unknown[]) => arg.map((a) => a?.toString() ?? ""),
+			},
+		})
+		.epilogue(vectorizeBetaWarning);
+}
+
+export async function handler(
+	args: StrictYargsOptionsToInterface<typeof options>
+) {
+	const config = readConfig(args.config, args);
+
+	if (args.ids.length === 0) {
+		logger.error("🚨 Please provide valid vector identifiers for deletion.");
+		return;
+	}
+
+	logger.log(`📋 Deleting vectors...`);
+	const mutation = await deleteByIds(config, args.name, args.ids);
+
+	logger.log(
+		`✅ Successfully enqueued ${args.ids.length} vectors into index '${args.name}' for deletion. Mutation changeset identifier: ${mutation.mutationId}.`
+	);
+}
