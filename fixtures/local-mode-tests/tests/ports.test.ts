18a0fcd3f91f33cdc5d7ec082f5d392f77206bf3
Samuel Macleod <smacleod@cloudflare.com>
2024-07-16 13:57:39 +0100

    run a lot

--- fixtures/local-mode-tests/tests/ports.test.ts
+++ fixtures/local-mode-tests/tests/ports.test.ts
@@ -3,58 +3,60 @@ import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import { unstable_dev } from "wrangler";
 import type { UnstableDevWorker } from "wrangler";
 
-describe("multiple workers", () => {
-	let workers: UnstableDevWorker[];
+for (let index = 0; index < 100; index++) {
+	describe("multiple workers", () => {
+		let workers: UnstableDevWorker[];
 
-	beforeAll(async () => {
-		//since the script is invoked from the directory above, need to specify index.js is in src/
+		beforeAll(async () => {
+			//since the script is invoked from the directory above, need to specify index.js is in src/
 
-		workers = [
-			await unstable_dev(path.resolve(__dirname, "..", "src", "module.ts"), {
-				ip: "127.0.0.1",
-				port: 4589,
-				experimental: {
-					disableExperimentalWarning: true,
-					disableDevRegistry: true,
-				},
-			}),
-			await unstable_dev(path.resolve(__dirname, "..", "src", "module.ts"), {
-				ip: "127.0.0.1",
-				port: 4590,
-				experimental: {
-					disableExperimentalWarning: true,
-					disableDevRegistry: true,
-				},
-			}),
-			await unstable_dev(path.resolve(__dirname, "..", "src", "module.ts"), {
-				ip: "127.0.0.1",
-				port: 4591,
-				experimental: {
-					disableExperimentalWarning: true,
-					disableDevRegistry: true,
-				},
-			}),
-		];
+			workers = [
+				await unstable_dev(path.resolve(__dirname, "..", "src", "module.ts"), {
+					ip: "127.0.0.1",
+					port: 4589,
+					experimental: {
+						disableExperimentalWarning: true,
+						disableDevRegistry: true,
+					},
+				}),
+				await unstable_dev(path.resolve(__dirname, "..", "src", "module.ts"), {
+					ip: "127.0.0.1",
+					port: 4590,
+					experimental: {
+						disableExperimentalWarning: true,
+						disableDevRegistry: true,
+					},
+				}),
+				await unstable_dev(path.resolve(__dirname, "..", "src", "module.ts"), {
+					ip: "127.0.0.1",
+					port: 4591,
+					experimental: {
+						disableExperimentalWarning: true,
+						disableDevRegistry: true,
+					},
+				}),
+			];
+		});
+
+		afterAll(async () => {
+			await Promise.all(workers.map(async (worker) => await worker.stop()));
+		});
+
+		it("all workers should be fetchable", async () => {
+			const responses = await Promise.all(
+				workers.map(async (worker) => await worker.fetch())
+			);
+			const parsedResponses = await Promise.all(
+				responses.map(async (resp) => await resp.text())
+			);
+
+			expect(parsedResponses).not.toBe(undefined);
+			expect(parsedResponses.length).toBe(3);
+			expect(parsedResponses).toEqual([
+				'{"greeting":"Hi!"}',
+				'{"greeting":"Hi!"}',
+				'{"greeting":"Hi!"}',
+			]);
+		});
 	});
-
-	afterAll(async () => {
-		await Promise.all(workers.map(async (worker) => await worker.stop()));
-	});
-
-	it.concurrent("all workers should be fetchable", async () => {
-		const responses = await Promise.all(
-			workers.map(async (worker) => await worker.fetch())
-		);
-		const parsedResponses = await Promise.all(
-			responses.map(async (resp) => await resp.text())
-		);
-
-		expect(parsedResponses).not.toBe(undefined);
-		expect(parsedResponses.length).toBe(3);
-		expect(parsedResponses).toEqual([
-			'{"greeting":"Hi!"}',
-			'{"greeting":"Hi!"}',
-			'{"greeting":"Hi!"}',
-		]);
-	});
-});
+}
