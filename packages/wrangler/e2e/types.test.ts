4de1cb947ed90d1680c41005e30db1bdde978345
Andy Jessop <ajessop@cloudflare.com>
2024-07-22 13:17:29 +0200

    chore: update e2e test

--- packages/wrangler/e2e/types.test.ts
+++ packages/wrangler/e2e/types.test.ts
@@ -56,7 +56,7 @@ describe("types", () => {
 			`"types": ["./.wrangler/types/runtime.d.ts"]`
 		);
 		expect(output.stdout).toContain(
-			`📣 To get Node.js typings, install with "npm i --save-dev @types/node".`
+			`📣 It looks like you have some node compatibility turned on in your project. You might want to consider adding Node.js typings with "npm i --save-dev @types/node@20.8.3". Please see the docs for more details: https://developers.cloudflare.com/workers/languages/typescript/#transitive-loading-of-typesnode-overrides-cloudflareworkers-types`
 		);
 		expect(output.stdout).toContain(
 			`Remember to run 'wrangler types --x-with-runtime' again if you change 'compatibility_date' or 'compatibility_flags' in your wrangler.toml.`
