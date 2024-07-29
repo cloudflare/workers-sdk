36a41056ce83ecd7faea494f46a551ba231019ad
Rahul Sethi <5822355+RamIdeas@users.noreply.github.com>
2024-07-29 10:42:19 +0100

    move top-level package dirs into packages/

---
+++ packages/vscode-extensions/scripts/bundle.ts
@@ -0,0 +1,58 @@
+import { readFile } from "fs/promises";
+import * as esbuild from "esbuild";
+
+type BuildFlags = {
+	watch?: boolean;
+};
+
+async function buildMain(flags: BuildFlags = {}) {
+	const options: esbuild.BuildOptions = {
+		watch: flags.watch,
+		entryPoints: ["./src/extension.ts"],
+		bundle: true,
+		outfile: "./dist/extension.js",
+		format: "cjs",
+		external: ["vscode"],
+		logLevel: "info",
+		plugins: [
+			{
+				name: "workers-types",
+				setup(build) {
+					build.onResolve({ filter: /^raw:.*/ }, async (args) => {
+						const result = await build.resolve(args.path.slice(4), {
+							kind: "import-statement",
+							resolveDir: args.resolveDir,
+						});
+						return {
+							path: result.path,
+							namespace: "raw-file",
+						};
+					});
+
+					build.onLoad(
+						{ filter: /.*/, namespace: "raw-file" },
+						async (args) => {
+							const contents = await readFile(args.path);
+							return { contents, loader: "text" };
+						}
+					);
+				},
+			},
+		],
+	};
+
+	await esbuild.build(options);
+}
+
+async function run() {
+	await buildMain();
+	if (process.argv.includes("--watch")) {
+		console.log("Built. Watching for changes...");
+		await Promise.all([buildMain({ watch: true })]);
+	}
+}
+
+run().catch((e) => {
+	console.error(e);
+	process.exit(1);
+});
---
+++ packages/vscode-extensions/src/ipc.ts
@@ -0,0 +1,63 @@
+import type { MessagePassingProtocol } from "vscode";
+
+export interface EditorMessage<T extends string, B> {
+	type: T;
+	body: B;
+}
+// Sent on first load of worker
+interface WorkerLoaded {
+	name: string;
+	entrypoint: string;
+	files: {
+		path: string;
+		contents: Uint8Array;
+	}[];
+}
+
+// Sent when a user updates a file
+interface UpdateFile {
+	path: string;
+	contents: Uint8Array;
+}
+
+// Sent when a user creates a file
+interface CreateFile {
+	path: string;
+	contents: Uint8Array;
+}
+// Sent when a user deletes a file
+interface DeleteFile {
+	path: string;
+}
+
+// Sent when a user sets an entrypoint
+interface SetEntryPoint {
+	path: string;
+}
+
+export type WorkerLoadedMessage = EditorMessage<"WorkerLoaded", WorkerLoaded>;
+
+export type UpdateFileMessage = EditorMessage<"UpdateFile", UpdateFile>;
+export type CreateFileMessage = EditorMessage<"CreateFile", CreateFile>;
+export type DeleteFileMessage = EditorMessage<"DeleteFile", DeleteFile>;
+export type SetEntryPointMessage = EditorMessage<
+	"SetEntryPoint",
+	SetEntryPoint
+>;
+
+export type FromQuickEditMessage =
+	| UpdateFileMessage
+	| CreateFileMessage
+	| DeleteFileMessage
+	| SetEntryPointMessage;
+export type ToQuickEditMessage = WorkerLoadedMessage;
+
+export class Channel<Send, Receive> {
+	constructor(readonly messagePassingProtocol: MessagePassingProtocol) {}
+	postMessage(message: Send, transfer: ArrayBuffer[] = []) {
+		return this.messagePassingProtocol.postMessage(message, transfer);
+	}
+	onMessage(cb: (data: Receive) => void) {
+		return this.messagePassingProtocol.onDidReceiveMessage(cb);
+	}
+}
