197e452eb6e08c756ea8b9bbb2970d12c7c4c22a
Samuel Macleod <smacleod@cloudflare.com>
2024-08-09 11:28:07 +0100

    be more explicit about exports

--- packages/workers-editor-shared/lib/index.ts
+++ packages/workers-editor-shared/lib/index.ts
@@ -1,6 +1,27 @@
 export { useInjectSources } from "./useInjectSources";
-export * from "./ipc";
+export { Channel } from "./ipc";
 export { useRefreshableIframe } from "./useRefreshableIframe";
 export { default as SplitPane, DragContext } from "./SplitPane";
 export { default as Frame } from "./Frame";
-export * from "./constants";
+export {
+	BAR_HEIGHT,
+	BACKGROUND_GRAY,
+	BORDER_GRAY,
+	STYLED_TAB_HEIGHT,
+} from "./constants";
+
+export type {
+	EditorMessage,
+	WorkerLoadedMessage,
+	UpdateFileMessage,
+	CreateFileMessage,
+	DeleteFileMessage,
+	SetEntryPointMessage,
+	RequestSourcesMessage,
+	SourcesLoadedMessage,
+	FromQuickEditMessage,
+	ToQuickEditMessage,
+	FromErrorPage,
+	ToErrorPage,
+	WrappedChannel,
+} from "./ipc";
