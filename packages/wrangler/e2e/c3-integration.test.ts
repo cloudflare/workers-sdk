42a385972ae965a484f638f35f5002a5e4d863c5
Edmund Hung <edmund@cloudflare.com>
2024-08-12 12:59:53 +0100

    chore(create-cloudflare): remove unnecessary assertion in wrangler init test

--- packages/wrangler/e2e/c3-integration.test.ts
+++ packages/wrangler/e2e/c3-integration.test.ts
@@ -34,11 +34,9 @@ describe("c3 integration", () => {
 			GIT_COMMITTER_EMAIL: "test-user@cloudflare.com",
 		};
 
-		const init = await helper.run(`wrangler init ${workerName} --yes`, {
+		await helper.run(`wrangler init ${workerName} --yes`, {
 			env,
 		});
-
-		expect(init.output).toContain("APPLICATION CREATED");
 
 		expect(existsSync(path.join(helper.tmpPath, workerName))).toBe(true);
 	});
