d63a603c0b3721f9baca3d50d7511dbd841fe41e
Andy Jessop <ajessop@cloudflare.com>
2024-08-19 12:05:12 +0200

    fix: use fork instead of spawn to ensure tsc runs in Node.js

--- packages/wrangler/src/type-generation/check-types.ts
+++ packages/wrangler/src/type-generation/check-types.ts
@@ -1,10 +1,12 @@
-import { spawn } from "node:child_process";
+import { fork } from "node:child_process";
 import path from "node:path";
 
 export async function checkTypes(tsconfig: string) {
 	const tsPath = path.resolve(require.resolve("typescript"), "../../bin/tsc");
 
-	spawn(tsPath, ["--project", tsconfig], {
+	// Using fork here instead of spawn to ensure that the child process runs in Node.js
+	// in all environments.
+	fork(tsPath, ["--project", tsconfig], {
 		stdio: "inherit",
 	}).on("exit", (code) =>
 		process.exit(code === undefined || code === null ? 0 : code)
