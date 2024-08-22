f5bde66914d24c59da35e051c5343c8f0554f782
emily-shen <69125074+emily-shen@users.noreply.github.com>
2024-08-22 10:27:49 +0100

    feat: add router worker to workers-shared (#6537)
    * add router worker to workers-shared
    
    * address review
    
    * add changeset

---
+++ packages/workers-shared/router-worker/README.md
@@ -0,0 +1,3 @@
+# `router-worker`
+
+The Router Worker is a [Cloudflare Worker](https://developers.cloudflare.com/workers/) that is responsible for routing between a user worker and static assets in a Workers + Assets project.
---
+++ packages/workers-shared/router-worker/src/index.ts
@@ -0,0 +1,21 @@
+import type { Fetcher, Request } from "@cloudflare/workers-types";
+
+interface Env {
+	ASSET_SERVER: Fetcher;
+	USER_WORKER: Fetcher;
+}
+export default {
+	async fetch(request: Request, env: Env) {
+		const result = await env.ASSET_SERVER.fetch(request);
+		if (!result.ok) {
+			if (result.status === 404) {
+				return await env.USER_WORKER.fetch(request);
+			}
+			// return failed response on non-404 errors
+			return new Response(`Failed to fetch content: ${result.statusText}`, {
+				status: result.status,
+			});
+		}
+		return result;
+	},
+};
