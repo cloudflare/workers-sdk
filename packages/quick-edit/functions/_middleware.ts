796b8f64b1609a8d9c86a797cb63a7d62924c510
Edmund Hung <edmund@cloudflare.com>
2024-08-23 14:49:47 +0100

    exclude files only on root

--- packages/quick-edit/functions/_middleware.ts
+++ packages/quick-edit/functions/_middleware.ts
@@ -20,8 +20,8 @@ export const onRequest = async ({
 				"files.exclude": {
 					"*.d.ts": true,
 					"jsconfig.json": true,
-					"**/package.json": true,
-					"**/wrangler.toml": true,
+					"package.json": true,
+					"wrangler.toml": true,
 				},
 				"telemetry.telemetryLevel": "off",
 				"window.menuBarVisibility": "hidden",
