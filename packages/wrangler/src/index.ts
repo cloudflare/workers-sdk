a2bebb7c4f289a59ab558dc82508e479974bb67d
Andy Jessop <ajessop@cloudflare.com>
2024-09-10 09:57:20 +0200

    Add exra line

--- packages/wrangler/src/index.ts
+++ packages/wrangler/src/index.ts
@@ -608,7 +608,7 @@ export function createCLIParser(argv: string[]) {
 	// pipelines
 	wrangler.command(
 		"pipelines",
-		`🚰 Manage Worker Pipelines ${chalk.hex(betaCmdColor)("[open beta]")}`,
+		`🚰 Manage Worker Pipelines ${chalk.hex(betaCmdColor)("[open beta]")}\n`,
 		(pipelinesYargs) => {
 			return pipelines(pipelinesYargs.command(subHelp));
 		}
