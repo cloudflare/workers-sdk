4db75e1eaf8eb088d869986efc28d5345789ff50
Somhairle MacLeòid <smacleod@cloudflare.com>
2024-09-23 16:45:26 +0100

    Revert "fix(quick-edit): disable autosave" (#6806)
    This reverts commit 2a5b64815455a324dd57cdcf98abbcc3f7156c98.

--- packages/quick-edit/functions/_middleware.ts
+++ packages/quick-edit/functions/_middleware.ts
@@ -16,7 +16,6 @@ export const onRequest = async ({
 						: "Solarflare Light",
 				"workbench.startupEditor": "none",
 				"editor.minimap.autohide": true,
-				"files.autoSave": "off",
 				"files.exclude": {
 					"*.d.ts": true,
 					"jsconfig.json": true,
