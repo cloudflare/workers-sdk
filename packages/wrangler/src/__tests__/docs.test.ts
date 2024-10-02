cloudflare/workers-sdk

commit 9c7df38871b9fcfda4890a00507e6ef149e0cbcd

[Wrangler] Update the commands list (#3735)
* Clean up `wrangler --help` output

This commit cleans up and standardizes the look and
of all `wrangler` commands as displayed by
`wrangler --help` and `wrangler <cmd> --help`.

---------

Co-authored-by: Carmen Popoviciu <cpopoviciu@cloudflare.com>

packages/wrangler/src/d1/index.ts
@@ -16,7 +16,7 @@ export function d1(yargs: CommonYargsArgv) {
 			.command("list", "List D1 databases", List.Options, List.Handler)
 			.command(
 				"info <name>",
-				"Get information about a D1 database, including the current database size and state.",
+				"Get information about a D1 database, including the current database size and state",
 				Info.Options,
 				Info.Handler
 			)
@@ -38,7 +38,7 @@ export function d1(yargs: CommonYargsArgv) {
 				Delete.Options,
 				Delete.Handler
 			)
-			.command("backup", "Interact with D1 Backups", (backupArgs) =>
+			.command("backup", "Interact with D1 backups", (backupArgs) =>
 				backupArgs
 					.demandCommand()
 					.command(
@@ -82,7 +82,7 @@ export function d1(yargs: CommonYargsArgv) {
 			// )
 			.command(
 				"execute <database>",
-				"Executed command or SQL file",
+				"Execute a command or SQL file",
 				Execute.Options,
 				Execute.Handler
 			)
@@ -94,24 +94,24 @@ export function d1(yargs: CommonYargsArgv) {
 			)
 			.command(
 				"time-travel",
-				"Use Time Travel to restore, fork or copy a database at a specific point-in-time.",
+				"Use Time Travel to restore, fork or copy a database at a specific point-in-time",
 				(yargs2) =>
 					yargs2
 						.demandCommand()
 						.command(
 							"info <database>",
-							"Retrieve information about a database at a specific point-in-time using Time Travel.",
+							"Retrieve information about a database at a specific point-in-time using Time Travel",
 							TimeTravel.InfoOptions,
 							TimeTravel.InfoHandler
 						)
 						.command(
 							"restore <database>",
-							"Restore a database back to a specific point-in-time.",
+							"Restore a database back to a specific point-in-time",
 							TimeTravel.RestoreOptions,
 							TimeTravel.RestoreHandler
 						)
 			)
-			.command("migrations", "Interact with D1 Migrations", (yargs2) =>
+			.command("migrations", "Interact with D1 migrations", (yargs2) =>
 				yargs2
 					.demandCommand()
 					.command(
@@ -122,13 +122,13 @@ export function d1(yargs: CommonYargsArgv) {
 					)
 					.command(
 						"create <database> <message>",
-						"Create a new Migration",
+						"Create a new migration",
 						Migrations.CreateOptions,
 						Migrations.CreateHandler
 					)
 					.command(
 						"apply <database>",
-						"Apply D1 Migrations",
+						"Apply D1 migrations",
 						Migrations.ApplyOptions,
 						Migrations.ApplyHandler
 					)

packages/wrangler/src/kv/index.ts
@@ -40,21 +40,21 @@ export function registerKvSubcommands(
 	return kvYargs
 		.command(
 			"namespace",
-			`🗂️  Interact with your Workers KV Namespaces`,
+			`Interact with your Workers KV Namespaces`,
 			(namespaceYargs) => {
 				return kvNamespace(namespaceYargs.command(subHelp));
 			}
 		)
 		.command(
 			"key",
-			`🔑 Individually manage Workers KV key-value pairs`,
+			`Individually manage Workers KV key-value pairs`,
 			(keyYargs) => {
 				return kvKey(keyYargs.command(subHelp));
 			}
 		)
 		.command(
 			"bulk",
-			`💪 Interact with multiple Workers KV key-value pairs at once`,
+			`Interact with multiple Workers KV key-value pairs at once`,
 			(bulkYargs) => {
 				return kvBulk(bulkYargs.command(subHelp));
 			}
@@ -118,7 +118,7 @@ export function kvNamespace(kvYargs: CommonYargsArgv) {
 		)
 		.command(
 			"list",
-			"Outputs a list of all KV namespaces associated with your account id.",
+			"Output a list of all KV namespaces associated with your account id",
 			(listArgs) => listArgs,
 			async (args) => {
 				const config = readConfig(args.config, args);
@@ -137,7 +137,7 @@ export function kvNamespace(kvYargs: CommonYargsArgv) {
 		)
 		.command(
 			"delete",
-			"Deletes a given namespace.",
+			"Delete a given namespace.",
 			(yargs) => {
 				return yargs
 					.option("binding", {
@@ -203,7 +203,7 @@ export const kvKey = (kvYargs: CommonYargsArgv) => {
 	return kvYargs
 		.command(
 			"put <key> [value]",
-			"Writes a single key/value pair to the given namespace.",
+			"Write a single key/value pair to the given namespace",
 			(yargs) => {
 				return yargs
 					.positional("key", {
@@ -322,7 +322,7 @@ export const kvKey = (kvYargs: CommonYargsArgv) => {
 		)
 		.command(
 			"list",
-			"Outputs a list of all keys in a given namespace.",
+			"Output a list of all keys in a given namespace",
 			(yargs) => {
 				return yargs
 					.option("binding", {
@@ -388,7 +388,7 @@ export const kvKey = (kvYargs: CommonYargsArgv) => {
 		)
 		.command(
 			"get <key>",
-			"Reads a single value by key from the given namespace.",
+			"Read a single value by key from the given namespace",
 			(yargs) => {
 				return yargs
 					.positional("key", {
@@ -478,7 +478,7 @@ export const kvKey = (kvYargs: CommonYargsArgv) => {
 		)
 		.command(
 			"delete <key>",
-			"Removes a single key value pair from the given namespace.",
+			"Remove a single key value pair from the given namespace",
 			(yargs) => {
 				return yargs
 					.positional("key", {

packages/wrangler/src/pages/index.ts
@@ -30,7 +30,7 @@ export function pages(yargs: CommonYargsArgv, subHelp: SubHelp) {
 			.command(subHelp)
 			.command(
 				"dev [directory] [-- command..]",
-				"🧑‍💻 Develop your full-stack Pages application locally",
+				"Develop your full-stack Pages application locally",
 				Dev.Options,
 				Dev.Handler
 			)
@@ -60,7 +60,7 @@ export function pages(yargs: CommonYargsArgv, subHelp: SubHelp) {
 						Functions.OptimizeRoutesHandler
 					)
 			)
-			.command("project", "⚡️ Interact with your Pages projects", (args) =>
+			.command("project", "Interact with your Pages projects", (args) =>
 				args
 					.command(subHelp)
 					.command(
@@ -91,7 +91,7 @@ export function pages(yargs: CommonYargsArgv, subHelp: SubHelp) {
 			)
 			.command(
 				"deployment",
-				"🚀 Interact with the deployments of a project",
+				"Interact with the deployments of a project",
 				(args) =>
 					args
 						.command(subHelp)
@@ -103,7 +103,7 @@ export function pages(yargs: CommonYargsArgv, subHelp: SubHelp) {
 						)
 						.command(
 							"create [directory]",
-							"🆙 Publish a directory of static assets as a Pages deployment",
+							"Publish a directory of static assets as a Pages deployment",
 							Deploy.Options,
 							Deploy.Handler
 						)
@@ -117,16 +117,16 @@ export function pages(yargs: CommonYargsArgv, subHelp: SubHelp) {
 			)
 			.command(
 				["deploy [directory]", "publish [directory]"],
-				"🆙 Deploy a directory of static assets as a Pages deployment",
+				"Deploy a directory of static assets as a Pages deployment",
 				Deploy.Options,
 				Deploy.Handler
 			)
 			.command(
 				"secret",
-				"🤫 Generate a secret that can be referenced in a Pages project",
+				"Generate a secret that can be referenced in a Pages project",
 				(secretYargs) => secret(secretYargs, subHelp)
 			)
-			.command("download", "⚡️ Download settings from your project", (args) =>
+			.command("download", "Download settings from your project", (args) =>
 				args.command(
 					"config [projectName]",
 					"Experimental: Download your Pages project config as a wrangler.toml file",

packages/wrangler/src/queues/cli/commands/index.ts
@@ -24,7 +24,7 @@ export function queues(yargs: CommonYargsArgv) {
 
 	yargs.command(
 		"consumer",
-		"Configure Queue Consumers",
+		"Configure Queue consumers",
 		async (consumersYargs) => {
 			await consumers(consumersYargs);
 		}

packages/wrangler/src/secret/index.ts
@@ -330,7 +330,7 @@ export const secret = (secretYargs: CommonYargsArgv) => {
 		)
 		.command(
 			"bulk [json]",
-			"🗄️  Bulk upload secrets for a Worker",
+			"Bulk upload secrets for a Worker",
 			secretBulkOptions,
 			secretBulkHandler
 		);

packages/wrangler/src/versions/deployments/index.ts
@@ -18,13 +18,13 @@ export default function registerVersionsDeploymentsSubcommands(
 	versionDeploymentsYargs
 		.command(
 			"list",
-			"Displays the 10 most recent deployments of your Worker [beta]",
+			"Displays the 10 most recent deployments of your Worker",
 			versionsDeploymentsListOptions,
 			versionsDeploymentsListHandler
 		)
 		.command(
 			"status",
-			"See the current state of your production [beta]",
+			"View the current state of your production",
 			versionsDeploymentsStatusOptions,
 			versionsDeploymentsStatusHandler
 		)

packages/wrangler/src/versions/rollback/index.ts
@@ -20,12 +20,13 @@ export type VersionsRollbackArgs = StrictYargsOptionsToInterface<
 export default function registerVersionsRollbackCommand(
 	yargs: CommonYargsArgv,
 	epilogue: string,
-	subHelp: SubHelp
+	subHelp: SubHelp,
+	description = "🔙 Rollback to a Worker Version"
 ) {
 	return yargs
 		.command(
 			"rollback [version-id]",
-			"🔙 Rollback to a Worker Version",
+			description,
 			versionsRollbackOptions,
 			versionsRollbackHandler
 		)

commit 11b5058f5022b58e258542e300417d05ddb1b678

fix: the docs command should not crash if given search terms
Fixes a regression accidentally introduced by #3735.

packages/wrangler/src/index.ts
@@ -340,7 +340,7 @@ export function createCLIParser(argv: string[]) {
 	/******************************************************/
 	// docs
 	wrangler.command(
-		"docs [command]",
+		"docs [command..]",
 		"📚 Open Wrangler's command documentation in your browser\n",
 		docsOptions,
 		docsHandler

packages/wrangler/src/__tests__/docs.test.ts
@@ -1,0 +1,122 @@
+import { http, HttpResponse } from "msw";
+import { afterEach, beforeEach, describe, test } from "vitest";
+import openInBrowser from "../open-in-browser";
+import { mockConsoleMethods } from "./helpers/mock-console";
+import { msw } from "./helpers/msw";
+import { runWrangler } from "./helpers/run-wrangler";
+
+// NOTE: in production builds we "esbuild define" Algolia constants as globals
+// but in tests we have to attach mocks values to the globalThis object.
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+declare module globalThis {
+	let ALGOLIA_APP_ID: string | undefined;
+	let ALGOLIA_PUBLIC_KEY: string | undefined;
+}
+
+describe("wrangler docs", () => {
+	const std = mockConsoleMethods();
+
+	beforeEach(() => {
+		globalThis.ALGOLIA_APP_ID = "FAKE-ID";
+		globalThis.ALGOLIA_PUBLIC_KEY = "FAKE-KEY";
+
+		msw.use(
+			http.post<Record<string, never>, { params: string | undefined }>(
+				`*/1/indexes/developers-cloudflare2/query`,
+				async ({ request }) => {
+					const json = await request.json();
+					const params = new URLSearchParams(json.params);
+					return HttpResponse.json({
+						hits: [
+							{
+								url: "FAKE_DOCS_URL?" + params.get("query"),
+							},
+						],
+					});
+				},
+				{ once: true }
+			)
+		);
+	});
+
+	afterEach(() => {
+		delete globalThis.ALGOLIA_APP_ID;
+		delete globalThis.ALGOLIA_PUBLIC_KEY;
+	});
+
+	test("--help", async ({ expect }) => {
+		const result = runWrangler("docs --help");
+
+		await expect(result).resolves.toBeUndefined();
+		expect(std.out).toMatchInlineSnapshot(`
+			"wrangler docs [command..]
+
+			📚 Open Wrangler's command documentation in your browser
+
+
+			POSITIONALS
+			  command  Enter the wrangler command you want to know more about  [array] [default: []]
+
+			GLOBAL FLAGS
+			  -j, --experimental-json-config  Experimental: support wrangler.json  [boolean]
+			  -c, --config                    Path to .toml configuration file  [string]
+			  -e, --env                       Environment to use for operations and .env files  [string]
+			  -h, --help                      Show help  [boolean]
+			  -v, --version                   Show version number  [boolean]
+
+			OPTIONS
+			  -y, --yes  Takes you to the docs, even if search fails  [boolean]"
+		`);
+	});
+
+	test("opens a browser to Cloudflare docs when given no search term", async ({
+		expect,
+	}) => {
+		await runWrangler("docs");
+		expect(std).toMatchInlineSnapshot(`
+			Object {
+			  "debug": "",
+			  "err": "",
+			  "info": "",
+			  "out": "Opening a link in your default browser: https://developers.cloudflare.com/workers/wrangler/commands/",
+			  "warn": "",
+			}
+		`);
+		expect(openInBrowser).toHaveBeenCalledWith(
+			"https://developers.cloudflare.com/workers/wrangler/commands/"
+		);
+	});
+
+	test("opens a browser to Cloudflare docs when given a single search term", async ({
+		expect,
+	}) => {
+		await runWrangler("docs dev");
+		expect(std).toMatchInlineSnapshot(`
+			Object {
+			  "debug": "",
+			  "err": "",
+			  "info": "",
+			  "out": "Opening a link in your default browser: FAKE_DOCS_URL?dev",
+			  "warn": "",
+			}
+		`);
+		expect(openInBrowser).toHaveBeenCalledWith("FAKE_DOCS_URL?dev");
+	});
+
+	test("opens a browser to Cloudflare docs when given multiple search terms", async ({
+		expect,
+	}) => {
+		await runWrangler("docs foo bar");
+		expect(std).toMatchInlineSnapshot(`
+			Object {
+			  "debug": "",
+			  "err": "",
+			  "info": "",
+			  "out": "Opening a link in your default browser: FAKE_DOCS_URL?foo bar",
+			  "warn": "",
+			}
+		`);
+		expect(openInBrowser).toHaveBeenCalledWith("FAKE_DOCS_URL?foo bar");
+	});
+});