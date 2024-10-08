1320f20b38d7b4623fe21d38118bdc9fb8514a99
Andy Jessop <andrewdjessop@gmail.com>
2024-10-08 12:08:03 +0200

    Fix test (#6923)
    * Fix test
    
    * chore: add changeset
    
    * chore: the actual fix
    
    * fix: add extra line in test
    
    * chore: hide eslint rule from output as it's too noisy

--- packages/wrangler/src/__tests__/type-generation.test.ts
+++ packages/wrangler/src/__tests__/type-generation.test.ts
@@ -444,8 +444,9 @@ describe("generateTypes()", () => {
 			);
 
 			await runWrangler("types");
-			expect(fs.readFileSync("./worker-configuration.d.ts", "utf-8")).toMatch(
-				/interface Env \{\s*\}/
+
+			expect(fs.readFileSync("./worker-configuration.d.ts", "utf-8")).toContain(
+				`// eslint-disable-next-line @typescript-eslint/no-empty-interface,@typescript-eslint/no-empty-object-type\ninterface Env {\n}`
 			);
 			expect(std.out).toMatchInlineSnapshot(`
 			"Generating project types...
