0ddcef85c7009b0171056dc348d3d6dfebf843ef
Andy Jessop <ajessop@cloudflare.com>
2024-10-14 18:31:56 +0200

    chore: fix entrypoint logic and refactor

---
+++ packages/wrangler/src/deployment-bundle/resolve-entry.ts
@@ -0,0 +1,48 @@
+import path from "path";
+import { getBasePath } from "../paths";
+
+export function resolveEntryWithScript(script: string): {
+	absolutePath: string;
+	relativePath: string;
+} {
+	const file = path.resolve(script);
+	const relativePath = path.relative(process.cwd(), file) || ".";
+	return { absolutePath: file, relativePath };
+}
+
+export function resolveEntryWithMain(
+	main: string,
+	configPath?: string
+): {
+	absolutePath: string;
+	relativePath: string;
+} {
+	const directory = path.resolve(path.dirname(configPath ?? "."));
+	const file = path.resolve(directory, main);
+	const relativePath = path.relative(directory, file) || ".";
+	return { absolutePath: file, relativePath };
+}
+
+export function resolveEntryWithEntryPoint(
+	entryPoint: string,
+	configPath?: string
+): {
+	absolutePath: string;
+	relativePath: string;
+} {
+	const directory = path.resolve(path.dirname(configPath ?? "."));
+	const file = path.extname(entryPoint)
+		? path.resolve(entryPoint)
+		: path.resolve(entryPoint, "index.js");
+	const relativePath = path.relative(directory, file) || ".";
+	return { absolutePath: file, relativePath };
+}
+
+export function resolveEntryWithAssets(): {
+	absolutePath: string;
+	relativePath: string;
+} {
+	const file = path.resolve(getBasePath(), "templates/no-op-worker.js");
+	const relativePath = path.relative(process.cwd(), file) || ".";
+	return { absolutePath: file, relativePath };
+}
