0cb8b8ca4cdaa5aedbd99564c21fadb2ee61bcc8
Rahul Sethi <5822355+RamIdeas@users.noreply.github.com>
2024-10-17 21:48:45 +0100

    remove commands for another PR

--- packages/wrangler/src/index.ts
+++ packages/wrangler/src/index.ts
@@ -77,7 +77,6 @@ import registerVersionsSubcommands from "./versions";
 import registerVersionsDeploymentsSubcommands from "./versions/deployments";
 import registerVersionsRollbackCommand from "./versions/rollback";
 import { whoami } from "./whoami";
-import { workflows } from "./workflows/workflows";
 import { asJson } from "./yargs-types";
 import type { Config } from "./config";
 import type { LoggerLevel } from "./logger";
@@ -607,11 +606,6 @@ export function createCLIParser(argv: string[]) {
 	// ai
 	wrangler.command("ai", "🤖 Manage AI models", (aiYargs) => {
 		return ai(aiYargs.command(subHelp));
-	});
-
-	// workflows
-	wrangler.command("workflows", false, (workflowArgs) => {
-		return workflows(workflowArgs.command(subHelp), subHelp);
 	});
 
 	// pipelines
