48152d69ee1440764b99e1d9b17656aaa1c1b20e
Rahul Sethi <5822355+RamIdeas@users.noreply.github.com>
2024-10-18 18:14:57 +0100

    Workflows commands (#7015)
    * add commands
    
    * add command description
    
    * change private beta message to open beta
    
    * refactor to use defineCommand util
    
    * add changeset
    
    * update snapshots
    
    * remove extraneous periods
    
    * remove redundant file
    
    Co-authored-by: Luís Duarte <lduarte@cloudflare.com>

--- packages/wrangler/src/__tests__/index.test.ts
+++ packages/wrangler/src/__tests__/index.test.ts
@@ -61,6 +61,7 @@ describe("wrangler", () => {
 				  wrangler pubsub                 📮 Manage Pub/Sub brokers [private beta]
 				  wrangler dispatch-namespace     🏗️  Manage dispatch namespaces
 				  wrangler ai                     🤖 Manage AI models
+				  wrangler workflows              🔁 Manage Workflows [open-beta]
 				  wrangler login                  🔓 Login to Cloudflare
 				  wrangler logout                 🚪 Logout from Cloudflare
 				  wrangler whoami                 🕵️  Retrieve your user information
@@ -117,6 +118,7 @@ describe("wrangler", () => {
 				  wrangler pubsub                 📮 Manage Pub/Sub brokers [private beta]
 				  wrangler dispatch-namespace     🏗️  Manage dispatch namespaces
 				  wrangler ai                     🤖 Manage AI models
+				  wrangler workflows              🔁 Manage Workflows [open-beta]
 				  wrangler login                  🔓 Login to Cloudflare
 				  wrangler logout                 🚪 Logout from Cloudflare
 				  wrangler whoami                 🕵️  Retrieve your user information
---
+++ packages/wrangler/src/workflows/commands/trigger.ts
@@ -0,0 +1,56 @@
+import { fetchResult } from "../../cfetch";
+import { defineCommand } from "../../core";
+import { requireAuth } from "../../user";
+import type { InstanceWithoutDates } from "../types";
+
+defineCommand({
+	command: "wrangler workflows trigger",
+
+	metadata: {
+		description:
+			"Trigger a workflow, creating a new instance. Can optionally take a JSON string to pass a parameter into the workflow instance",
+		owner: "Product: Workflows",
+		status: "open-beta",
+	},
+
+	args: {
+		name: {
+			describe: "Name of the workflow",
+			type: "string",
+			demandOption: true,
+		},
+		params: {
+			describe: "Params for the workflow instance, encoded as a JSON string",
+			type: "string",
+			default: "",
+		},
+	},
+	positionalArgs: ["name", "params"],
+
+	async handler(args, { config, logger }) {
+		const accountId = await requireAuth(config);
+
+		if (args.params.length != 0) {
+			try {
+				JSON.parse(args.params);
+			} catch (e) {
+				logger.error(
+					`Error while parsing instance parameters: "${args.params}" with ${e}' `
+				);
+				return;
+			}
+		}
+
+		const response = await fetchResult<InstanceWithoutDates>(
+			`/accounts/${accountId}/workflows/${args.name}/instances`,
+			{
+				method: "POST",
+				body: args.params.length != 0 ? args.params : undefined,
+			}
+		);
+
+		logger.info(
+			`🚀 Workflow instance "${response.id}" has been queued successfully`
+		);
+	},
+});
