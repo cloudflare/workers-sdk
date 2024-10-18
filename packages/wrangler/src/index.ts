80af061ac87a9c7d44fb0707ff41e629c10cd1b9
Rahul Sethi <5822355+RamIdeas@users.noreply.github.com>
2024-10-18 01:06:19 +0100

    add commands

--- packages/wrangler/src/index.ts
+++ packages/wrangler/src/index.ts
@@ -77,6 +77,7 @@ import registerVersionsSubcommands from "./versions";
 import registerVersionsDeploymentsSubcommands from "./versions/deployments";
 import registerVersionsRollbackCommand from "./versions/rollback";
 import { whoami } from "./whoami";
+import { workflows } from "./workflows/workflows";
 import { asJson } from "./yargs-types";
 import type { Config } from "./config";
 import type { LoggerLevel } from "./logger";
@@ -611,6 +612,11 @@ export function createCLIParser(argv: string[]) {
 	// pipelines
 	wrangler.command("pipelines", false, (pipelinesYargs) => {
 		return pipelines(pipelinesYargs.command(subHelp));
+	});
+
+	// workflows
+	wrangler.command("workflows", false, (workflowArgs) => {
+		return workflows(workflowArgs.command(subHelp), subHelp);
 	});
 
 	/******************** CMD GROUP ***********************/
