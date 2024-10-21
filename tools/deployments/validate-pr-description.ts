77a23ab4b9cdd3280c51c8a60e64b399e13824a2
Somhairle MacLeòid <smacleod@cloudflare.com>
2024-10-21 12:19:55 +0100

    Simplify pull_request_template.md (#6985)

--- tools/deployments/validate-pr-description.ts
+++ tools/deployments/validate-pr-description.ts
@@ -3,7 +3,8 @@ if (require.main === module) {
 	const errors = validateDescription(
 		process.env.TITLE as string,
 		process.env.BODY as string,
-		process.env.LABELS as string
+		process.env.LABELS as string,
+		process.env.FILES as string
 	);
 	if (errors.length > 0) {
 		console.error("Validation errors in PR description:");
@@ -17,12 +18,14 @@ if (require.main === module) {
 export function validateDescription(
 	title: string,
 	body: string,
-	labels: string
+	labels: string,
+	changedFilesJson: string
 ) {
 	const errors: string[] = [];
 
 	console.log("PR:", title);
-	const parsedLabels = JSON.parse(labels);
+
+	const parsedLabels = JSON.parse(labels) as string[];
 
 	if (parsedLabels.includes("skip-pr-description-validation")) {
 		console.log(
@@ -71,14 +74,14 @@ export function validateDescription(
 		);
 	}
 
-	if (
-		!(
-			/- \[x\] Changeset included/i.test(body) ||
-			/- \[x\] Changeset not necessary because: .+/i.test(body)
-		)
-	) {
+	const changedFiles = JSON.parse(changedFilesJson) as string[];
+	const changesetIncluded = changedFiles.some((f) =>
+		f.startsWith(".changeset/")
+	);
+
+	if (!changesetIncluded && !parsedLabels.includes("no-changeset-required")) {
 		errors.push(
-			"Your PR must include a changeset, or provide justification for why no changesets are required"
+			"Your PR doesn't include a changeset. Either include one (following the instructions in CONTRIBUTING.md) or add the 'no-changeset-required' label to bypass this check. Most PRs should have a changeset, so only bypass this check if you're sure that your change doesn't need one: see https://github.com/cloudflare/workers-sdk/blob/main/CONTRIBUTING.md#changesets for more details."
 		);
 	}
 
