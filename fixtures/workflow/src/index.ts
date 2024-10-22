fbe99f3c7051c25b060b25b57ab3718258c4a22d
Rahul Sethi <5822355+RamIdeas@users.noreply.github.com>
2024-10-22 12:46:10 +0100

    fixture tests

--- fixtures/workflow/src/index.ts
+++ fixtures/workflow/src/index.ts
@@ -8,18 +8,20 @@ import {
 type Params = {
 	name: string;
 };
+
 export class Demo extends WorkflowEntrypoint<{}, Params> {
 	async run(event: WorkflowEvent<Params>, step: WorkflowStep) {
 		const { timestamp, payload } = event;
-		const result = await step.do("First step", async function () {
+
+		const result = await step.do("First step", {}, async function () {
 			return {
 				output: "First step result",
 			};
 		});
 
-		await step.sleep("Wait", "1 minute");
+		await step.sleep("Wait", "5 seconds");
 
-		const result2 = await step.do("Second step", async function () {
+		const result2 = await step.do("Second step", {}, async function () {
 			return {
 				output: "Second step result",
 			};
@@ -38,11 +40,22 @@ type Env = {
 	WORKFLOW: Workflow;
 };
 export default class extends WorkerEntrypoint<Env> {
-	async fetch() {
-		const handle = await this.env.WORKFLOW.create({
-			name: crypto.randomUUID(),
-		});
-		// await handle.pause();
+	async fetch(req: Request) {
+		const url = new URL(req.url);
+		const name = url.searchParams.get("workflowName");
+
+		if (url.pathname === "/favicon.ico") {
+			return new Response(null, { status: 404 });
+		}
+
+		let handle: Instance;
+		if (url.pathname === "/create") {
+			handle = await this.env.WORKFLOW.create({ name });
+		} else {
+			// @ts-ignore getByName exists in the next version of workerd
+			handle = await this.env.WORKFLOW.getByName(name);
+		}
+
 		return Response.json(await handle.status());
 	}
 }
