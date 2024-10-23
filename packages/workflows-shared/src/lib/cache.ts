4c500dd1f31d623150423e5fd5f018540a252556
Rahul Sethi <5822355+RamIdeas@users.noreply.github.com>
2024-10-23 22:44:22 +0100

    run prettier

--- packages/workflows-shared/src/lib/cache.ts
+++ packages/workflows-shared/src/lib/cache.ts
@@ -1,8 +1,10 @@
 export async function computeHash(value: string) {
 	const msgUint8 = new TextEncoder().encode(value); // encode as (utf-8) Uint8Array
-	const hashBuffer = await crypto.subtle.digest('SHA-1', msgUint8); // hash the message
+	const hashBuffer = await crypto.subtle.digest("SHA-1", msgUint8); // hash the message
 	const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
-	const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
+	const hashHex = hashArray
+		.map((b) => b.toString(16).padStart(2, "0"))
+		.join(""); // convert bytes to hex string
 
 	return hashHex;
 }
--- packages/workflows-shared/src/lib/validators.ts
+++ packages/workflows-shared/src/lib/validators.ts
@@ -1,6 +1,6 @@
 export const MAX_STEP_NAME_LENGTH = 256;
 // eslint-disable-next-line no-control-regex
-const CONTROL_CHAR_REGEX = new RegExp('[\x00-\x1F]');
+const CONTROL_CHAR_REGEX = new RegExp("[\x00-\x1F]");
 
 export function validateStepName(string: string): boolean {
 	if (string.length > MAX_STEP_NAME_LENGTH) {
