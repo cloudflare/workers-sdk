9781ec8c6ad64378c123094a9bff4cde152632d5
Samuel Macleod <smacleod@cloudflare.com>
2024-10-25 09:33:35 +0100

    fix types

--- packages/pages-shared/environment-polyfills/miniflare.ts
+++ packages/pages-shared/environment-polyfills/miniflare.ts
@@ -2,6 +2,7 @@ import { polyfill } from ".";
 
 export default async () => {
 	const { HTMLRewriter } = await import("@miniflare/html-rewriter");
+	// @ts-expect-error We should properly clean up the Miniflare v2 v3 mixing in this package at some point
 	const mf = await import("miniflare");
 	polyfill({
 		fetch: mf.fetch,
