fd76c0a1a9913150d9cff66ebefd9f24c04a4c19
Edmund Hung <edmund@cloudflare.com>
2024-11-15 15:45:14 +0000

    implement common event properties

--- packages/wrangler/src/metrics/metrics-dispatcher.ts
+++ packages/wrangler/src/metrics/metrics-dispatcher.ts
@@ -1,15 +1,38 @@
 import { fetch } from "undici";
+import whichPmRuns from "which-pm-runs";
 import { version as wranglerVersion } from "../../package.json";
 import { logger } from "../logger";
-import { getMetricsConfig } from "./metrics-config";
+import { getMetricsConfig, readMetricsConfig } from "./metrics-config";
 import type { MetricsConfigOptions } from "./metrics-config";
+import type { CommonEventProperties } from "./send-event";
 
 // The SPARROW_SOURCE_KEY is provided at esbuild time as a `define` for production and beta
 // releases. Otherwise it is left undefined, which automatically disables metrics requests.
 declare const SPARROW_SOURCE_KEY: string;
 const SPARROW_URL = "https://sparrow.cloudflare.com";
 
+function getPlatform() {
+	const platform = process.platform;
+
+	switch (platform) {
+		case "win32":
+			return "Windows";
+		case "darwin":
+			return "Mac OS";
+		case "linux":
+			return "Linux";
+		default:
+			return `Others: ${platform}`;
+	}
+}
+
 export async function getMetricsDispatcher(options: MetricsConfigOptions) {
+	const platform = getPlatform();
+	const packageManager = whichPmRuns()?.name;
+	const isFirstUsage = readMetricsConfig().permission === undefined;
+	const amplitude_session_id = Date.now();
+	let amplitude_event_id = 0;
+
 	return {
 		/**
 		 * Dispatch a event to the analytics target.
@@ -60,13 +83,20 @@ export async function getMetricsDispatcher(options: MetricsConfigOptions) {
 		}
 
 		logger.debug(`Metrics dispatcher: Posting data ${JSON.stringify(event)}`);
+		const commonEventProperties: CommonEventProperties = {
+			amplitude_session_id,
+			amplitude_event_id: amplitude_event_id++,
+			wranglerVersion,
+			platform,
+			packageManager,
+			isFirstUsage,
+		};
 		const body = JSON.stringify({
 			deviceId: metricsConfig.deviceId,
 			event: event.name,
+			timestamp: Date.now(),
 			properties: {
-				category: "Workers",
-				wranglerVersion,
-				os: process.platform + ":" + process.arch,
+				...commonEventProperties,
 				...event.properties,
 			},
 		});
