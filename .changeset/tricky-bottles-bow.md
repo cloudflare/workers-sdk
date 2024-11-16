ab1ddb10b59ba8408daac1a35fe77e36bae3a970
Somhairle MacLeòid <smacleod@cloudflare.com>
2024-11-16 00:34:05 +0000

    Default `--x-registry` to on (#7081)

---
+++ .changeset/tricky-bottles-bow.md
@@ -0,0 +1,5 @@
+---
+"wrangler": patch
+---
+
+Default the file based registry (`--x-registry`) to on. This should improve stability of multi-worker development
--- packages/wrangler/src/dev.ts
+++ packages/wrangler/src/dev.ts
@@ -314,7 +314,7 @@ const command = defineCommand({
 			type: "boolean",
 			describe:
 				"Use the experimental file based dev registry for multi-worker development",
-			default: false,
+			default: true,
 		},
 		"experimental-vectorize-bind-to-prod": {
 			type: "boolean",
