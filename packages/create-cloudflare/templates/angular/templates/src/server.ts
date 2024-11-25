8f6da45b258e6fcf09128b3f29ada6ae6b9f331b
jjong <jjong84@gmail.com>
2024-11-26 05:32:00 +0900

    fixed: related to issues-7341 AngularAppEngine class does not have .render, instead it should be .handle method

--- packages/create-cloudflare/templates/angular/templates/src/server.ts
+++ packages/create-cloudflare/templates/angular/templates/src/server.ts
@@ -6,7 +6,7 @@ const angularApp = new AngularAppEngine();
  * This is a request handler used by the Angular CLI (dev-server and during build).
  */
 export const reqHandler = createRequestHandler(async (req) => {
-	const res = await angularApp.render(req);
+	const res = await angularApp.handle(req);
 
 	return res ?? new Response('Page not found.', { status: 404 });
 });
