9b6143ef31f79b36388db63130d1872500c689a6
Samuel Macleod <smacleod@cloudflare.com>
2024-12-02 13:27:53 +0000

    Ignore failures

--- tools/deployments/open-v3-pr.ts
+++ tools/deployments/open-v3-pr.ts
@@ -10,10 +10,14 @@ if (require.main === module) {
 
 		execSync(`git push origin HEAD --force`);
 
-		// Open PR
-		execSync(
-			`gh pr create --head v3-maintenance-${process.env.PR_NUMBER} --label "skip-pr-description-validation" --title "Backport #${process.env.PR_NUMBER} to Wrangler v3" --body "This is an automatically opened PR to backport patch changes from #${process.env.PR_NUMBER} to Wrangler v3"`
-		);
+		try {
+			// Open PR
+			execSync(
+				`gh pr create --head v3-maintenance-${process.env.PR_NUMBER} --label "skip-pr-description-validation" --title "Backport #${process.env.PR_NUMBER} to Wrangler v3" --body "This is an automatically opened PR to backport patch changes from #${process.env.PR_NUMBER} to Wrangler v3"`
+			);
+		} catch {
+			// Ignore "PR already created failures"
+		}
 	}
 }
 
