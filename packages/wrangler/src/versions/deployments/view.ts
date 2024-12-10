204e92e1241f35912501a0251dddd2abb26210c0
Andy Jessop <ajessop@cloudflare.com>
2024-12-10 10:53:47 +0100

    chore: convert deployments to createCommand
    kebab case for positional
    
    remove print banner
    
    chore: register namesapce

--- packages/wrangler/src/versions/deployments/view.ts
+++ packages/wrangler/src/versions/deployments/view.ts
@@ -1,41 +1,36 @@
+import { createCommand } from "../../core/create-command";
 import { UserError } from "../../errors";
 import { printWranglerBanner } from "../../wrangler-banner";
-import type {
-	CommonYargsArgv,
-	StrictYargsOptionsToInterface,
-} from "../../yargs-types";
 
-export type VersionsDeploymentsViewArgs = StrictYargsOptionsToInterface<
-	typeof versionsDeploymentsViewOptions
->;
-
-export function versionsDeploymentsViewOptions(yargs: CommonYargsArgv) {
-	return yargs
-		.option("name", {
+export const deploymentsViewCommand = createCommand({
+	metadata: {
+		description: "View a deployment of a Worker",
+		owner: "Workers: Authoring and Testing",
+		status: "stable",
+	},
+	args: {
+		name: {
 			describe: "Name of the worker",
 			type: "string",
 			requiresArg: true,
-		})
-		.positional("deployment-id", {
+		},
+		["deployment-id"]: {
 			describe:
 				"Deprecated. Deployment ID is now referred to as Version ID. Please use `wrangler versions view [version-id]` instead.",
 			type: "string",
 			requiresArg: true,
-		});
-}
-
-export async function versionsDeploymentsViewHandler(
-	args: VersionsDeploymentsViewArgs
-) {
-	await printWranglerBanner();
-
-	if (args.deploymentId === undefined) {
-		throw new UserError(
-			"`wrangler deployments view` has been renamed `wrangler deployments status`. Please use that command instead."
-		);
-	} else {
-		throw new UserError(
-			"`wrangler deployments view <deployment-id>` has been renamed `wrangler versions view [version-id]`. Please use that command instead."
-		);
-	}
-}
+		},
+	},
+	positionalArgs: ["deployment-id"],
+	handler: async function versionsDeploymentsViewHandler(args) {
+		if (args.deploymentId === undefined) {
+			throw new UserError(
+				"`wrangler deployments view` has been renamed `wrangler deployments status`. Please use that command instead."
+			);
+		} else {
+			throw new UserError(
+				"`wrangler deployments view <deployment-id>` has been renamed `wrangler versions view [version-id]`. Please use that command instead."
+			);
+		}
+	},
+});
