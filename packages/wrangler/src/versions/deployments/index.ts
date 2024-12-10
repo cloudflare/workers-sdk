d37079f64a61618b386cc274e63c4f45212369c8
Andy Jessop <ajessop@cloudflare.com>
2024-12-10 10:53:47 +0100

    chore: convert deployments to createCommand
    kebab case for positional
    
    remove print banner
    
    chore: register namesapce

--- packages/wrangler/src/versions/deployments/index.ts
+++ packages/wrangler/src/versions/deployments/index.ts
@@ -1,37 +1,10 @@
-import {
-	versionsDeploymentsListHandler,
-	versionsDeploymentsListOptions,
-} from "./list";
-import {
-	versionsDeploymentsStatusHandler,
-	versionsDeploymentsStatusOptions,
-} from "./status";
-import {
-	versionsDeploymentsViewHandler,
-	versionsDeploymentsViewOptions,
-} from "./view";
-import type { CommonYargsArgv } from "../../yargs-types";
+import { createNamespace } from "../../core/create-command";
 
-export default function registerVersionsDeploymentsSubcommands(
-	versionDeploymentsYargs: CommonYargsArgv
-) {
-	versionDeploymentsYargs
-		.command(
-			"list",
-			"Displays the 10 most recent deployments of your Worker",
-			versionsDeploymentsListOptions,
-			versionsDeploymentsListHandler
-		)
-		.command(
-			"status",
-			"View the current state of your production",
-			versionsDeploymentsStatusOptions,
-			versionsDeploymentsStatusHandler
-		)
-		.command(
-			"view [deployment-id]",
-			false,
-			versionsDeploymentsViewOptions,
-			versionsDeploymentsViewHandler
-		);
-}
+export const deploymentsNamespace = createNamespace({
+	metadata: {
+		description:
+			"🚢 List and view the current and past deployments for your Worker",
+		owner: "Workers: Authoring and Testing",
+		status: "stable",
+	},
+});
--- packages/wrangler/src/versions/deployments/list.ts
+++ packages/wrangler/src/versions/deployments/list.ts
@@ -1,7 +1,7 @@
 import assert from "assert";
 import { logRaw } from "@cloudflare/cli";
 import { brandColor, gray } from "@cloudflare/cli/colors";
-import { readConfig } from "../../config";
+import { createCommand } from "../../core/create-command";
 import { UserError } from "../../errors";
 import * as metrics from "../../metrics";
 import { requireAuth } from "../../user";
@@ -9,107 +9,100 @@ import formatLabelledValues from "../../utils/render-labelled-values";
 import { printWranglerBanner } from "../../wrangler-banner";
 import { fetchLatestDeployments, fetchVersions } from "../api";
 import { getVersionSource } from "../list";
-import type {
-	CommonYargsArgv,
-	StrictYargsOptionsToInterface,
-} from "../../yargs-types";
 import type { ApiDeployment, VersionCache } from "../types";
 
 const BLANK_INPUT = "-"; // To be used where optional user-input is displayed and the value is nullish
 
-export type VersionsDeloymentsListArgs = StrictYargsOptionsToInterface<
-	typeof versionsDeploymentsListOptions
->;
-
-export function versionsDeploymentsListOptions(yargs: CommonYargsArgv) {
-	return yargs
-		.option("name", {
+export const deploymentsListCommand = createCommand({
+	metadata: {
+		description: "Displays the 10 most recent deployments of your Worker",
+		owner: "Workers: Authoring and Testing",
+		status: "stable",
+	},
+	args: {
+		name: {
 			describe: "Name of the worker",
 			type: "string",
 			requiresArg: true,
-		})
-		.option("json", {
+		},
+		json: {
 			describe: "Display output as clean JSON",
 			type: "boolean",
 			default: false,
-		});
-}
-
-export async function versionsDeploymentsListHandler(
-	args: VersionsDeloymentsListArgs
-) {
-	if (!args.json) {
-		await printWranglerBanner();
-	}
-
-	const config = readConfig(args);
-	metrics.sendMetricsEvent(
-		"list versioned deployments",
-		{ json: args.json },
-		{
-			sendMetrics: config.send_metrics,
-		}
-	);
-
-	const accountId = await requireAuth(config);
-	const workerName = args.name ?? config.name;
-
-	if (workerName === undefined) {
-		throw new UserError(
-			'You need to provide a name of your worker. Either pass it as a cli arg with `--name <name>` or in your config file as `name = "<name>"`'
+		},
+	},
+	behaviour: {
+		printBanner: false,
+	},
+	handler: async function versionsDeploymentsListHandler(args, { config }) {
+		metrics.sendMetricsEvent(
+			"list versioned deployments",
+			{ json: args.json },
+			{
+				sendMetrics: config.send_metrics,
+			}
 		);
-	}
 
-	const deployments = (
-		await fetchLatestDeployments(accountId, workerName)
-	).sort((a, b) => a.created_on.localeCompare(b.created_on));
+		const accountId = await requireAuth(config);
+		const workerName = args.name ?? config.name;
 
-	if (args.json) {
-		logRaw(JSON.stringify(deployments, null, 2));
-		return;
-	}
-
-	const versionCache: VersionCache = new Map();
-	const versionIds = deployments.flatMap((d) =>
-		d.versions.map((v) => v.version_id)
-	);
-	await fetchVersions(accountId, workerName, versionCache, ...versionIds);
-
-	const formattedDeployments = deployments.map((deployment) => {
-		const formattedVersions = deployment.versions.map((traffic) => {
-			const version = versionCache.get(traffic.version_id);
-			assert(version);
-
-			const percentage = brandColor(`(${traffic.percentage}%)`);
-			const details = formatLabelledValues(
-				{
-					Created: new Date(version.metadata["created_on"]).toISOString(),
-					Tag: version.annotations?.["workers/tag"] || BLANK_INPUT,
-					Message: version.annotations?.["workers/message"] || BLANK_INPUT,
-				},
-				{
-					indentationCount: 4,
-					labelJustification: "right",
-					formatLabel: (label) => gray(label + ":"),
-					formatValue: (value) => gray(value),
-				}
+		if (workerName === undefined) {
+			throw new UserError(
+				'You need to provide a name of your worker. Either pass it as a cli arg with `--name <name>` or in your config file as `name = "<name>"`'
 			);
+		}
 
-			return `${percentage} ${version.id}\n${details}`;
+		const deployments = (
+			await fetchLatestDeployments(accountId, workerName)
+		).sort((a, b) => a.created_on.localeCompare(b.created_on));
+
+		if (args.json) {
+			logRaw(JSON.stringify(deployments, null, 2));
+			return;
+		}
+
+		const versionCache: VersionCache = new Map();
+		const versionIds = deployments.flatMap((d) =>
+			d.versions.map((v) => v.version_id)
+		);
+		await fetchVersions(accountId, workerName, versionCache, ...versionIds);
+
+		const formattedDeployments = deployments.map((deployment) => {
+			const formattedVersions = deployment.versions.map((traffic) => {
+				const version = versionCache.get(traffic.version_id);
+				assert(version);
+
+				const percentage = brandColor(`(${traffic.percentage}%)`);
+				const details = formatLabelledValues(
+					{
+						Created: new Date(version.metadata["created_on"]).toISOString(),
+						Tag: version.annotations?.["workers/tag"] || BLANK_INPUT,
+						Message: version.annotations?.["workers/message"] || BLANK_INPUT,
+					},
+					{
+						indentationCount: 4,
+						labelJustification: "right",
+						formatLabel: (label) => gray(label + ":"),
+						formatValue: (value) => gray(value),
+					}
+				);
+
+				return `${percentage} ${version.id}\n${details}`;
+			});
+
+			return formatLabelledValues({
+				// explicitly not outputting Deployment ID
+				Created: new Date(deployment.created_on).toISOString(),
+				Author: deployment.author_email,
+				Source: getDeploymentSource(deployment),
+				Message: deployment.annotations?.["workers/message"] || BLANK_INPUT,
+				"Version(s)": formattedVersions.join("\n\n"),
+			});
 		});
 
-		return formatLabelledValues({
-			// explicitly not outputting Deployment ID
-			Created: new Date(deployment.created_on).toISOString(),
-			Author: deployment.author_email,
-			Source: getDeploymentSource(deployment),
-			Message: deployment.annotations?.["workers/message"] || BLANK_INPUT,
-			"Version(s)": formattedVersions.join("\n\n"),
-		});
-	});
-
-	logRaw(formattedDeployments.join("\n\n"));
-}
+		logRaw(formattedDeployments.join("\n\n"));
+	},
+});
 
 export function getDeploymentSource(deployment: ApiDeployment) {
 	return getVersionSource({
