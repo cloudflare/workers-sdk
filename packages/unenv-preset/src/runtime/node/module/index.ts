32e06164d8d6f50d5584e2bdb8c8c49e80b23e19
Victor Berchet <victor@suumit.com>
2024-12-17 12:18:28 +0100

    import unenv-preset  (#7370)
    feat(unenv-preset): import the package
    
    sync at unjs/unenv 7e05819
    https://github.com/pi0/unenv-preset-cloudflare
    
    Co-authored-by: Pooya Parsa <pooya@pi0.io>

---
+++ packages/unenv-preset/src/runtime/node/module/index.ts
@@ -0,0 +1,99 @@
+// https://nodejs.org/api/module.html
+import { notImplemented } from "unenv/runtime/_internal/utils";
+import {
+	_cache,
+	_debug,
+	_extensions,
+	_findPath,
+	_initPaths,
+	_load,
+	_nodeModulePaths,
+	_pathCache,
+	_preloadModules,
+	_resolveFilename,
+	_resolveLookupPaths,
+	builtinModules,
+	constants,
+	enableCompileCache,
+	findSourceMap,
+	getCompileCacheDir,
+	globalPaths,
+	isBuiltin,
+	Module,
+	register,
+	runMain,
+	SourceMap,
+	syncBuiltinESMExports,
+	wrap,
+} from "unenv/runtime/node/module/index";
+import type nodeModule from "node:module";
+
+export {
+	Module,
+	SourceMap,
+	_cache,
+	_extensions,
+	_debug,
+	_pathCache,
+	_findPath,
+	_initPaths,
+	_load,
+	_nodeModulePaths,
+	_preloadModules,
+	_resolveFilename,
+	_resolveLookupPaths,
+	builtinModules,
+	constants,
+	enableCompileCache,
+	findSourceMap,
+	getCompileCacheDir,
+	globalPaths,
+	isBuiltin,
+	register,
+	runMain,
+	syncBuiltinESMExports,
+	wrap,
+} from "unenv/runtime/node/module/index";
+
+const workerdModule = process.getBuiltinModule("node:module");
+
+export const createRequire: typeof nodeModule.createRequire = (
+	file: string
+) => {
+	return Object.assign(workerdModule.createRequire(file), {
+		resolve: Object.assign(notImplemented("module.require.resolve"), {
+			paths: notImplemented("module.require.resolve.paths"),
+		}),
+		cache: Object.create(null),
+		extensions: _extensions,
+		main: undefined,
+	});
+};
+
+export default {
+	Module,
+	SourceMap,
+	_cache,
+	_extensions,
+	_debug,
+	_pathCache,
+	_findPath,
+	_initPaths,
+	_load,
+	_nodeModulePaths,
+	_preloadModules,
+	_resolveFilename,
+	_resolveLookupPaths,
+	builtinModules,
+	enableCompileCache,
+	constants,
+	createRequire,
+	findSourceMap,
+	getCompileCacheDir,
+	globalPaths,
+	isBuiltin,
+	register,
+	runMain,
+	syncBuiltinESMExports,
+	wrap,
+};
