3cd179eb82db426269414dedb44c6c04b004e01a
Samuel Macleod <smacleod@cloudflare.com>
2025-01-16 20:06:27 +0000

    Add fixtures using startWorker() and Miniflare directly

---
+++ fixtures/miniflare-node-test/src/index-with-bindings.test.js
@@ -0,0 +1,51 @@
+import assert from "node:assert";
+import test, { after, before, describe } from "node:test";
+import { Miniflare } from "miniflare";
+
+describe("worker", () => {
+	/**
+	 * @type {Miniflare}
+	 */
+	let worker;
+
+	before(async () => {
+		worker = new Miniflare({
+			modules: [
+				{
+					type: "ESModule",
+					path: "src/index.js",
+				},
+			],
+			bindings: {
+				FOO: "Hello Bindings",
+			},
+			kvNamespaces: ["KV"],
+		});
+		await worker.ready;
+	});
+
+	test("hello world", async () => {
+		assert.strictEqual(
+			await (await worker.dispatchFetch("http://example.com")).text(),
+			"Hello World"
+		);
+	});
+
+	test("text binding", async () => {
+		const bindings = await worker.getBindings();
+		assert.strictEqual(bindings.FOO, "Hello Bindings");
+	});
+
+	test("kv binding", async () => {
+		/**
+		 * @type {{KV: import("@cloudflare/workers-types/experimental").KVNamespace, FOO: string}}
+		 */
+		const bindings = await worker.getBindings();
+		await bindings.KV.put("key", "value");
+		assert.strictEqual(await bindings.KV.get("key"), "value");
+	});
+
+	after(async () => {
+		await worker.dispose();
+	});
+});
---
+++ fixtures/miniflare-node-test/src/index.test.js
@@ -0,0 +1,33 @@
+import assert from "node:assert";
+import test, { after, before, describe } from "node:test";
+import { Miniflare } from "miniflare";
+
+describe("worker", () => {
+	/**
+	 * @type {Miniflare}
+	 */
+	let worker;
+
+	before(async () => {
+		worker = new Miniflare({
+			modules: [
+				{
+					type: "ESModule",
+					path: "src/index.js",
+				},
+			],
+		});
+		await worker.ready;
+	});
+
+	test("hello world", async () => {
+		assert.strictEqual(
+			await (await worker.dispatchFetch("http://example.com")).text(),
+			"Hello World"
+		);
+	});
+
+	after(async () => {
+		await worker.dispose();
+	});
+});
