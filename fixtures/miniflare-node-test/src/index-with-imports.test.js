30db2376f49f6d3295ec4ac566b4ec2e1341edc0
Samuel Macleod <smacleod@cloudflare.com>
2025-01-16 20:06:27 +0000

    Add fixtures using startWorker() and Miniflare directly

---
+++ fixtures/miniflare-node-test/src/index-with-imports.test.js
@@ -0,0 +1,34 @@
+import assert from "node:assert";
+import test, { after, before, describe } from "node:test";
+import { Miniflare } from "miniflare";
+
+describe("worker", () => {
+	/**
+	 * @type {Miniflare}
+	 */
+	let worker;
+
+	before(async () => {
+		worker = new Miniflare({
+			scriptPath: "src/index-with-imports.js",
+			modules: true,
+			modulesRules: [{ type: "ESModule", include: ["**/*.js"] }],
+		});
+		try {
+			await worker.ready;
+		} catch (e) {
+			console.error(e);
+		}
+	});
+
+	test("hello world", async () => {
+		assert.strictEqual(
+			await (await worker.dispatchFetch("http://example.com/path")).text(),
+			"Hello from http://example.com/path"
+		);
+	});
+
+	after(async () => {
+		await worker.dispose();
+	});
+});
