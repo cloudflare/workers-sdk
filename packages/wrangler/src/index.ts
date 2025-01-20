0f88d85fcbaaecafa62b960b7b569d80d72420dc
emily-shen <69125074+emily-shen@users.noreply.github.com>
2025-01-20 17:12:41 +0000

    types refactor (#7817)

--- packages/wrangler/src/index.ts
+++ packages/wrangler/src/index.ts
@@ -140,7 +140,7 @@ import {
 } from "./sentry";
 import { tailCommand } from "./tail";
 import registerTriggersSubcommands from "./triggers";
-import { typesHandler, typesOptions } from "./type-generation";
+import { typesCommand } from "./type-generation";
 import { getAuthFromEnv } from "./user";
 import { loginCommand, logoutCommand, whoamiCommand } from "./user/commands";
 import { whoami } from "./user/whoami";
@@ -473,12 +473,8 @@ export function createCLIParser(argv: string[]) {
 	);
 
 	// types
-	wrangler.command(
-		"types [path]",
-		"📝 Generate types from bindings and module rules in configuration\n",
-		typesOptions,
-		typesHandler
-	);
+	registry.define([{ command: "wrangler types", definition: typesCommand }]);
+	registry.registerNamespace("types");
 
 	/******************** CMD GROUP ***********************/
 	registry.define([
