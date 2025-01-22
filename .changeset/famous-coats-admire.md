972d414fb209571a84285259d47e2293455aca01
Berat BAYRAM <berat.bayram@tubitak.gov.tr>
2025-01-22 14:36:22 +0300

    fix(create-cloudflare): removed top-level await in next.config.ts

---
+++ .changeset/famous-coats-admire.md
@@ -0,0 +1,7 @@
+---
+"create-cloudflare": patch
+---
+
+# \[create-cloudflare\] next.js with ts fix #7854
+
+Removed top-level await in next.config.ts
--- packages/create-cloudflare/templates/next/c3.ts
+++ packages/create-cloudflare/templates/next/c3.ts
@@ -43,11 +43,9 @@ const updateNextConfig = (usesTs: boolean) => {
 		`import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 		// Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
-		// (when running the application with \`next dev\`), for more information see:
+		// (when running the application with \`next dev\`) and has no effect outside of development, for more information see:
 		// https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
-		if (process.env.NODE_ENV === 'development') {
-		  await setupDevPlatform();
-		}
+		setupDevPlatform().catch(console.error);
 
 		`.replace(/\n\t*/g, "\n") + configContent;
 
