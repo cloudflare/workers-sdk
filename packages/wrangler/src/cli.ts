ab84eeb57036a9c249e6541815868d1dd46d8cbc
Samuel Macleod <smacleod@cloudflare.com>
2025-01-29 13:21:34 +0000

    remove wrangler change

--- packages/wrangler/src/cli.ts
+++ packages/wrangler/src/cli.ts
@@ -60,5 +60,3 @@ export { generateASSETSBinding as unstable_generateASSETSBinding };
 
 export { experimental_readRawConfig } from "./config";
 export { experimental_patchConfig } from "./config/patch-config";
-
-export const HELLO = "WORLD";
