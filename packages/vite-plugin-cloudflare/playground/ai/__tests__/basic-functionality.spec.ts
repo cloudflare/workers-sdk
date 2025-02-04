b4d16b5ab247638ff15e62d1060ad22e1cb04132
Justin van der Merwe <justnvdm@gmail.com>
2025-02-04 15:48:31 +0200

    Skip e2e test for now until avoid cli interactivity with plugin can be avoided for auth for workers AI API

--- packages/vite-plugin-cloudflare/playground/ai/__tests__/basic-functionality.spec.ts
+++ packages/vite-plugin-cloudflare/playground/ai/__tests__/basic-functionality.spec.ts
@@ -1,6 +1,9 @@
 import { expect, test } from "vitest";
 import { getTextResponse } from "../../__test-utils__";
 
-test("basic hello-world functionality", async () => {
+// todo(justinvdm, 2025-02-04): Since this example uses Workers AI, there is CLI interactivity required to choose and authenticate for the account
+// that should be used when talking to the Workers AI API, making it difficult to run this test automatically. Once the plugin accepts an account id
+// this test might be able to be unskipped.
+test.skip("basic hello-world functionality", async () => {
 	expect(JSON.parse(await getTextResponse())).toContain("PONG");
 });
