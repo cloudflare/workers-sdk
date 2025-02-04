97e86af7c68f8764c5c60b5a4c614b3147947a8c
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-02-04 13:42:37 +0000

    Added setup scripts to Prisma tests

--- packages/vite-plugin-cloudflare/playground/prisma/__tests__/prisma.spec.ts
+++ packages/vite-plugin-cloudflare/playground/prisma/__tests__/prisma.spec.ts
@@ -1,5 +1,18 @@
-import { expect, test } from "vitest";
+import * as child_process from "node:child_process";
+import * as fs from "node:fs";
+import { beforeAll, expect, test } from "vitest";
 import { getJsonResponse, isBuild } from "../../__test-utils__";
+
+beforeAll(() => {
+	fs.rmSync(".wrangler", { recursive: true, force: true });
+	// Apply database migration
+	child_process.execSync(
+		`pnpm wrangler d1 migrations apply prisma-demo-db --local`
+	);
+	// Seed data
+	child_process.execSync(`pnpm wrangler d1 execute prisma-demo-db --command "INSERT INTO  \"User\" (\"email\", \"name\") VALUES
+('jane@prisma.io', 'Jane Doe (Local)');" --local`);
+});
 
 test.runIf(isBuild)("runs D1 query using Prisma", async () => {
 	const result = await getJsonResponse();
