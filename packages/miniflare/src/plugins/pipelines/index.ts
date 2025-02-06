e8eb4478ce18cc826b5183297b5b677f8ae9c49b
Cole MacKenzie <cole@cloudflare.com>
2025-02-06 12:21:05 -0800

    Make pipelines not backed by DO resources
    building off of Emily's feedback on PR #7950 and example commit 56c1e43e39313af99dbd2e46507a8afce97b9a80

--- packages/miniflare/src/plugins/pipelines/index.ts
+++ packages/miniflare/src/plugins/pipelines/index.ts
@@ -1,19 +1,7 @@
-import { SharedBindings } from "miniflare:shared";
 import SCRIPT_PIPELINE_OBJECT from "worker:pipelines/pipeline";
 import { z } from "zod";
-import {
-	kVoid,
-	Service,
-	Worker_Binding_DurableObjectNamespaceDesignator,
-} from "../../runtime";
-import {
-	getMiniflareObjectBindings,
-	namespaceKeys,
-	objectEntryWorker,
-	Plugin,
-	ProxyNodeBinding,
-	SERVICE_LOOPBACK,
-} from "../shared";
+import { Service } from "../../runtime";
+import { namespaceKeys, Plugin, ProxyNodeBinding } from "../shared";
 
 export const PipelineOptionsSchema = z.object({
 	pipelines: z.union([z.record(z.string()), z.string().array()]).optional(),
@@ -21,11 +9,6 @@ export const PipelineOptionsSchema = z.object({
 
 export const PIPELINES_PLUGIN_NAME = "pipelines";
 const SERVICE_PIPELINE_PREFIX = `${PIPELINES_PLUGIN_NAME}:pipeline`;
-const PIPELINE_OBJECT_CLASS_NAME = "Pipeline";
-const PIPELINE_OBJECT: Worker_Binding_DurableObjectNamespaceDesignator = {
-	serviceName: SERVICE_PIPELINE_PREFIX,
-	className: PIPELINE_OBJECT_CLASS_NAME,
-};
 
 export const PIPELINE_PLUGIN: Plugin<typeof PipelineOptionsSchema> = {
 	options: PipelineOptionsSchema,
@@ -42,17 +25,13 @@ export const PIPELINE_PLUGIN: Plugin<typeof PipelineOptionsSchema> = {
 			buckets.map((name) => [name, new ProxyNodeBinding()])
 		);
 	},
-	async getServices({ options, unsafeStickyBlobs }) {
+	async getServices({ options }) {
 		const pipelines = bindingEntries(options.pipelines);
-		const services = pipelines.map<Service>(([_, id]) => ({
-			name: `${SERVICE_PIPELINE_PREFIX}:${id}`,
-			worker: objectEntryWorker(PIPELINE_OBJECT, id),
-		}));
 
-		if (pipelines.length > 0) {
-			const uniqueKey = `miniflare-${PIPELINE_OBJECT_CLASS_NAME}`;
-			const pipelineService: Service = {
-				name: SERVICE_PIPELINE_PREFIX,
+		const services = [];
+		for (const pipeline of pipelines) {
+			services.push({
+				name: `${SERVICE_PIPELINE_PREFIX}:${pipeline[1]}`,
 				worker: {
 					compatibilityDate: "2024-12-30",
 					compatibilityFlags: ["nodejs_compat"],
@@ -62,23 +41,8 @@ export const PIPELINE_PLUGIN: Plugin<typeof PipelineOptionsSchema> = {
 							esModule: SCRIPT_PIPELINE_OBJECT(),
 						},
 					],
-					durableObjectNamespaces: [
-						{
-							className: PIPELINE_OBJECT_CLASS_NAME,
-							uniqueKey,
-						},
-					],
-					durableObjectStorage: { inMemory: kVoid },
-					bindings: [
-						{
-							name: SharedBindings.MAYBE_SERVICE_LOOPBACK,
-							service: { name: SERVICE_LOOPBACK },
-						},
-						...getMiniflareObjectBindings(unsafeStickyBlobs),
-					],
 				},
-			};
-			services.push(pipelineService);
+			});
 		}
 
 		return services;
