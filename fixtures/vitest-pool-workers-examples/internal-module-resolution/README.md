25a2dfa574224b8ff24d5753034965ddfcafb978
Edmund Hung <edmund@cloudflare.com>
2025-02-07 13:04:09 +0000

    merge external-package-resolution and internal-module-resolution example with guidelines on readme

--- fixtures/vitest-pool-workers-examples/internal-module-resolution/README.md
+++
@@ -1,5 +0,0 @@
-# internal-module-resolution
-
-This simple fixture checks that the Vitest integration import resolution works correctly when a CommonJS packages has a require to a directory rather than a specific file.
-
-There is no Worker defined here but we still need a minimal wrangler.toml that we can pass to the vitest-pool-workers plugin to trigger that plugin to handle import resolution and file loading.
--- fixtures/vitest-pool-workers-examples/module-resolution/src/index.ts
+++ fixtures/vitest-pool-workers-examples/module-resolution/src/index.ts
@@ -1,8 +1,8 @@
 import { Toucan } from "toucan-js";
-// Test pre-bundling dependency relies on exports field without a default entrypoint
+// Testing dependency without a main entrypoint
 // @see https://github.com/cloudflare/workers-sdk/issues/6591
 import "discord-api-types/v10";
-// Test pre-bundling dependencies with browser field mapping
+// Testing dependency with browser field mapping
 // @see https://github.com/cloudflare/workers-sdk/issues/6581
 import "@microlabs/otel-cf-workers";
 
