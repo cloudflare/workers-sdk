16a50427858b159472c1ecffbbf681cb2bbe3e2e
Samuel Macleod <smacleod@cloudflare.com>
2025-02-07 13:37:22 +0000

    Don't break bun in C3, just warn

--- packages/create-cloudflare/src/helpers/packages.ts
+++ packages/create-cloudflare/src/helpers/packages.ts
@@ -33,6 +33,10 @@ export const installPackages = async (
 			cmd = "add";
 			saveFlag = config.dev ? "-D" : "";
 			break;
+		case "bun":
+			cmd = "add";
+			saveFlag = config.dev ? "-d" : "";
+			break;
 		case "npm":
 		case "pnpm":
 		default:
