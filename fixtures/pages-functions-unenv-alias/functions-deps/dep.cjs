972e4f5818b3082f4f36302a23f64e2279b6e6f6
0xD34DC0DE <emerickpoulin31415@gmail.com>
2025-02-10 17:56:19 -0500

    fixup! add fixture for #7572
    remove unnecessary packages from fixture

--- fixtures/pages-functions-unenv-alias/functions-deps/dep.cjs
+++ fixtures/pages-functions-unenv-alias/functions-deps/dep.cjs
@@ -1,9 +1,6 @@
-// 2 imports required to trigger https://github.com/cloudflare/workers-sdk/issues/7572
 const fetch = require("cross-fetch");
-const env = require("cross-env");
 
 module.exports = () => {
     return "OK!";
 };
 module.exports.fetch = fetch;
-module.exports.env = env;
--- fixtures/pages-functions-unenv-alias/functions/[[path]].ts
+++ fixtures/pages-functions-unenv-alias/functions/[[path]].ts
@@ -2,7 +2,7 @@ import * as dep from "../functions-deps/dep.cjs";
 
 export const onRequest = () => {
 	const supportsDefaultExports = typeof dep.default === "function" && dep.default() === "OK!";
-	const supportsNamedExports = typeof dep.fetch === "function" && typeof dep.fetch.Headers === "function" && typeof dep.env === "function";
+	const supportsNamedExports = typeof dep.fetch === "function" && typeof dep.fetch.Headers === "function";
 
 	return new Response(
 		supportsDefaultExports && supportsNamedExports ? "OK!" : "KO!",
