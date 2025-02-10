bff209d853ae081ec692c41a080071956bf4ae2d
Berat BAYRAM <beratbayram7@gmail.com>
2025-02-10 18:58:57 +0300

    [create-cloudflare] next.js with ts fix (#7854)
    * resolves #925
    
    * fixes issue name
    
    * fix(create-cloudflare): update version from major to patch
    
    * Revert last 3 commits
    
    * fix(create-cloudflare): removed top-level await in next.config.ts
    
    * fix(create-cloudflare): typo fix
    
    * fix(create-cloudflare): changeset fix
    
    * fix(create-cloudflare): Update .changeset/famous-coats-admire.md
    
    Co-authored-by: Victor Berchet <victor@suumit.com>
    
    * fix(create-cloudflare): Update .changeset/famous-coats-admire.md 2
    
    * fix(create-cloudflare): Update .changeset/famous-coats-admire.md 3
    
    Co-authored-by: Victor Berchet <victor@suumit.com>
    
    * fix(create-cloudflare): pnpm fix on changeset
    
    ---------
    
    Co-authored-by: Berat BAYRAM <berat.bayram@tubitak.gov.tr>
    Co-authored-by: Victor Berchet <victor@suumit.com>
    Co-authored-by: Carmen Popoviciu <cpopoviciu@cloudflare.com>

--- packages/create-cloudflare/templates/next/c3.ts
+++ packages/create-cloudflare/templates/next/c3.ts
@@ -42,12 +42,12 @@ const updateNextConfig = (usesTs: boolean) => {
 	const updatedConfigFile =
 		`import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
-		// Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
-		// (when running the application with \`next dev\`), for more information see:
+		// Here we use the @cloudflare/next-on-pages next-dev module to allow us to
+		// use bindings during local development (when running the application with
+		// \`next dev\`). This function is only necessary during development and
+		// has no impact outside of that. For more information see:
 		// https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
-		if (process.env.NODE_ENV === 'development') {
-		  await setupDevPlatform();
-		}
+		setupDevPlatform().catch(console.error);
 
 		`.replace(/\n\t*/g, "\n") + configContent;
 
