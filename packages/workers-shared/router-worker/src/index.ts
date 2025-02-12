41555f831ddc53807c58470f6a02a5d1d88e5a00
WillTaylorDev <williamtaylor@cloudflare.com>
2025-02-11 19:59:07 -0500

    Pass through version tag as sentry release (#8102)

--- packages/workers-shared/router-worker/src/index.ts
+++ packages/workers-shared/router-worker/src/index.ts
@@ -47,6 +47,7 @@ export default {
 				env.SENTRY_ACCESS_CLIENT_ID,
 				env.SENTRY_ACCESS_CLIENT_SECRET,
 				env.COLO_METADATA,
+				env.VERSION_METADATA,
 				env.CONFIG?.account_id,
 				env.CONFIG?.script_id
 			);
