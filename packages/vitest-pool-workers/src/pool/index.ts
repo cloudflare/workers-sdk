c8e2e8ead8ad5245fc3db5204688c3372ac9c615
Edmund Hung <edmund@cloudflare.com>
2025-02-17 19:53:46 +0000

    replace chars with underscore

--- packages/vitest-pool-workers/src/pool/index.ts
+++ packages/vitest-pool-workers/src/pool/index.ts
@@ -181,7 +181,7 @@ interface Project {
 const allProjects = new Map<string /* projectName */, Project>();
 
 function getRunnerName(project: WorkspaceProject, testFile?: string) {
-	const name = `${WORKER_NAME_PREFIX}runner-${encodeURIComponent(project.getName().replace("@", "-at-").replace("/", "-"))}`;
+	const name = `${WORKER_NAME_PREFIX}runner-${project.getName().replace(/[^a-z0-9-]/gi, "_")}`;
 	if (testFile === undefined) {
 		return name;
 	}
