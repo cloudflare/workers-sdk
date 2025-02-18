3ed673d442fbbfb88184c33915973a08f3ce513f
Edmund Hung <edmund@cloudflare.com>
2025-02-18 17:11:47 +0000

    try without importing vi

--- packages/vitest-pool-workers/src/worker/lib/cloudflare/test-runner.ts
+++ packages/vitest-pool-workers/src/worker/lib/cloudflare/test-runner.ts
@@ -8,7 +8,6 @@ import {
 	registerHandlerAndGlobalWaitUntil,
 	waitForGlobalWaitUntil,
 } from "cloudflare:test-internal";
-import { vi } from "vitest";
 import { VitestTestRunner } from "vitest/runners";
 import workerdUnsafe from "workerd:unsafe";
 import type { CancelReason, Suite, Test } from "@vitest/runner";
@@ -256,7 +255,25 @@ export default class WorkersTestRunner extends VitestTestRunner {
 
 		// Reset the module graphs so module mock will be re-evalated on watch mode
 		// See https://github.com/cloudflare/workers-sdk/issues/6844
-		vi.resetModules();
+		// This is basically `vi.resetModules()`
+		// From https://github.com/vitest-dev/vitest/blob/9584be337f00e22475895360cb5cfa5dc0775e79/packages/vitest/src/runtime/utils.ts#L50-L68
+		const skipPaths = [
+			// Vitest
+			/\/vitest\/dist\//,
+			/\/vite-node\/dist\//,
+			// yarn's .store folder
+			/vitest-virtual-\w+\/dist/,
+			// cnpm
+			/@vitest\/dist/,
+			// don't clear mocks
+			/^mock:/,
+		];
+		this.state.moduleCache.forEach((mod, path) => {
+			if (skipPaths.some((re) => re.test(path))) {
+				return;
+			}
+			this.state.moduleCache.invalidateModule(mod);
+		});
 
 		return super.onAfterRunSuite(suite);
 	}
