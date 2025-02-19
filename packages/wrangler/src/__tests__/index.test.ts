b10a85de2afaa258f9bc452b5c9baff6726a0a5f
Samuel Macleod <smacleod@cloudflare.com>
2025-02-19 15:55:02 +0000

    fix tests

--- packages/wrangler/src/__tests__/index.test.ts
+++ packages/wrangler/src/__tests__/index.test.ts
@@ -194,7 +194,7 @@ describe("wrangler", () => {
 				  wrangler secret put <key>     Create or update a secret variable for a Worker
 				  wrangler secret delete <key>  Delete a secret variable from a Worker
 				  wrangler secret list          List all secrets for a Worker
-				  wrangler secret bulk [json]   Bulk upload secrets for a Worker
+				  wrangler secret bulk [file]   Bulk upload secrets for a Worker
 
 				GLOBAL FLAGS
 				  -c, --config   Path to Wrangler configuration file  [string]
