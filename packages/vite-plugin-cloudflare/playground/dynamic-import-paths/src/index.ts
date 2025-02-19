826c439e8ea312705baebd616cddc982c6e950bb
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-02-19 13:34:08 +0000

    Added dynamic-import-paths playground

---
+++ packages/vite-plugin-cloudflare/playground/dynamic-import-paths/src/index.ts
@@ -0,0 +1,12 @@
+export default {
+	async fetch() {
+		const staticPathImport = await import("./another");
+		const dynamicPath = "./another";
+		const dynamicPathImport = await import(/* @vite-ignore */ dynamicPath);
+
+		return Response.json({
+			staticPathImportResult: staticPathImport.name,
+			dynamicPathImportResult: dynamicPathImport.name,
+		});
+	},
+} satisfies ExportedHandler;
