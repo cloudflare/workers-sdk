23b46b836c0c0297055a739989f548900d3e3dd6
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-02-19 13:38:54 +0000

    Added failing test

---
+++ packages/vite-plugin-cloudflare/playground/dynamic-import-paths/__tests__/dynamic-import-paths.spec.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "vitest";
+import { getJsonResponse } from "../../__test-utils__";
+
+test("supports imports with both static and dynamic paths", async () => {
+	const result = await getJsonResponse();
+	expect(result).toEqual({
+		staticPathImportResult: "Cloudflare",
+		dynamicPathImportResult: "Cloudflare",
+	});
+});
