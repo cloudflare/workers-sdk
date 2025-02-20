8682b713b047ec6b2bb0178a587c0f46edde64e1
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-02-20 15:51:06 +0000

    Added Cloudflare builtins to optimizeDeps.exclude and added partyserver playground

---
+++ packages/vite-plugin-cloudflare/playground/partyserver/worker/index.ts
@@ -0,0 +1,21 @@
+import { routePartykitRequest, Server } from "partyserver";
+import type { Connection } from "partyserver";
+
+interface Env extends Record<string, unknown> {
+	Assets: Fetcher;
+	MyServer: DurableObjectNamespace<MyServer>;
+}
+
+export class MyServer extends Server<Env> {
+	override onMessage(connection: Connection<unknown>, message: string) {
+		connection.send(`Received '${message}'`);
+	}
+}
+
+export default {
+	async fetch(request, env) {
+		const response = await routePartykitRequest(request, env);
+
+		return response ?? env.Assets.fetch(request);
+	},
+} satisfies ExportedHandler<Env>;
