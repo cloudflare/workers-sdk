c53693a137731ea1d2c6565295fc49fa9d0205fb
Samuel Macleod <smacleod@cloudflare.com>
2025-02-20 12:09:36 +0000

    Remove metadata

--- packages/workers-playground/src/QuickEditor/getPlaygroundWorker.ts
+++ packages/workers-playground/src/QuickEditor/getPlaygroundWorker.ts
@@ -52,10 +52,6 @@ async function parseSerialisedPlaygroundWorker(
 				])
 		)
 	);
-	worker.modules["metadata.json"] = {
-		contents: new TextEncoder().encode(JSON.stringify(worker.metadata)),
-		type: "text/plain",
-	};
 
 	return worker;
 }
