32c80e836881d6708f128e16dc45c2d4203abdec
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2025-02-24 15:19:08 +0000

    update template files to match target formatting

--- packages/create-cloudflare/templates-experimental/react/js/src/App.jsx
+++ packages/create-cloudflare/templates-experimental/react/js/src/App.jsx
@@ -1,27 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("unknown");
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState('unknown')
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href='https://vite.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className='card'>
         <button
           onClick={() => setCount((count) => count + 1)}
-          aria-label="increment"
+          aria-label='increment'
         >
           count is {count}
         </button>
@@ -29,14 +29,14 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <div className="card">
+      <div className='card'>
         <button
           onClick={() => {
-            fetch("/api/")
+            fetch('/api/')
               .then((res) => res.json())
-              .then((data) => setName(data.name));
+              .then((data) => setName(data.name))
           }}
-          aria-label="get name"
+          aria-label='get name'
         >
           Name from API is: {name}
         </button>
@@ -44,11 +44,11 @@ function App() {
           Edit <code>api/index.js</code> to change the name
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
