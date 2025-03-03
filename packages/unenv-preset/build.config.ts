8ae084f9e855fc866e03c6884d1e140dcaf55fb0
Victor Berchet <victor@suumit.com>
2025-03-03 00:06:20 -0800

    fix(unenv-preset): define __filename (#8309)

--- packages/unenv-preset/build.config.ts
+++ packages/unenv-preset/build.config.ts
@@ -2,6 +2,9 @@ import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
 	declaration: true,
+	rollup: {
+		cjsBridge: true,
+	},
 	entries: [
 		"src/index",
 		{ input: "src/runtime/", outDir: "dist/runtime", format: "esm" },
