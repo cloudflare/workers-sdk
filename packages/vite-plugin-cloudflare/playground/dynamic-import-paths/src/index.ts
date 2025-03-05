a6128f00b4b188364fc5dfd1b9e5422f9def471b
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-03-05 12:49:22 +0000

    Again

--- packages/vite-plugin-cloudflare/playground/dynamic-import-paths/src/index.ts
+++ packages/vite-plugin-cloudflare/playground/dynamic-import-paths/src/index.ts
@@ -1,7 +1,7 @@
 export default {
 	async fetch() {
 		const staticPathImport = await import("./another");
-		const dynamicPath = "/another";
+		const dynamicPath = "/another.js";
 		const dynamicPathImport = await import(/* @vite-ignore */ dynamicPath);
 
 		return Response.json({
