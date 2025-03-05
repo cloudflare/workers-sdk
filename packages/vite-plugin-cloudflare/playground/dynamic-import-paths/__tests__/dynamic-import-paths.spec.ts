ce9f191ff65623519747d44f00bb8c5c87469898
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-03-05 12:12:45 +0000

    Run test for build only

--- packages/vite-plugin-cloudflare/playground/dynamic-import-paths/__tests__/dynamic-import-paths.spec.ts
+++ packages/vite-plugin-cloudflare/playground/dynamic-import-paths/__tests__/dynamic-import-paths.spec.ts
@@ -1,10 +1,13 @@
 import { expect, test } from "vitest";
-import { getJsonResponse } from "../../__test-utils__";
+import { getJsonResponse, isBuild } from "../../__test-utils__";
 
-test("supports imports with both static and dynamic paths", async () => {
-	const result = await getJsonResponse();
-	expect(result).toEqual({
-		staticPathImportResult: "Cloudflare",
-		// dynamicPathImportResult: "Cloudflare",
-	});
-});
+test.runIf(isBuild)(
+	"supports imports with both static and dynamic paths",
+	async () => {
+		const result = await getJsonResponse();
+		expect(result).toEqual({
+			staticPathImportResult: "Cloudflare",
+			dynamicPathImportResult: "Cloudflare",
+		});
+	}
+);
--- packages/vite-plugin-cloudflare/playground/dynamic-import-paths/src/index.ts
+++ packages/vite-plugin-cloudflare/playground/dynamic-import-paths/src/index.ts
@@ -1,12 +1,12 @@
 export default {
 	async fetch() {
 		const staticPathImport = await import("./another");
-		// const dynamicPath = "./another.js";
-		// const dynamicPathImport = await import(/* @vite-ignore */ dynamicPath);
+		const dynamicPath = "./another.js";
+		const dynamicPathImport = await import(/* @vite-ignore */ dynamicPath);
 
 		return Response.json({
 			staticPathImportResult: staticPathImport.name,
-			// dynamicPathImportResult: dynamicPathImport.name,
+			dynamicPathImportResult: dynamicPathImport.name,
 		});
 	},
 } satisfies ExportedHandler;
