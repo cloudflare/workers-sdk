594b613a70464952cfb3e03c6c3242cd9e2ff9a0
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-03-05 13:01:53 +0000

    Only run test if build

--- packages/vite-plugin-cloudflare/playground/dynamic-import-paths/src/index.ts
+++ packages/vite-plugin-cloudflare/playground/dynamic-import-paths/src/index.ts
@@ -1,6 +1,6 @@
 export default {
 	async fetch() {
-		const staticPathImport = await import("./another");
+		const staticPathImport = await import("./another.js");
 		const dynamicPath = "./another.js";
 		const dynamicPathImport = await import(/* @vite-ignore */ dynamicPath);
 
