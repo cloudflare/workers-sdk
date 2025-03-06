784c2b539ef309be03859c9ff13a262465f7a0da
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-03-06 20:58:19 +0000

    Added tests to additional modules playground

--- packages/vite-plugin-cloudflare/playground/additional-modules/src/index.ts
+++ packages/vite-plugin-cloudflare/playground/additional-modules/src/index.ts
@@ -1,7 +1,40 @@
-import textContent from "./text-file.txt";
+import bin from "./bin-example.bin";
+import html from "./html-example.html";
+import text from "./text-example.txt";
+import wasm from "./wasm-example.wasm";
+import wasmWithParam from "./wasm-example.wasm?module";
 
 export default {
-	async fetch() {
-		return new Response(textContent);
+	async fetch(request) {
+		const url = new URL(request.url);
+
+		switch (url.pathname) {
+			case "/bin": {
+				return Response.json({ byteLength: bin.byteLength });
+			}
+			case "/html": {
+				return new Response(html, { headers: { "Content-Type": "text/html" } });
+			}
+			case "/text": {
+				return new Response(text, {
+					headers: { "Content-Type": "text/plain" },
+				});
+			}
+			case "/wasm": {
+				const instance = await WebAssembly.instantiate(wasm);
+				const result = instance.exports.add(3, 4);
+
+				return Response.json({ result });
+			}
+			case "/wasm-with-param": {
+				const instance = await WebAssembly.instantiate(wasmWithParam);
+				const result = instance.exports.add(5, 6);
+
+				return Response.json({ result });
+			}
+			default: {
+				return new Response(null, { status: 404 });
+			}
+		}
 	},
 } satisfies ExportedHandler;
