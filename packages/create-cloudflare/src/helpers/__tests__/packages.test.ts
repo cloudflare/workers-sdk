a22135b2b6ddc6b8ff330298c5f920619f429cd1
Samuel Macleod <smacleod@cloudflare.com>
2025-03-07 12:36:22 +0000

    Add --legacy-peer-deps so that installing Wrangler v4 doesn't case issues with frameworks that haven't updated their peer dependency for Wrangler v4

--- packages/create-cloudflare/src/helpers/__tests__/packages.test.ts
+++ packages/create-cloudflare/src/helpers/__tests__/packages.test.ts
@@ -25,7 +25,7 @@ describe("Package Helpers", () => {
 			await npmInstall(createTestContext());
 
 			expect(vi.mocked(runCommand)).toHaveBeenCalledWith(
-				["npm", "install"],
+				["npm", "install", "--legacy-peer-deps"],
 				expect.anything(),
 			);
 		});
@@ -45,28 +45,40 @@ describe("Package Helpers", () => {
 		type TestCase = {
 			pm: PmName;
 			initialArgs: string[];
+			additionalArgs?: string[];
 		};
 
 		const cases: TestCase[] = [
-			{ pm: "npm", initialArgs: ["npm", "install"] },
+			{
+				pm: "npm",
+				initialArgs: ["npm", "install"],
+				additionalArgs: ["--legacy-peer-deps"],
+			},
 			{ pm: "pnpm", initialArgs: ["pnpm", "install"] },
 			{ pm: "bun", initialArgs: ["bun", "add"] },
 			{ pm: "yarn", initialArgs: ["yarn", "add"] },
 		];
 
-		test.each(cases)("with $pm", async ({ pm, initialArgs }) => {
-			mockPackageManager(pm);
-			const packages = ["foo", "bar", "baz"];
-			await installPackages(packages);
+		test.each(cases)(
+			"with $pm",
+			async ({ pm, initialArgs, additionalArgs }) => {
+				mockPackageManager(pm);
+				const packages = ["foo", "bar", "baz"];
+				await installPackages(packages);
 
-			expect(vi.mocked(runCommand)).toHaveBeenCalledWith(
-				[...initialArgs, ...packages],
-				expect.anything(),
-			);
-		});
+				expect(vi.mocked(runCommand)).toHaveBeenCalledWith(
+					[...initialArgs, ...packages, ...(additionalArgs ?? [])],
+					expect.anything(),
+				);
+			},
+		);
 
 		const devCases: TestCase[] = [
-			{ pm: "npm", initialArgs: ["npm", "install", "--save-dev"] },
+			{
+				pm: "npm",
+				initialArgs: ["npm", "install", "--save-dev"],
+				additionalArgs: ["--legacy-peer-deps"],
+			},
 			{ pm: "pnpm", initialArgs: ["pnpm", "install", "--save-dev"] },
 			{ pm: "bun", initialArgs: ["bun", "add", "-d"] },
 			{ pm: "yarn", initialArgs: ["yarn", "add", "-D"] },
@@ -74,13 +86,13 @@ describe("Package Helpers", () => {
 
 		test.each(devCases)(
 			"with $pm (dev = true)",
-			async ({ pm, initialArgs }) => {
+			async ({ pm, initialArgs, additionalArgs }) => {
 				mockPackageManager(pm);
 				const packages = ["foo", "bar", "baz"];
 				await installPackages(packages, { dev: true });
 
 				expect(vi.mocked(runCommand)).toHaveBeenCalledWith(
-					[...initialArgs, ...packages],
+					[...initialArgs, ...packages, ...(additionalArgs ?? [])],
 					expect.anything(),
 				);
 			},
@@ -91,7 +103,7 @@ describe("Package Helpers", () => {
 		await installWrangler();
 
 		expect(vi.mocked(runCommand)).toHaveBeenCalledWith(
-			["npm", "install", "--save-dev", "wrangler@latest"],
+			["npm", "install", "--save-dev", "wrangler@latest", "--legacy-peer-deps"],
 			expect.anything(),
 		);
 	});
