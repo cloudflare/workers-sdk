18a609d88e0a682659b3976221250766ca14eeaf
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-03-08 14:24:46 +0000

    Reuse additional module regex

--- packages/vite-plugin-cloudflare/src/shared.ts
+++ packages/vite-plugin-cloudflare/src/shared.ts
@@ -3,3 +3,4 @@ import { ADDITIONAL_MODULE_TYPES } from "./constants";
 export const UNKNOWN_HOST = "http://localhost";
 export const INIT_PATH = "/__vite_plugin_cloudflare_init__";
 export const ADDITIONAL_MODULE_PATTERN = `__CLOUDFLARE_MODULE__(${ADDITIONAL_MODULE_TYPES.join("|")})__(.*?)__`;
+export const additionalModuleRE = new RegExp(ADDITIONAL_MODULE_PATTERN);
