54c809450ea80d316a970abc12e1d6c8a6e318c2
Pete Bacon Darwin <pete@bacondarwin.com>
2025-03-10 14:31:41 +0000

    test: add e2e tests of the build+preview use case (#8384)
    * test: add e2e tests of the build+preview use case
    
    * fixups: refactor tests to avoid boilerplate and make it easier to test all the combinations
    
    * Apply suggestions from code review
    
    Co-authored-by: James Opstad <13586373+jamesopstad@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: James Opstad <13586373+jamesopstad@users.noreply.github.com>

--- packages/vite-plugin-cloudflare/e2e/basic.test.ts
+++ packages/vite-plugin-cloudflare/e2e/basic.test.ts
@@ -1,30 +1,47 @@
 import { describe } from "vitest";
 import { fetchJson, runCommand, test, waitForReady } from "./helpers.js";
 
-describe("node compatibility", () => {
-	describe.each(["pnpm --no-store", "npm", "yarn"])("using %s", (pm) => {
-		test("can serve a Worker request", async ({ expect, seed, viteDev }) => {
-			const projectPath = await seed("basic");
-			runCommand(`${pm} install`, projectPath);
+const packageManagers = ["pnpm", "npm", "yarn"] as const;
+const commands = ["dev", "preview"] as const;
 
-			const proc = await viteDev(projectPath);
-			const url = await waitForReady(proc);
-			expect(await fetchJson(url + "/api/")).toEqual({ name: "Cloudflare" });
-		});
-	});
-});
+describe("basic e2e tests", () => {
+	describe.each(packageManagers)('with "%s" package manager', (pm) => {
+		describe.each(commands)('with "%s" command', (command) => {
+			describe("node compatibility", () => {
+				test("can serve a Worker request", async ({
+					expect,
+					seed,
+					viteCommand,
+				}) => {
+					const projectPath = await seed("basic");
+					runCommand(`${pm} install`, projectPath);
 
-// This test checks that wrapped bindings which rely on additional workers with an authed connection to the CF API work
-describe("Workers AI", () => {
-	test("can serve a Worker request", async ({ expect, seed, viteDev }) => {
-		const projectPath = await seed("basic");
-		runCommand(`npm install`, projectPath);
+					const proc = await viteCommand(pm, command, projectPath);
+					const url = await waitForReady(proc);
+					expect(await fetchJson(url + "/api/")).toEqual({
+						name: "Cloudflare",
+					});
+				});
+			});
 
-		const proc = await viteDev(projectPath);
-		const url = await waitForReady(proc);
+			// This test checks that wrapped bindings which rely on additional workers with an authed connection to the CF API work
+			describe("Workers AI", () => {
+				test("can serve a Worker request", async ({
+					expect,
+					seed,
+					viteCommand,
+				}) => {
+					const projectPath = await seed("basic");
+					runCommand(`${pm} install`, projectPath);
 
-		expect(await fetchJson(url + "/ai/")).toEqual({
-			response: expect.stringContaining("Workers AI"),
+					const proc = await viteCommand(pm, command, projectPath);
+					const url = await waitForReady(proc);
+
+					expect(await fetchJson(url + "/ai/")).toEqual({
+						response: expect.stringContaining("Workers AI"),
+					});
+				});
+			});
 		});
 	});
 });
