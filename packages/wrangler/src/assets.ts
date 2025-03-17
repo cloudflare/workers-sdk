edf169d15062a31dec1d32427fb72438425b45bf
Greg Brimble <gbrimble@cloudflare.com>
2025-03-17 15:56:07 -0400

    Fix metafiles in child directory (#8503)

--- packages/wrangler/src/assets.ts
+++ packages/wrangler/src/assets.ts
@@ -426,8 +426,10 @@ export function getAssetsOptions(
 		);
 	}
 
-	const redirects = maybeGetFile(path.join(directory, REDIRECTS_FILENAME));
-	const headers = maybeGetFile(path.join(directory, HEADERS_FILENAME));
+	const redirects = maybeGetFile(
+		path.join(resolvedAssetsPath, REDIRECTS_FILENAME)
+	);
+	const headers = maybeGetFile(path.join(resolvedAssetsPath, HEADERS_FILENAME));
 
 	// defaults are set in asset worker
 	const assetConfig: AssetConfig = {
