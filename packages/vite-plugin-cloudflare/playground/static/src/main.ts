1b317a2267cdb7352fee48c779fe4cfa7e0fe032
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-03-18 11:03:29 +0000

    Added tests for static playground, with and without api

--- packages/vite-plugin-cloudflare/playground/static/src/main.ts
+++ packages/vite-plugin-cloudflare/playground/static/src/main.ts
@@ -1,9 +1,9 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter";
+import typescriptLogo from "./typescript.svg";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -19,6 +19,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
