307bbe49930b0d9395fd38b6ae32a032a2de69ad
Victor Berchet <victor@suumit.com>
2025-03-19 16:30:18 +0100

    Allow usage of `createSecureContext` to create a default context

---
+++ .changeset/five-actors-cheat.md
@@ -0,0 +1,5 @@
+---
+"@cloudflare/unenv-preset": minor
+---
+
+Allow usage of `createSecureContext` to create a default context
--- packages/unenv-preset/src/runtime/node/tls.ts
+++ packages/unenv-preset/src/runtime/node/tls.ts
@@ -1,9 +1,9 @@
+import nodeTls from "node:tls";
 import {
 	checkServerIdentity,
 	CLIENT_RENEG_LIMIT,
 	CLIENT_RENEG_WINDOW,
 	convertALPNProtocols,
-	createSecureContext,
 	createSecurePair,
 	createServer,
 	DEFAULT_CIPHERS,
@@ -15,14 +15,12 @@ import {
 	SecureContext,
 	Server,
 } from "unenv/node/tls";
-import type nodeTls from "node:tls";
 
 export {
 	checkServerIdentity,
 	CLIENT_RENEG_LIMIT,
 	CLIENT_RENEG_WINDOW,
 	convertALPNProtocols,
-	createSecureContext,
 	createSecurePair,
 	createServer,
 	DEFAULT_CIPHERS,
@@ -39,6 +37,45 @@ const workerdTls = process.getBuiltinModule("node:tls");
 
 // Natively implemented in workerd
 export const { connect, TLSSocket } = workerdTls;
+
+// `connect` implementation only supports a default SecureContext for now.
+export const createSecureContext: typeof nodeTls.createSecureContext = (
+	options?: nodeTls.SecureContextOptions
+) => {
+	const keys: (keyof nodeTls.SecureContextOptions)[] = [
+		"ALPNCallback",
+		"allowPartialTrustChain",
+		"ca",
+		"cert",
+		"sigalgs",
+		"ciphers",
+		"clientCertEngine",
+		"crl",
+		"dhparam",
+		"ecdhCurve",
+		"honorCipherOrder",
+		"key",
+		"privateKeyEngine",
+		"privateKeyIdentifier",
+		"maxVersion",
+		"minVersion",
+		"passphrase",
+		"pfx",
+		"secureOptions",
+		"secureProtocol",
+		"ticketKeys",
+		"sessionTimeout",
+	];
+
+	for (const k of keys) {
+		if (options?.[k] !== undefined) {
+			throw new Error("createSecureContext only supports default options.");
+		}
+	}
+	return {
+		context: undefined,
+	};
+};
 
 export default {
 	CLIENT_RENEG_LIMIT,
