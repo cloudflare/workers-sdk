4a5f270129f4a2d8995ba2fdd7fc220ee7c75300
Luís Duarte <lduarte@cloudflare.com>
2025-03-21 19:45:13 +0000

    feat: add endpoint for workflow deletion (#8575)
    Co-authored-by: Carmen Popoviciu <cpopoviciu@cloudflare.com>
    Co-authored-by: Somhairle MacLeòid <smacleod@cloudflare.com>

---
+++ .changeset/floppy-snails-fetch.md
@@ -0,0 +1,5 @@
+---
+"wrangler": minor
+---
+
+Add workflows delete API endpoint
--- packages/wrangler/src/__tests__/workflows.test.ts
+++ packages/wrangler/src/__tests__/workflows.test.ts
@@ -53,6 +53,24 @@ describe("wrangler workflows", () => {
 		);
 	};
 
+	const mockDeleteWorkflowRequest = async (workflowName: string) => {
+		msw.use(
+			http.delete(
+				`*/accounts/:accountId/workflows/:workflowName`,
+				async ({ params }) => {
+					expect(params.workflowName).toEqual(workflowName);
+					return HttpResponse.json({
+						success: true,
+						errors: [],
+						messages: [],
+						result: {},
+					});
+				},
+				{ once: true }
+			)
+		);
+	};
+
 	const mockInstancesTerminateAll = async (
 		expectedWorkflow: string,
 		responseStatus: "ok" | "already_running",
@@ -515,15 +533,17 @@ describe("wrangler workflows", () => {
 	});
 
 	describe("delete", () => {
-		it("should delete a workflow - check not implemented", async () => {
+		it("should delete a workflow - green path", async () => {
 			writeWranglerConfig();
+
+			await mockDeleteWorkflowRequest("some-workflow");
 
 			await runWrangler(`workflows delete some-workflow`);
 			expect(std.out).toMatchInlineSnapshot(
-				`"🚫 Workflow \\"some-workflow\\" NOT removed"`
-			);
-			expect(std.info).toMatchInlineSnapshot(
-				`"🚫 delete command not yet implement"`
+				`
+				"✅ Workflow \\"some-workflow\\" removed successfully.
+				 Note that running instances might take a few minutes to be properly terminated."
+			`
 			);
 		});
 	});
