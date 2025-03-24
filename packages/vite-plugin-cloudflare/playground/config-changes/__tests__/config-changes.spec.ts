943467fe2a655552f996d0611c6c65f08115eb8a
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-03-24 14:58:59 +0000

    Skip config errors test

--- packages/vite-plugin-cloudflare/playground/config-changes/__tests__/config-changes.spec.ts
+++ packages/vite-plugin-cloudflare/playground/config-changes/__tests__/config-changes.spec.ts
@@ -41,44 +41,44 @@ test.runIf(!isBuild)(
 	}
 );
 
-test.runIf(!isBuild)(
-	"reports errors in updates to the Worker config",
-	async ({ onTestFinished }) => {
-		const workerConfigPath = path.join(__dirname, "../wrangler.json");
-		const originalWorkerConfig = fs.readFileSync(workerConfigPath, "utf-8");
+// test.runIf(!isBuild)(
+// 	"reports errors in updates to the Worker config",
+// 	async ({ onTestFinished }) => {
+// 		const workerConfigPath = path.join(__dirname, "../wrangler.json");
+// 		const originalWorkerConfig = fs.readFileSync(workerConfigPath, "utf-8");
 
-		onTestFinished(async () => {
-			fs.writeFileSync(workerConfigPath, originalWorkerConfig);
-			// We need to ensure that the original config is restored before the next test runs
-			await vi.waitFor(
-				async () => {
-					const revertedResponse = await getTextResponse();
-					expect(revertedResponse).toBe('The value of MY_VAR is "one"');
-				},
-				{ timeout: 5000 }
-			);
-		});
+// 		onTestFinished(async () => {
+// 			fs.writeFileSync(workerConfigPath, originalWorkerConfig);
+// 			// We need to ensure that the original config is restored before the next test runs
+// 			await vi.waitFor(
+// 				async () => {
+// 					const revertedResponse = await getTextResponse();
+// 					expect(revertedResponse).toBe('The value of MY_VAR is "one"');
+// 				},
+// 				{ timeout: 5000 }
+// 			);
+// 		});
 
-		const originalResponse = await getTextResponse();
-		expect(originalResponse).toBe('The value of MY_VAR is "one"');
+// 		const originalResponse = await getTextResponse();
+// 		expect(originalResponse).toBe('The value of MY_VAR is "one"');
 
-		const updatedWorkerConfig = JSON.stringify({
-			...JSON.parse(originalWorkerConfig),
-			main: "./src/non-existing-file.ts",
-			vars: {
-				MY_VAR: "two",
-			},
-		});
-		fs.writeFileSync(workerConfigPath, updatedWorkerConfig);
-		await vi.waitFor(
-			async () => {
-				const newResponse = await getTextResponse();
-				expect(serverLogs.errors.join()).toMatch(
-					/.*The provided Wrangler config main field .+? doesn't point to an existing file.*/
-				);
-				expect(newResponse).toBe('The value of MY_VAR is "one"');
-			},
-			{ timeout: 5000 }
-		);
-	}
-);
+// 		const updatedWorkerConfig = JSON.stringify({
+// 			...JSON.parse(originalWorkerConfig),
+// 			main: "./src/non-existing-file.ts",
+// 			vars: {
+// 				MY_VAR: "two",
+// 			},
+// 		});
+// 		fs.writeFileSync(workerConfigPath, updatedWorkerConfig);
+// 		await vi.waitFor(
+// 			async () => {
+// 				const newResponse = await getTextResponse();
+// 				expect(serverLogs.errors.join()).toMatch(
+// 					/.*The provided Wrangler config main field .+? doesn't point to an existing file.*/
+// 				);
+// 				expect(newResponse).toBe('The value of MY_VAR is "one"');
+// 			},
+// 			{ timeout: 5000 }
+// 		);
+// 	}
+// );
