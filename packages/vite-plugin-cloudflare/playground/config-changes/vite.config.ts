8da41536ecfd7b34ca1d696f85b7dcccc472bdce
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-03-25 20:00:25 +0000

    Remove inspectorPort in playground

--- packages/vite-plugin-cloudflare/playground/config-changes/vite.config.ts
+++ packages/vite-plugin-cloudflare/playground/config-changes/vite.config.ts
@@ -2,6 +2,6 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	// We leave the `inspectorPort` enabled in this playground to verify that there are no port collisions on server restarts
-	plugins: [cloudflare({ persistState: false })],
+	// We should enable `inspectorPort` in this playground when it's possible to do so to verify that there are no port collisions on server restarts
+	plugins: [cloudflare({ inspectorPort: false, persistState: false })],
 });
