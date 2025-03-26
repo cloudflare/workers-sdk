8ecb3d26dcd054659c9f1bb285729e5950615e8b
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2025-03-26 00:07:24 +0000

    run the tests sequentially under windows and don't skip the prisma tests as suggested

--- packages/vite-plugin-cloudflare/playground/vitest.config.serve.e2e.ts
+++ packages/vite-plugin-cloudflare/playground/vitest.config.serve.e2e.ts
@@ -2,6 +2,13 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
+		poolOptions:
+			// for some reason the prisma tests this seems to be problematic on windows
+			// when running the various playground tests in parallel, so under windows
+			// run the tests in a single fork to avoid to avoid them running in parallel.
+			process.platform === "win32"
+				? { forks: { singleFork: true } }
+				: undefined,
 		include: ["./**/__tests__/**/*.spec.[tj]s"],
 		setupFiles: ["./vitest-setup.ts"],
 		globalSetup: ["./vitest-global-setup.ts"],
