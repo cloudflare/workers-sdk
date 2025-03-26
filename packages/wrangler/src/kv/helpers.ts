14602d9f39f3fb1df7303dab5c91a77fa21e46f9
Pedro Leal <pmiguel@users.noreply.github.com>
2025-03-26 09:42:18 +0000

    feat(wrangler): Add Bulk Key Get command to KV (#8620)
    * feat(wrangler): Add Bulk Key Get command to KV
    
    * chore(wrangler): Add unit tests for bulk get command in KV
    
    * chore(wrangler): Add e2e tests for bulk get command in KV
    
    * chore(wrangler): Add changeset
    
    * chore(wrangler): Removed refactoring of bulk get file.
    
    * chore(wrangler): Remove sendMetrics call from get bulk
    
    * chore(wrangler): Change binding arg description for KV commands
    
    * ˇ
    
    * Update packages/wrangler/src/kv/index.ts
    
    Co-authored-by: Carmen Popoviciu <cpopoviciu@cloudflare.com>
    
    * Update packages/wrangler/src/__tests__/kv.test.ts
    
    Co-authored-by: Carmen Popoviciu <cpopoviciu@cloudflare.com>
    
    * Update packages/wrangler/src/__tests__/kv.test.ts
    
    Co-authored-by: Carmen Popoviciu <cpopoviciu@cloudflare.com>
    
    * Update packages/wrangler/src/__tests__/kv.test.ts
    
    Co-authored-by: Carmen Popoviciu <cpopoviciu@cloudflare.com>
    
    * Update packages/wrangler/src/__tests__/kv.test.ts
    
    Co-authored-by: Carmen Popoviciu <cpopoviciu@cloudflare.com>
    
    * Update packages/wrangler/src/__tests__/kv.test.ts
    
    Co-authored-by: Carmen Popoviciu <cpopoviciu@cloudflare.com>
    
    * Update packages/wrangler/src/__tests__/kv.test.ts
    
    Co-authored-by: Carmen Popoviciu <cpopoviciu@cloudflare.com>
    
    ---------
    
    Co-authored-by: Carmen Popoviciu <cpopoviciu@cloudflare.com>

--- packages/wrangler/src/kv/helpers.ts
+++ packages/wrangler/src/kv/helpers.ts
@@ -286,6 +286,32 @@ function logBulkProgress(
 	);
 }
 
+type BulkGetResponse = {
+	values: {
+		[key: string]: {
+			value: string | object | null;
+			metadata?: object;
+		};
+	};
+};
+
+export async function getKVBulkKeyValue(
+	accountId: string,
+	namespaceId: string,
+	keys: string[]
+) {
+	const requestPayload = { keys };
+	const result = await fetchResult<BulkGetResponse>(
+		`/accounts/${accountId}/storage/kv/namespaces/${namespaceId}/bulk/get`,
+		{
+			method: "POST",
+			body: JSON.stringify(requestPayload),
+			headers: { "Content-Type": "application/json" },
+		}
+	);
+	return result.values;
+}
+
 export async function putKVBulkKeyValue(
 	accountId: string,
 	namespaceId: string,
