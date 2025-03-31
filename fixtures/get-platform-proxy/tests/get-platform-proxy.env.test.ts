ecae6dd140a4fa1ede061061a37c2ef0306def80
emily-shen <69125074+emily-shen@users.noreply.github.com>
2025-03-31 13:32:07 +0100

    update link

--- fixtures/get-platform-proxy/tests/get-platform-proxy.env.test.ts
+++ fixtures/get-platform-proxy/tests/get-platform-proxy.env.test.ts
@@ -227,8 +227,8 @@ describe("getPlatformProxy - env", () => {
 				  				- {"class_name":"MyDurableObject","name":"MY_DURABLE_OBJECT"}
 				  				These will not work in local development, but they should work in production.
 				  
-				  				If you want to develop these locally, you can define your DO in a separate Worker, specified by a separate configuration file.
-				  				Refer to [4mhttps://developers.cloudflare.com/workers/wrangler/api#durable-objects[0m for guidance on how to do this.
+				  				If you want to develop these locally, you can define your DO in a separate Worker, with a separate configuration file.
+				  				For detailed instructions, refer to the Durable Objects section here: [4mhttps://developers.cloudflare.com/workers/wrangler/api#supported-bindings[0m
 
 				",
 				    ],
