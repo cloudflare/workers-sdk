6879b34234e174c821e5257d69c52d19c8a57e51
Dario Piotrowicz <dario@cloudflare.com>
2025-04-07 15:01:52 +0100

    combine tests

--- fixtures/redirected-config-worker-with-environments/tests/index.test.ts
+++ fixtures/redirected-config-worker-with-environments/tests/index.test.ts
@@ -5,7 +5,7 @@ import { runWranglerDev } from "../../shared/src/run-wrangler-long-lived";
 const basePath = resolve(__dirname, "..");
 
 describe("'wrangler dev' errors when a redirected config includes environments", () => {
-	it("A clear error message is presented which includes the offending environments", async ({
+	it("A clear error message is presented which includes the offending environments and prompting the user to contact the tool's author", async ({
 		expect,
 	}) => {
 		let error: string | undefined;
@@ -22,20 +22,7 @@ describe("'wrangler dev' errors when a redirected config includes environments",
 		);
 		expect(error).toContain("staging");
 		expect(error).toContain("testing");
-	});
 
-	it("The error message prompts the user to contact the tool's author", async ({
-		expect,
-	}) => {
-		let error: string | undefined;
-		try {
-			await runWranglerDev(basePath, ["--port=0", "--inspector-port=0"]);
-		} catch (e) {
-			if (typeof e === "string") {
-				error = e;
-			}
-		}
-		expect(error).toBeTruthy();
 		expect(error).toContain("Report this issue to the tool's author");
 	});
 });
