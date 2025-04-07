cf747399522d9f1e924db06dd426dba31e03647e
Dario Piotrowicz <dario@cloudflare.com>
2025-04-07 11:10:26 +0100

    update wrong test `it` text

--- fixtures/redirected-config-worker/tests/index.test.ts
+++ fixtures/redirected-config-worker/tests/index.test.ts
@@ -20,7 +20,7 @@ describe("'wrangler dev' correctly renders pages", () => {
 		expect(text).toMatchInlineSnapshot(`"Generated: true"`);
 	});
 
-	it("uses a custom config from command line rather than generated config", async ({
+	it("specifying an environment causes an error since they are not supported in redirected configs", async ({
 		expect,
 		onTestFinished,
 	}) => {
