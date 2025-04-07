36fa799014807ab6db09e429a949c72d5f077a09
Luís Duarte <lduarte@cloudflare.com>
2025-04-07 16:43:53 +0100

    fix: make console.logs not hang vitest-pool-workers when using workflows
    This goes way beyond me, but I noticed that I didn't use
    `patchAndRunWithHandlerContext` in `createWorkflowEntrypointWrapper`
    but other entrypoints did it - that seems to fix it.
    
    However, I'm not really sure how does `ExecutionContext` relate to our
    custom `Console` mock. Probably the test runner uses `waitUntil`
    to write into the vitest's stdout?

--- fixtures/vitest-pool-workers-examples/workflows/src/index.ts
+++ fixtures/vitest-pool-workers-examples/workflows/src/index.ts
@@ -7,6 +7,7 @@ import {
 
 export class TestWorkflow extends WorkflowEntrypoint<Env> {
 	async run(_event: Readonly<WorkflowEvent<unknown>>, step: WorkflowStep) {
+		console.log("ola");
 		return "test-workflow";
 	}
 }
