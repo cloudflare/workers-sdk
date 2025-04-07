09464a6c0d5bbc7b5ac2e33d68621e84f4fb4557
Dario Piotrowicz <dario@cloudflare.com>
2025-04-07 21:07:13 +0100

    Improve error message displayed when a redirected config includes multiple environments (#8809)
    * update wrong test `it` text
    
    * improve error message when redirected config contains environments
    
    * combine tests

--- fixtures/redirected-config-worker/tests/index.test.ts
+++ fixtures/redirected-config-worker/tests/index.test.ts
@@ -20,7 +20,7 @@ describe("'wrangler dev' correctly renders pages", () => {
 		expect(text).toMatchInlineSnapshot(`"Generated: true"`);
 	});
 
-	it("specifying an environment cause an error since they are supported in redirected configs", async ({
+	it("specifying an environment causes an error since they are not supported in redirected configs", async ({
 		expect,
 	}) => {
 		await expect(
