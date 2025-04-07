7477774875b2e848df3d4f221276f1bb40909879
Dario Piotrowicz <dario@cloudflare.com>
2025-04-07 11:29:45 +0100

    improve error message when redirected config contains environments

--- fixtures/redirected-config-worker-with-environments/tests/index.test.ts
+++ fixtures/redirected-config-worker-with-environments/tests/index.test.ts
@@ -4,12 +4,38 @@ import { runWranglerDev } from "../../shared/src/run-wrangler-long-lived";
 
 const basePath = resolve(__dirname, "..");
 
-describe("'wrangler dev' errors because redirected configs can't include environments", () => {
-	it("uses the generated config", async ({ expect }) => {
-		await expect(
-			runWranglerDev(basePath, ["--port=0", "--inspector-port=0"])
-		).rejects.toThrowError(
-			/Redirected configurations cannot include environments but the following have been found: \"staging\"/
+describe("'wrangler dev' errors when a redirected config includes environments", () => {
+	it("A clear error message is presented which includes the offending environments", async ({
+		expect,
+	}) => {
+		let error: string | undefined;
+		try {
+			await runWranglerDev(basePath, ["--port=0", "--inspector-port=0"]);
+		} catch (e) {
+			if (typeof e === "string") {
+				error = e;
+			}
+		}
+		expect(error).toBeTruthy();
+		expect(error).toMatch(
+			/Redirected configurations cannot include environments but the following have been found:/
 		);
+		expect(error).toContain("staging");
+		expect(error).toContain("testing");
+	});
+
+	it("The error message prompts the user to contact the tool's author", async ({
+		expect,
+	}) => {
+		let error: string | undefined;
+		try {
+			await runWranglerDev(basePath, ["--port=0", "--inspector-port=0"]);
+		} catch (e) {
+			if (typeof e === "string") {
+				error = e;
+			}
+		}
+		expect(error).toBeTruthy();
+		expect(error).toContain("Report this issue to the tool's author");
 	});
 });
