6b1f7ea9e67822a6de8e3be734934d24a312216e
emily-shen <69125074+emily-shen@users.noreply.github.com>
2025-04-10 16:47:49 +0100

    update tests

--- packages/create-cloudflare/src/__tests__/workers.test.ts
+++ packages/create-cloudflare/src/__tests__/workers.test.ts
@@ -3,7 +3,7 @@ import { mockSpinner } from "helpers/__tests__/mocks";
 import { getLatestTypesEntrypoint } from "helpers/compatDate";
 import { readFile, writeFile } from "helpers/files";
 import { beforeEach, describe, expect, test, vi } from "vitest";
-import { addWorkersTypesToTsConfig } from "../workers";
+import { updateTsConfig } from "../workers";
 import { createTestContext } from "./helpers";
 import type { C3Context } from "types";
 
@@ -18,12 +18,13 @@ beforeEach(() => {
 
 const mockCompatDate = "2024-01-17";
 
-describe("addWorkersTypesToTsConfig", () => {
+describe("updateTsConfig", () => {
 	let ctx: C3Context;
 
 	beforeEach(() => {
 		ctx = createTestContext();
 		ctx.args.ts = true;
+		ctx.template.installWorkersTypes = false;
 
 		vi.mocked(existsSync).mockImplementation(() => true);
 		vi.mocked(getLatestTypesEntrypoint).mockReturnValue(mockCompatDate);
@@ -34,8 +35,9 @@ describe("addWorkersTypesToTsConfig", () => {
 		);
 	});
 
-	test("happy path", async () => {
-		await addWorkersTypesToTsConfig(ctx);
+	test("installing workers types", async () => {
+		ctx.template.installWorkersTypes = true;
+		await updateTsConfig(ctx);
 
 		expect(writeFile).toHaveBeenCalled();
 
@@ -46,26 +48,46 @@ describe("addWorkersTypesToTsConfig", () => {
 
 	test("tsconfig.json not found", async () => {
 		vi.mocked(existsSync).mockImplementation(() => false);
-		await addWorkersTypesToTsConfig(ctx);
+		await updateTsConfig(ctx);
 		expect(writeFile).not.toHaveBeenCalled();
 	});
 
 	test("latest entrypoint not found", async () => {
+		ctx.template.installWorkersTypes = true;
 		vi.mocked(getLatestTypesEntrypoint).mockReturnValue(null);
-		await addWorkersTypesToTsConfig(ctx);
+		await updateTsConfig(ctx);
 
 		expect(writeFile).not.toHaveBeenCalled();
 	});
 
 	test("don't clobber existing entrypoints", async () => {
+		ctx.template.installWorkersTypes = true;
 		vi.mocked(readFile).mockImplementation(
 			() =>
 				`{ "compilerOptions": { "types" : ["@cloudflare/workers-types/2021-03-20"]} }`,
 		);
-		await addWorkersTypesToTsConfig(ctx);
+		await updateTsConfig(ctx);
 
 		expect(vi.mocked(writeFile).mock.calls[0][1]).toContain(
 			`"@cloudflare/workers-types/2021-03-20"`,
+		);
+	});
+
+	test("will remove workers-types when generating types", async () => {
+		vi.mocked(readFile).mockImplementation(
+			() =>
+				`{ "compilerOptions": { "types" : ["@cloudflare/workers-types/2021-03-20"]} }`,
+		);
+		await updateTsConfig(ctx);
+		expect(vi.mocked(writeFile).mock.calls[0][1]).not.toContain(
+			`"@cloudflare/workers-types/2021-03-20"`,
+		);
+	});
+
+	test("will add generated types file", async () => {
+		await updateTsConfig(ctx);
+		expect(vi.mocked(writeFile).mock.calls[0][1]).toContain(
+			`./worker-configuration.d.ts`,
 		);
 	});
 });
