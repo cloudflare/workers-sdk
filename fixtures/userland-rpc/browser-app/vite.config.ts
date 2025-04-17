06a768ab1d41b395a1d0ab9ce0c2feb6facf20c3
Samuel Macleod <smacleod@cloudflare.com>
2025-04-17 22:29:30 +0100

    reduce diff

--- fixtures/userland-rpc/browser-app/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-import { cloudflare } from "@cloudflare/vite-plugin";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), cloudflare()],
-})
