0d025c34bcde61afa2739388e3dc46ee7057490b
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2025-04-23 22:00:01 +0100

    skip e2e build tests on Windows

--- packages/vite-plugin-cloudflare/e2e/basic.test.ts
+++ packages/vite-plugin-cloudflare/e2e/basic.test.ts
@@ -1,6 +1,7 @@
 import { describe, test } from "vitest";
 import { fetchJson, runLongLived, seed, waitForReady } from "./helpers.js";
 
+const isWindows = process.platform === "win32";
 const packageManagers = ["pnpm", , "npm", "yarn"] as const;
 const commands = ["dev", "buildAndPreview"] as const;
 
@@ -10,13 +11,16 @@ describe("basic e2e tests", () => {
 
 		describe.each(commands)('with "%s" command', (command) => {
 			describe("node compatibility", () => {
-				test("can serve a Worker request", async ({ expect }) => {
-					const proc = await runLongLived(pm, command, projectPath);
-					const url = await waitForReady(proc);
-					expect(await fetchJson(url + "/api/")).toEqual({
-						name: "Cloudflare",
-					});
-				});
+				test.skipIf(isWindows && command === "buildAndPreview")(
+					"can serve a Worker request",
+					async ({ expect }) => {
+						const proc = await runLongLived(pm, command, projectPath);
+						const url = await waitForReady(proc);
+						expect(await fetchJson(url + "/api/")).toEqual({
+							name: "Cloudflare",
+						});
+					}
+				);
 			});
 
 			// This test checks that wrapped bindings which rely on additional workers with an authed connection to the CF API work
@@ -24,14 +28,17 @@ describe("basic e2e tests", () => {
 			describe.skipIf(
 				!process.env.CLOUDFLARE_ACCOUNT_ID || !process.env.CLOUDFLARE_API_TOKEN
 			)("Workers AI", () => {
-				test("can serve a Worker request", async ({ expect }) => {
-					const proc = await runLongLived(pm, command, projectPath);
-					const url = await waitForReady(proc);
+				test.skipIf(isWindows && command === "buildAndPreview")(
+					"can serve a Worker request",
+					async ({ expect }) => {
+						const proc = await runLongLived(pm, command, projectPath);
+						const url = await waitForReady(proc);
 
-					expect(await fetchJson(url + "/ai/")).toEqual({
-						response: expect.stringContaining("Workers AI"),
-					});
-				});
+						expect(await fetchJson(url + "/ai/")).toEqual({
+							response: expect.stringContaining("Workers AI"),
+						});
+					}
+				);
 			});
 		});
 	});
