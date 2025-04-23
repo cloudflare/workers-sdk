a9190de1340dfe43c38fec729e01139f499351dd
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-04-23 19:14:18 +0100

    Resolve bugs related to not handling `tailStream` (#9039)

---
+++ packages/vite-plugin-cloudflare/playground/block-concurrency-while/__tests__/block-concurrency-while.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from "vitest";
+import { getJsonResponse } from "../../__test-utils__";
+
+test("blocks execution until `ctx.blockConcurrencyWhile` has completed", async () => {
+	const response = await getJsonResponse("/durable-object");
+	expect(response).toEqual({ isInitialized: true });
+});
---
+++ packages/vite-plugin-cloudflare/playground/block-concurrency-while/src/index.ts
@@ -0,0 +1,38 @@
+import { DurableObject } from "cloudflare:workers";
+
+interface Env {
+	MyDurableObject: DurableObjectNamespace<MyDurableObject>;
+}
+
+export class MyDurableObject extends DurableObject {
+	#isInitialized = false;
+
+	constructor(ctx: DurableObjectState, env: Env) {
+		super(ctx, env);
+		console.log("constructor");
+		this.ctx.blockConcurrencyWhile(async () => {
+			console.log("blockConcurrencyWhile");
+			this.#isInitialized = true;
+		});
+	}
+
+	get isInitialized() {
+		console.log("isInitialized");
+		return this.#isInitialized;
+	}
+}
+
+export default {
+	async fetch(request, env) {
+		const url = new URL(request.url);
+
+		if (url.pathname === "/durable-object") {
+			const id = env.MyDurableObject.idFromName("test-id");
+			const stub = env.MyDurableObject.get(id);
+
+			return Response.json({ isInitialized: await stub.isInitialized });
+		}
+
+		return new Response(null, { status: 404 });
+	},
+} satisfies ExportedHandler<Env>;
