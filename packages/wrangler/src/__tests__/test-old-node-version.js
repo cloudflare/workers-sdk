2848b851e63ece0d41d71ba8ee0d7bbf83facfd8
Samuel Macleod <smacleod@cloudflare.com>
2025-04-30 20:20:55 +0100

    err => warning

--- packages/wrangler/src/__tests__/test-old-node-version.js
+++ packages/wrangler/src/__tests__/test-old-node-version.js
@@ -5,23 +5,40 @@ const path = require("path");
 
 const assert = require("assert");
 
+const mode = process.argv[2];
+
 const wranglerProcess = spawn(
 	"node",
-	[path.join(__dirname, "../../bin/wrangler.js")],
+	[path.join(__dirname, "../../bin/wrangler.js"), "help"],
 	{ stdio: "pipe" }
 );
 
-const messageToMatch = "Wrangler requires at least Node.js v20";
+const messageToMatch = "Wrangler requires at least Node.js v";
 
 wranglerProcess.once("exit", (code) => {
 	try {
-		const errorMessage = wranglerProcess.stderr.read().toString();
+		if (mode === "error") {
+			const errorMessage = wranglerProcess.stderr.read().toString();
 
-		assert(code === 1, "Expected exit code 1");
-		assert(
-			errorMessage.includes(messageToMatch),
-			`Expected error message to include "${messageToMatch}"`
-		);
+			assert(code === 1, "Expected exit code 1");
+			assert(
+				errorMessage.includes(messageToMatch),
+				`Expected error message to include "${messageToMatch}"`
+			);
+		} else {
+			const warningMessage = wranglerProcess.stderr.read().toString();
+
+			assert(code === 1, "Expected exit code 1");
+			assert(
+				warningMessage.includes(messageToMatch),
+				`Expected error message to include "${messageToMatch}"`
+			);
+			// Wrangler should only warn, and so validate that the help text was shown
+			assert(
+				warningMessage.includes("Show help"),
+				`Expected error message to include "Show help"`
+			);
+		}
 	} catch (err) {
 		console.error("Error:", err);
 		throw new Error(
