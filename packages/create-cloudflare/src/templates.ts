0ed6457caf87030fa0c82713353395a5a093f1a9
avenceslau <avenceslau@cloudflare.com>
2025-04-30 10:26:13 +0100

    fix typo on template

--- packages/create-cloudflare/src/templates.ts
+++ packages/create-cloudflare/src/templates.ts
@@ -244,7 +244,7 @@ export function getHelloWorldTemplateMap({
 			scheduled: scheduledTemplate,
 			queues: queuesTemplate,
 			openapi: openapiTemplate,
-			"workflows-starter": workflowsTemplate,
+			"workflow-starter": workflowsTemplate,
 			"pre-existing": preExistingTemplate,
 		} as Record<string, TemplateConfig>;
 	}
