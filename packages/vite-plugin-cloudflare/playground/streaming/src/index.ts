8db1e9cc251213d4834669ddd6639599175eb258
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-05-07 08:46:01 +0100

    Added failing streaming https build test

--- packages/vite-plugin-cloudflare/playground/streaming/src/index.ts
+++ packages/vite-plugin-cloudflare/playground/streaming/src/index.ts
@@ -8,24 +8,25 @@ async function streamResponse(writable: WritableStream) {
 
 	await writer.write(
 		encoder.encode(`
-	<!DOCTYPE html>
-	<html>
-	<head>
-		<title>Streaming example</title>
-	</head>
-	<body>
-		<h1>Streaming example</h1>
-		<p>Loading content...</p>
-	`)
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Streaming example</title>
+</head>
+<body>
+	<h1>Streaming example</h1>
+	<p>Loading content...</p>
+		`)
 	);
 
 	await sleep(1000);
 	await writer.write(encoder.encode(`<p id="one">Chunk after 1 second`));
 	await sleep(1000);
-	await writer.write(encoder.encode(`<p id="two">Chunk after 2 seconds`));
-	await sleep(1000);
 	await writer.write(
-		encoder.encode(`<p id="three">Chunk after 3 seconds</p></body></html>`)
+		encoder.encode(`
+	<p id="two">Chunk after 2 seconds</p>
+</body></html>
+		`)
 	);
 	await writer.close();
 }
--- packages/vite-plugin-cloudflare/playground/streaming/vite.config.ts
+++ packages/vite-plugin-cloudflare/playground/streaming/vite.config.ts
@@ -1,10 +1,6 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [
-		cloudflare({ inspectorPort: false, persistState: false }),
-		basicSsl(),
-	],
+	plugins: [cloudflare({ inspectorPort: false, persistState: false })],
 });
