3b8ece2ed0b679839987699a21dccaaa4290516f
Josh Seba <jseba@cloudflare.com>
2025-05-08 13:49:37 -0700

    Fix captilization typo in curl test

--- packages/wrangler/src/__tests__/cloudchamber/curl.test.ts
+++ packages/wrangler/src/__tests__/cloudchamber/curl.test.ts
@@ -33,7 +33,7 @@ describe("cloudchamber curl", () => {
 		expect(helpStd.out).toMatchInlineSnapshot(`
 			"wrangler cloudchamber curl <path>
 
-			send a request to an arbitrary cloudchamber endpoint
+			send a request to an arbitrary Cloudchamber endpoint
 
 			POSITIONALS
 			  path  [string] [required] [default: \\"/\\"]
