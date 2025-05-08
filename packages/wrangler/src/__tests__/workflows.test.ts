f17ee08687bad59f1d921fb8da1472b8e92b2c6f
avenceslau <63984882+avenceslau@users.noreply.github.com>
2025-05-08 10:19:48 +0100

    Wrangler delete workflow (#9122)
    * make workflows delete command not hidden
    
    * update wranglers workflow test to include delete command
    
    * fix wrangler workflows test
    
    * add changeset
    
    * update snapshot

--- packages/wrangler/src/__tests__/workflows.test.ts
+++ packages/wrangler/src/__tests__/workflows.test.ts
@@ -113,6 +113,7 @@ describe("wrangler workflows", () => {
 				COMMANDS
 				  wrangler workflows list                     List Workflows associated to account
 				  wrangler workflows describe <name>          Describe Workflow resource
+				  wrangler workflows delete <name>            Delete workflow - when deleting a workflow, it will also delete it's own instances
 				  wrangler workflows trigger <name> [params]  Trigger a workflow, creating a new instance. Can optionally take a JSON string to pass a parameter into the workflow instance
 				  wrangler workflows instances                Manage Workflow instances
 
