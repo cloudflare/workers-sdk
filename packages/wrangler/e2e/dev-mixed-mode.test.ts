f04df109a311721fa3971e3eb5bb4fb26331f0ce
Dario Piotrowicz <dario@cloudflare.com>
2025-05-14 00:17:11 +0100

    add e2e mixed mode test

---
+++ packages/wrangler/e2e/dev-mixed-mode.test.ts
@@ -0,0 +1,58 @@
+import dedent from "ts-dedent";
+import { describe, expect, it } from "vitest";
+import { WranglerE2ETestHelper } from "./helpers/e2e-wrangler-test";
+import { fetchText } from "./helpers/fetch-text";
+
+describe("wrangler dev - mixed mode", () => {
+	it("can handle both remote and local service bindings at the same time", async () => {
+		const helper = new WranglerE2ETestHelper();
+		await helper.seed({
+			"local/wrangler.json": JSON.stringify({
+				name: "local-worker",
+				main: "index.js",
+				compatibility_date: "2025-05-07",
+			}),
+			"local/index.js": dedent`
+							export default {
+								fetch(request) {
+									return new Response("Hello from local worker");
+								}
+							}`,
+		});
+		await helper.seed({
+			"wrangler.json": JSON.stringify({
+				name: "mixed-mode-mixed-bindings-test",
+				main: "index.js",
+				compatibility_date: "2025-05-07",
+				services: [
+					{ binding: "LOCAL_WORKER", service: "local-worker", remote: false },
+					{
+						binding: "REMOTE_WORKER",
+						service: "mixed-mode-test-target",
+						remote: true,
+					},
+				],
+			}),
+			"index.js": dedent`
+							export default {
+								async fetch(request, env) {
+									const localWorkerText = await (await env.LOCAL_WORKER.fetch(request)).text();
+									const remoteWorkerText = await (await env.REMOTE_WORKER.fetch(request)).text();
+									return new Response(\`LOCAL:\${localWorkerText}\\nREMOTE: \${remoteWorkerText}\n\`);
+								}
+							}`,
+		});
+		const localWorker = helper.runLongLived("wrangler dev");
+		await localWorker.waitForReady();
+
+		const worker = helper.runLongLived("wrangler dev --x-mixed-mode");
+
+		const { url } = await worker.waitForReady();
+
+		await expect(fetchText(url)).resolves.toMatchInlineSnapshot(`
+			"LOCAL:Hello from a local worker
+			REMOTE: Hello World
+			"
+		`);
+	});
+});
