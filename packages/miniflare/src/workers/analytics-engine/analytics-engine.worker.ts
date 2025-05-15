6814162558812faaa312146bf64462ee939a2a50
Samuel Macleod <smacleod@cloudflare.com>
2025-05-15 12:33:31 +0100

    fixes

--- packages/miniflare/src/workers/analytics-engine/analytics-engine.worker.ts
+++ packages/miniflare/src/workers/analytics-engine/analytics-engine.worker.ts
@@ -1,9 +1,13 @@
-export class LocalAnalyticsEngineDataset implements AnalyticsEngineDataset {
+interface Env {
+	dataset: string;
+}
+class LocalAnalyticsEngineDataset implements AnalyticsEngineDataset {
+	constructor(private env: Env) {}
 	writeDataPoint(_event?: AnalyticsEngineDataPoint): void {
 		// no op in dev
 	}
 }
 
-export default function () {
-	return new LocalAnalyticsEngineDataset();
+export default function (env: Env) {
+	return new LocalAnalyticsEngineDataset(env);
 }
