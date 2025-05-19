49c57f34c3a0df6ae2443de1712edb5d83ef5ea0
emily-shen <69125074+emily-shen@users.noreply.github.com>
2025-05-19 13:39:01 +0100

    wrangler plumbing

--- packages/wrangler/src/assets.ts
+++ packages/wrangler/src/assets.ts
@@ -2,17 +2,20 @@ import assert from "node:assert";
 import { existsSync } from "node:fs";
 import { readdir, readFile, stat } from "node:fs/promises";
 import * as path from "node:path";
+import { parseStaticRouting } from "@cloudflare/workers-shared/utils/configuration/parseStaticRouting";
 import {
 	CF_ASSETS_IGNORE_FILENAME,
-	createAssetsIgnoreFunction,
-	getContentType,
 	HEADERS_FILENAME,
 	MAX_ASSET_COUNT,
 	MAX_ASSET_SIZE,
+	REDIRECTS_FILENAME,
+} from "@cloudflare/workers-shared/utils/constants";
+import {
+	createAssetsIgnoreFunction,
+	getContentType,
 	maybeGetFile,
 	normalizeFilePath,
-	REDIRECTS_FILENAME,
-} from "@cloudflare/workers-shared";
+} from "@cloudflare/workers-shared/utils/helpers";
 import chalk from "chalk";
 import PQueue from "p-queue";
 import prettyBytes from "pretty-bytes";
@@ -387,6 +390,7 @@ export type AssetsOptions = {
 	assetConfig: AssetConfig;
 	_redirects?: string;
 	_headers?: string;
+	_routes?: string;
 };
 
 export function getAssetsOptions(
@@ -437,10 +441,24 @@ export function getAssetsOptions(
 
 	const routerConfig: RouterConfig = {
 		has_user_worker: Boolean(args.script || config.main),
-		invoke_user_worker_ahead_of_assets: config.assets?.run_worker_first,
 	};
 
-	// User Worker ahead of assets, but no assets binding provided
+	if (typeof config.assets?.run_worker_first === "boolean") {
+		routerConfig.invoke_user_worker_ahead_of_assets =
+			config.assets.run_worker_first;
+	} else if (typeof config.assets?.run_worker_first === "string") {
+		const { parsed, errorMessage } = parseStaticRouting(
+			config.assets.run_worker_first
+		);
+		if (errorMessage) {
+			throw new UserError(errorMessage, {
+				telemetryMessage: "invalid run_worker_first rules",
+			});
+		}
+		routerConfig.static_routing = parsed;
+	}
+
+	// User Worker always ahead of assets, but no assets binding provided
 	if (
 		routerConfig.invoke_user_worker_ahead_of_assets &&
 		!config?.assets?.binding
@@ -453,13 +471,14 @@ export function getAssetsOptions(
 		);
 	}
 
-	// Using run_worker_first = true but didn't provide a Worker script
+	// Using run_worker_first but didn't provide a Worker script
 	if (
 		!routerConfig.has_user_worker &&
-		routerConfig.invoke_user_worker_ahead_of_assets === true
+		(routerConfig.invoke_user_worker_ahead_of_assets === true ||
+			routerConfig.static_routing)
 	) {
 		throw new UserError(
-			"Cannot set run_worker_first=true without a Worker script.\n" +
+			"Cannot set run_worker_first without a Worker script.\n" +
 				"Please remove run_worker_first from your configuration file, or provide a Worker script in your configuration file (`main`)."
 		);
 	}
@@ -471,7 +490,7 @@ export function getAssetsOptions(
 	const assetConfig: AssetConfig = {
 		html_handling: config.assets?.html_handling,
 		not_found_handling: config.assets?.not_found_handling,
-		// The _redirects and _headers files are parsed in Miniflare in dev and parsing is not required for deploy
+		// The _redirects, _headers and _routes.json files are parsed in Miniflare in dev and parsing is not required for deploy
 		compatibility_date: config.compatibility_date,
 		compatibility_flags: config.compatibility_flags,
 	};
