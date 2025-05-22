a5a3d48d341daef36a9442e8571bbae0e1929bff
justinvdm <justinvderm@gmail.com>
2025-05-22 04:10:24 +0200

    Update to use prisma 6.8.2 and prisma-client generator

--- packages/vite-plugin-cloudflare/playground/prisma/src/index.ts
+++ packages/vite-plugin-cloudflare/playground/prisma/src/index.ts
@@ -1,5 +1,5 @@
 import { PrismaD1 } from "@prisma/adapter-d1";
-import { PrismaClient } from "@prisma/client-generated";
+import { PrismaClient } from "../generated/prisma";
 
 interface Env {
 	DB: D1Database;
@@ -8,7 +8,15 @@ interface Env {
 export default {
 	async fetch(request, env) {
 		const adapter = new PrismaD1(env.DB);
-		const prisma = new PrismaClient({ adapter });
+
+		const prisma = new PrismaClient({
+			// context(justinvdm, 21-05-2025): prisma-client generated type appears to
+			// consider D1 adapter incompatible, though in runtime (dev and production)
+			// it works
+			// @ts-ignore
+			adapter,
+		});
+
 		if (!request.url.includes("/b")) {
 			return new Response(null, {
 				status: 302,
