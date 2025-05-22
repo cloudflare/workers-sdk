b5b5d34900a8ecc4978083f4d00b955a85e79ef4
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-05-22 14:28:20 +0100

    Added tests

--- packages/vite-plugin-cloudflare/playground/assets/__tests__/assets.spec.ts
+++ packages/vite-plugin-cloudflare/playground/assets/__tests__/assets.spec.ts
@@ -1,6 +1,37 @@
 import { expect, test } from "vitest";
-import { getTextResponse, serverLogs, viteTestUrl } from "../../__test-utils__";
+import {
+	getResponse,
+	getTextResponse,
+	isBuild,
+	page,
+	viteTestUrl,
+} from "../../__test-utils__";
 
-test("basic hello-world functionality", async () => {
-	expect(await getTextResponse()).toEqual("Hello World!");
+test("fetches public directory asset", async () => {
+	const response = await getResponse("/public-directory-asset");
+	const contentType = await response.headerValue("content-type");
+	const additionalHeader = await response.headerValue("additional-header");
+	expect(contentType).toBe("image/svg+xml");
+	expect(additionalHeader).toBe("public-directory-asset");
+});
+
+// TODO: enable build test when assets are copied to client output directory
+test.skipIf(isBuild)("fetches imported asset", async () => {
+	const response = await getResponse("/imported-asset");
+	const contentType = await response.headerValue("content-type");
+	const additionalHeader = await response.headerValue("additional-header");
+	expect(contentType).toBe("image/svg+xml");
+	expect(additionalHeader).toBe("imported-asset");
+});
+
+// TODO: enable build test when assets are copied to client output directory
+test.skipIf(isBuild)("fetches imported asset with url suffix", async () => {
+	const text = await getTextResponse("/imported-asset-url-suffix");
+	expect(text).toBe(`The text content is "Text content"`);
+});
+
+test("fetches transformed HTML asset", async () => {
+	await page.goto(`${viteTestUrl}/transformed-html`);
+	const content = await page.textContent("h1");
+	expect(content).toBe("Modified content");
 });
