3306228f502fbb48102705b30593a6d308a9daf3
Matthew Rodgers <matthewrodgers@cloudflare.com>
2025-05-23 15:22:02 -0700

    WC-3584 Add eyeball routing config
    Provided via a new optional param binding. When provided, it contains
    info about free tier limiting for this account.

--- packages/workers-shared/router-worker/src/configuration.ts
+++ packages/workers-shared/router-worker/src/configuration.ts
@@ -1,18 +1,26 @@
-import type { RouterConfig } from "../../utils/types";
+import type { EyeballRouterConfig, RouterConfig } from "../../utils/types";
+
+type RequiredEyeballRouterConfig = Required<Exclude<EyeballRouterConfig, null>>;
 
 export const applyConfigurationDefaults = (
-	configuration?: RouterConfig
-): Required<RouterConfig> => {
-	return {
-		invoke_user_worker_ahead_of_assets:
-			configuration?.invoke_user_worker_ahead_of_assets ?? false,
-		has_user_worker: configuration?.has_user_worker ?? false,
-		account_id: configuration?.account_id ?? -1,
-		script_id: configuration?.script_id ?? -1,
-		debug: configuration?.debug ?? false,
-		static_routing: configuration?.static_routing ?? {
-			version: 1,
-			include: [],
+	configuration?: RouterConfig,
+	eyeballConfiguration?: EyeballRouterConfig
+): [Required<RouterConfig>, RequiredEyeballRouterConfig] => {
+	return [
+		{
+			invoke_user_worker_ahead_of_assets:
+				configuration?.invoke_user_worker_ahead_of_assets ?? false,
+			has_user_worker: configuration?.has_user_worker ?? false,
+			account_id: configuration?.account_id ?? -1,
+			script_id: configuration?.script_id ?? -1,
+			debug: configuration?.debug ?? false,
+			static_routing: configuration?.static_routing ?? {
+				version: 1,
+				include: [],
+			},
 		},
-	};
+		{
+			limitedAssetsOnly: eyeballConfiguration?.limitedAssetsOnly ?? false,
+		},
+	];
 };
