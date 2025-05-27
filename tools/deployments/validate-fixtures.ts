e6ec1c99965a81ab5fb961d9046e80a50e87090b
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2025-05-27 15:26:19 +0100

    test: ensure that fixture packages always start with `fixture-`

--- tools/deployments/validate-fixtures.ts
+++ tools/deployments/validate-fixtures.ts
@@ -3,12 +3,27 @@ import { resolve } from "node:path";
 
 if (require.main === module) {
 	console.log("::group::Checking fixtures");
-	const errors = ensureFixturesAreNotDeployable();
+	const errors = checkFixtures();
 	if (errors.length > 0) {
 		console.error("::error::Fixture checks:" + errors.map((e) => `\n- ${e}`));
 	}
 	console.log("::endgroup::");
 	process.exit(errors.length > 0 ? 1 : 0);
+}
+
+function getFixtures() {
+	const fixturesDirectory = resolve(__dirname, "../../fixtures");
+	const allFixtureDirectories = readdirSync(fixturesDirectory);
+	return allFixtureDirectories
+		.map((dir) => {
+			return {
+				dir,
+				packageJson: resolve(fixturesDirectory, dir, "package.json"),
+			};
+		})
+		.filter(({ packageJson }) => {
+			return existsSync(packageJson);
+		});
 }
 
 /**
@@ -17,28 +32,34 @@ if (require.main === module) {
  *
  * Check the package.json for each fixture and ensure that they are all private and have no `version` property.
  */
-export function ensureFixturesAreNotDeployable(): string[] {
-	const fixturesDirectory = resolve(__dirname, "../../fixtures");
-	const allFixtureDirectories = readdirSync(fixturesDirectory);
+export function checkFixtures(): string[] {
+	const allFixtures = getFixtures();
 	const errors: string[] = [];
-	for (const dir of allFixtureDirectories) {
+	for (const { dir, packageJson } of allFixtures) {
 		try {
 			console.log(`- ${dir}`);
-			const packageJSONPath = resolve(fixturesDirectory, dir, "package.json");
-			if (!existsSync(packageJSONPath)) {
-				continue;
-			}
 			const fixturePackage = JSON.parse(
-				readFileSync(packageJSONPath, "utf-8")
+				readFileSync(packageJson, "utf-8")
 			) as PackageJSON;
+
+			// Check the package is not deployable.
 			if (fixturePackage.private !== true) {
 				errors.push(
 					`Fixture "${dir}" is not private. Add \`"private": true\` to its package.json`
 				);
 			}
+
+			// Check the package does not have a version.
 			if (fixturePackage.version) {
 				errors.push(
 					`Fixture "${dir}" has the disallowed "version" property. Please remove this.`
+				);
+			}
+
+			// Check the package has a name starting with "fixture-".
+			if (!fixturePackage.name.startsWith("fixture-")) {
+				errors.push(
+					`Fixture in directory "fixtures/${dir}" has a name that does not start with "fixture-". Please rename it to start with "fixture-".`
 				);
 			}
 		} catch {
