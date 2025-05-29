6dc5f221b0e20f4fba9ecfc1dd9670835a5c40be
Dario Piotrowicz <dario@cloudflare.com>
2025-05-29 11:36:03 +0100

    revert changes applied to vite-plugin-cloudflare/e2e/helpers.ts (#9398)
    revert the changes applied to the helpers.ts file which allowed
    fixtures isolation since the changes didn't work as intended on
    windows

--- packages/vite-plugin-cloudflare/e2e/invalid-worker-env-configs.test.ts
+++ packages/vite-plugin-cloudflare/e2e/invalid-worker-env-configs.test.ts
@@ -1,11 +1,11 @@
 import { describe, test } from "vitest";
-import { runLongLived, testSeed } from "./helpers";
+import { runLongLived, seed } from "./helpers";
 
 // Note: the test here just makes sure that the validation does take place, for more fine grained
 //       testing regarding the validation there are unit tests in src/__tests__/validate_worker_environments_resolved_configs.spec.ts
 
 describe("during development wrangler config files are validated", () => {
-	const projectPath = testSeed("invalid-worker-env-configs", "pnpm");
+	const projectPath = seed("invalid-worker-env-configs", "pnpm");
 	test("for the entry worker", async ({ expect }) => {
 		const proc = await runLongLived("pnpm", "dev", projectPath);
 		expect(await proc.exitCode).not.toBe(0);
--- packages/vite-plugin-cloudflare/e2e/wrangler-configs-validation.test.ts
+++ packages/vite-plugin-cloudflare/e2e/wrangler-configs-validation.test.ts
@@ -1,11 +1,11 @@
 import { describe, test } from "vitest";
-import { runLongLived, testSeed } from "./helpers";
+import { runLongLived, seed } from "./helpers";
 
 // Note: the tests here just make sure that the validation does take place, for more fine grained
 //       testing regarding the validation there are unit tests in src/__tests__/get-validated-wrangler-config-path.spec.ts
 
 describe("during development wrangler config files are validated", () => {
-	const noWranglerConfigProjectPath = testSeed("no-wrangler-config", "pnpm");
+	const noWranglerConfigProjectPath = seed("no-wrangler-config", "pnpm");
 	test("for the entry worker", async ({ expect }) => {
 		const proc = await runLongLived("pnpm", "dev", noWranglerConfigProjectPath);
 		expect(await proc.exitCode).not.toBe(0);
@@ -14,7 +14,7 @@ describe("during development wrangler config files are validated", () => {
 		);
 	});
 
-	const noWranglerConfigAuxProjectPath = testSeed(
+	const noWranglerConfigAuxProjectPath = seed(
 		"no-wrangler-config-for-auxiliary-worker",
 		"pnpm"
 	);
