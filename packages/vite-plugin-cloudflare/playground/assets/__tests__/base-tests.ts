3488b0e095b006802a72881e40958ff39dc61559
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-06-05 17:41:53 +0100

    Move server assets to client build output and enable build tests

--- packages/vite-plugin-cloudflare/playground/assets/__tests__/base-tests.ts
+++ packages/vite-plugin-cloudflare/playground/assets/__tests__/base-tests.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { getResponse, getTextResponse, isBuild } from "../../__test-utils__";
+import { getResponse, getTextResponse } from "../../__test-utils__";
 
 test("fetches public directory asset", async () => {
 	const response = await getResponse("/public-directory-asset");
@@ -9,8 +9,7 @@ test("fetches public directory asset", async () => {
 	expect(additionalHeader).toBe("public-directory-asset");
 });
 
-// TODO: enable build test when assets are copied to client output directory
-test.skipIf(isBuild)("fetches imported asset", async () => {
+test("fetches imported asset", async () => {
 	const response = await getResponse("/imported-asset");
 	const contentType = await response.headerValue("content-type");
 	const additionalHeader = await response.headerValue("additional-header");
@@ -18,8 +17,7 @@ test.skipIf(isBuild)("fetches imported asset", async () => {
 	expect(additionalHeader).toBe("imported-asset");
 });
 
-// TODO: enable build test when assets are copied to client output directory
-test.skipIf(isBuild)("fetches imported asset with url suffix", async () => {
+test("fetches imported asset with url suffix", async () => {
 	const text = await getTextResponse("/imported-asset-url-suffix");
 	expect(text).toBe(`The text content is "Text content"`);
 });
--- packages/vite-plugin-cloudflare/src/build.ts
+++ packages/vite-plugin-cloudflare/src/build.ts
@@ -1,7 +1,8 @@
 import * as fs from "node:fs";
+import * as path from "node:path";
 import type * as vite from "vite";
 
-export function checkPublicDir({ publicDir }: vite.ResolvedConfig) {
+export function checkPublicDir({ publicDir }: vite.ResolvedConfig): boolean {
 	let hasPublicAssets = false;
 
 	if (publicDir) {
@@ -15,4 +16,23 @@ export function checkPublicDir({ publicDir }: vite.ResolvedConfig) {
 	}
 
 	return hasPublicAssets;
+}
+
+export function loadViteManifest(directory: string) {
+	const contents = fs.readFileSync(
+		path.resolve(directory, ".vite", "manifest.json"),
+		"utf-8"
+	);
+
+	return JSON.parse(contents) as vite.Manifest;
+}
+
+export function getImportedAssetPaths(
+	viteManifest: vite.Manifest
+): Set<string> {
+	const assetPaths = Object.values(viteManifest).flatMap(
+		(chunk) => chunk.assets ?? []
+	);
+
+	return new Set(assetPaths);
 }
