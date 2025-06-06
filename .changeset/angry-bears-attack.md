7b59c849972c8454fd7837990ab88238484601e7
Cody Roseborough <croseborough@cloudflare.com>
2025-06-06 12:46:15 -0700

    Add validation to containerAppConfiguration that disk.size is not set
    disk.size field is deprecated. Add an error pointing the user to the
    correct field if it is set.

---
+++ .changeset/angry-bears-attack.md
@@ -0,0 +1,5 @@
+---
+"wrangler": patch
+---
+
+Add validation that the deprecated field containers.disk.size is not set.
--- packages/wrangler/src/config/validation.ts
+++ packages/wrangler/src/config/validation.ts
@@ -2389,16 +2389,22 @@ const validateContainerAppConfig: ValidatorFn = (
 			diagnostics.errors.push(
 				`"containers.configuration" is defined as an array, it should be an object`
 			);
-		} else if (
-			!isRequiredProperty(
-				containerAppOptional.configuration as UserDeploymentConfiguration,
-				"image",
-				"string"
-			)
-		) {
-			diagnostics.errors.push(
-				`"containers.image" should be defined and a string`
-			);
+		} else {
+			const userDeploymentConfig =
+				containerAppOptional.configuration as UserDeploymentConfiguration;
+			if (!isRequiredProperty(userDeploymentConfig, "image", "string")) {
+				diagnostics.errors.push(
+					`"containers.image" should be defined and a string`
+				);
+			}
+			if (
+				hasProperty(userDeploymentConfig, "disk") &&
+				hasProperty(userDeploymentConfig.disk, "size")
+			) {
+				diagnostics.errors.push(
+					`"containers.disk.size" is deprecated and should not be used. Use "containers.disk.size_mb" instead`
+				);
+			}
 		}
 	}
 
