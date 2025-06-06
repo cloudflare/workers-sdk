0c0f2e1147fc5d94668900c28924301ffe68e287
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-06-06 15:56:36 +0100

    Deny acceess to .dev.vars and .dev.vars.*

---
+++ packages/vite-plugin-cloudflare/playground/sensitive-files/__tests__/sensitive-files.spec.ts
@@ -0,0 +1,56 @@
+import { describe, expect, test } from "vitest";
+import { getResponse, getTextResponse, isBuild } from "../../__test-utils__";
+
+describe.skipIf(isBuild)("denies access to sensitive files in dev", () => {
+	test("denies access to .env", async () => {
+		const response = await getResponse("/.env");
+		expect(response.status()).toBe(403);
+	});
+
+	test("denies access to .env.*", async () => {
+		const response = await getResponse("/.env.staging");
+		expect(response.status()).toBe(403);
+	});
+
+	test("denies access to .dev.vars", async () => {
+		const response = await getResponse("/.dev.vars");
+		expect(response.status()).toBe(403);
+	});
+
+	test("denies access to .dev.vars.*", async () => {
+		const response = await getResponse("/.dev.vars.staging");
+		expect(response.status()).toBe(403);
+	});
+
+	test("denies access to custom-sensitive-file", async () => {
+		const response = await getResponse("/custom-sensitive-file");
+		expect(response.status()).toBe(403);
+	});
+});
+
+describe.runIf(isBuild)("doesn't serve sensitive files in preview", () => {
+	test("doesn't serve .env", async () => {
+		const response = await getTextResponse("/.env");
+		expect(response).toBe("Worker response");
+	});
+
+	test("doesn't serve .env.*", async () => {
+		const response = await getTextResponse("/.env.staging");
+		expect(response).toBe("Worker response");
+	});
+
+	test("doesn't serve .dev.vars", async () => {
+		const response = await getTextResponse("/.dev.vars");
+		expect(response).toBe("Worker response");
+	});
+
+	test("doesn't serve .dev.vars.*", async () => {
+		const response = await getTextResponse("/.dev.vars.staging");
+		expect(response).toBe("Worker response");
+	});
+
+	test("doesn't serve custom-sensitive-file", async () => {
+		const response = await getTextResponse("/custom-sensitive-file");
+		expect(response).toBe("Worker response");
+	});
+});
