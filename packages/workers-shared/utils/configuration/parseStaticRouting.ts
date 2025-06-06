db6e206f2c5f217822a0a056eaa8578cc056fae5
emily-shen <69125074+emily-shen@users.noreply.github.com>
2025-06-06 12:10:58 +0100

    add static_routing validation

---
+++ packages/workers-shared/utils/configuration/parseStaticRouting.ts
@@ -0,0 +1,89 @@
+import { MAX_ROUTES_RULE_LENGTH, MAX_ROUTES_RULES } from "./constants";
+import type { StaticRouting } from "../types";
+
+export function parseStaticRouting(input: string[]): {
+	parsed: StaticRouting;
+	errorMessage: string | undefined;
+} {
+	if (input.length === 0) {
+		throw new Error(
+			"No `run_worker_first` rules were provided; must provide at least 1 rule."
+		);
+	}
+	if (input.length > MAX_ROUTES_RULES) {
+		throw new Error(
+			`Too many rules were provided; ${input.length} rules provided exceeds max of ${MAX_ROUTES_RULES}`
+		);
+	}
+
+	const rawAssetRules = [];
+	const assetRules = [];
+	const userWorkerRules = [];
+	const invalidRules = [];
+
+	for (const rule of input) {
+		if (rule.startsWith("!/")) {
+			assetRules.push(rule.slice(1)); // Remove leading !
+			rawAssetRules.push(rule);
+		} else if (rule.startsWith("/")) {
+			userWorkerRules.push(rule);
+		} else {
+			if (rule.startsWith("!")) {
+				invalidRules.push(`'${rule}': negative rules must start with '!/'`);
+			} else {
+				invalidRules.push(`'${rule}': rules must start with '/' or '!/'`);
+			}
+		}
+	}
+
+	if (assetRules.length > 0 && userWorkerRules.length === 0) {
+		throw new Error(
+			`Only negative rulews were provided; must provide at least 1 non-negative rule`
+		);
+	}
+
+	const invalidAssetRules = validateStaticRoutingRules(rawAssetRules);
+	const invalidUserWorkerRules = validateStaticRoutingRules(userWorkerRules);
+
+	const errorMessage = formatInvalidRoutes([
+		...invalidRules,
+		...invalidUserWorkerRules,
+		...invalidAssetRules,
+	]);
+	return {
+		parsed: { asset_worker: assetRules, user_worker: userWorkerRules },
+		errorMessage,
+	};
+}
+
+function validateStaticRoutingRules(rules: string[]): string[] {
+	const invalid: string[] = [];
+	const seen = new Set<string>();
+	for (const rule of rules) {
+		if (rule.length > MAX_ROUTES_RULE_LENGTH) {
+			invalid.push(
+				`'${rule}': all rules must be less than ${MAX_ROUTES_RULE_LENGTH} characters in length`
+			);
+		}
+		if (rule.endsWith("/*")) {
+			// Check for redundant rules due to a glob
+			for (const otherRule of rules) {
+				if (otherRule !== rule && otherRule.startsWith(rule.slice(0, -1))) {
+					invalid.push(`'${rule}': rule '${otherRule}' makes it redundant`);
+				}
+			}
+		}
+		if (seen.has(rule)) {
+			invalid.push(`'${rule}': rule is redundant`);
+		}
+		seen.add(rule);
+	}
+	return invalid;
+}
+
+const formatInvalidRoutes = (invalidRules: string[]) => {
+	if (invalidRules.length === 0) {
+		return undefined;
+	}
+	return `Invalid routes in run_worker_first:\n` + invalidRules.join("\n");
+};
