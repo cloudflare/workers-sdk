ff9e1ba4b616b44d59e0e5712dd0218fc54fdb4b
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-06-09 13:21:47 +0100

    Add additional assets playground tests

--- packages/vite-plugin-cloudflare/playground/assets/__tests__/assets.spec.ts
+++ packages/vite-plugin-cloudflare/playground/assets/__tests__/assets.spec.ts
@@ -7,3 +7,17 @@ test("fetches transformed HTML asset", async () => {
 	const content = await page.textContent("h1");
 	expect(content).toBe("Modified content");
 });
+
+test("fetches original public directory asset if requested directly", async () => {
+	const response = await getResponse("/public-image.svg");
+	const contentType = await response.headerValue("content-type");
+	const additionalHeader = await response.headerValue("additional-header");
+	expect(contentType).toBe("image/svg+xml");
+	expect(additionalHeader).toBe(null);
+});
+
+test("fetches original HTML asset if requested directly", async () => {
+	await page.goto(`${viteTestUrl}/html-page`);
+	const content = await page.textContent("h1");
+	expect(content).toBe("Original content");
+});
