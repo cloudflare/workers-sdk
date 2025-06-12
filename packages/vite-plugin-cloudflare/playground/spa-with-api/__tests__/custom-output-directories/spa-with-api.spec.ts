416870aea6892cb4006272de59adff81ae3f85b0
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-06-12 12:50:24 +0100

    Add vi.waitFor in file system test

--- packages/vite-plugin-cloudflare/playground/spa-with-api/__tests__/custom-output-directories/spa-with-api.spec.ts
+++ packages/vite-plugin-cloudflare/playground/spa-with-api/__tests__/custom-output-directories/spa-with-api.spec.ts
@@ -1,16 +1,18 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { isBuild, rootDir } from "../../../__test-utils__";
 import "../base-tests";
 
 describe.runIf(isBuild)("output directories", () => {
-	test("creates the correct output directories", () => {
-		expect(
-			fs.existsSync(path.join(rootDir, "custom-root-output-directory", "api"))
-		).toBe(true);
-		expect(
-			fs.existsSync(path.join(rootDir, "custom-client-output-directory"))
-		).toBe(true);
+	test("creates the correct output directories", async () => {
+		await vi.waitFor(() => {
+			expect(
+				fs.existsSync(path.join(rootDir, "custom-root-output-directory", "api"))
+			).toBe(true);
+			expect(
+				fs.existsSync(path.join(rootDir, "custom-client-output-directory"))
+			).toBe(true);
+		});
 	});
 });
