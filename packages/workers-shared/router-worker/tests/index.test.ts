e216a76fba20acf06faad2d9b310a189fe3ccb08
matthewdavidrodgers <39633382+matthewdavidrodgers@users.noreply.github.com>
2025-06-19 00:54:14 -0700

    Router worker performs free tier limiting (#9661)
    * WC-3584 Refactor routing to asset or user worker
    
    Centralizing some of these pieces will make it easier to add free tier
    limiting
    
    * WC-3584 Add eyeball routing config
    
    Provided via a new optional param binding. When provided, it contains
    info about free tier limiting for this account.
    
    * WC-3584 Deny requests to the user worker when over free tier limits
    
    Returns an html page (included as direct module, via esbuild) with a 429
    status code, mirroring the current free tier limiting behavior. It also
    sets a new field in our analytics so we can observe free tier invocation
    denials.
    
    This page can evolve as we need it to, this is just my first pass at the
    response.
    
    * WC-3584 Add changeset
    
    * WC-3584 Move the limited response page to be inlined
    
    There's no loader configured for html files by default in wrangler it
    seems. So rather than mess with loaders or a custom build, inlining this
    page is easy enough.
    
    This pages copies what fl returns exactly, and there's a few templated
    things to reproduce that. Namely: the hostname in the title of the page,
    the ip address of the eyeball, and the current date (with some formatting)

--- packages/workers-shared/router-worker/tests/index.test.ts
+++ packages/workers-shared/router-worker/tests/index.test.ts
@@ -377,4 +377,163 @@ describe("unit tests", async () => {
 		const response = await worker.fetch(request, env, ctx);
 		expect(response.status).toBe(304);
 	});
+
+	describe("free tier limiting", () => {
+		it("returns fetch from asset worker for assets", async () => {
+			const request = new Request("https://example.com/asset");
+			const ctx = createExecutionContext();
+
+			const env = {
+				CONFIG: {
+					has_user_worker: true,
+				},
+				EYEBALL_CONFIG: { limitedAssetsOnly: true },
+				USER_WORKER: {
+					async fetch(_: Request): Promise<Response> {
+						return new Response("hello from user worker");
+					},
+				},
+				ASSET_WORKER: {
+					async fetch(_: Request): Promise<Response> {
+						return new Response("hello from asset worker");
+					},
+					async unstable_canFetch(_: Request): Promise<boolean> {
+						return true;
+					},
+				},
+			} as Env;
+
+			const response = await worker.fetch(request, env, ctx);
+			expect(await response.text()).toEqual("hello from asset worker");
+		});
+
+		it("returns error page instead of user worker when no asset found", async () => {
+			const request = new Request("https://example.com/asset");
+			const ctx = createExecutionContext();
+
+			const env = {
+				CONFIG: {
+					has_user_worker: true,
+				},
+				EYEBALL_CONFIG: { limitedAssetsOnly: true },
+				USER_WORKER: {
+					async fetch(_: Request): Promise<Response> {
+						return new Response("hello from user worker");
+					},
+				},
+				ASSET_WORKER: {
+					async fetch(_: Request): Promise<Response> {
+						return new Response("hello from asset worker");
+					},
+					async unstable_canFetch(_: Request): Promise<boolean> {
+						return false;
+					},
+				},
+			} as Env;
+
+			const response = await worker.fetch(request, env, ctx);
+			expect(response.status).toEqual(429);
+			const text = await response.text();
+			expect(text).not.toEqual("hello from user worker");
+			expect(text).toContain("This website has been temporarily rate limited");
+		});
+
+		it("returns error page instead of user worker for invoke_user_worker_ahead_of_assets", async () => {
+			const request = new Request("https://example.com/asset");
+			const ctx = createExecutionContext();
+
+			const env = {
+				CONFIG: {
+					has_user_worker: true,
+					invoke_user_worker_ahead_of_assets: true,
+				},
+				EYEBALL_CONFIG: { limitedAssetsOnly: true },
+				USER_WORKER: {
+					async fetch(_: Request): Promise<Response> {
+						return new Response("hello from user worker");
+					},
+				},
+				ASSET_WORKER: {
+					async fetch(_: Request): Promise<Response> {
+						return new Response("hello from asset worker");
+					},
+					async unstable_canFetch(_: Request): Promise<boolean> {
+						return true;
+					},
+				},
+			} as Env;
+
+			const response = await worker.fetch(request, env, ctx);
+			expect(response.status).toEqual(429);
+			const text = await response.text();
+			expect(text).not.toEqual("hello from user worker");
+			expect(text).toContain("This website has been temporarily rate limited");
+		});
+
+		it("returns error page instead of user worker for user_worker rules", async () => {
+			const request = new Request("https://example.com/api/asset");
+			const ctx = createExecutionContext();
+
+			const env = {
+				CONFIG: {
+					has_user_worker: true,
+					static_routing: {
+						user_worker: ["/api/*"],
+					},
+				},
+				EYEBALL_CONFIG: { limitedAssetsOnly: true },
+				USER_WORKER: {
+					async fetch(_: Request): Promise<Response> {
+						return new Response("hello from user worker");
+					},
+				},
+				ASSET_WORKER: {
+					async fetch(_: Request): Promise<Response> {
+						return new Response("hello from asset worker");
+					},
+					async unstable_canFetch(_: Request): Promise<boolean> {
+						return true;
+					},
+				},
+			} as Env;
+
+			const response = await worker.fetch(request, env, ctx);
+			expect(response.status).toEqual(429);
+			const text = await response.text();
+			expect(text).not.toEqual("hello from user worker");
+			expect(text).toContain("This website has been temporarily rate limited");
+		});
+
+		it("returns fetch from asset worker for asset_worker rules", async () => {
+			const request = new Request("https://example.com/api/asset");
+			const ctx = createExecutionContext();
+
+			const env = {
+				CONFIG: {
+					has_user_worker: true,
+					static_routing: {
+						user_worker: ["/api/*"],
+						asset_worker: ["/api/asset"],
+					},
+				},
+				EYEBALL_CONFIG: { limitedAssetsOnly: true },
+				USER_WORKER: {
+					async fetch(_: Request): Promise<Response> {
+						return new Response("hello from user worker");
+					},
+				},
+				ASSET_WORKER: {
+					async fetch(_: Request): Promise<Response> {
+						return new Response("hello from asset worker");
+					},
+					async unstable_canFetch(_: Request): Promise<boolean> {
+						return true;
+					},
+				},
+			} as Env;
+
+			const response = await worker.fetch(request, env, ctx);
+			expect(await response.text()).toEqual("hello from asset worker");
+		});
+	});
 });
