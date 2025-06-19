00c3130662f5b25ab05ab36eece0b2f9eefc2bba
Carmen Popoviciu <cpopoviciu@cloudflare.com>
2025-06-19 12:50:47 +0200

    test: change in containers-shared code path

--- packages/wrangler/src/cloudchamber/build.ts
+++ packages/wrangler/src/cloudchamber/build.ts
@@ -228,7 +228,7 @@ export async function ensureDiskLimits(options: {
 	// if appDiskSize is defined and configured to be more than the accountDiskSize, error
 	if (appDiskSize && appDiskSize > accountDiskSize) {
 		throw new UserError(
-			`OH NOOOOO! Exceeded account limits: Your container is configured to use a disk size of ${appDiskSize / MB} MB. However, that exceeds the account limit of ${accountDiskSize / MB}`
+			`Exceeded account limits: Your container is configured to use a disk size of ${appDiskSize / MB} MB. However, that exceeds the account limit of ${accountDiskSize / MB}`
 		);
 	}
 	const maxAllowedImageSizeBytes = appDiskSize ?? accountDiskSize;
