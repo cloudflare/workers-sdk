96ae732703d57f391e6a2531098a164a7be4ebf4
Victor Berchet <victor@suumit.com>
2025-07-04 09:32:46 +0200

    Add an experiment for the update search algo

--- packages/workers-shared/asset-worker/tests/assets-manifest.2.test.ts
+++ packages/workers-shared/asset-worker/tests/assets-manifest.2.test.ts
@@ -1,13 +1,16 @@
 import {
 	CONTENT_HASH_OFFSET,
-	CONTENT_HASH_SIZE,
 	ENTRY_SIZE,
 	HEADER_SIZE,
 	PATH_HASH_OFFSET,
 	PATH_HASH_SIZE,
 } from "../../utils/constants";
 import AssetManifestFixture from "../fixtures/AssetManifest.bin";
-import { binarySearch, compare, hashPath } from "../src/assets-manifest";
+import {
+	AssetsManifest,
+	binarySearch,
+	hashPath,
+} from "../src/assets-manifest.2";
 
 const encoder = new TextEncoder();
 
@@ -132,7 +135,7 @@ describe("search methods", async () => {
 		it("doesn't error for an empty manifest", async () => {
 			const { manifest } = await makeManifestOfLength(0);
 			const foundEntry = binarySearch(
-				new Uint8Array(manifest, HEADER_SIZE),
+				new Uint8Array(manifest),
 				await hashPath("/path")
 			);
 			expect(foundEntry).toBe(false);
@@ -143,17 +146,11 @@ describe("search methods", async () => {
 			for (const searchEntry of entries) {
 				const path = await hashPath(searchEntry.path);
 				const foundEntry = binarySearch(
-					new Uint8Array(manifest, HEADER_SIZE),
+					new Uint8Array(manifest),
 					path
 				) as Uint8Array;
 				expect(foundEntry).not.toBe(false);
-				expect(
-					new Uint8Array(
-						foundEntry.buffer,
-						CONTENT_HASH_OFFSET + foundEntry.byteOffset,
-						CONTENT_HASH_SIZE
-					)
-				).toEqual(hexToBytes(searchEntry.contentHash));
+				expect(foundEntry).toEqual(hexToBytes(searchEntry.contentHash));
 			}
 		});
 
@@ -162,17 +159,11 @@ describe("search methods", async () => {
 			for (const searchEntry of entries) {
 				const path = await hashPath(searchEntry.path);
 				const foundEntry = binarySearch(
-					new Uint8Array(manifest, HEADER_SIZE),
+					new Uint8Array(manifest),
 					path
 				) as Uint8Array;
 				expect(foundEntry).not.toBe(false);
-				expect(
-					new Uint8Array(
-						foundEntry.buffer,
-						CONTENT_HASH_OFFSET + foundEntry.byteOffset,
-						CONTENT_HASH_SIZE
-					)
-				).toEqual(hexToBytes(searchEntry.contentHash));
+				expect(foundEntry).toEqual(hexToBytes(searchEntry.contentHash));
 			}
 		});
 
@@ -180,37 +171,27 @@ describe("search methods", async () => {
 			const { manifest, entries } = await makeManifestOfLength(3);
 			for (const searchEntry of entries) {
 				const path = await hashPath(searchEntry.path);
-				const foundEntry = binarySearch(
-					new Uint8Array(manifest, HEADER_SIZE),
-					path
-				) as Uint8Array;
+				const foundEntry = binarySearch(new Uint8Array(manifest), path);
 				expect(foundEntry).not.toBe(false);
-				expect(
-					new Uint8Array(
-						foundEntry.buffer,
-						CONTENT_HASH_OFFSET + foundEntry.byteOffset,
-						CONTENT_HASH_SIZE
-					)
-				).toEqual(hexToBytes(searchEntry.contentHash));
+				expect(foundEntry).toEqual(hexToBytes(searchEntry.contentHash));
 			}
 		});
 
 		it("works for a 20,000 entry manifest", async () => {
 			const { manifest, entries } = await makeManifestOfLength(20_000);
+			const assetManifest = new AssetsManifest(manifest);
 			for (const searchEntry of entries) {
 				const path = await hashPath(searchEntry.path);
-				const foundEntry = binarySearch(
-					new Uint8Array(manifest, HEADER_SIZE),
-					path
-				) as Uint8Array;
+				const foundEntry = binarySearch(new Uint8Array(manifest), path);
 				expect(foundEntry).not.toBe(false);
-				expect(
-					new Uint8Array(
-						foundEntry.buffer,
-						CONTENT_HASH_OFFSET + foundEntry.byteOffset,
-						CONTENT_HASH_SIZE
-					)
-				).toEqual(hexToBytes(searchEntry.contentHash));
+				const foundEntryWithAssetManifest = await assetManifest.get(
+					searchEntry.path
+				);
+				expect(foundEntryWithAssetManifest).not.toBe(null);
+				expect(foundEntry).toEqual(
+					hexToBytes(foundEntryWithAssetManifest as string)
+				);
+				expect(foundEntry).toEqual(hexToBytes(searchEntry.contentHash));
 			}
 		});
 
@@ -221,10 +202,7 @@ describe("search methods", async () => {
 				new Uint8Array(PATH_HASH_SIZE).fill(127),
 				new Uint8Array(PATH_HASH_SIZE).fill(255),
 			]) {
-				const foundEntry = binarySearch(
-					new Uint8Array(manifest, HEADER_SIZE),
-					searchEntry
-				);
+				const foundEntry = binarySearch(new Uint8Array(manifest), searchEntry);
 				expect(foundEntry).toBe(false);
 			}
 		});
@@ -233,12 +211,33 @@ describe("search methods", async () => {
 			const { manifest } = await makeManifestOfLength(10);
 			expect(() => {
 				binarySearch(
-					new Uint8Array(manifest, HEADER_SIZE),
+					new Uint8Array(manifest),
 					new Uint8Array(PATH_HASH_SIZE + 1).fill(127)
 				);
 			}).toThrowErrorMatchingInlineSnapshot(
-				`[TypeError: Search value and current value are of different lengths]`
+				`[TypeError: Search value should have a length of 16]`
 			);
 		});
 	});
 });
+
+function compare(a: Uint8Array, b: Uint8Array) {
+	if (a.byteLength < b.byteLength) {
+		return -1;
+	}
+	if (a.byteLength > b.byteLength) {
+		return 1;
+	}
+
+	for (const [i, v] of a.entries()) {
+		const bVal = b[i] as number;
+		if (v < bVal) {
+			return -1;
+		}
+		if (v > bVal) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
