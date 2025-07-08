6a16744d7436fd77d3482d8e6da01e874fe92678
helloimalastair <helloimalastair@users.noreply.github.com>
2025-07-08 10:16:40 -0700

    add testing for banner being hidden

---
+++ packages/wrangler/src/__tests__/r2/pipe.test.ts
@@ -0,0 +1,54 @@
+import { writeFileSync } from "node:fs";
+import { mockConsoleMethods } from "../helpers/mock-console";
+import { runInTempDir } from "../helpers/run-in-tmp";
+import { runWrangler } from "../helpers/run-wrangler";
+
+vi.unmock("../../wrangler-banner");
+const decoder = new TextDecoder();
+describe("pipe test", () => {
+	const consoleSpy = mockConsoleMethods();
+	const stdSpy = vi
+		.spyOn(process.stdout, "write")
+		.mockImplementation(() => true);
+	runInTempDir();
+
+	it("should display banner", async () => {
+		writeFileSync("wormhole.txt", "passageway");
+		await runWrangler(
+			`r2 object put bucket-object-test/wormhole.txt --file ./wormhole.txt `
+		);
+		await runWrangler("r2 object get bucket-object-test/wormhole.txt");
+
+		expect(stdSpy).not.toBeCalled();
+		expect(consoleSpy.out).toMatchInlineSnapshot(`
+			"
+			 ⛅️ wrangler x.x.x
+			──────────────────
+			Resource location: local
+			Use --remote if you want to access the remote instance.
+
+			Creating object \\"wormhole.txt\\" in bucket \\"bucket-object-test\\".
+			Upload complete.
+
+			 ⛅️ wrangler x.x.x
+			──────────────────
+			Resource location: local
+			Use --remote if you want to access the remote instance.
+
+			Downloading \\"wormhole.txt\\" from \\"bucket-object-test\\".
+			Download complete."
+		`);
+	});
+
+	it("should not display banner in pipe mode", async () => {
+		writeFileSync("wormhole.txt", "passageway");
+		await runWrangler(
+			`r2 object put bucket-object-test/wormhole.txt --file ./wormhole.txt `
+		);
+		await runWrangler("r2 object get bucket-object-test/wormhole.txt --pipe");
+
+		expect(
+			decoder.decode(stdSpy.mock.calls[0][0] as unknown as Uint8Array)
+		).toMatchInlineSnapshot(`"passageway"`);
+	});
+});
