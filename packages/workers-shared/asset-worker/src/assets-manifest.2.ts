1f3050bc7b649d1a189fb24bb266e157abb1099c
Victor Berchet <victor@suumit.com>
2025-07-08 19:07:03 +0200

    perf: experiment with an alternate binary search implementation (#9852)
    * Optimize the binary search for assets
    
    Optimizations:
    - remove recursion to use a while loop instead
    - do not allocate unnecessary array (to reduce GC)
    - convert the manifest to a Uint8Array once upfront
    - added a faster vesrion of compare (compareSearchValueWithEntry). The compare implementatiopn was moved to the tests where it is used
    
    Other changes
    - BS: use indexes instead of offest (IMO easier to read/debug)
    - BS: return the content hash instead of the whole entry
    
    * Add an experiment for the update search algo
    
    * fixup! fallback to the current implementation on error

---
+++ packages/workers-shared/asset-worker/src/assets-manifest.2.ts
@@ -0,0 +1,122 @@
+import {
+	CONTENT_HASH_OFFSET,
+	CONTENT_HASH_SIZE,
+	ENTRY_SIZE,
+	HEADER_SIZE,
+	PATH_HASH_SIZE,
+} from "../../utils/constants";
+
+export class AssetsManifest {
+	private data: Uint8Array;
+
+	constructor(data: ArrayBuffer) {
+		this.data = new Uint8Array(data);
+	}
+
+	async get(pathname: string) {
+		const pathHash = await hashPath(pathname);
+		const entry = binarySearch(this.data, pathHash);
+		return entry ? Uint8ToHexString(entry) : null;
+	}
+}
+
+export const hashPath = async (path: string) => {
+	const encoder = new TextEncoder();
+	const data = encoder.encode(path);
+	const hashBuffer = await crypto.subtle.digest(
+		"SHA-256",
+		data.buffer as ArrayBuffer
+	);
+	return new Uint8Array(hashBuffer, 0, PATH_HASH_SIZE);
+};
+
+/**
+ * Search for an entry with the given hash path.
+ *
+ * @param manifest the manifest bytes
+ * @param pathHash the path hash to find in the manifest
+ * @returns The content hash when the entry is found and `false` otherwise
+ */
+export const binarySearch = (
+	manifest: Uint8Array,
+	pathHash: Uint8Array
+): Uint8Array | false => {
+	if (pathHash.byteLength !== PATH_HASH_SIZE) {
+		throw new TypeError(
+			`Search value should have a length of ${PATH_HASH_SIZE}`
+		);
+	}
+
+	const numberOfEntries = (manifest.byteLength - HEADER_SIZE) / ENTRY_SIZE;
+
+	if (numberOfEntries === 0) {
+		return false;
+	}
+
+	let lowIndex = 0;
+	let highIndex = numberOfEntries - 1;
+
+	while (lowIndex <= highIndex) {
+		const middleIndex = (lowIndex + highIndex) >> 1;
+
+		const cmp = comparePathHashWithEntry(pathHash, manifest, middleIndex);
+
+		if (cmp < 0) {
+			highIndex = middleIndex - 1;
+			continue;
+		}
+
+		if (cmp > 0) {
+			lowIndex = middleIndex + 1;
+			continue;
+		}
+
+		return new Uint8Array(
+			manifest.buffer,
+			HEADER_SIZE + middleIndex * ENTRY_SIZE + CONTENT_HASH_OFFSET,
+			CONTENT_HASH_SIZE
+		);
+	}
+
+	return false;
+};
+
+/**
+ * Compares a search value with an entry in the manifest
+ *
+ * @param searchValue a `Uint8Array` of size `PATH_HASH_SIZE`
+ * @param manifest the manifest bytes
+ * @param entryIndex the index in the manifest of the entry to compare
+ */
+function comparePathHashWithEntry(
+	searchValue: Uint8Array,
+	manifest: Uint8Array,
+	entryIndex: number
+) {
+	let entryOffset = HEADER_SIZE + entryIndex * ENTRY_SIZE;
+	for (let offset = 0; offset < PATH_HASH_SIZE; offset++, entryOffset++) {
+		// We know that both values could not be undefined
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		const s = searchValue[offset]!;
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		const e = manifest[entryOffset]!;
+		if (s < e) {
+			return -1;
+		}
+		if (s > e) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+/**
+ * Converts an Uint8Array to an hex string
+ *
+ * @param array The content hash
+ * @returns padded hex string
+ */
+const Uint8ToHexString = (array: Uint8Array) => {
+	return [...array].map((b) => b.toString(16).padStart(2, "0")).join("");
+};
---
+++ packages/workers-shared/asset-worker/tests/assets-manifest.2.test.ts
@@ -0,0 +1,243 @@
+import {
+	CONTENT_HASH_OFFSET,
+	ENTRY_SIZE,
+	HEADER_SIZE,
+	PATH_HASH_OFFSET,
+	PATH_HASH_SIZE,
+} from "../../utils/constants";
+import AssetManifestFixture from "../fixtures/AssetManifest.bin";
+import {
+	AssetsManifest,
+	binarySearch,
+	hashPath,
+} from "../src/assets-manifest.2";
+
+const encoder = new TextEncoder();
+
+async function SHA_256(value: string, length: number) {
+	const data = encoder.encode(value);
+	const hashBuffer = await crypto.subtle.digest(
+		"SHA-256",
+		data.buffer as ArrayBuffer
+	);
+	return new Uint8Array(hashBuffer, 0, length);
+}
+
+function hexToBytes(hex: string) {
+	if (!hex.match(/^([0-9a-f]{2})+$/gi)) {
+		throw new TypeError(`Invalid byte string:  ${hex}`);
+	}
+
+	return new Uint8Array(
+		hex.match(/[0-9a-f]{2}/gi)?.map((b) => parseInt(b, 16)) ?? []
+	);
+}
+
+const encode = async (
+	assetEntries: { path: string; contentHash: string }[]
+) => {
+	const entries = await Promise.all(
+		assetEntries.map(async (entry) => ({
+			path: entry.path,
+			contentHash: entry.contentHash,
+			pathHashBytes: await SHA_256(entry.path, PATH_HASH_SIZE),
+		}))
+	);
+	entries.sort((a, b) => compare(a.pathHashBytes, b.pathHashBytes));
+
+	const assetManifestBytes = new Uint8Array(
+		HEADER_SIZE + entries.length * ENTRY_SIZE
+	);
+
+	for (const [i, { pathHashBytes, contentHash }] of entries.entries()) {
+		const contentHashBytes = hexToBytes(contentHash);
+		const entryOffset = HEADER_SIZE + i * ENTRY_SIZE;
+
+		assetManifestBytes.set(pathHashBytes, entryOffset + PATH_HASH_OFFSET);
+		assetManifestBytes.set(contentHashBytes, entryOffset + CONTENT_HASH_OFFSET);
+	}
+
+	return assetManifestBytes.buffer;
+};
+
+describe("encode()", () => {
+	it("works", async () => {
+		const snapshotValue = new Uint8Array(AssetManifestFixture);
+
+		const computedValue = new Uint8Array(
+			await encode([
+				{
+					path: "/path1",
+					contentHash: "0123456789abcdef0123456789abcdef",
+				},
+				{
+					path: "/path2",
+					contentHash: "1123456789abcdef0123456789abcdef",
+				},
+				{
+					path: "/path3",
+					contentHash: "ABCDEF01231230123131231FDFFEDFDF",
+				},
+			])
+		);
+		expect(compare(computedValue, snapshotValue)).toBe(0);
+
+		const invalidContentHashValue = new Uint8Array(
+			await encode([
+				{
+					path: "/path1",
+					contentHash: "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE",
+				},
+				{
+					path: "/path2",
+					contentHash: "1123456789abcdef0123456789abcdef",
+				},
+				{
+					path: "/path3",
+					contentHash: "ABCDEF01231230123131231FDFFEDFDF",
+				},
+			])
+		);
+		expect(compare(invalidContentHashValue, snapshotValue)).not.toBe(0);
+
+		const invalidPathValue = new Uint8Array(
+			await encode([
+				{
+					path: "/path123",
+					contentHash: "0123456789abcdef0123456789abcdef",
+				},
+				{
+					path: "/path2",
+					contentHash: "1123456789abcdef0123456789abcdef",
+				},
+				{
+					path: "/path3",
+					contentHash: "ABCDEF01231230123131231FDFFEDFDF",
+				},
+			])
+		);
+		expect(compare(invalidPathValue, snapshotValue)).not.toBe(0);
+	});
+});
+
+const makePathForId = (id: number) => `/path${id}`;
+
+const makeManifestOfLength = async (length: number) => {
+	const entries = new Array(length).fill(undefined).map((_, i) => ({
+		path: makePathForId(i),
+		contentHash: String(i).padEnd(32, "f"),
+	}));
+	return { entries, manifest: await encode(entries) };
+};
+
+describe("search methods", async () => {
+	describe("binary search", () => {
+		it("doesn't error for an empty manifest", async () => {
+			const { manifest } = await makeManifestOfLength(0);
+			const foundEntry = binarySearch(
+				new Uint8Array(manifest),
+				await hashPath("/path")
+			);
+			expect(foundEntry).toBe(false);
+		});
+
+		it("works for a single entry manifest", async () => {
+			const { manifest, entries } = await makeManifestOfLength(1);
+			for (const searchEntry of entries) {
+				const path = await hashPath(searchEntry.path);
+				const foundEntry = binarySearch(
+					new Uint8Array(manifest),
+					path
+				) as Uint8Array;
+				expect(foundEntry).not.toBe(false);
+				expect(foundEntry).toEqual(hexToBytes(searchEntry.contentHash));
+			}
+		});
+
+		it("works for a two entry manifest", async () => {
+			const { manifest, entries } = await makeManifestOfLength(2);
+			for (const searchEntry of entries) {
+				const path = await hashPath(searchEntry.path);
+				const foundEntry = binarySearch(
+					new Uint8Array(manifest),
+					path
+				) as Uint8Array;
+				expect(foundEntry).not.toBe(false);
+				expect(foundEntry).toEqual(hexToBytes(searchEntry.contentHash));
+			}
+		});
+
+		it("works for a three entry manifest", async () => {
+			const { manifest, entries } = await makeManifestOfLength(3);
+			for (const searchEntry of entries) {
+				const path = await hashPath(searchEntry.path);
+				const foundEntry = binarySearch(new Uint8Array(manifest), path);
+				expect(foundEntry).not.toBe(false);
+				expect(foundEntry).toEqual(hexToBytes(searchEntry.contentHash));
+			}
+		});
+
+		it("works for a 20,000 entry manifest", async () => {
+			const { manifest, entries } = await makeManifestOfLength(20_000);
+			const assetManifest = new AssetsManifest(manifest);
+			for (const searchEntry of entries) {
+				const path = await hashPath(searchEntry.path);
+				const foundEntry = binarySearch(new Uint8Array(manifest), path);
+				expect(foundEntry).not.toBe(false);
+				const foundEntryWithAssetManifest = await assetManifest.get(
+					searchEntry.path
+				);
+				expect(foundEntryWithAssetManifest).not.toBe(null);
+				expect(foundEntry).toEqual(
+					hexToBytes(foundEntryWithAssetManifest as string)
+				);
+				expect(foundEntry).toEqual(hexToBytes(searchEntry.contentHash));
+			}
+		});
+
+		it("returns false for non-existent paths", async () => {
+			const { manifest } = await makeManifestOfLength(10);
+			for (const searchEntry of [
+				new Uint8Array(PATH_HASH_SIZE),
+				new Uint8Array(PATH_HASH_SIZE).fill(127),
+				new Uint8Array(PATH_HASH_SIZE).fill(255),
+			]) {
+				const foundEntry = binarySearch(new Uint8Array(manifest), searchEntry);
+				expect(foundEntry).toBe(false);
+			}
+		});
+
+		it("throws an error if the search value is longer than the current value", async () => {
+			const { manifest } = await makeManifestOfLength(10);
+			expect(() => {
+				binarySearch(
+					new Uint8Array(manifest),
+					new Uint8Array(PATH_HASH_SIZE + 1).fill(127)
+				);
+			}).toThrowErrorMatchingInlineSnapshot(
+				`[TypeError: Search value should have a length of 16]`
+			);
+		});
+	});
+});
+
+function compare(a: Uint8Array, b: Uint8Array) {
+	if (a.byteLength < b.byteLength) {
+		return -1;
+	}
+	if (a.byteLength > b.byteLength) {
+		return 1;
+	}
+
+	for (const [i, v] of a.entries()) {
+		const bVal = b[i] as number;
+		if (v < bVal) {
+			return -1;
+		}
+		if (v > bVal) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
