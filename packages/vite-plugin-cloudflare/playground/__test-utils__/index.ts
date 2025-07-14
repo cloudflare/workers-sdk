631e63f17da53005c31744a8ab2040b843a98749
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2025-07-14 13:22:00 +0100

    Add support for loading local vars from .env files to the vite-plugin

--- packages/vite-plugin-cloudflare/playground/__test-utils__/index.ts
+++ packages/vite-plugin-cloudflare/playground/__test-utils__/index.ts
@@ -1,8 +1,20 @@
-import { test } from "vitest";
+import fs from "node:fs";
+import { onTestFinished, test } from "vitest";
 
 export * from "../vitest-setup";
 export * from "./responses";
 
 export function failsIf(condition: boolean) {
 	return condition ? test.fails : test;
+}
+
+export function mockFileChange(
+	filePath: string,
+	mutateFn: (originalContent: string) => string
+) {
+	const originalContent = fs.readFileSync(filePath, "utf-8");
+	onTestFinished(() => {
+		fs.writeFileSync(filePath, originalContent);
+	});
+	fs.writeFileSync(filePath, mutateFn(originalContent));
 }
---
+++ packages/vite-plugin-cloudflare/playground/dot-env/__tests__/vars-changes.spec.ts
@@ -0,0 +1,38 @@
+import * as path from "node:path";
+import { expect, test, vi } from "vitest";
+import { getJsonResponse, isBuild, mockFileChange } from "../../__test-utils__";
+
+test.runIf(!isBuild)(
+	"successfully updates when a var is updated in a .env file",
+	async () => {
+		const originalResponseContent = {
+			"variables loaded from .env": {
+				MY_DEV_VAR_A: "my .env variable A",
+				MY_DEV_VAR_B: "my .env variable B",
+				MY_DEV_VAR_C: "my .env variable C", // Note that unlike .dev.vars, we merge .env files
+			},
+		};
+		const originalResponse = await getJsonResponse();
+		expect(originalResponse).toEqual(
+			expect.objectContaining(originalResponseContent)
+		);
+
+		mockFileChange(path.join(__dirname, "../.env"), (content) =>
+			content.replace(/my \.env/g, "my .env UPDATED")
+		);
+
+		await vi.waitFor(
+			async () => {
+				const updatedResponse = await getJsonResponse();
+				expect(updatedResponse).toEqual({
+					"variables loaded from .env": {
+						MY_DEV_VAR_A: "my .env UPDATED variable A",
+						MY_DEV_VAR_B: "my .env UPDATED variable B",
+						MY_DEV_VAR_C: "my .env UPDATED variable C", // Note that unlike .dev.vars, we merge .env files
+					},
+				});
+			},
+			{ timeout: 5000 }
+		);
+	}
+);
