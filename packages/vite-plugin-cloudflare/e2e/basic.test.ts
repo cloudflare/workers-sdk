6bb75c59be0605353aa0f11507f52b479822d33c
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2025-07-15 13:58:48 +0100

    add various e2e and fixture tests for dotenv support
    - `wrangler types`
    - `wrangler pages dev`
    - `wrangler dev`
    - nodejs app using `process.env`

--- packages/vite-plugin-cloudflare/e2e/basic.test.ts
+++ packages/vite-plugin-cloudflare/e2e/basic.test.ts
@@ -1,8 +1,8 @@
+import { rm, writeFile } from "node:fs/promises";
 import { describe, test } from "vitest";
 import { fetchJson, runLongLived, seed, waitForReady } from "./helpers.js";
 
-const isWindows = process.platform === "win32";
-const packageManagers = ["pnpm", , "npm", "yarn"] as const;
+const packageManagers = ["pnpm", "npm", "yarn"] as const;
 const commands = ["dev", "buildAndPreview"] as const;
 
 describe("basic e2e tests", () => {
@@ -10,16 +10,112 @@ describe("basic e2e tests", () => {
 		const projectPath = seed("basic", pm);
 
 		describe.each(commands)('with "%s" command', (command) => {
-			describe("node compatibility", () => {
-				test.skipIf(command === "buildAndPreview")(
-					"can serve a Worker request",
-					async ({ expect }) => {
-						const proc = await runLongLived(pm, command, projectPath);
-						const url = await waitForReady(proc);
-						expect(await fetchJson(url + "/api/")).toEqual({
-							name: "Cloudflare",
-						});
-					}
+			test("can serve a Worker that uses a Node.js API (crypto)", async ({
+				expect,
+			}) => {
+				const proc = await runLongLived(pm, command, projectPath);
+				const url = await waitForReady(proc);
+				expect(await fetchJson(url + "/api/")).toEqual({
+					name: "Cloudflare",
+				});
+			});
+
+			describe("environment variables", () => {
+				test("can read vars from wrangler configuration and .env", async ({
+					expect,
+					onTestFinished,
+				}) => {
+					await writeFile(
+						projectPath + "/.env",
+						"SECRET_A=dev-1\nSECRET_B=dev-2"
+					);
+					onTestFinished(async () => {
+						await rm(projectPath + "/.env");
+					});
+					const proc = await runLongLived(pm, command, projectPath);
+					const url = await waitForReady(proc);
+					expect(await fetchJson(url + "/env/")).toEqual(
+						expect.objectContaining({
+							SECRET_A: "dev-1",
+							SECRET_B: "dev-2",
+							VAR_1: "var-1",
+						})
+					);
+				});
+
+				test("can merge vars from wrangler configuration, .env, and .env.local", async ({
+					expect,
+					onTestFinished,
+				}) => {
+					await writeFile(
+						projectPath + "/.env",
+						"SECRET_A=dev-1\nSECRET_B=dev-2"
+					);
+					await writeFile(projectPath + "/.env.local", "SECRET_A=local-dev-1");
+					onTestFinished(async () => {
+						await rm(projectPath + "/.env");
+						await rm(projectPath + "/.env.local");
+					});
+					const proc = await runLongLived(pm, command, projectPath);
+					const url = await waitForReady(proc);
+					expect(await fetchJson(url + "/env/")).toEqual(
+						expect.objectContaining({
+							SECRET_A: "local-dev-1",
+							SECRET_B: "dev-2",
+							VAR_1: "var-1",
+						})
+					);
+				});
+			});
+
+			test("can merge vars from wrangler configuration, .env, and .env.local, and environment specific files", async ({
+				expect,
+				onTestFinished,
+			}) => {
+				await writeFile(
+					projectPath + "/.env",
+					"SECRET_A=dev-1\nSECRET_B=dev-2"
+				);
+				await writeFile(projectPath + "/.env.local", "SECRET_A=local-dev-1");
+				await writeFile(
+					projectPath + "/.env.staging",
+					"SECRET_B=staging-2\nSECRET_C=staging-3"
+				);
+				await writeFile(
+					projectPath + "/.env.staging.local",
+					"SECRET_C=local-staging-3"
+				);
+				onTestFinished(async () => {
+					await rm(projectPath + "/.env");
+					await rm(projectPath + "/.env.local");
+					await rm(projectPath + "/.env.staging");
+					await rm(projectPath + "/.env.staging.local");
+				});
+				const proc = await runLongLived(pm, command, projectPath, {
+					CLOUDFLARE_ENV: "staging",
+				});
+				const url = await waitForReady(proc);
+				expect(await fetchJson(url + "/env/")).toEqual(
+					expect.objectContaining({
+						SECRET_A: "local-dev-1",
+						SECRET_B: "staging-2",
+						SECRET_C: "local-staging-3",
+						VAR_1: "var-1",
+					})
+				);
+			});
+
+			test("can read vars from process.env if CLOUDFLARE_INCLUDE_PROCESS_ENV is set", async ({
+				expect,
+			}) => {
+				const proc = await runLongLived(pm, command, projectPath, {
+					CLOUDFLARE_INCLUDE_PROCESS_ENV: "true",
+				});
+				const url = await waitForReady(proc);
+				expect(await fetchJson(url + "/env/")).toEqual(
+					expect.objectContaining({
+						CLOUDFLARE_INCLUDE_PROCESS_ENV: "true", // this proves we read the process.env
+					})
 				);
 			});
 
