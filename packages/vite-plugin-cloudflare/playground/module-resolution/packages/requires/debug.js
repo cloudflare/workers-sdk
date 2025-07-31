ba38805b5489f72f649f942a4e040f0a18b43dc8
Edmund Hung <edmund@cloudflare.com>
2025-07-31 15:22:23 +0100

    update debug patch to use cjs syntax with test

--- packages/vite-plugin-cloudflare/playground/module-resolution/packages/requires/debug.js
+++ packages/vite-plugin-cloudflare/playground/module-resolution/packages/requires/debug.js
@@ -1,5 +1,8 @@
 const debug = require("debug");
-debug("test")("some message");
+
+debug("enabled")("This should be logged");
+debug("enabled").extend("disabled")("This should not be logged");
+debug("disabled")("This should not be logged");
 
 module.exports = {
 	value: "OK",
