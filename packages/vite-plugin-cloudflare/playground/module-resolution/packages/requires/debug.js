f8c61ff5f7ca66f636a62b5fb2f790f7f1938d5c
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-07-31 14:02:42 +0100

    Reproduce issue with require('debug')

---
+++ packages/vite-plugin-cloudflare/playground/module-resolution/packages/requires/debug.js
@@ -0,0 +1,6 @@
+const debug = require("debug");
+debug("test")("some message");
+
+module.exports = {
+	value: "OK",
+};
---
+++ packages/vite-plugin-cloudflare/playground/module-resolution/src/require-debug.ts
@@ -0,0 +1,5 @@
+import { value } from "@playground/module-resolution-requires/debug";
+
+export default {
+	"(requires/debug)": value,
+};
