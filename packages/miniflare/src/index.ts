8ba7736a8ae5666870d12945a1cb6185b6ac3633
Edmund Hung <edmund@cloudflare.com>
2025-08-01 10:43:40 +0100

    fix(vite-plugin-cloudflare): ensure cross-process service bindings route through asset worker (#9556)

--- packages/miniflare/src/index.ts
+++ packages/miniflare/src/index.ts
@@ -1819,6 +1819,7 @@ export class Miniflare {
 			for (let j = 0; j < directSockets.length; j++) {
 				const previousDirectSocket = previousDirectSockets[j];
 				const directSocket = directSockets[j];
+				const serviceName = directSocket.serviceName ?? workerName;
 				const entrypoint = directSocket.entrypoint ?? "default";
 				const name = getDirectSocketName(i, entrypoint);
 				const address = this.#getSocketAddress(
@@ -1835,7 +1836,7 @@ export class Miniflare {
 								name: `${RPC_PROXY_SERVICE_NAME}:${workerOpts.core.name}`,
 							}
 						: {
-								name: getUserServiceName(workerName),
+								name: getUserServiceName(serviceName),
 								entrypoint: entrypoint === "default" ? undefined : entrypoint,
 							};
 
