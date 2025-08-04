43b6f7b33fd64f44948e1334174c97778890bd09
Dario Piotrowicz <dario@cloudflare.com>
2025-08-04 15:22:26 +0100

    chore: rename old mixed-mode references to remote-bindings (#10134)

--- packages/wrangler/e2e/remote-binding/workers/r2.js
+++ packages/wrangler/e2e/remote-binding/workers/r2.js
@@ -1,7 +1,7 @@
 export default {
 	async fetch(request, env) {
 		return new Response(
-			`The pre-existing value is: ${await (await env.R2_BINDING.get("test-mixed-mode-key")).text()}`
+			`The pre-existing value is: ${await (await env.R2_BINDING.get("test-remote-bindings-key")).text()}`
 		);
 	},
 };
--- packages/wrangler/src/dev/miniflare/index.ts
+++ packages/wrangler/src/dev/miniflare/index.ts
@@ -1205,7 +1205,7 @@ export async function buildMiniflareOptions(
 			if (!didWarnMiniflareVectorizeSupport) {
 				didWarnMiniflareVectorizeSupport = true;
 				logger.warn(
-					"You are using a mixed-mode binding for Vectorize (through `--experimental-vectorize-bind-to-prod`). It may incur usage charges and modify your databases even in local development. "
+					"You are using Vectorize as a remote binding (through `--experimental-vectorize-bind-to-prod`). It may incur usage charges and modify your databases even in local development. "
 				);
 			}
 		}
