fc4c24f834bd141b82f35ad72744eb8e66b36dce
Olga Silva <osilva@cloudflare.com>
2025-08-05 15:01:42 +0100

    Add mockStepResult

--- packages/workflows-shared/src/modifier.ts
+++ packages/workflows-shared/src/modifier.ts
@@ -23,26 +23,6 @@ export class InstanceModifier extends RpcTarget {
 		this.#state = state;
 	}
 
-	async disableSleeps(): Promise<void> {
-		await this.#state.storage.put("disableSleeps", true);
-	}
-
-	async mockEvent(event: UserEvent): Promise<void> {
-		console.log("SENDING THE EVENT WITH TYPE", event.type);
-		// could maybe:
-		// WorkflowInstance.sendEvent()
-		// Engine.receiveEvent()
-		//const waitForEventKey = await this.#getWaitForEventCacheKey(step);
-
-		const myEvent: Event = {
-			timestamp: new Date(),
-			payload: event.payload,
-			type: event.type,
-		};
-
-		await this.#engine.receiveEvent(myEvent);
-	}
-
 	async #getWaitForEventCacheKey(step: StepSelector): Promise<string> {
 		let count = 1;
 		if (step.index) {
@@ -54,6 +34,42 @@ export class InstanceModifier extends RpcTarget {
 		const waitForEventKey = `${cacheKey}-value`;
 
 		return waitForEventKey;
+	}
+
+	async #getStepCacheKey(step: StepSelector): Promise<string> {
+		const hash = await computeHash(step.name);
+		let count = 1;
+		if (step.index) {
+			count = step.index;
+		}
+		const cacheKey = `${hash}-${count}`;
+		const valueKey = `${cacheKey}-value`;
+
+		return valueKey;
+	}
+
+	async disableSleeps(): Promise<void> {
+		await this.#state.storage.put("disableSleeps", true);
+	}
+
+	async mockStepResult(step: StepSelector, stepResult: unknown): Promise<void> {
+		const valueKey = await this.#getStepCacheKey(step);
+		await this.#state.storage.put(`replace-result-${valueKey}`, stepResult);
+	}
+
+	async mockEvent(event: UserEvent): Promise<void> {
+		// could maybe:
+		// WorkflowInstance.sendEvent()
+		// Engine.receiveEvent()
+		// flag with waitForEventKey
+
+		const myEvent: Event = {
+			timestamp: new Date(),
+			payload: event.payload,
+			type: event.type,
+		};
+
+		await this.#engine.receiveEvent(myEvent);
 	}
 
 	async forceEventTimeout(step: StepSelector): Promise<void> {
