3d2d6818e444a783be05e2ead4f296903a3a2edd
tgarg <tgarg@cloudflare.com>
2025-08-07 14:50:57 -0400

    Add changeset

--- packages/wrangler/src/__tests__/deploy.test.ts
+++ packages/wrangler/src/__tests__/deploy.test.ts
@@ -11489,6 +11489,12 @@ export default{
 				modified_on: "",
 			};
 			mockGetQueueByName(queueName, existingQueue);
+			mockPutQueueById(queueId, {
+				queue_name: queueName,
+				settings: {
+					delivery_delay: 10,
+				},
+			});
 			await runWrangler("deploy index.js");
 			expect(std.out).toMatchInlineSnapshot(`
 				"Total Upload: xx KiB / gzip: xx KiB
