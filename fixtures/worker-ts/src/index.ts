58f261d4dad2ad4cdc4cd65640d94b8053b66091
Samuel Macleod <smacleod@cloudflare.com>
2025-08-08 14:13:50 +0100

    Add test

--- fixtures/worker-ts/src/index.ts
+++ fixtures/worker-ts/src/index.ts
@@ -28,24 +28,7 @@ export default {
 		ctx: ExecutionContext
 	): Promise<Response> {
 		const url = new URL(request.url);
-
-		let worker = env.LOADER.get(url.pathname, () => {
-			return {
-				compatibilityDate: "2025-06-01",
-
-				mainModule: "foo.js",
-
-				modules: {
-					"foo.js":
-						"export default {\n" +
-						`  fetch(req, env, ctx) { return new Response('Hello with a dynamic worker loaded for ${url.pathname}'); }\n` +
-						"}\n",
-				},
-			};
-		});
-
-		// Now you can get its entrypoint.
-		let defaultEntrypoint = worker.getEntrypoint();
-		return await defaultEntrypoint.fetch(request);
+		if (url.pathname === "/error") throw new Error("Hello Error");
+		return new Response("Hello World!");
 	},
 };
