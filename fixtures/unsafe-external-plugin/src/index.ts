8f6e37783d590341d34b246ea0f12a051b3eb7b7
Daniel Herzig <dherzig@cloudflare.com>
2025-08-13 10:27:44 -0500

    chore: Address comments, remove superfluous options checks

--- fixtures/unsafe-external-plugin/src/index.ts
+++ fixtures/unsafe-external-plugin/src/index.ts
@@ -2,10 +2,11 @@ import { PluginLoader } from "miniflare";
 import {
 	UNSAFE_PLUGIN_NAME,
 	UNSAFE_SERVICE_PLUGIN,
-} from "./plugins/unsafeService";
+} from "./plugins/unsafe-service";
 
 const pluginRegistry: PluginLoader = {
 	// TODO: I need to type this better
+	// @ts-expect-error Needs help with typing
 	registerMiniflarePlugins<
 		UnsafeServiceBindingOption,
 		UnsafeServiceBindingSharedOption,
