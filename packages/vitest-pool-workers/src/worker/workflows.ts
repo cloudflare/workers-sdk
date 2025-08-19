bd649d790f91db3698504778532802173f6a2553
Olga Silva <osilva@cloudflare.com>
2025-08-19 15:30:55 +0100

    Update abort for testing

--- packages/vitest-pool-workers/src/worker/workflows.ts
+++ packages/vitest-pool-workers/src/worker/workflows.ts
@@ -1,6 +1,5 @@
 import { WORKFLOW_ENGINE_BINDING } from "../shared/workflows";
 import { internalEnv } from "./env";
-// import type { InstanceStatus } from "@cloudflare/workflows-shared/src/instance";
 import type { InstanceModifier } from "@cloudflare/workflows-shared/src/modifier";
 
 export type StepSelector = {
@@ -36,13 +35,11 @@ export async function introspectWorkflowInstance(
 		`[Vitest-Workflows] Introspecting workflow instance: ${instanceId}`
 	);
 
-	//await workflow.create({ id: instanceId });
-
 	// @ts-expect-error getWorkflowName() not exposed
 	const engineBindingName = `${WORKFLOW_ENGINE_BINDING}${(await workflow.getWorkflowName()).toUpperCase()}`;
-	// @ts-expect-error binding created at in runner worker start
+	// @ts-expect-error DO binding created in runner worker start
 	const engineStubId = internalEnv[engineBindingName].idFromName(instanceId);
-	// @ts-expect-error binding created at in runner worker start
+	// @ts-expect-error DO binding created in runner worker start
 	const engineStub = internalEnv[engineBindingName].get(engineStubId);
 
 	const instanceModifier = await engineStub.getInstanceModifier();
@@ -63,7 +60,7 @@ class WorkflowInstanceIntrospectorHandle
 		this.instanceModifier = instanceModifier;
 	}
 
-	public async modify(
+	async modify(
 		fn: (m: InstanceModifier) => Promise<void>
 	): Promise<WorkflowInstanceIntrospector> {
 		console.log("[Vitest-Workflows] I should go call a modifier");
@@ -71,7 +68,7 @@ class WorkflowInstanceIntrospectorHandle
 		return this;
 	}
 
-	public async waitForStepResult(step: StepSelector): Promise<unknown> {
+	async waitForStepResult(step: StepSelector): Promise<unknown> {
 		console.log("waiting for step result of step", step.name);
 		// @ts-expect-error waitForStepResult not exposed
 		const stepResult = await this.engineStub.waitForStepResult(
@@ -83,7 +80,7 @@ class WorkflowInstanceIntrospectorHandle
 		return stepResult;
 	}
 
-	public async waitForStatus(status: string): Promise<void> {
+	async waitForStatus(status: string): Promise<void> {
 		console.log("[Vitest-Workflows] waiting for status");
 		// @ts-expect-error waitForStatus not exposed
 		await this.engineStub.waitForStatus(status);
@@ -91,10 +88,11 @@ class WorkflowInstanceIntrospectorHandle
 		console.log("[Vitest-Workflows] status awaited");
 	}
 
-	public async cleanUp(): Promise<void> {
+	async cleanUp(): Promise<void> {
 		// works with isolatedStorage = false
 		try {
-			await this.engineStub.abort("user called delete");
+			// @ts-expect-error DO binding created in runner worker start
+			await this.engineStub.unsafeAbort("user called delete");
 		} catch {
 			// do nothing because we want to clean up this instance
 		}
