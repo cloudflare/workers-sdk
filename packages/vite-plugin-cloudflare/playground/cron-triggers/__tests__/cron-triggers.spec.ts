e984eba5aa054d435ada7d696d9f8d08c358b8ac
Carmen Popoviciu <cpopoviciu@cloudflare.com>
2025-08-22 10:20:59 +0200

    feedback fixes

--- packages/vite-plugin-cloudflare/playground/cron-triggers/__tests__/cron-triggers.spec.ts
+++ packages/vite-plugin-cloudflare/playground/cron-triggers/__tests__/cron-triggers.spec.ts
@@ -1,10 +1,7 @@
 import { expect, test } from "vitest";
 import { getTextResponse } from "../../__test-utils__";
 
-test("supports testing Cron Triggers at /cdn-cgi/handler/scheduled route", async () => {
-	const rootResponse = await getTextResponse("/");
-	expect(rootResponse).toBe("Hello cron trigger Worker playground!");
-
+test("Supports testing Cron Triggers at '/cdn-cgi/handler/scheduled' route", async () => {
 	const cronResponse = await getTextResponse("/cdn-cgi/handler/scheduled");
 	expect(cronResponse).toBe("ok");
 });
--- packages/vite-plugin-cloudflare/playground/email-worker/__tests__/email-triggers.spec.ts
+++ packages/vite-plugin-cloudflare/playground/email-worker/__tests__/email-triggers.spec.ts
@@ -1,24 +1,21 @@
-import { expect, test, vi } from "vitest";
+import { expect, test } from "vitest";
 import { getTextResponse, viteTestUrl } from "../../__test-utils__";
 
 test("Supports sending email via the email binding", async () => {
-	const rootResponse = await getTextResponse("/");
-	expect(rootResponse).toBe("Hello Email Workers playground!");
-
 	const sendEmailResponse = await getTextResponse("/send");
 	expect(sendEmailResponse).toBe("Email message sent successfully!");
+});
 
-	await vi.waitFor(
-		async () => {
-			const params = new URLSearchParams();
-			params.append("from", "sender@example.com");
-			params.append("to", "recipient@example.com");
+test("Supports testing Email Workers at '/cdn-cgi/handler/scheduled' route", async () => {
+	const params = new URLSearchParams();
+	params.append("from", "sender@example.com");
+	params.append("to", "recipient@example.com");
 
-			const fetchResponse = await fetch(
-				`${viteTestUrl}/cdn-cgi/handler/email?${params}`,
-				{
-					method: "POST",
-					body: `From: "John" <sender@example.com>
+	const fetchResponse = await fetch(
+		`${viteTestUrl}/cdn-cgi/handler/email?${params}`,
+		{
+			method: "POST",
+			body: `From: "John" <sender@example.com>
 Reply-To: sender@example.com
 To: recipient@example.com
 Subject: Testing Email Workers Local Dev
@@ -28,12 +25,10 @@ Date: Tue, 27 Aug 2024 08:49:44 -0700
 Message-ID: <6114391943504294873000@ZSH-GHOSTTY>
 
 Hi there`,
-				}
-			);
-			expect(await fetchResponse.text()).toBe(
-				"Worker successfully processed email"
-			);
-		},
-		{ timeout: 2000, interval: 500 }
+		}
+	);
+
+	expect(await fetchResponse.text()).toBe(
+		"Worker successfully processed email"
 	);
 });
