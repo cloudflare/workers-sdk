b2ff6204779fd94cefd647e4eed08957d170d70e
Samuel Macleod <smacleod@cloudflare.com>
2025-09-11 21:34:38 +0100

    fix lint

--- packages/wrangler/e2e/remote-binding/wrangler-remote-resources.test.ts
+++ packages/wrangler/e2e/remote-binding/wrangler-remote-resources.test.ts
@@ -1,6 +1,5 @@
 import assert from "node:assert";
 import path from "node:path";
-import { setTimeout } from "node:timers/promises";
 import dedent from "ts-dedent";
 import { fetch } from "undici";
 import {
