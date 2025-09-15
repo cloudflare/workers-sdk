6619feb7a169623fb19e98aa2cbd66f30b7cbfb3
Greg Brimble <gbrimble@cloudflare.com>
2025-09-15 14:24:21 -0400

    Build @cloudflare/pages-shared (#10648)
    * Build @cloudflare/pages-shared
    
    * Update .changeset/twelve-hands-throw.md
    
    ---------
    
    Co-authored-by: Somhairle MacLeòid <smacleod@cloudflare.com>

---
+++ .changeset/twelve-hands-throw.md
@@ -0,0 +1,5 @@
+---
+"@cloudflare/pages-shared": patch
+---
+
+Build `@cloudflare/pages-shared`. `@cloudflare/pages-shared/dist` now contains a build and bundled copy of `asset-server` and `metadata-generator`.
---
+++ packages/pages-shared/scripts/build.ts
@@ -0,0 +1,49 @@
+import { execFileSync } from "child_process";
+import { rmSync } from "fs";
+import path from "path";
+import * as esbuild from "esbuild";
+
+function resolve(...segments: string[]) {
+	return path.resolve(__dirname, "..", ...segments);
+}
+
+async function main() {
+	try {
+		rmSync(resolve("dist"), { recursive: true });
+	} catch {}
+
+	console.log("Building asset-server...");
+	await esbuild.build({
+		entryPoints: ["asset-server/**/*"],
+		format: "esm",
+		bundle: true,
+		sourcemap: "external",
+		external: ["cloudflare:*", "node:*"],
+		outdir: resolve("dist/asset-server/"),
+	});
+
+	console.log("Building metadata-generator...");
+	await esbuild.build({
+		entryPoints: ["metadata-generator/**/*"],
+		format: "esm",
+		bundle: true,
+		sourcemap: "external",
+		external: ["cloudflare:*", "node:*"],
+		outdir: resolve("dist/metadata-generator"),
+	});
+
+	console.log("Generating types...");
+	execFileSync("tsc", ["-p", "tsconfig.build.json"], {
+		stdio: "inherit",
+		shell: true,
+	});
+
+	console.log("\n✅ Build completed.");
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	});
