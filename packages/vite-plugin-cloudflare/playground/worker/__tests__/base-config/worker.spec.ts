58b7eff5d7c9e64533359bb95074ef13998e047a
BlankParticle <blankparticle@gmail.com>
2025-09-16 10:48:22 +0530

    chore: add tests for base config

---
+++ packages/vite-plugin-cloudflare/playground/worker/__tests__/base-config/worker.spec.ts
@@ -0,0 +1,33 @@
+import { expect, test, vi } from "vitest";
+import {
+	getTextResponse,
+	viteTestUrl,
+	WAIT_FOR_OPTIONS,
+} from "../../../__test-utils__";
+
+test("basic base config functionality and url restoration handling", async () => {
+	// We don't need to specify the `/custom-mount` path when calling getTextResponse because its part of the viteTestUrl already
+	expect(viteTestUrl).toContain("/custom-mount");
+
+	await vi.waitFor(
+		async () => expect(await getTextResponse()).toEqual("Hello World!"),
+		WAIT_FOR_OPTIONS
+	);
+
+	await vi.waitFor(
+		async () =>
+			expect(await getTextResponse("/a/b/c?path")).toEqual(
+				"/custom-mount/a/b/c"
+			),
+		WAIT_FOR_OPTIONS
+	);
+});
+
+test("`X-Forwarded-Host` header but on custom mount path", async () => {
+	const testUrl = new URL(viteTestUrl);
+	await vi.waitFor(
+		async () =>
+			expect(await getTextResponse("/x-forwarded-host")).toEqual(testUrl.host),
+		WAIT_FOR_OPTIONS
+	);
+});
