3b23c1be8ce74ac92e0b08f12c68e9be79f13e3e
Dario Piotrowicz <dario@cloudflare.com>
2025-09-18 13:25:50 +0100

    skip check if `args.platform` is falsy

--- packages/create-cloudflare/src/templates.ts
+++ packages/create-cloudflare/src/templates.ts
@@ -530,7 +530,7 @@ export const createContext = async (
 
 			frameworkConfig = frameworkConfig.platformVariants[platform];
 		} else {
-			if (frameworkConfig.platform !== args.platform) {
+			if (args.platform && args.platform !== frameworkConfig.platform) {
 				throw new Error(
 					`The ${frameworkConfig.displayName} framework doesn't support the "${args.platform}" platform`,
 				);
