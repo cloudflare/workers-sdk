278ac6d475f4ab4b1254fd6aecfd14a2fcb2514d
Dario Piotrowicz <dario@cloudflare.com>
2025-09-18 13:31:12 +0100

    add comment explaining why solid is used

--- packages/create-cloudflare/e2e/tests/cli/cli.test.ts
+++ packages/create-cloudflare/e2e/tests/cli/cli.test.ts
@@ -639,6 +639,11 @@ describe("Create Cloudflare CLI", () => {
 		logStream,
 	}) => {
 		const { errors } = await runC3(
+			// The solid framework template doesn't support the pages platform that's why solid
+			// is being selected here, there isn't anything specific/special about solid being used
+			// here, if in the future the solid template is removed or updated to support the pages
+			// platform a different framework template not supporting pages can be used here
+			// (and if no such framework template will exist we can remove the check altogether?)
 			["--platform=pages", "--framework=solid", "my-app"],
 			[],
 			logStream,
