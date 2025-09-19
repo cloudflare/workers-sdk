5f2e499091d0360393c6ee3012a8bbb4afd2a0d0
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2025-09-19 11:37:57 +0100

    check URL matching (windows)

--- packages/wrangler/src/__tests__/dev/remote-bindings.test.ts
+++ packages/wrangler/src/__tests__/dev/remote-bindings.test.ts
@@ -555,7 +555,7 @@ describe(
 				});
 				const match = await vi.waitUntil(
 					() =>
-						std.out.match(/Ready on (?<url>http:\/\/localhost:\d{4}.+)/) ||
+						std.out.match(/Ready on (?<url>http:\/\/[^:]+:\d{4}.+)/) ||
 						console.dir(std.out),
 
 					{ timeout: 2_000 }
