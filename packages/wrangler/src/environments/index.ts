08b7c7cf018680c3add0bb0c4d1fea0f5c934072
Cina Saffary <cina@cloudflare.com>
2025-09-19 04:51:27 -0500

    chore: refactor service and environment tagging to consolidate with existing script-settings update requests

--- packages/wrangler/src/environments/index.ts
+++ packages/wrangler/src/environments/index.ts
@@ -1,6 +1,5 @@
 import { logger } from "../logger";
 import { isLegacyEnv } from "../utils/isLegacyEnv";
-import { patchNonVersionedScriptSettings } from "../versions/api";
 import type { Config } from "../config";
 
 const SERVICE_TAG_PREFIX = "cf:service=";
@@ -10,70 +9,47 @@ export function hasDefinedEnvironments(config: Config) {
 	return isLegacyEnv(config) && Boolean(config.definedEnvironments?.length);
 }
 
-export async function applyServiceAndEnvironmentTags(
-	config: Config,
-	accountId: string,
-	scriptName: string,
-	tags: string[] | null
-) {
-	tags ??= [];
+export function applyServiceAndEnvironmentTags(config: Config, tags: string[]) {
 	const env = config.targetEnvironment;
 	const serviceName = config.topLevelName;
+	const shouldApplyTags = hasDefinedEnvironments(config);
 
-	if (!serviceName) {
+	if (shouldApplyTags && !serviceName) {
 		logger.warn(
 			"No top-level `name` has been defined in Wrangler configuration. Add a top-level `name` to group this Worker together with its sibling environments in the Cloudflare dashboard."
 		);
-
-		if (tags.some((tag) => tag.startsWith(SERVICE_TAG_PREFIX))) {
-			try {
-				return await patchNonVersionedScriptSettings(
-					config,
-					accountId,
-					scriptName,
-					{
-						tags: tags.filter((tag) => !tag.startsWith(SERVICE_TAG_PREFIX)),
-					}
-				);
-			} catch {}
-		}
-		return;
 	}
 
-	const serviceTag = `${SERVICE_TAG_PREFIX}${serviceName}`;
-	const environmentTag = env ? `${ENVIRONMENT_TAG_PREFIX}${env}` : null;
+	const serviceTag =
+		shouldApplyTags && serviceName
+			? `${SERVICE_TAG_PREFIX}${serviceName}`
+			: null;
+	const environmentTag =
+		serviceTag && env ? `${ENVIRONMENT_TAG_PREFIX}${env}` : null;
 
-	const hasMissingServiceTag = !tags.includes(serviceTag);
-	const hasMissingOrStaleEnvironmentTag = environmentTag
-		? !tags.includes(environmentTag)
-		: tags.some((tag) => tag.startsWith(ENVIRONMENT_TAG_PREFIX)); // check if there's a stale environment tag on a top-level worker that we should remove
+	tags = tags.filter(
+		(tag) =>
+			!tag.startsWith(SERVICE_TAG_PREFIX) &&
+			!tag.startsWith(ENVIRONMENT_TAG_PREFIX)
+	);
 
-	if (hasMissingServiceTag || hasMissingOrStaleEnvironmentTag) {
-		const nextTags = tags
-			.filter(
-				(tag) =>
-					!tag.startsWith(SERVICE_TAG_PREFIX) &&
-					!tag.startsWith(ENVIRONMENT_TAG_PREFIX)
-			)
-			.concat([serviceTag]);
-
-		if (environmentTag) {
-			nextTags.push(environmentTag);
-		}
-
-		try {
-			return await patchNonVersionedScriptSettings(
-				config,
-				accountId,
-				scriptName,
-				{
-					tags: nextTags,
-				}
-			);
-		} catch {
-			logger.warn(
-				"Could not apply service and environment tags. This Worker will not appear grouped together with its sibling environments in the Cloudflare dashboard."
-			);
-		}
+	if (serviceTag) {
+		tags.push(serviceTag);
 	}
+
+	if (environmentTag) {
+		tags.push(environmentTag);
+	}
+
+	return tags;
+}
+
+export function warnOnErrorUpdatingServiceAndEnvironmentTags() {
+	logger.warn(
+		"Could not apply service and environment tags. This Worker will not appear grouped together with its sibling environments in the Cloudflare dashboard."
+	);
+}
+
+export function tagsAreEqual(a: string[], b: string[]) {
+	return a.length === b.length && a.every((el, i) => b[i] === el);
 }
