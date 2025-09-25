54434149a53ad9f452e72af3902d06df3a29b7e6
Dario Piotrowicz <dario@cloudflare.com>
2025-09-25 18:29:12 +0100

    vite-plugin-cloudflare: ensure that logs are printed at the correct log level

---
+++ packages/vite-plugin-cloudflare/playground/worker/__tests__/log-level-error/worker.spec.ts
@@ -0,0 +1,12 @@
+import { expect, test } from "vitest";
+import { serverLogs } from "../../../__test-utils__";
+
+test("basic dev logging with logLevel: error", async () => {
+	expect(serverLogs.info.join()).toEqual("");
+	expect(serverLogs.warns.join()).toEqual("");
+
+	expect(serverLogs.errors.join()).toContain("__console error__");
+
+	// Historically we've been printing warnings as errors, that's why we also include the following check
+	expect(serverLogs.errors.join()).not.toContain("__console warn__");
+});
