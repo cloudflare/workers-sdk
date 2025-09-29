7e0d750edcf8dc3e0cd7f8acdbb8af6fb68f67f4
Rob Marscher <rob@robmarscher.com>
2025-09-29 16:02:55 -0400

    feat: add Waku to C3 (#8314)
    * feat: add Waku to C3
    
    Fixes #7594.
    
    Adding a C3 template for Waku, a minimal React framework. https://waku.gg/
    
    - Tests
      - [ ] TODO (before merge)
      - [x] Tests included
      - [ ] Tests not necessary because:
    - E2E Tests CI Job required? (Use "e2e" label or ask maintainer to run separately)
      - [ ] I don't know
      - [x] Required
      - [ ] Not required because:
    - Public documentation
      - [ ] TODO (before merge)
      - [x] Cloudflare docs PR(s): <https://github.com/cloudflare/cloudflare-docs/pull/20444>
      - [ ] Documentation not necessary because:
    
    * Update .changeset/moody-hands-sort.md
    
    * fix up snapshots
    
    * update snapshots more
    
    * Latest create-waku
    
    * Fix waku build by forcing NODE_ENV=production
    
    ---------
    
    Co-authored-by: Carmen Popoviciu <carmen.popoviciu@gmail.com>
    Co-authored-by: Peter Bacon Darwin <pbacondarwin@cloudflare.com>

--- packages/create-cloudflare/e2e/tests/cli/cli.test.ts
+++ packages/create-cloudflare/e2e/tests/cli/cli.test.ts
@@ -584,7 +584,7 @@ describe("Create Cloudflare CLI", () => {
 					    npm create cloudflare -- --framework next -- --ts
 					    pnpm create cloudflare --framework next -- --ts
 					    Allowed Values:
-					      analog, angular, astro, docusaurus, gatsby, hono, next, nuxt, qwik, react, react-router, solid, svelte, vue
+					      analog, angular, astro, docusaurus, gatsby, hono, next, nuxt, qwik, react, react-router, solid, svelte, vue, waku
 					  --platform=<value>
 					    Whether the application should be deployed to Pages or Workers. This is only applicable for Frameworks templates that support both Pages and Workers.
 					    Allowed Values:
