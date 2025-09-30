e548870d5b3da6ea6fca694210010da9e4b422ef
emily-shen <69125074+emily-shen@users.noreply.github.com>
2025-09-30 16:27:37 +0100

    Update packages/containers-shared/src/images.ts
    Co-authored-by: Edmund Hung <edmund@cloudflare.com>

--- packages/containers-shared/src/images.ts
+++ packages/containers-shared/src/images.ts
@@ -98,7 +98,7 @@ export async function prepareContainerImagesForDev(args: {
 			if (!isCloudflareRegistryLink(options.image_uri)) {
 				throw new UserError(
 					`Image "${options.image_uri}" is a registry link but does not point to the Cloudflare container registry.\n` +
-						`To use an existing image from another repository, see https://https://developers.cloudflare.com/containers/platform-details/image-management/#using-pre-built-container-images`
+						`To use an existing image from another repository, see https://developers.cloudflare.com/containers/platform-details/image-management/#using-pre-built-container-images`
 				);
 			}
 			const pull = await pullImage(dockerPath, options);
