c89050008e3b5e154f52bd81e6a538033b52062e
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2025-10-03 11:59:27 +0100

    fixup! Add a check to vite-plugin that ensures that the version of Wrangler being used internally is correct

--- packages/vite-plugin-cloudflare/src/assert-wrangler-version.ts
+++ packages/vite-plugin-cloudflare/src/assert-wrangler-version.ts
@@ -1,4 +1,5 @@
-import { satisfies } from "semver";
+import { createRequire } from "node:module";
+import { compare, Range, satisfies, SemVer } from "semver";
 
 /**
  * Asserts that the installed version of Wrangler that gets pulled in at runtime by the `@cloudflare/vite-plugin`
@@ -6,31 +7,35 @@ import { satisfies } from "semver";
  *
  * This can sometime be broken by package managers that deduplicate dependencies, such as `pnpm`.
  */
-export async function assertWranglerVersion() {
-	const installedVersion = (
-		await import("wrangler/package.json", {
-			with: { type: "json" },
-		})
-	).default.version;
+export function assertWranglerVersion() {
+	const require = createRequire(import.meta.url);
+	const installedVersion = new SemVer(require("wrangler/package.json").version);
+	const vitePackage = require("@cloudflare/vite-plugin/package.json");
 
-	const ourPackageJson = (
-		await import("../package.json", {
-			with: { type: "json" },
-		})
-	).default;
-
-	const peerDependency = ourPackageJson.peerDependencies.wrangler;
-
-	if (peerDependency.startsWith("workspace:")) {
+	if (vitePackage.dependencies.wrangler.startsWith("workspace:")) {
 		// We are running in the monorepo, so these deps are not yet computed to specific semver strings.
 		// We don't need to worry in this case and can skip the check.
 		return;
 	}
 
-	if (!satisfies(installedVersion, peerDependency)) {
+	const wranglerDependency = new SemVer(vitePackage.dependencies.wrangler);
+	const wranglerPeerDependency = new Range(
+		vitePackage.peerDependencies.wrangler
+	);
+
+	if (compare(installedVersion, wranglerDependency) < 0) {
 		throw new Error(
-			`The installed version of Wrangler (${installedVersion}) does not satisfy the peer dependency required by @cloudflare/vite-plugin (${peerDependency}).\n` +
-				`Please install wrangler@${peerDependency}.`
+			`The installed version of Wrangler (${installedVersion.format()}) is older than the version required by @cloudflare/vite-plugin (${wranglerDependency.format()}).\n` +
+				`Since the current package manager is flattening the versions of Wrangler, the @cloudflare/vite-plugin is using this version to read and write config files.\n` +
+				`Please upgrade your installation of Wrangler to at least ${wranglerDependency}.`
+		);
+	}
+
+	if (!satisfies(installedVersion, wranglerPeerDependency)) {
+		console.warn(
+			`The installed version of Wrangler (${installedVersion.format()}) does not satisfy the peer dependency required by @cloudflare/vite-plugin (${wranglerDependency.format()}).\n` +
+				`This may lead to unexpected issues when you come to deploy the application.\n` +
+				`Please install a version of Wrangler that satisfies the peer dependency: ${wranglerPeerDependency}.`
 		);
 	}
 }
