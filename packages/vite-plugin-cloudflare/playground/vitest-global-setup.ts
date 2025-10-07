230d9789b47593e3961f6c6a588c0294fbeea138
Dario Piotrowicz <dario@cloudflare.com>
2025-10-07 12:30:57 +0100

    add `VITE_VERSION_TO_TEST` environment variable to control what version of vite is used for the vite plugin playground tests

--- packages/vite-plugin-cloudflare/playground/vitest-global-setup.ts
+++ packages/vite-plugin-cloudflare/playground/vitest-global-setup.ts
@@ -1,4 +1,5 @@
 import { chromium } from "playwright-chromium";
+import { getViteModuleToTest } from "./vite-module-to-test";
 import type { BrowserServer } from "playwright-chromium";
 import type { GlobalSetupContext } from "vitest/node";
 
@@ -17,6 +18,14 @@ export async function setup({ provide }: GlobalSetupContext): Promise<void> {
 	});
 
 	provide("wsEndpoint", browserServer.wsEndpoint());
+
+	const viteModule = getViteModuleToTest();
+	const viteInUseMessage = `Running playground tests against ${viteModule.packageName}@${viteModule.version}`;
+	const lineLength = viteInUseMessage.length + 8;
+	console.log(`┌${"─".repeat(lineLength)}┐`);
+	console.log(`│    ${viteInUseMessage}    │`);
+	console.log(`└${"─".repeat(lineLength)}┘`);
+	console.log("");
 }
 
 export async function teardown(): Promise<void> {
