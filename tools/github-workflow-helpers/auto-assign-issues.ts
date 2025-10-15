380488b9390c9b2725d03d9d3870477327083e2d
Pete Bacon Darwin <pete@bacondarwin.com>
2025-10-15 08:59:21 +0100

    improve auto-assign tooling (#10974)
    * iterative improvements to the auto-assigner
    
    * add initial ANT assignees
    
    * fixup! add initial ANT assignees
    
    * fixup! iterative improvements to the auto-assigner

--- tools/github-workflow-helpers/auto-assign-issues.ts
+++ tools/github-workflow-helpers/auto-assign-issues.ts
@@ -7,7 +7,47 @@ import type { IssuesLabeledEvent } from "@octokit/webhooks-types";
  * The mapping of github issue labels to team members for assignment.
  */
 const TEAM_ASSIGNMENTS: { [label: string]: { [jobRole: string]: string } } = {
-	miniflare: { em: "lrapoport", pm: "mikenomitch" },
+	agents: {},
+	"analytics engine": {},
+	api: {},
+	auth: {},
+	"auto-provisioning": { em: "lrapoport-cf", pm: "mattietk" },
+	c3: { em: "lrapoport-cf", pm: "mattietk" },
+	constellation: {},
+	containers: {},
+	d1: {},
+	documentation: {},
+	"durable objects": {},
+	hyperdrive: {},
+	"kv-asset-handler": { em: "lrapoport-cf", pm: "mattietk" },
+	miniflare: { em: "lrapoport-cf", pm: "mattietk" },
+	multiworker: { em: "lrapoport-cf", pm: "mattietk" },
+	"node compat": { em: "lrapoport-cf", pm: "mattietk" },
+	"nodejs compat": { em: "lrapoport-cf", pm: "mattietk" },
+	observability: {},
+	opennext: { em: "lrapoport-cf", pm: "mattietk", tl: "vicb" },
+	pages: { em: "lrapoport-cf", pm: "mattietk" },
+	"pages-shared": {},
+	pipelines: {},
+	"playground-worker": { em: "lrapoport-cf", pm: "mattietk" },
+	python: {},
+	queues: {},
+	r2: {},
+	"remote-bindings": { em: "lrapoport-cf", pm: "mattietk" },
+	"secrets-store": {},
+	"start-dev-worker": { em: "lrapoport-cf", pm: "mattietk" },
+	templates: { em: "lrapoport-cf", pm: "mattietk" },
+	types: { em: "lrapoport-cf", pm: "mattietk" },
+	unenv: { em: "lrapoport-cf", pm: "mattietk" },
+	unstable_dev: { em: "lrapoport-cf", pm: "mattietk" },
+	vectorize: {},
+	"vite-plugin": { em: "lrapoport-cf", pm: "mattietk", tl: "jamesopstad" },
+	vitest: { em: "lrapoport-cf", pm: "mattietk" },
+	"Workers + Assets": {},
+	"workers ai": {},
+	"workers-builds": {},
+	workflows: {},
+	wrangler: { em: "lrapoport-cf", pm: "mattietk" },
 };
 
 if (require.main === module) {
@@ -23,9 +63,10 @@ async function run() {
 			`);
 
 	if (isIssuesLabeledEvent(context.payload)) {
-		const issue = context.payload.issue;
-		const label = context.payload.label;
-		const labelName = label.name;
+		const {
+			issue,
+			label: { name: labelName },
+		} = context.payload;
 
 		core.info(`Processing new label: ${labelName} for issue #${issue.number}`);
 
@@ -35,31 +76,44 @@ async function run() {
 			return;
 		}
 
-		const assignees = new Set(Object.values(teamConfig));
-		for (const assignee of issue.assignees ?? []) {
-			assignees.delete(assignee.login);
-		}
-		if (assignees.size === 0) {
+		const teamAssignees = new Set(Object.values(teamConfig));
+		const currentAssignees = new Set(issue.assignees?.map((a) => a.login));
+		const toBeAssigned = teamAssignees.difference(currentAssignees);
+
+		if (toBeAssigned.size === 0) {
 			core.info(
-				`All potential assignees are already assigned to issue #${issue.number}. Skipping auto-assignment.`
+				`All potential assignees are already assigned to issue. Skipping auto-assignment.`
 			);
 			return;
 		}
 
-		const assigneeList = new Intl.ListFormat("en").format(assignees);
-		core.info(`Assigning issue #${issue.number} to: ${assigneeList}`);
+		core.info(
+			`Assigning to: ${new Intl.ListFormat("en").format(toBeAssigned)}`
+		);
 
 		const token = core.getInput("github_token", { required: true });
 		const octokit = getOctokit(token);
-		await octokit.rest.issues.addAssignees({
+		const result = await octokit.rest.issues.addAssignees({
 			owner: context.repo.owner,
 			repo: context.repo.repo,
 			issue_number: issue.number,
-			assignees: Array.from(assignees),
+			assignees: Array.from(toBeAssigned),
 		});
 
+		const assigned = new Set(result.data.assignees?.map((a) => a.login));
+		const missing = toBeAssigned.difference(assigned);
+
+		if (missing.size > 0) {
+			core.warning(
+				dedent`
+          Not all assignees were added to the issue. They may not be collaborators on the repository.
+          Missing assignees: ${new Intl.ListFormat("en").format(missing)}
+        `
+			);
+		}
+
 		core.info(
-			`Successfully assigned issue #${issue.number} to ${assigneeList}`
+			`Issue assigned to ${new Intl.ListFormat("en").format(assigned)}`
 		);
 	}
 }
