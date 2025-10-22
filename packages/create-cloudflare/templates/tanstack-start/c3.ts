af18b483c37d13b33df5dfff5cefc32e5e5bee2d
Dario Piotrowicz <dario@cloudflare.com>
2025-10-22 18:35:45 +0100

    C3: add TanStack Start framework template (#10842)
    * C3: add TanStack Start framework template
    
    * adapt c3 tanstack template to latest tanstack CLI
    
    * bump `@tanstack/create-start`
    
    * Apply suggestions from code review
    
    Co-authored-by: James Opstad <13586373+jamesopstad@users.noreply.github.com>
    
    * rename tanstack directory to tanstack-start
    
    * update snapshot
    
    * add windows as unsupported os in c3 e2es
    
    ---------
    
    Co-authored-by: James Opstad <13586373+jamesopstad@users.noreply.github.com>

---
+++ packages/create-cloudflare/templates/tanstack-start/c3.ts
@@ -0,0 +1,41 @@
+import { logRaw } from "@cloudflare/cli";
+import { runFrameworkGenerator } from "frameworks/index";
+import { detectPackageManager } from "helpers/packageManagers";
+import type { TemplateConfig } from "../../src/templates";
+import type { C3Context } from "types";
+
+const { npm } = detectPackageManager();
+
+const generate = async (ctx: C3Context) => {
+	await runFrameworkGenerator(ctx, [
+		ctx.project.name,
+		"--host",
+		"cloudflare",
+		"--framework",
+		"react",
+		// to prevent asking about git twice, just let c3 do it
+		"--no-git",
+	]);
+
+	logRaw(""); // newline
+};
+
+const config: TemplateConfig = {
+	configVersion: 1,
+	id: "tanstack-start",
+	platform: "workers",
+	frameworkCli: "@tanstack/create-start",
+	displayName: "TanStack Start",
+	generate,
+	transformPackageJson: async () => ({
+		scripts: {
+			deploy: `${npm} run build && wrangler deploy`,
+			preview: `${npm} run build && vite preview`,
+			"cf-typegen": `wrangler types`,
+		},
+	}),
+	devScript: "dev",
+	deployScript: "deploy",
+	previewScript: "preview",
+};
+export default config;
