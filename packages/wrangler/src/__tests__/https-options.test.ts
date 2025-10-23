62e61259a282541c34ad241373504e609f47d10b
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2025-10-23 15:34:34 +0100

    Get user config paths from `@cloudflare/workers-utils` shared package

--- packages/wrangler/src/__tests__/https-options.test.ts
+++ packages/wrangler/src/__tests__/https-options.test.ts
@@ -1,7 +1,7 @@
-import * as fs from "node:fs";
+import fs from "node:fs";
 import os from "node:os";
 import path from "node:path";
-import { getGlobalWranglerConfigPath } from "../global-wrangler-config-path";
+import { getGlobalWranglerConfigPath } from "@cloudflare/workers-utils";
 import { getHttpsOptions } from "../https-options";
 import { mockConsoleMethods } from "./helpers/mock-console";
 import { runInTempDir } from "./helpers/run-in-tmp";
@@ -103,7 +103,13 @@ describe("getHttpsOptions()", () => {
 	});
 
 	it("should warn if not able to write to the cache (legacy config path)", async () => {
+		// Create a legacy config path at ~/.wrangler
 		fs.mkdirSync(path.join(os.homedir(), ".wrangler"));
+		// Ensure that we cannot migrate from legacy path to the new XDG path
+		vi.spyOn(fs, "renameSync").mockImplementation(() => {
+			throw new Error("Cannot move directory");
+		});
+		// Prevent writing certificates
 		await mockWriteFileSyncThrow(/\.pem$/);
 		await getHttpsOptions();
 		expect(
@@ -148,7 +154,7 @@ describe("getHttpsOptions()", () => {
 			`"Generating new self-signed certificate..."`
 		);
 		expect(std.warn).toMatchInlineSnapshot(`
-			"[33m▲ [43;33m[[43;30mWARNING[43;33m][0m [1mUnable to cache generated self-signed certificate in test-xdg-config/local-cert.[0m
+			"[33m▲ [43;33m[[43;30mWARNING[43;33m][0m [1mUnable to cache generated self-signed certificate in home/Library/Preferences/wrangler/local-cert.[0m
 
 			  ERROR: Cannot write file
 
--- packages/wrangler/src/https-options.ts
+++ packages/wrangler/src/https-options.ts
@@ -1,9 +1,11 @@
-import * as fs from "node:fs";
-import * as path from "node:path";
-import { UserError } from "@cloudflare/workers-utils";
+import fs from "node:fs";
+import path from "node:path";
+import {
+	getGlobalWranglerConfigPath,
+	UserError,
+} from "@cloudflare/workers-utils";
 import { getAccessibleHosts } from "miniflare";
 import { getEnvironmentVariableFactory } from "./environment-variables/factory";
-import { getGlobalWranglerConfigPath } from "./global-wrangler-config-path";
 import { logger } from "./logger";
 import type { Attributes, Options } from "selfsigned";
 
