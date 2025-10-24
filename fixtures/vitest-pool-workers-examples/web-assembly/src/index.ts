2b589556402d028f322dcd8f9f28a38d4032fa69
Bradley Bridges <bradley@bbridges.io>
2025-10-24 11:12:02 -0700

    Remove `?module` suffix when loading `.wasm?module` files

--- fixtures/vitest-pool-workers-examples/web-assembly/src/index.ts
+++ fixtures/vitest-pool-workers-examples/web-assembly/src/index.ts
@@ -1,7 +1,13 @@
 import addModule from "./add.wasm";
+// Verify that we can also load this with "?module".
+// Generated Prisma clients can include this import style as an example.
+import addModule2 from "./add.wasm?module";
 
 const addInstance = new WebAssembly.Instance(addModule);
 const add = addInstance.exports.add as (a: number, b: number) => number;
+
+const add2Instance = new WebAssembly.Instance(addModule2);
+const add2 = addInstance.exports.add as (a: number, b: number) => number;
 
 export default <ExportedHandler>{
 	fetch(request, env, ctx) {
@@ -9,6 +15,11 @@ export default <ExportedHandler>{
 		const a = parseInt(url.searchParams.get("a") ?? "0");
 		const b = parseInt(url.searchParams.get("b") ?? "0");
 		const result = add(a, b);
+
+		if (result !== add2(a, b)) {
+			return new Response("add mismatch");
+		}
+
 		return new Response(result.toString());
 	},
 };
