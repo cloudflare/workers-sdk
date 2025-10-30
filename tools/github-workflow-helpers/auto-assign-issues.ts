fb99a732980af5bdd379a0fa66e34a209eb59503
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-10-30 09:59:56 +0000

    Remove tl auto-assignment for Vite plugin

--- tools/github-workflow-helpers/auto-assign-issues.ts
+++ tools/github-workflow-helpers/auto-assign-issues.ts
@@ -40,7 +40,7 @@ const TEAM_ASSIGNMENTS: { [label: string]: { [jobRole: string]: string } } = {
 	unenv: { em: "lrapoport-cf", pm: "mattietk" },
 	unstable_dev: { em: "lrapoport-cf", pm: "mattietk" },
 	vectorize: { em: "sejoker", pm: "jonesphillip" },
-	"vite-plugin": { em: "lrapoport-cf", pm: "mattietk", tl: "jamesopstad" },
+	"vite-plugin": { em: "lrapoport-cf", pm: "mattietk" },
 	vitest: { em: "lrapoport-cf", pm: "mattietk" },
 	"Workers + Assets": { em: "dcartertwo", pm: "irvinebroque" },
 	"workers ai": {},
