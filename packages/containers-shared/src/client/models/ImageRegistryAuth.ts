a51a69b0e2db1008473591a9238e09e790a07110
emily-shen <eshen@cloudflare.com>
2025-11-05 13:59:11 +0000

    non-secret-store mode

--- packages/containers-shared/src/client/models/ImageRegistryAuth.ts
+++ packages/containers-shared/src/client/models/ImageRegistryAuth.ts
@@ -8,8 +8,14 @@
 export type ImageRegistryAuth = {
 	/** The format of this value is determined by the registry being configured. */
 	public_credential: string;
-	private_credential: {
-		store_id: string;
-		secret_name: string;
-	};
+	/**
+	 * Either a reference to a secret in secret store, or the secret itself,
+	 * which is used when secret-store is not available, e.g. in FedRAMP
+	 */
+	private_credential:
+		| {
+				store_id: string;
+				secret_name: string;
+		  }
+		| string;
 };
