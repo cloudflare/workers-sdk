0acaa5add98506b84bb71ddfd65a0992f4232563
Pete Bacon Darwin <pete@bacondarwin.com>
2025-11-10 08:04:53 +0000

    use the Cloudflare maintained Next.js template (#11132)
    * use the Cloudflare maintained Next.js template
    
    * skip next.js C3 tests on Windows

--- packages/create-cloudflare/templates/next/c3.ts
+++ packages/create-cloudflare/templates/next/c3.ts
@@ -1,46 +1,19 @@
-import { brandColor, dim } from "@cloudflare/cli/colors";
-import { spinner } from "@cloudflare/cli/interactive";
 import { runFrameworkGenerator } from "frameworks/index";
-import { readFile, usesTypescript, writeFile } from "helpers/files";
-import { installPackages } from "helpers/packages";
 import type { TemplateConfig } from "../../src/templates";
 import type { C3Context } from "types";
 
 const generate = async (ctx: C3Context) => {
 	// Disable Turbopack as it is not currently compatible with the Cloudflare adapter
-	await runFrameworkGenerator(ctx, [ctx.project.name, "--no-turbopack"]);
-};
-
-const configure = async (ctx: C3Context) => {
-	await installPackages(["@opennextjs/cloudflare@^1.3.0"], {
-		startText: "Adding the Cloudflare adapter",
-		doneText: `${brandColor(`installed`)} @opennextjs/cloudflare)}`,
-	});
-
-	const usesTs = usesTypescript(ctx);
-
-	updateNextConfig(usesTs);
-};
-
-const updateNextConfig = (usesTs: boolean) => {
-	const s = spinner();
-
-	const configFile = `next.config.${usesTs ? "ts" : "mjs"}`;
-	s.start(`Updating \`${configFile}\``);
-
-	const configContent = readFile(configFile);
-
-	const updatedConfigFile =
-		configContent +
-		`
-		// added by create cloudflare to enable calling \`getCloudflareContext()\` in \`next dev\`
-		import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
-		initOpenNextCloudflareForDev();
-		`.replace(/\n\t*/g, "\n");
-
-	writeFile(configFile, updatedConfigFile);
-
-	s.stop(`${brandColor(`updated`)} ${dim(`\`${configFile}\``)}`);
+	await runFrameworkGenerator(ctx, [
+		ctx.project.name,
+		"--no-turbopack",
+		"--skip-install",
+		// We are using the Cloudflare template for Next.js projects
+		// This is a template maintained by Cloudflare that based on a standard Next.js starter project,
+		// and has already been configured to work with Cloudflare Workers
+		"--example",
+		"https://github.com/opennextjs/opennextjs-cloudflare/tree/main/create-cloudflare/next",
+	]);
 };
 
 const envInterfaceName = "CloudflareEnv";
@@ -55,7 +28,6 @@ export default {
 		path: "./templates",
 	},
 	generate,
-	configure,
 	transformPackageJson: async () => ({
 		scripts: {
 			deploy: `opennextjs-cloudflare build && opennextjs-cloudflare deploy`,
