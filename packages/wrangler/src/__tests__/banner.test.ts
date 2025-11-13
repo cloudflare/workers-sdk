f634b1654803ea889322b0a907e3072949c90999
Samuel Macleod <smacleod@cloudflare.com>
2025-11-13 00:39:20 +0000

    Support the WRANGLER_HIDE_BANNER environment variable across Wrangler & C3

---
+++ packages/wrangler/src/__tests__/banner.test.ts
@@ -0,0 +1,28 @@
+import { mockConsoleMethods } from "./helpers/mock-console";
+import { useMockIsTTY } from "./helpers/mock-istty";
+import { runWrangler } from "./helpers/run-wrangler";
+
+describe("wrangler banner", () => {
+	const std = mockConsoleMethods();
+	const { setIsTTY } = useMockIsTTY();
+	beforeEach(() => {
+		setIsTTY(true);
+	});
+
+	it("should display the version banner by default", async () => {
+		await runWrangler("--version");
+
+		expect(std.out).toMatchInlineSnapshot(`
+			"
+			 ⛅️ wrangler x.x.x
+			──────────────────"
+		`);
+	});
+
+	it("should hide the version banner when WRANGLER_HIDE_BANNER is present", async () => {
+		vi.stubEnv("WRANGLER_HIDE_BANNER", "true");
+		await runWrangler("--version");
+
+		expect(std.out).toMatchInlineSnapshot(`""`);
+	});
+});
