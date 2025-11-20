a23c4ca4d2dc828db96051dcfd1741e66dd4519e
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-11-20 13:11:03 +0000

    Renamed module-worker to exported-handler

--- fixtures/dev-registry/vite.exported-handler.config.ts
+++ fixtures/dev-registry/vite.exported-handler.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	plugins: [
 		cloudflare({
-			configPath: "./wrangler.module-worker.jsonc",
+			configPath: "./wrangler.exported-handler.jsonc",
 			inspectorPort: false,
 			persistState: false,
 		}),
