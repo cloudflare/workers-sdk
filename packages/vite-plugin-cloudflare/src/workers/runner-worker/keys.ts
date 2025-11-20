c616027cf1b7c3f14437dc8b0d460e037a001a59
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-11-20 13:50:58 +0000

    Move exhaustiveness checks into function

--- packages/vite-plugin-cloudflare/src/workers/runner-worker/keys.ts
+++ packages/vite-plugin-cloudflare/src/workers/runner-worker/keys.ts
@@ -31,13 +31,23 @@ export const DURABLE_OBJECT_KEYS = [
 /** Available methods for `WorkflowEntrypoint` classes */
 export const WORKFLOW_ENTRYPOINT_KEYS = ["run"] as const;
 
-// Remove branded keys
-type UnbrandedKeys<T> = Exclude<keyof T, `__${string}_BRAND`>;
+/**
+ * This function is not used.
+ * It is is included to check that we have included all the necessary keys.
+ */
+// @ts-expect-error: function declaration only used to check types
+function _checkExhaustiveKeys() {
+	// Remove branded keys
+	type UnbrandedKeys<T> = Exclude<keyof T, `__${string}_BRAND`>;
 
-// Check that we've included all possible keys
-export const _workerEntrypointExhaustive: (typeof WORKER_ENTRYPOINT_KEYS)[number] =
-	undefined as unknown as UnbrandedKeys<WorkerEntrypoint>;
-export const _durableObjectExhaustive: (typeof DURABLE_OBJECT_KEYS)[number] =
-	undefined as unknown as UnbrandedKeys<DurableObject>;
-export const _workflowEntrypointExhaustive: (typeof WORKFLOW_ENTRYPOINT_KEYS)[number] =
-	undefined as unknown as UnbrandedKeys<WorkflowEntrypoint>;
+	const _workerEntrypointExhaustive: (typeof WORKER_ENTRYPOINT_KEYS)[number] =
+		undefined as unknown as UnbrandedKeys<WorkerEntrypoint>;
+	const _durableObjectExhaustive: (typeof DURABLE_OBJECT_KEYS)[number] =
+		undefined as unknown as UnbrandedKeys<DurableObject>;
+	const _workflowEntrypointExhaustive: (typeof WORKFLOW_ENTRYPOINT_KEYS)[number] =
+		undefined as unknown as UnbrandedKeys<WorkflowEntrypoint>;
+
+	_workerEntrypointExhaustive ||
+		_durableObjectExhaustive ||
+		_workflowEntrypointExhaustive;
+}
