eea287405346eb286ffae2834e6f9aeb2593b194
Samuel Macleod <smacleod@cloudflare.com>
2025-11-21 14:48:26 +0000

    Support TanStack

--- packages/wrangler/src/autoconfig/frameworks/get-framework.ts
+++ packages/wrangler/src/autoconfig/frameworks/get-framework.ts
@@ -1,6 +1,7 @@
 import { Astro } from "./astro";
 import { Static } from "./static";
 import { SvelteKit } from "./sveltekit";
+import { TanstackStart } from "./tanstack";
 
 export function getFramework(id: string) {
 	if (id === "astro") {
@@ -8,6 +9,9 @@ export function getFramework(id: string) {
 	}
 	if (id === "svelte-kit") {
 		return new SvelteKit();
+	}
+	if (id === "tanstack-start") {
+		return new TanstackStart();
 	}
 
 	return new Static(id);
