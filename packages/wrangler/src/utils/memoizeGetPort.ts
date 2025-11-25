d7b6189733b9125d24fed37eb2f6282f72300a3a
Victor Berchet <victor@suumit.com>
2025-11-25 14:59:34 +0100

    Revert "Change ports selection for wrangler dev sessions. (#11246)" (#11407)
    This reverts commit 4febcdaf0f863abe3d53fbbea08eee075605a2b5.

--- packages/wrangler/src/utils/memoizeGetPort.ts
+++ packages/wrangler/src/utils/memoizeGetPort.ts
@@ -1,19 +1,9 @@
-import getPort, { portNumbers } from "get-port";
-import isCI from "is-ci";
-
-// Probe consecutive ports to increase the chance to get the same port across dev sessions.
-// In CI, we avoid probing consecutive ports to reduce the chance of collisions
-const NUM_CONSECUTIVE_PORTS_TO_PROBE = isCI ? 0 : 10;
+import getPort from "get-port";
 
 /**
  * Get an available TCP port number.
  *
- * Notes:
- * - We probe `NUM_CONSECUTIVE_PORTS_TO_PROBE` consecutive ports starting from `defaultPort` before falling back to a random port.
- * - Avoiding calling `getPort()` multiple times by memoizing the first result.
- *
- * @param defaultPort The preferred port to use when available
- * @param host The host to probe for available ports
+ * Avoiding calling `getPort()` multiple times by memoizing the first result.
  */
 export function memoizeGetPort(defaultPort: number, host: string) {
 	let portValue: number | undefined;
@@ -24,15 +14,7 @@ export function memoizeGetPort(defaultPort: number, host: string) {
 			cachedHost = forHost;
 		}
 		// Check a specific host to avoid probing all local addresses.
-		portValue =
-			portValue ??
-			(await getPort({
-				port: portNumbers(
-					defaultPort,
-					defaultPort + NUM_CONSECUTIVE_PORTS_TO_PROBE
-				),
-				host,
-			}));
+		portValue = portValue ?? (await getPort({ port: defaultPort, host: host }));
 		return portValue;
 	};
 }
