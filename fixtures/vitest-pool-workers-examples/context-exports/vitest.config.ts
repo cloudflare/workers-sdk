63b83d61329d215c7960c3e9976fd7963cad1b2d
Pete Bacon Darwin <pbacondarwin@cloudflare.com>
2025-11-27 11:47:48 +0000

    test with isolated storage and non-single worker setups

--- fixtures/vitest-pool-workers-examples/context-exports/vitest.config.ts
+++ fixtures/vitest-pool-workers-examples/context-exports/vitest.config.ts
@@ -1,34 +1,28 @@
 import { defineWorkersProject } from "@cloudflare/vitest-pool-workers/config";
 
+// Configuration for the "auxiliary" Worker under test.
+// Unfortunately, auxiliary Workers cannot load their configuration
+// from wrangler config files, and must be configured with Miniflare
+// `WorkerOptions`.
+export const auxiliaryWorker = {
+	name: "auxiliary-worker",
+	modules: true,
+	scriptPath: "./auxiliary-worker/dist/index.js", // Built by `global-setup.ts`
+	compatibilityDate: "2025-11-01",
+	compatibilityFlags: ["nodejs_compat", "enable_ctx_exports"],
+	bindings: {
+		NAME: "AuxiliaryWorker",
+	},
+};
+
 export default defineWorkersProject({
 	test: {
 		globalSetup: ["./global-setup.ts"],
 		poolOptions: {
 			workers: {
-				singleWorker: true,
-				wrangler: {
-					configPath: "./src/wrangler.jsonc",
-				},
+				wrangler: { configPath: "./src/wrangler.jsonc" },
 				miniflare: {
-					serviceBindings: {
-						AUXILIARY_WORKER: "worker-under-test",
-					},
-					workers: [
-						// Configuration for the "auxiliary" Worker under test.
-						// Unfortunately, auxiliary Workers cannot load their configuration
-						// from wrangler config files, and must be configured with Miniflare
-						// `WorkerOptions`.
-						{
-							name: "worker-under-test",
-							modules: true,
-							scriptPath: "./auxiliary-worker/dist/index.js", // Built by `global-setup.ts`
-							compatibilityDate: "2025-11-01",
-							compatibilityFlags: ["nodejs_compat", "enable_ctx_exports"],
-							bindings: {
-								NAME: "AuxiliaryWorker",
-							},
-						},
-					],
+					workers: [auxiliaryWorker],
 				},
 			},
 		},
