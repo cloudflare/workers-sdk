88bb67f4e8c8e06fc64cd68027aa5e9900a10465
Pete Bacon Darwin <pbacondarwin@cloudflare.com>
2025-11-27 16:15:19 +0000

    add durable object test cases

--- fixtures/vitest-pool-workers-examples/context-exports/src/worker-configuration.d.ts
+++ fixtures/vitest-pool-workers-examples/context-exports/src/worker-configuration.d.ts
@@ -1,9 +1,11 @@
 declare namespace Cloudflare {
 	interface GlobalProps {
 		mainModule: typeof import("./index");
+		durableNamespaces: "Counter";
 	}
 	interface Env {
 		NAME: string;
+		COUNTER: DurableObjectNamespace<import("./index").Counter>;
 	}
 }
 interface Env extends Cloudflare.Env {}
