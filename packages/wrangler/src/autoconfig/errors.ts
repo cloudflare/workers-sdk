8b6630c9ad141d81f3bf59ccc5b2701c1a05b389
Dario Piotrowicz <dario@cloudflare.com>
2025-11-28 22:52:59 +0000

    update `AutoConfigFrameworkConfigurationError` class to extend `UserError`

--- packages/wrangler/src/autoconfig/errors.ts
+++ packages/wrangler/src/autoconfig/errors.ts
@@ -1,31 +1,7 @@
-/**
- * This is used to provide telemetry with a sanitised error
- * message that could not have any user-identifying information.
- * Set to `true` to duplicate `message`.
- *  */
-type TelemetryMessage = {
-	telemetryMessage?: string | true;
-};
+import { UserError } from "@cloudflare/workers-utils";
 
 /**
  * Base class for errors where something in a autoconfig frameworks' configuration goes
  * something wrong. These are not reported to Sentry.
  */
-export class AutoConfigFrameworkConfigurationError extends Error {
-	telemetryMessage: string | undefined;
-	constructor(
-		message?: string | undefined,
-		options?:
-			| ({
-					cause?: unknown;
-			  } & TelemetryMessage)
-			| undefined
-	) {
-		super(message, options);
-		// Restore prototype chain:
-		// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-2.html#support-for-newtarget
-		Object.setPrototypeOf(this, new.target.prototype);
-		this.telemetryMessage =
-			options?.telemetryMessage === true ? message : options?.telemetryMessage;
-	}
-}
+export class AutoConfigFrameworkConfigurationError extends UserError {}
