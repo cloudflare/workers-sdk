d4e28c5dedb7fbbd89663a3adbbe61b4ef9c5f98
Matt Kane <mkane@cloudflare.com>
2025-12-03 16:28:57 +0000

    tsdown config fix

--- packages/workers-utils/tsdown.config.ts
+++ packages/workers-utils/tsdown.config.ts
@@ -3,23 +3,13 @@ import { defineConfig } from "tsdown";
 export default defineConfig([
 	{
 		treeshake: true,
-		// keepNames: true,
 		entry: ["src/index.ts", "src/browser.ts", "src/test-helpers/index.ts"],
 		platform: "node",
 		format: "esm",
-		dts: {
-			resolve: [/^@cloudflare\//],
-			compilerOptions: {
-				// workaround for https://github.com/rolldown/tsdown/issues/345
-				paths: {
-					"@cloudflare/workers-shared": ["../workers-shared"],
-				},
-			},
-		},
+		dts: true,
 		outDir: "dist",
 		tsconfig: "tsconfig.json",
-		// metafile: true,
-		external: ["@cloudflare/*", "vitest", "msw", "undici"],
+		external: [/@cloudflare\/.*/, "vitest", "msw", "undici"],
 		sourcemap: process.env.SOURCEMAPS !== "false",
 		define: {
 			"process.env.NODE_ENV": `'${"production"}'`,
