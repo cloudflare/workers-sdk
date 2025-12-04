eb4cafbe241aad24142825a7af2a387a06c49c77
Matthew Rodgers <matthewrodgers@cloudflare.com>
2025-12-04 13:01:43 -0800

    WC-4185 Support variable assets count max based on claims from upstream
    As noted in the comments, normally we'd need to validate this jwt, but
    since any uploads depend on a valid jwt (and are validated later) it's
    fine to just decode the jwt and check for this feature

--- packages/wrangler/src/pages/constants.ts
+++ packages/wrangler/src/pages/constants.ts
@@ -2,7 +2,7 @@ import { version as wranglerVersion } from "../../package.json";
 
 const isWindows = process.platform === "win32";
 
-export const MAX_ASSET_COUNT = 20_000;
+export const MAX_ASSET_COUNT_DEFAULT = 20_000;
 export const MAX_ASSET_SIZE = 25 * 1024 * 1024;
 export const PAGES_CONFIG_CACHE_FILENAME = "pages.json";
 export const MAX_BUCKET_SIZE = 40 * 1024 * 1024;
