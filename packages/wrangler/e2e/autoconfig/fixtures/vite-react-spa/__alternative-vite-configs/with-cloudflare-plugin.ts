4b3c5b4a0bc59046933ef03b16c460e2e9b322ff
Dario Piotrowicz <dario@cloudflare.com>
2025-12-04 00:03:15 +0000

    remove wrangler e2e and test React with C3, also add React in the c3 menu

--- packages/wrangler/e2e/autoconfig/fixtures/vite-react-spa/__alternative-vite-configs/with-cloudflare-plugin.ts
+++
@@ -1,8 +0,0 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-
-// https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), cloudflare()],
-});
--- packages/wrangler/eslint.config.mjs
+++ packages/wrangler/eslint.config.mjs
@@ -13,7 +13,6 @@ export default defineConfig([
 		"**/templates/**/*.*",
 		".e2e-test-report",
 		".tmp",
-		"e2e/**/fixtures",
 	]),
 	sharedConfig,
 	{
