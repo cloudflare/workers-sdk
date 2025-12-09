75130b51cd2ef4eaa70972e5d0e80bb969259320
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2025-12-09 13:32:41 +0000

    Try prefixing wrangler package version with `^`

--- packages/create-cloudflare/templates/redwood/c3.ts
+++ packages/create-cloudflare/templates/redwood/c3.ts
@@ -1,13 +1,30 @@
+import { resolve } from "node:path";
 import { logRaw } from "@cloudflare/cli";
 import { runFrameworkGenerator } from "frameworks/index";
+import { readJSON, writeJSON } from "helpers/files";
 import { detectPackageManager } from "helpers/packageManagers";
 import type { TemplateConfig } from "../../src/templates";
-import type { C3Context } from "types";
+import type { C3Context, PackageJson } from "types";
 
 const { npm } = detectPackageManager();
 
 const generate = async (ctx: C3Context) => {
 	await runFrameworkGenerator(ctx, [ctx.project.name]);
+
+	const pkgJsonPath = resolve(ctx.project.path, "package.json");
+	const pkgJson = readJSON(pkgJsonPath) as PackageJson;
+
+	if (
+		!pkgJson.devDependencies ||
+		!/^\d+\.\d+\.\d+$/.test(pkgJson.devDependencies["wrangler"])
+	) {
+		return;
+	}
+
+	pkgJson.devDependencies["wrangler"] =
+		`^${pkgJson.devDependencies["wrangler"]}`;
+
+	writeJSON(pkgJsonPath, pkgJson);
 
 	logRaw("");
 };
