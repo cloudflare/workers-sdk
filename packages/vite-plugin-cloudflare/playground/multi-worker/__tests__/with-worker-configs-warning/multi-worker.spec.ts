647bed22ca9e0a68fb238cbe092ed5d1185a3be4
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-12-12 15:14:19 +0000

    Allow for additional logs

--- packages/vite-plugin-cloudflare/playground/multi-worker/__tests__/with-worker-configs-warning/multi-worker.spec.ts
+++ packages/vite-plugin-cloudflare/playground/multi-worker/__tests__/with-worker-configs-warning/multi-worker.spec.ts
@@ -10,11 +10,13 @@ describe("multi-worker basic functionality", async () => {
 			 *       For dev is obvious, for builds we do get the warning once because we get it when we
 			 *       build the application but not when we run its preview (since that reads the generated wrangler.json)
 			 */
-			expect(serverLogs.warns).toEqual([
-				expect.stringMatching(
-					/your workers configs contain configuration options which are ignored[\s\S]+preserve_file_names[\s\S]+tsconfig[\s\S]+build/
-				),
-			]);
+			expect(serverLogs.warns).toEqual(
+				expect.arrayContaining([
+					expect.stringMatching(
+						/your workers configs contain configuration options which are ignored[\s\S]+preserve_file_names[\s\S]+tsconfig[\s\S]+build/
+					),
+				])
+			);
 		}
 	);
 
--- packages/vite-plugin-cloudflare/playground/static-mpa/__tests__/with-worker-configs-warning/static-mpa.spec.ts
+++ packages/vite-plugin-cloudflare/playground/static-mpa/__tests__/with-worker-configs-warning/static-mpa.spec.ts
@@ -7,9 +7,11 @@ test("a worker configs warning is present in the terminal", async () => {
 	 *       For dev is obvious, for builds we do get the warning once because we get it when we
 	 *       build the application but not when we run its preview (since that reads the generated wrangler.json)
 	 */
-	expect(serverLogs.warns).toEqual([
-		expect.stringMatching(
-			/your worker config \(at `.*?wrangler.with-warning.jsonc`\) contains the following configuration options which are ignored since they are not applicable when using Vite:[\s\S]+preserve_file_names/
-		),
-	]);
+	expect(serverLogs.warns).toEqual(
+		expect.arrayContaining([
+			expect.stringMatching(
+				/your worker config \(at `.*?wrangler.with-warning.jsonc`\) contains the following configuration options which are ignored since they are not applicable when using Vite:[\s\S]+preserve_file_names/
+			),
+		])
+	);
 });
