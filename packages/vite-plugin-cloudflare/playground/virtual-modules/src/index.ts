12614044a414d2916be44b68572c8a329e7c4386
Victor Berchet <victor@suumit.com>
2025-12-15 09:28:12 +0100

    fixup! fix

--- packages/vite-plugin-cloudflare/playground/virtual-modules/src/index.ts
+++ packages/vite-plugin-cloudflare/playground/virtual-modules/src/index.ts
@@ -1,6 +1,7 @@
 export default {
 	async fetch() {
 		// @ts-expect-error virtual module
+		const virtualModule = await import("virtual:module");
 		return new Response(virtualModule.default);
 	},
 } satisfies ExportedHandler;
