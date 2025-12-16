1a1024c653b9e2537d6739d24ae48d69bd975aa1
Pete Bacon Darwin <pbacondarwin@cloudflare.com>
2025-12-16 15:57:32 +0000

    silence Miniflare pipeline plugin tests

--- packages/miniflare/src/workers/pipelines/pipeline.worker.ts
+++ packages/miniflare/src/workers/pipelines/pipeline.worker.ts
@@ -1,7 +1,5 @@
 import { WorkerEntrypoint } from "cloudflare:workers";
 
 export default class Pipeline extends WorkerEntrypoint {
-	async send(data: object[]): Promise<void> {
-		console.log("Request received", data);
-	}
+	async send(_data: object[]): Promise<void> {}
 }
