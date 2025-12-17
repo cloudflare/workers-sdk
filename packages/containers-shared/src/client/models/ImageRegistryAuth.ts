1a3f90e0d5c581b73e35ddba3cae3f83fa40fd74
Nikita Sharma <nsharma@cloudflare.com>
2025-12-17 09:16:33 -0600

    update containers api client

--- packages/containers-shared/src/client/models/ImageRegistryAuth.ts
+++ packages/containers-shared/src/client/models/ImageRegistryAuth.ts
@@ -2,20 +2,15 @@
 /* tslint:disable */
 /* eslint-disable */
 
+import type { SecretsStoreRef } from "./SecretsStoreRef";
+
 /**
  * Credentials needed to authenticate with an external image registry.
  */
 export type ImageRegistryAuth = {
-	/** The format of this value is determined by the registry being configured. */
-	public_credential: string;
 	/**
-	 * Either a reference to a secret in secret store, or the secret itself,
-	 * which is used when secret-store is not available, e.g. in FedRAMP
+	 * The format of this value is determined by the registry being configured.
 	 */
-	private_credential:
-		| {
-				store_id: string;
-				secret_name: string;
-		  }
-		| string;
+	public_credential: string;
+	private_credential: string | SecretsStoreRef;
 };
