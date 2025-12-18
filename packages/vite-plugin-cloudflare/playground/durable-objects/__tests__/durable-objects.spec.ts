40f191cb6d6bda370ba2071b68e7b61ccee217e8
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-12-18 14:24:07 +0000

    Add failing tests

--- packages/vite-plugin-cloudflare/playground/durable-objects/__tests__/durable-objects.spec.ts
+++ packages/vite-plugin-cloudflare/playground/durable-objects/__tests__/durable-objects.spec.ts
@@ -2,6 +2,9 @@ import { describe, expect, test } from "vitest";
 import { getTextResponse } from "../../__test-utils__";
 
 describe("in-worker defined durable objects", async () => {
+	test("can bind to a Durable Object that does not extend the `DurableObject` class", async () => {
+		expect(await getTextResponse("/legacy")).toEqual("Legacy Durable Object");
+	});
 	test("can bind and use a Durable Object defined in the worker", async () => {
 		expect(await getTextResponse("/?name=my-do")).toEqual(
 			"Durable Object 'my-do' count: 0"
