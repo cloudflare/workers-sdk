c6af16d3e39ee37242e844dee8f8bd71f9a29fdc
Samuel Macleod <smacleod@cloudflare.com>
2025-12-19 22:40:34 +0000

    green

--- packages/vitest-pool-workers/src/worker/types-globals.d.ts
+++ packages/vitest-pool-workers/src/worker/types-globals.d.ts
@@ -8,7 +8,7 @@ declare global {
 	// https://github.com/vitest-dev/vitest/blob/1ec3a8b687c57153ed3d0d4777d7765c18f3cc82/packages/vitest/src/runtime/moduleRunner/moduleRunner.ts#L86
 	const __vitest_mocker__: VitestUtils & {
 		moduleRunner: {
-			import: (id: string) => Promise<any>;
+			import: (id: string) => Promise<Record<string, unknown>>;
 		};
 	};
 	// Original, un-patched console that always logs directly to stdout/err,
