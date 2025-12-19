4b0dc63607e600df69b68de5f8554ccc2ebd7118
Victor Berchet <victor@suumit.com>
2025-12-19 08:14:08 +0100

    Format command description consistently

--- packages/wrangler/src/d1/index.ts
+++ packages/wrangler/src/d1/index.ts
@@ -2,7 +2,7 @@ import { createNamespace } from "../core/create-command";
 
 export const d1Namespace = createNamespace({
 	metadata: {
-		description: "🗄  Manage Workers D1 databases",
+		description: "🗄 Manage Workers D1 databases",
 		status: "stable",
 		owner: "Product: D1",
 	},
