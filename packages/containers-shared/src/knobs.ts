553dd9175ef184a8e4a55c5484332bd6bf436eb6
Nikita Sharma <nsharma@cloudflare.com>
2025-12-22 16:27:08 -0600

    set default registry in frh realm

--- packages/containers-shared/src/knobs.ts
+++ packages/containers-shared/src/knobs.ts
@@ -1,15 +1,20 @@
+import { getCloudflareComplianceRegion } from "@cloudflare/workers-utils";
 import { MF_DEV_CONTAINER_PREFIX } from "./registry";
 
-// Will return the default cloudflare managed registry for either a staging or production envrionment
-// based on the env var WRANGLER_API_ENVIRONMENT. The default registry can be overriden with the env
-// var CLOUDFLARE_CONTAINER_REGISTRY.
+// Will return the default cloudflare managed registry for the environment being used.
+// If WRANGLER_API_ENVIRONMENT is set to "staging", it will return a staging registry.
+// If CLOUDFLARE_COMPLIANCE_REGION is set to "fedramp_high", it will return a fedramp_high registry.
+// If nothing is set, it will return the public production registry.
+// Override the default registry by setting the env var CLOUDFLARE_CONTAINER_REGISTRY.
 export const getCloudflareContainerRegistry = () => {
 	// previously defaulted to registry.cloudchamber.cfdata.org
+	const fed = getCloudflareComplianceRegion() === "fedramp_high" ? ".fed" : "";
+	const env =
+		process.env.WRANGLER_API_ENVIRONMENT === "staging" ? "staging." : "";
+
 	return (
 		process.env.CLOUDFLARE_CONTAINER_REGISTRY ??
-		(process.env.WRANGLER_API_ENVIRONMENT === "staging"
-			? "staging.registry.cloudflare.com"
-			: "registry.cloudflare.com")
+		`${env}registry${fed}.cloudflare.com`
 	);
 };
 
