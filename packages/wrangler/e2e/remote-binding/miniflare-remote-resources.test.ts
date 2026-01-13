b62a9a507700505c9e47b3585033d8bf3dd8ab89
emily-shen <eshen@cloudflare.com>
2026-01-13 17:42:53 +0000

    temporarily disable media binding test

--- packages/wrangler/e2e/remote-binding/miniflare-remote-resources.test.ts
+++ packages/wrangler/e2e/remote-binding/miniflare-remote-resources.test.ts
@@ -347,26 +347,26 @@ const testCases: TestCase[] = [
 		}),
 		expectFetchToMatch: [expect.stringContaining(`image/avif`)],
 	},
-	{
-		name: "Media",
-		scriptPath: "media.js",
-		setup: () => ({
-			remoteProxySessionConfig: {
-				bindings: {
-					MEDIA: {
-						type: "media",
-					},
-				},
-			},
-			miniflareConfig: (connection) => ({
-				media: {
-					binding: "MEDIA",
-					remoteProxyConnectionString: connection,
-				},
-			}),
-		}),
-		expectFetchToMatch: [expect.stringContaining(`image/jpeg`)],
-	},
+	// {
+	// 	name: "Media",
+	// 	scriptPath: "media.js",
+	// 	setup: () => ({
+	// 		remoteProxySessionConfig: {
+	// 			bindings: {
+	// 				MEDIA: {
+	// 					type: "media",
+	// 				},
+	// 			},
+	// 		},
+	// 		miniflareConfig: (connection) => ({
+	// 			media: {
+	// 				binding: "MEDIA",
+	// 				remoteProxyConnectionString: connection,
+	// 			},
+	// 		}),
+	// 	}),
+	// 	expectFetchToMatch: [expect.stringContaining(`image/jpeg`)],
+	// },
 	{
 		name: "Dispatch Namespace",
 		scriptPath: "dispatch-namespace.js",
