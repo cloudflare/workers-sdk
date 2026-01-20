7143d5d4e753b2f8285dcdb593f987efb7a4455a
Samuel Macleod <smacleod@cloudflare.com>
2026-01-20 16:32:02 +0000

    fix?

--- packages/vitest-pool-workers/src/worker/lib/cloudflare/snapshot.ts
+++ packages/vitest-pool-workers/src/worker/lib/cloudflare/snapshot.ts
@@ -1,7 +1,7 @@
 import assert from "node:assert";
 import { dirname } from "node:path";
 import { env } from "cloudflare:workers";
-import { VitestSnapshotEnvironment } from "vitest/snapshot";
+import { VitestSnapshotEnvironment } from "vitest/runtime";
 
 // Define a custom `SnapshotEnvironment` that uses a service binding for file
 // system operations, rather than `node:fs`
