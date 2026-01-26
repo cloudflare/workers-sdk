90619cb94737f3d23892d045fa9b582f5e4f8052
emily-shen <eshen@cloudflare.com>
2026-01-26 16:44:25 +0000

    rename getLocalExplorerFromEnv() to getLocalExplorerEnabledFromEnv()

--- packages/wrangler/src/dev/miniflare/index.ts
+++ packages/wrangler/src/dev/miniflare/index.ts
@@ -2,7 +2,7 @@ import assert from "node:assert";
 import { randomUUID } from "node:crypto";
 import path from "node:path";
 import { getDevContainerImageName } from "@cloudflare/containers-shared";
-import { getLocalExplorerFromEnv } from "@cloudflare/workers-utils";
+import { getLocalExplorerEnabledFromEnv } from "@cloudflare/workers-utils";
 import { Log, LogLevel } from "miniflare";
 import { ModuleTypeToRuleType } from "../../deployment-bundle/module-collection";
 import { withSourceURLs } from "../../deployment-bundle/source-url";
@@ -865,7 +865,7 @@ export async function buildMiniflareOptions(
 		unsafeHandleDevRegistryUpdate: onDevRegistryUpdate,
 		unsafeProxySharedSecret: proxyToUserWorkerAuthenticationSecret,
 		unsafeTriggerHandlers: true,
-		unsafeLocalExplorer: getLocalExplorerFromEnv(),
+		unsafeLocalExplorer: getLocalExplorerEnabledFromEnv(),
 		// The way we run Miniflare instances with wrangler dev is that there are two:
 		//  - one holding the proxy worker,
 		//  - and one holding the user worker.
