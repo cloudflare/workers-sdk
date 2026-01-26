7297ed393dae547544786460e3594557df5b66b1
emily-shen <eshen@cloudflare.com>
2026-01-26 17:24:13 +0000

    add implementation for KV API

--- packages/miniflare/src/workers/local-explorer/api.worker.ts
+++ packages/miniflare/src/workers/local-explorer/api.worker.ts
@@ -1,10 +1,68 @@
-// local explorer API Worker
+// Local Explorer API Worker
 // Provides a REST API for viewing and manipulating user resources
 
-import { WorkerEntrypoint } from "cloudflare:workers";
+import { Hono } from "hono/tiny";
+import { validateQuery, validateRequestBody } from "./common";
+import {
+	zWorkersKvNamespaceGetMultipleKeyValuePairsData,
+	zWorkersKvNamespaceListANamespaceSKeysData,
+	zWorkersKvNamespaceListNamespacesData,
+} from "./generated/zod.gen";
+import {
+	bulkGetKVValues,
+	deleteKVValue,
+	getKVValue,
+	listKVKeys,
+	listKVNamespaces,
+	putKVValue,
+} from "./resources/kv";
 
-export default class LocalExplorerAPI extends WorkerEntrypoint {
-	async fetch(): Promise<Response> {
-		return new Response("Hello from local explorer API");
-	}
-}
+type BindingIdMap = {
+	kv: Record<string, string>; // namespaceId -> bindingName
+};
+export type Env = {
+	[key: string]: unknown;
+	LOCAL_EXPLORER_BINDING_MAP: BindingIdMap;
+};
+
+export type AppBindings = { Bindings: Env };
+
+const BASE_PATH = "/cdn-cgi/explorer/api";
+
+const app = new Hono<AppBindings>().basePath(BASE_PATH);
+
+// ============================================================================
+// KV Endpoints
+// ============================================================================
+
+app.get(
+	"/storage/kv/namespaces",
+	// we are unwrapping (ie removing optional) because h
+	validateQuery(zWorkersKvNamespaceListNamespacesData.shape.query.unwrap()),
+	(c) => listKVNamespaces(c, c.req.valid("query"))
+);
+
+app.get(
+	"/storage/kv/namespaces/:namespace_id/keys",
+	validateQuery(
+		zWorkersKvNamespaceListANamespaceSKeysData.shape.query.unwrap()
+	),
+	(c) => listKVKeys(c, c.req.valid("query"))
+);
+
+app.get("/storage/kv/namespaces/:namespace_id/values/:key_name", getKVValue);
+app.put("/storage/kv/namespaces/:namespace_id/values/:key_name", putKVValue);
+app.delete(
+	"/storage/kv/namespaces/:namespace_id/values/:key_name",
+	deleteKVValue
+);
+
+app.post(
+	"/storage/kv/namespaces/:namespace_id/bulk/get",
+	validateRequestBody(
+		zWorkersKvNamespaceGetMultipleKeyValuePairsData.shape.body
+	),
+	(c) => bulkGetKVValues(c, c.req.valid("json"))
+);
+
+export default app;
