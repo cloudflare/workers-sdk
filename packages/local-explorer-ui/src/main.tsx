4da185b958c42bc22966a2c5b0bd293cc58871dd
emily-shen <eshen@cloudflare.com>
2026-01-29 15:23:14 +0000

    scaffold new local explorer ui package

---
+++ packages/local-explorer-ui/src/main.tsx
@@ -0,0 +1,22 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+const rootElement = document.getElementById("root");
+if (rootElement) {
+	createRoot(rootElement).render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>
+	);
+}
