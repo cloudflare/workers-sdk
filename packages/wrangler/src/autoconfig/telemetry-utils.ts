8fa7e1ae7b00adfaf131439e6bed13c26ad9f1ac
Dario Piotrowicz <dario@cloudflare.com>
2026-02-04 12:01:07 +0000

    fix incorrect check

--- packages/wrangler/src/autoconfig/telemetry-utils.ts
+++ packages/wrangler/src/autoconfig/telemetry-utils.ts
@@ -35,7 +35,7 @@ const autoConfigLocalState: Partial<AutoConfigLocalState> = {};
 function setAutoConfigTriggerCommand(
 	command: NonNullable<AutoConfigWranglerTriggerCommand>
 ) {
-	if ("trigger" in autoConfigLocalState) {
+	if (autoConfigLocalState.triggerCommand) {
 		throw new Error('Trying to set "trigger" in the autoconfig state again');
 	}
 	autoConfigLocalState.triggerCommand = command;
