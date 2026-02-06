0bfb890326395977ed86dd3fb711bfa8bf4c92f9
Victor Berchet <victor@suumit.com>
2026-02-06 15:40:47 +0100

    Use vitest#expect from the local context

--- packages/vite-plugin-cloudflare/playground/worker-♫/__tests__/log-level-silent/worker.spec.ts
+++ packages/vite-plugin-cloudflare/playground/worker-♫/__tests__/log-level-silent/worker.spec.ts
@@ -1,7 +1,7 @@
-import { expect, test } from "vitest";
+import { test } from "vitest";
 import { serverLogs } from "../../../__test-utils__";
 
-test("basic dev logging with logLevel: silent", async () => {
+test("basic dev logging with logLevel: silent", async ({ expect }) => {
 	expect(serverLogs.info.join()).toEqual("");
 	expect(serverLogs.warns.join()).toEqual("");
 	expect(serverLogs.errors.join()).toEqual("");
