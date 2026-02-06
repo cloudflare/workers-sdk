debcdf36e0b499fbaf51e264fcaba0d8f4689150
Ben Dixon <ben@nuro.dev>
2026-02-06 22:27:14 +0000

    Updated index page sub-header

--- packages/local-explorer-ui/src/routes/index.tsx
+++ packages/local-explorer-ui/src/routes/index.tsx
@@ -8,7 +8,7 @@ function IndexPage() {
 	return (
 		<div className="text-center p-12 text-text-secondary">
 			<h2 className="text-text mb-2">Welcome to Local Explorer</h2>
-			<p>Select a KV namespace from the sidebar to view and manage its keys.</p>
+			<p>Select a resource from the sidebar to view and manage its.</p>
 		</div>
 	);
 }
