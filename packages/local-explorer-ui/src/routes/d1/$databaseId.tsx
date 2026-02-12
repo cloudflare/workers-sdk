1bafa1487679d8265dc722eb027fc6d1229feea2
Ben Dixon <ben@nuro.dev>
2026-02-12 12:23:14 +0000

    Refactored to use a unified breadcrumb component

--- packages/local-explorer-ui/src/routes/d1/$databaseId.tsx
+++ packages/local-explorer-ui/src/routes/d1/$databaseId.tsx
@@ -1,5 +1,6 @@
-import { CaretRightIcon, DatabaseIcon } from "@phosphor-icons/react";
+import { DatabaseIcon } from "@phosphor-icons/react";
 import { createFileRoute } from "@tanstack/react-router";
+import { Breadcrumbs } from "../../components/Breadcrumbs";
 
 export const Route = createFileRoute("/d1/$databaseId")({
 	component: DatabaseView,
@@ -13,14 +14,7 @@ function DatabaseView(): JSX.Element {
 
 	return (
 		<div className="flex flex-col h-full">
-			<div className="flex items-center gap-2 py-4 px-6 min-h-16.75 box-border bg-bg-secondary border-b border-border text-sm shrink-0">
-				<span className="flex items-center gap-1.5">
-					<DatabaseIcon />
-					D1
-				</span>
-				<CaretRightIcon className="w-4 h-4" />
-				<span className="flex items-center gap-1.5">{params.databaseId}</span>
-			</div>
+			<Breadcrumbs icon={DatabaseIcon} title="D1" items={[params.databaseId]} />
 
 			<div className="flex-1 overflow-hidden">
 				{/* TODO: Add `<Studio />` component */}
