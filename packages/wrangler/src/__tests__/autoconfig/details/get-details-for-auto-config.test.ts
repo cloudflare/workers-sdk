1cadd48d019fa2f9f57eff734a641386a5e5d57a
Dario Piotrowicz <dario@cloudflare.com>
2026-02-16 11:07:50 +0000

    fix wrangler formatting

--- packages/wrangler/src/__tests__/autoconfig/details/get-details-for-auto-config.test.ts
+++ packages/wrangler/src/__tests__/autoconfig/details/get-details-for-auto-config.test.ts
@@ -119,7 +119,6 @@ describe("autoconfig details - getDetailsForAutoConfig()", () => {
 			details.getDetailsForAutoConfig()
 		).rejects.toThrowErrorMatchingInlineSnapshot(
 			`[Error: The Wrangler application detection logic has been run in the root of a workspace, this is not supported. Change your working directory to one of the applications in the workspace and try again.]`
-
 		);
 	});
 
