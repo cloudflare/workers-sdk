ddb946ff9b306441f84104a8bf5e818d6227b56e
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2026-02-19 14:53:38 +0000

    Include child environment in build and add failing test

--- packages/vite-plugin-cloudflare/playground/child-environment/__tests__/child-environment.spec.ts
+++ packages/vite-plugin-cloudflare/playground/child-environment/__tests__/child-environment.spec.ts
@@ -1,10 +1,24 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
 import { test } from "vitest";
-import { getTextResponse, isBuild } from "../../__test-utils__";
+import { getTextResponse, isBuild, testDir } from "../../__test-utils__";
 
 test.runIf(!isBuild)(
 	"can import module from child environment",
 	async ({ expect }) => {
 		const response = await getTextResponse();
 		expect(response).toBe("Hello from the child environment");
+	}
+);
+
+test.runIf(isBuild)(
+	"nests child environment output in parent environment output directory",
+	({ expect }) => {
+		const childEnvironmentEntryPath = path.join(
+			testDir,
+			"dist/parent/child/child-environment-module.js"
+		);
+
+		expect(fs.existsSync(childEnvironmentEntryPath)).toBe(true);
 	}
 );
