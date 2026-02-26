c47b8f098f614fa83dda0b8327963dedbad9939b
Ehbraheem <ehbraheem@gmail.com>
2026-02-26 23:27:03 +0100

    [wrangler/d1] Address PR comment suggestions

--- .changeset/auto-apply-migrations.md
+++ .changeset/auto-apply-migrations.md
@@ -2,12 +2,12 @@
 "wrangler": patch
 ---
 
-Add a new `--force-non-interactive` flag to `d1 migrations apply` which skips the interactive
+Add a new `--force` flag to `d1 migrations apply` which skips the interactive
 confirmation and applies any pending migrations non-interactively. This is useful
 for CI and automation.
 
 Usage example:
 
-wrangler d1 migrations apply <db> --remote --force-non-interactive
+wrangler d1 migrations apply <db> --remote --force
 
 Fixes #5017
--- packages/wrangler/src/__tests__/d1/migrate.test.ts
+++ packages/wrangler/src/__tests__/d1/migrate.test.ts
@@ -12,6 +12,7 @@ import { msw } from "../helpers/msw";
 import { runInTempDir } from "../helpers/run-in-tmp";
 import { runWrangler } from "../helpers/run-wrangler";
 
+
 describe("migrate", () => {
 	runInTempDir();
 	const mockStd = mockConsoleMethods();
@@ -311,7 +312,7 @@ Your database may not be available to serve requests during the migration, conti
 			);
 		});
 
-		it("should not prompt when --force-non-interactive is passed", async () => {
+		it("should not prompt when --force is passed", async () => {
 			setIsTTY(false);
 			const std = mockConsoleMethods();
 
@@ -378,12 +379,10 @@ Your database may not be available to serve requests during the migration, conti
 
 			await runWrangler("d1 migrations create db test");
 
-			await runWrangler(
-				"d1 migrations apply db --remote --force-non-interactive"
-			);
+			await runWrangler("d1 migrations apply db --remote --force");
 
 			expect(std.out).toContain(
-				"--force-non-interactive passed, applying 1 migration(s) without prompt"
+				"--force passed, applying 1 migration(s) without prompt"
 			);
 		});
 	});
