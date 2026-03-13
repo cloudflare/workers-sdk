2b9a186dceebdae8fb57617c1c129971a9d20d68
Gregory Anders <ganders@cloudflare.com>
2026-03-13 12:23:28 -0500

    Add wrangler containers instances command (#12873)

---
+++ packages/containers-shared/src/client/models/DashApplicationDurableObjectInstance.ts
@@ -0,0 +1,11 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type DashApplicationDurableObjectInstance = {
+	id: string;
+	deployment_id?: string;
+	placement_id?: string;
+	assigned_at: string;
+	name?: string;
+};
