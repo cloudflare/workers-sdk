6faae10dc1a7a806a92c6f63d0a7ec1288d01864
Somhairle MacLeòid <samuel@macleod.space>
2026-03-26 14:30:09 +0000

    fix: fix TS errors in dynamic-import fixture

--- fixtures/vitest-pool-workers-examples/dynamic-import/src/worker-configuration.d.ts
+++ fixtures/vitest-pool-workers-examples/dynamic-import/src/worker-configuration.d.ts
@@ -2,5 +2,8 @@ declare namespace Cloudflare {
 	interface GlobalProps {
 		mainModule: typeof import("./index");
 	}
+	interface Env {
+		GREETER: DurableObjectNamespace;
+	}
 }
 interface Env extends Cloudflare.Env {}
