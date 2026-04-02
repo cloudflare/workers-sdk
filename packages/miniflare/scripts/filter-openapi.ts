bb5f7821f786426d34ab9de16b1592dc86f0be0f
emily-shen <eshen@cloudflare.com>
2026-04-02 16:30:31 +1100

    update openapi description a little

--- packages/miniflare/scripts/filter-openapi.ts
+++ packages/miniflare/scripts/filter-openapi.ts
@@ -32,7 +32,7 @@ const DEFAULT_OUTPUT_PATH = join(
 const LOCAL_EXPLORER_INFO = {
 	title: "Local Explorer API",
 	description:
-		"Local subset of Cloudflare API for exploring resources during local development.",
+		"A local subset of Cloudflare API for inspecting and modifying resource state during local development. Supports D1, R2, KV, Durable Objects and Workflows.",
 	version: "0.0.1",
 };
 
