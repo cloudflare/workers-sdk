c05eac21ed1d49ca192cc499b27e24cba5db4975
Courtney Sims <csims@cloudflare.com>
2026-04-09 10:03:02 -0500

    (asset) Fix worker config lint suppression

--- packages/workers-shared/asset-worker/worker-configuration.d.ts
+++ packages/workers-shared/asset-worker/worker-configuration.d.ts
@@ -1,8 +1,8 @@
 // Populates Cloudflare.Exports (the type of ctx.exports) with loopback
 // bindings derived from the main module's exports.
 declare namespace Cloudflare {
-	// eslint-disable-next-line @typescript-eslint/consistent-type-imports -- Cloudflare typegen requires `typeof import()` for mainModule
 	interface GlobalProps {
+		// eslint-disable-next-line @typescript-eslint/consistent-type-imports -- Cloudflare typegen requires `typeof import()` for mainModule
 		mainModule: typeof import("./src/worker");
 	}
 }
