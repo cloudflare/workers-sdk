e98a270ed4a6f339957c379861d5e2c1fc958d96
Courtney Sims <csims@cloudflare.com>
2026-04-13 17:19:46 -0500

    Fix formatting

--- packages/workers-shared/router-worker/src/worker.ts
+++ packages/workers-shared/router-worker/src/worker.ts
@@ -1,22 +1,22 @@
 import { WorkerEntrypoint } from "cloudflare:workers";
-import type AssetWorker from "../../asset-worker";
 import { generateStaticRoutingRuleMatcher } from "../../asset-worker/src/utils/rules-engine";
 import { PerformanceTimer } from "../../utils/performance";
 import { TemporaryRedirectResponse } from "../../utils/responses";
 import { setupSentry } from "../../utils/sentry";
 import { mockJaegerBinding } from "../../utils/tracing";
-import type {
-	EyeballRouterConfig,
-	JaegerTracing,
-	RouterConfig,
-	UnsafePerformanceTimer,
-} from "../../utils/types";
 import { Analytics, DISPATCH_TYPE, STATIC_ROUTING_DECISION } from "./analytics";
 import {
 	applyEyeballConfigDefaults,
 	applyRouterConfigDefaults,
 } from "./configuration";
 import { renderLimitedResponse } from "./limited-response";
+import type AssetWorker from "../../asset-worker";
+import type {
+	EyeballRouterConfig,
+	JaegerTracing,
+	RouterConfig,
+	UnsafePerformanceTimer,
+} from "../../utils/types";
 import type { ColoMetadata, Environment, ReadyAnalytics } from "./types";
 
 export interface Env {
