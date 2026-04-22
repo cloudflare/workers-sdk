0f159ce82f01bdf1f6b6d1c544c767beecb11b71
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2026-04-22 09:25:33 +0100

    Revert "Support local explorer `/cdn-cgi/` routes (#12855)"
    This reverts commit c2b76bc35bd038200b9919179f63b66e190e00eb.

--- packages/vite-plugin-cloudflare/src/index.ts
+++ packages/vite-plugin-cloudflare/src/index.ts
@@ -3,7 +3,6 @@ import { assertWranglerVersion } from "./assert-wrangler-version";
 import { PluginContext } from "./context";
 import { resolvePluginConfig } from "./plugin-config";
 import { additionalModulesPlugin } from "./plugins/additional-modules";
-import { cdnCgiPlugin } from "./plugins/cdn-cgi";
 import { configPlugin } from "./plugins/config";
 import { debugPlugin } from "./plugins/debug";
 import { devPlugin } from "./plugins/dev";
@@ -16,6 +15,7 @@ import { outputConfigPlugin } from "./plugins/output-config";
 import { previewPlugin } from "./plugins/preview";
 import { rscPlugin } from "./plugins/rsc";
 import { shortcutsPlugin } from "./plugins/shortcuts";
+import { triggerHandlersPlugin } from "./plugins/trigger-handlers";
 import {
 	virtualClientFallbackPlugin,
 	virtualModulesPlugin,
@@ -99,7 +99,7 @@ export function cloudflare(pluginConfig: PluginConfig = {}): vite.Plugin[] {
 		previewPlugin(ctx),
 		shortcutsPlugin(ctx),
 		debugPlugin(ctx),
-		cdnCgiPlugin(ctx),
+		triggerHandlersPlugin(ctx),
 		virtualModulesPlugin(ctx),
 		virtualClientFallbackPlugin(ctx),
 		outputConfigPlugin(ctx),
