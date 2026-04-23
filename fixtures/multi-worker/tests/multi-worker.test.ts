2831b545efe86c71fe1930909ca9e891c27a0722
Jan Peer Stöcklmair <jan.oster94@gmail.com>
2026-04-23 18:39:47 +0200

    fix(wrangler): Bind the console methods directly instead of using a global proxy (#11784)
    Co-authored-by: Pete Bacon Darwin <pbacondarwin@cloudflare.com>

---
+++ fixtures/multi-worker/tests/multi-worker.test.ts
@@ -0,0 +1,21 @@
+import * as path from "node:path";
+import { describe, it, vi } from "vitest";
+import { runWranglerDev } from "../../shared/src/run-wrangler-long-lived";
+
+describe("Multi Worker", () => {
+	it("can start a multi worker application with Sentry", async ({ expect }) => {
+		const { ip, port, stop } = await runWranglerDev(
+			path.resolve(__dirname, ".."),
+			["-c=workers/sentry/wrangler.jsonc", "-c=workers/default/wrangler.jsonc"]
+		);
+		try {
+			await vi.waitFor(async () => {
+				const response = await fetch(`http://${ip}:${port}/`);
+				const text = await response.text();
+				expect(text).toBe(`Hello World!`);
+			});
+		} finally {
+			await stop();
+		}
+	});
+});
