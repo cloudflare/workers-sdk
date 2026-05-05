dbff6506c8d92f58744349da90df83683dcf0eae
Grey Vugrin <3294460+greyvugrin@users.noreply.github.com>
2026-05-05 16:17:54 -0700

    [wrangler] Update warning message to mention CLOUDFLARE_ENV option

--- packages/wrangler/src/core/register-yargs-command.ts
+++ packages/wrangler/src/core/register-yargs-command.ts
@@ -212,7 +212,7 @@ function createHandler(def: InternalCommandDefinition, argv: string[]) {
 							logger.warn(
 								dedent`
 										Multiple environments are defined in the Wrangler configuration file, but no target environment was specified for the ${sanitizedCommand} command.
-										To avoid unintentional changes to the wrong environment, it is recommended to explicitly specify the target environment using the \`-e|--env\` flag.
+										To avoid unintentional changes to the wrong environment, it is recommended to explicitly specify the target environment using the \`-e|--env\` flag or CLOUDFLARE_ENV env variable.
 										If your intention is to use the top-level environment of your configuration simply pass an empty string to the flag to target such environment. For example \`--env=""\`.
 									`
 							);
