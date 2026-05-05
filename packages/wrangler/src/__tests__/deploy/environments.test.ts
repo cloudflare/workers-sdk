5db9d5d209291253009249f5bdc53a0b50bea839
Grey Vugrin <3294460+greyvugrin@users.noreply.github.com>
2026-05-05 16:05:46 -0700

    [wrangler] Update tests to use a helper to return the expected multi-env warning message

--- packages/wrangler/src/__tests__/deploy/environments.test.ts
+++ packages/wrangler/src/__tests__/deploy/environments.test.ts
@@ -15,6 +15,7 @@ import { useMockIsTTY } from "../helpers/mock-istty";
 import { mockOAuthFlow } from "../helpers/mock-oauth-flow";
 import { mockUploadWorkerRequest } from "../helpers/mock-upload-worker";
 import { mockGetSettings } from "../helpers/mock-worker-settings";
+import { multiEnvWarning } from "../helpers/multi-env-warning";
 import {
 	mockGetWorkerSubdomain,
 	mockSubDomainRequest,
@@ -954,16 +955,7 @@ describe("deploy", () => {
 
 			await runWrangler("deploy");
 
-			expect(std.warn).toMatchInlineSnapshot(`
-				"[33m▲ [43;33m[[43;30mWARNING[43;33m][0m [1mMultiple environments are defined in the Wrangler configuration file, but no target environment was specified for the deploy command.[0m
-
-				  To avoid unintentional changes to the wrong environment, it is recommended to explicitly specify
-				  the target environment using the \`-e|--env\` flag.
-				  If your intention is to use the top-level environment of your configuration simply pass an empty
-				  string to the flag to target such environment. For example \`--env=""\`.
-
-				"
-			`);
+			expect(std.warn).toMatchInlineSnapshot(multiEnvWarning("deploy"));
 		});
 
 		it("should not warn if the wrangler config contains environments and one was specified in the command", async ({
