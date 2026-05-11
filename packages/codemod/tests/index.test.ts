5ce752515932f3ebd1dc967efdbc455934a8b937
tstewart <tstewart@klaudhaus.com>
2026-05-11 20:08:42 +1000

    Switch to use babel JS parsing in codemod to resolve Svelte project init failure when selecting Drizzle and/or Better Auth options.

--- packages/codemod/tests/index.test.ts
+++ packages/codemod/tests/index.test.ts
@@ -1,7 +1,7 @@
 import * as recast from "recast";
 import parser from "recast/parsers/babel";
-import { describe, test } from "vitest";
-import { mergeObjectProperties } from "../src/index";
+import {describe, expect, test} from "vitest";
+import {mergeObjectProperties, parseJs, parseTs} from "../src/index";
 
 describe("mergeObjectProperties", () => {
 	const tests = [
@@ -127,3 +127,48 @@ const createObjectExpression = (
 		).declarations[0] as recast.types.namedTypes.VariableDeclarator
 	).init as recast.types.namedTypes.ObjectExpression;
 };
+
+
+/*
+	This code, auto-generated during SvelteKit project init with Drizzle and/or Better Auth options,
+	caused a parse failure with the ESPrima-based version of `codemod`,
+	causing failure during `npm create cloudflare`.
+ */
+const svConfigWithSpread = `
+import adapter from '@sveltejs/adapter-auto';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	compilerOptions: {
+		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+		runes: ({ filename }) => filename.split(/[/\\\\]/).includes('node_modules') ? undefined : true
+	},
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter(),
+
+		typescript: {
+			config: (config) => ({
+				...config,
+				include: [...config.include, '../drizzle.config.ts']
+			})
+		}
+	}
+};
+
+export default config;
+`;
+
+describe("spread syntax parsing", () => {
+	test("can parse formerly failing svelte config file as TypeScript", () => {
+		const result = parseTs(svConfigWithSpread)
+		expect(result).toBe(result)
+	})
+
+	test("can parse formerly failing svelte config file as JavaScript", () => {
+		const result = parseJs(svConfigWithSpread)
+		expect(result).toBe(result)
+	})
+})
