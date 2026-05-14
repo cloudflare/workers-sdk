be8a98c2be0c83a270415ff4591e1b9971aab747
emily-shen <69125074+emily-shen@users.noreply.github.com>
2026-05-14 15:41:26 +0100

    refactor deploy/versions upload (part 1) (#13916)

--- packages/wrangler/src/versions/upload.ts
+++ packages/wrangler/src/versions/upload.ts
@@ -26,6 +26,10 @@ import { getBindings, provisionBindings } from "../deployment-bundle/bindings";
 import { bundleWorker } from "../deployment-bundle/bundle";
 import { printBundleSize } from "../deployment-bundle/bundle-reporter";
 import { createWorkerUploadForm } from "../deployment-bundle/create-worker-upload-form";
+import {
+	sharedDeployVersionsArgs,
+	validateDeployVersionsArgs,
+} from "../deployment-bundle/deploy-args";
 import { getEntry } from "../deployment-bundle/entry";
 import { logBuildOutput } from "../deployment-bundle/esbuild-plugins/log-build-output";
 import {
@@ -120,161 +124,9 @@ export const versionsUploadCommand = createCommand({
 	},
 	positionalArgs: ["script"],
 	args: {
-		script: {
-			describe: "The path to an entry point for your Worker",
-			type: "string",
-			requiresArg: true,
-		},
-		name: {
-			describe: "Name of the Worker",
-			type: "string",
-			requiresArg: true,
-		},
-		tag: {
-			describe: "A tag for this Worker Gradual Rollouts Version",
-			type: "string",
-			requiresArg: true,
-		},
-		message: {
-			describe:
-				"A descriptive message for this Worker Gradual Rollouts Version",
-			type: "string",
-			requiresArg: true,
-		},
+		...sharedDeployVersionsArgs,
 		"preview-alias": {
 			describe: "Name of an alias for this Worker version",
-			type: "string",
-			requiresArg: true,
-		},
-		bundle: {
-			describe: "Run Wrangler's compilation step before publishing",
-			type: "boolean",
-			hidden: true,
-		},
-		"no-bundle": {
-			describe: "Skip internal build steps and directly upload Worker",
-			type: "boolean",
-			default: false,
-		},
-		outdir: {
-			describe: "Output directory for the bundled Worker",
-			type: "string",
-			requiresArg: true,
-		},
-		outfile: {
-			describe: "Output file for the bundled worker",
-			type: "string",
-			requiresArg: true,
-		},
-		"compatibility-date": {
-			describe: "Date to use for compatibility checks",
-			type: "string",
-			requiresArg: true,
-		},
-		"compatibility-flags": {
-			describe: "Flags to use for compatibility checks",
-			alias: "compatibility-flag",
-			type: "string",
-			requiresArg: true,
-			array: true,
-		},
-		latest: {
-			describe: "Use the latest version of the Worker runtime",
-			type: "boolean",
-			default: false,
-		},
-		assets: {
-			describe: "Static assets to be served. Replaces Workers Sites.",
-			type: "string",
-			requiresArg: true,
-		},
-		site: {
-			describe: "Root folder of static assets for Workers Sites",
-			type: "string",
-			requiresArg: true,
-			hidden: true,
-			deprecated: true,
-		},
-		"site-include": {
-			describe:
-				"Array of .gitignore-style patterns that match file or directory names from the sites directory. Only matched items will be uploaded.",
-			type: "string",
-			requiresArg: true,
-			array: true,
-			hidden: true,
-			deprecated: true,
-		},
-		"site-exclude": {
-			describe:
-				"Array of .gitignore-style patterns that match file or directory names from the sites directory. Matched items will not be uploaded.",
-			type: "string",
-			requiresArg: true,
-			array: true,
-			hidden: true,
-			deprecated: true,
-		},
-		var: {
-			describe: "A key-value pair to be injected into the script as a variable",
-			type: "string",
-			requiresArg: true,
-			array: true,
-		},
-		define: {
-			describe: "A key-value pair to be substituted in the script",
-			type: "string",
-			requiresArg: true,
-			array: true,
-		},
-		alias: {
-			describe: "A module pair to be substituted in the script",
-			type: "string",
-			requiresArg: true,
-			array: true,
-		},
-		"jsx-factory": {
-			describe: "The function that is called for each JSX element",
-			type: "string",
-			requiresArg: true,
-		},
-		"jsx-fragment": {
-			describe: "The function that is called for each JSX fragment",
-			type: "string",
-			requiresArg: true,
-		},
-		tsconfig: {
-			describe: "Path to a custom tsconfig.json file",
-			type: "string",
-			requiresArg: true,
-		},
-		minify: {
-			describe: "Minify the Worker",
-			type: "boolean",
-		},
-		"upload-source-maps": {
-			describe:
-				"Include source maps when uploading this Worker Gradual Rollouts Version.",
-			type: "boolean",
-		},
-		"node-compat": {
-			describe: "Enable Node.js compatibility",
-			type: "boolean",
-			hidden: true,
-			deprecated: true,
-		},
-		"dry-run": {
-			describe: "Compile a project without actually uploading the version.",
-			type: "boolean",
-		},
-		"experimental-auto-create": {
-			describe: "Automatically provision draft bindings with new resources",
-			type: "boolean",
-			default: true,
-			hidden: true,
-			alias: "x-auto-create",
-		},
-		"secrets-file": {
-			describe:
-				"Path to a file containing secrets to upload with the version (JSON or .env format). Secrets from previous deployments will not be deleted - see `--keep-secrets`",
 			type: "string",
 			requiresArg: true,
 		},
@@ -288,6 +140,9 @@ export const versionsUploadCommand = createCommand({
 		}),
 		warnIfMultipleEnvsConfiguredButNoneSpecified: true,
 	},
+	validateArgs(args) {
+		validateDeployVersionsArgs(args);
+	},
 	handler: async function versionsUploadHandler(args, { config }) {
 		const entry = await getEntry(args, config, "versions upload");
 		metrics.sendMetricsEvent(
@@ -299,13 +154,6 @@ export const versionsUploadCommand = createCommand({
 				sendMetrics: config.send_metrics,
 			}
 		);
-
-		if (args.nodeCompat) {
-			throw new UserError(
-				`The --node-compat flag is no longer supported as of Wrangler v4. Instead, use the \`nodejs_compat\` compatibility flag. This includes the functionality from legacy \`node_compat\` polyfills and natively implemented Node.js APIs. See https://developers.cloudflare.com/workers/runtime-apis/nodejs for more information.`,
-				{ telemetryMessage: "versions upload node compat unsupported" }
-			);
-		}
 
 		if (args.site || config.site) {
 			throw new UserError(
@@ -327,12 +175,6 @@ export const versionsUploadCommand = createCommand({
 			args,
 			config,
 		});
-
-		if (args.latest) {
-			logger.warn(
-				`Using the latest version of the Workers runtime. To silence this warning, please choose a specific version of the runtime with --compatibility-date, or add a compatibility_date to your ${configFileName(config.configPath)} file.\n`
-			);
-		}
 
 		const cliVars = collectKeyValues(args.var);
 		const cliDefines = collectKeyValues(args.define);
@@ -400,7 +242,7 @@ export const versionsUploadCommand = createCommand({
 				outDir: args.outdir,
 				dryRun: args.dryRun,
 				noBundle: !(args.bundle ?? !config.no_bundle),
-				keepVars: config.keep_vars,
+				keepVars: args.keepVars || config.keep_vars,
 				projectRoot: entry.projectRoot,
 				tag: args.tag,
 				message: args.message,
