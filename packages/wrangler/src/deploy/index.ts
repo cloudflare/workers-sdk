a41f5a0b804a0bb69805c78ea52885fc42f4e77a
emily-shen <eshen@cloudflare.com>
2026-05-14 10:59:43 +0100

    pull out shared args and args validation

--- packages/wrangler/src/deploy/index.ts
+++ packages/wrangler/src/deploy/index.ts
@@ -17,6 +17,10 @@ import {
 } from "../autoconfig/telemetry-utils";
 import { readConfig } from "../config";
 import { createCommand } from "../core/create-command";
+import {
+	sharedDeployVersionsArgs,
+	validateDeployVersionsArgs,
+} from "../deployment-bundle/deploy-args";
 import { getEntry } from "../deployment-bundle/entry";
 import { confirm, prompt } from "../dialogs";
 import { isNonInteractiveOrCI } from "../is-interactive";
@@ -42,105 +46,7 @@ export const deployCommand = createCommand({
 	},
 	positionalArgs: ["script"],
 	args: {
-		script: {
-			describe: "The path to an entry point for your Worker",
-			type: "string",
-			requiresArg: true,
-		},
-		name: {
-			describe: "Name of the Worker",
-			type: "string",
-			requiresArg: true,
-		},
-		// We want to have a --no-bundle flag, but yargs requires that
-		// we also have a --bundle flag (that it adds the --no to by itself)
-		// So we make a --bundle flag, but hide it, and then add a --no-bundle flag
-		// that's visible to the user but doesn't "do" anything.
-		bundle: {
-			describe: "Run Wrangler's compilation step before publishing",
-			type: "boolean",
-			hidden: true,
-		},
-		"no-bundle": {
-			describe: "Skip internal build steps and directly deploy Worker",
-			type: "boolean",
-			default: false,
-		},
-		outdir: {
-			describe: "Output directory for the bundled Worker",
-			type: "string",
-			requiresArg: true,
-		},
-		outfile: {
-			describe: "Output file for the bundled worker",
-			type: "string",
-			requiresArg: true,
-		},
-		"compatibility-date": {
-			describe: "Date to use for compatibility checks",
-			type: "string",
-			requiresArg: true,
-		},
-		"compatibility-flags": {
-			describe: "Flags to use for compatibility checks",
-			alias: "compatibility-flag",
-			type: "string",
-			requiresArg: true,
-			array: true,
-		},
-		latest: {
-			describe: "Use the latest version of the Workers runtime",
-			type: "boolean",
-			default: false,
-		},
-		assets: {
-			describe: "Static assets to be served. Replaces Workers Sites.",
-			type: "string",
-			requiresArg: true,
-		},
-		site: {
-			describe: "Root folder of static assets for Workers Sites",
-			type: "string",
-			requiresArg: true,
-			hidden: true,
-			deprecated: true,
-		},
-		"site-include": {
-			describe:
-				"Array of .gitignore-style patterns that match file or directory names from the sites directory. Only matched items will be uploaded.",
-			type: "string",
-			requiresArg: true,
-			array: true,
-			hidden: true,
-			deprecated: true,
-		},
-		"site-exclude": {
-			describe:
-				"Array of .gitignore-style patterns that match file or directory names from the sites directory. Matched items will not be uploaded.",
-			type: "string",
-			requiresArg: true,
-			array: true,
-			hidden: true,
-			deprecated: true,
-		},
-		var: {
-			describe: "A key-value pair to be injected into the script as a variable",
-			type: "string",
-			requiresArg: true,
-			array: true,
-		},
-		define: {
-			describe: "A key-value pair to be substituted in the script",
-			type: "string",
-			requiresArg: true,
-			array: true,
-		},
-		alias: {
-			describe: "A module pair to be substituted in the script",
-			type: "string",
-			requiresArg: true,
-			array: true,
-		},
+		...sharedDeployVersionsArgs,
 		triggers: {
 			describe: "cron schedules to attach",
 			alias: ["schedule", "schedules"],
@@ -161,35 +67,6 @@ export const deployCommand = createCommand({
 			type: "string",
 			requiresArg: true,
 			array: true,
-		},
-		"jsx-factory": {
-			describe: "The function that is called for each JSX element",
-			type: "string",
-			requiresArg: true,
-		},
-		"jsx-fragment": {
-			describe: "The function that is called for each JSX fragment",
-			type: "string",
-			requiresArg: true,
-		},
-		tsconfig: {
-			describe: "Path to a custom tsconfig.json file",
-			type: "string",
-			requiresArg: true,
-		},
-		minify: {
-			describe: "Minify the Worker",
-			type: "boolean",
-		},
-		"node-compat": {
-			describe: "Enable Node.js compatibility",
-			type: "boolean",
-			hidden: true,
-			deprecated: true,
-		},
-		"dry-run": {
-			describe: "Don't actually deploy",
-			type: "boolean",
 		},
 		metafile: {
 			describe:
@@ -216,10 +93,6 @@ export const deployCommand = createCommand({
 			describe:
 				"Send Trace Events from this Worker to Workers Logpush.\nThis will not configure a corresponding Logpush job automatically.",
 		},
-		"upload-source-maps": {
-			type: "boolean",
-			describe: "Include source maps when uploading this Worker.",
-		},
 		"old-asset-ttl": {
 			describe:
 				"Expire old assets in given seconds rather than immediate deletion.",
@@ -235,16 +108,6 @@ export const deployCommand = createCommand({
 				"Rollout strategy for Containers changes. If set to immediate, it will override `rollout_percentage_steps` if configured and roll out to 100% of instances in one step. ",
 			choices: ["immediate", "gradual"] as const,
 		},
-		tag: {
-			describe: "A tag for this Worker Version",
-			type: "string",
-			requiresArg: true,
-		},
-		message: {
-			describe: "A descriptive message for this Worker Version and Deployment",
-			type: "string",
-			requiresArg: true,
-		},
 		strict: {
 			describe:
 				"Enables strict mode for the deploy command, this prevents deployments to occur when there are even small potential risks.",
@@ -258,12 +121,6 @@ export const deployCommand = createCommand({
 			type: "boolean",
 			default: true,
 		},
-		"secrets-file": {
-			describe:
-				"Path to a file containing secrets to upload with the deployment (JSON or .env format). Secrets from previous deployments will not be deleted - see `--keep-secrets`",
-			type: "string",
-			requiresArg: true,
-		},
 	},
 	behaviour: {
 		useConfigRedirectIfAvailable: true,
@@ -276,12 +133,7 @@ export const deployCommand = createCommand({
 		printMetricsBanner: true,
 	},
 	validateArgs(args) {
-		if (args.nodeCompat) {
-			throw new UserError(
-				"The --node-compat flag is no longer supported as of Wrangler v4. Instead, use the `nodejs_compat` compatibility flag. This includes the functionality from legacy `node_compat` polyfills and natively implemented Node.js APIs. See https://developers.cloudflare.com/workers/runtime-apis/nodejs for more information.",
-				{ telemetryMessage: "deploy command node compat unsupported" }
-			);
-		}
+		validateDeployVersionsArgs(args);
 	},
 	async handler(args, { config }) {
 		const shouldRunAutoConfig =
@@ -425,14 +277,6 @@ export const deployCommand = createCommand({
 			args,
 			config,
 		});
-
-		if (args.latest) {
-			logger.warn(
-				`Using the latest version of the Workers runtime. To silence this warning, please choose a specific version of the runtime with --compatibility-date, or add a compatibility_date to your ${configFileName(
-					config.configPath
-				)} file.`
-			);
-		}
 
 		const cliVars = collectKeyValues(args.var);
 		const cliDefines = collectKeyValues(args.define);
