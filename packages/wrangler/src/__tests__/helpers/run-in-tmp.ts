4252b41ff5c6d263cd4d78cc41eeb5072f625662
emily-shen <eshen@cloudflare.com>
2026-05-19 17:16:44 +0100

    fix rebase

--- packages/wrangler/src/__tests__/helpers/run-in-tmp.ts
+++
@@ -1,7 +0,0 @@
-import { runInTempDir as runInTempDirCommon } from "@cloudflare/workers-utils/test-helpers";
-import { beforeEach } from "vitest";
-
-export function runInTempDir(options?: { homedir: string }) {
-	runInTempDirCommon(options);
-	beforeEach(() => {});
-}
