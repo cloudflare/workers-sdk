88a94a37d943a6e8fbea1345d382d88dd28f472b
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2026-05-22 14:54:46 +0100

    Update playground

--- packages/vite-plugin-cloudflare/playground/experimental-config/worker.config.ts
+++ packages/vite-plugin-cloudflare/playground/experimental-config/worker.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from "@cloudflare/vite-plugin/experimental-config";
+import {
+	defineConfig,
+	bindings,
+} from "@cloudflare/vite-plugin/experimental-config";
+import * as entrypoint from "./src/index.ts" with { type: "cf-worker" };
 
 export default defineConfig({
 	name: "experimental-config-worker",
-	entrypoint: "./src/index.ts",
+	entrypoint,
 	compatibilityDate: "2024-12-30",
+	env: {
+		MY_TEXT: bindings.text("Hello world"),
+	},
 });
