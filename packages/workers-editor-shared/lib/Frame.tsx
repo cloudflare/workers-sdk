1750d564f7ec0be85ecd30cab2c10aef2c5abc1f
Jibin Jose <jibin7jose@users.noreply.github.com>
2026-07-07 11:51:45 +0530

    fix(workers-editor-shared): avoid leaking Frame dependency types

--- packages/workers-editor-shared/lib/Frame.tsx
+++ packages/workers-editor-shared/lib/Frame.tsx
@@ -1,17 +1,22 @@
-import type React from "react";
 import { createComponent } from "@cloudflare/style-container";
+import type React from "react";
 
-const Frame: React.ComponentType<React.IframeHTMLAttributes<HTMLIFrameElement> & { innerRef?: React.RefObject<HTMLIFrameElement> }> =
-	createComponent<"iframe">(
-		() => ({
-			position: "absolute",
-			top: 0,
-			left: 0,
-			width: "100%",
-			height: "100%",
-			border: 0,
-		}),
-		"iframe"
-	);
+type FrameProps = React.IframeHTMLAttributes<HTMLIFrameElement> & {
+	innerRef?: React.Ref<HTMLIFrameElement>;
+};
+
+const StyledFrame = createComponent<"iframe">(
+	() => ({
+		position: "absolute",
+		top: 0,
+		left: 0,
+		width: "100%",
+		height: "100%",
+		border: 0,
+	}),
+	"iframe"
+) as React.ComponentType<FrameProps>;
+
+const Frame = (props: FrameProps) => <StyledFrame {...props} />;
 
 export default Frame;
