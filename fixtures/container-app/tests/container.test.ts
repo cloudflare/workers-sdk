d20f3c318d888f99d71e045a2d8cbdc529df109f
Edmund Hung <edmund@cloudflare.com>
2026-07-16 16:28:50 +0100

    run container test on linux only

--- fixtures/container-app/tests/container.test.ts
+++ fixtures/container-app/tests/container.test.ts
@@ -1,11 +1,37 @@
+import { execSync } from "child_process";
 import { afterAll, beforeAll, describe, test, vi } from "vitest";
 import { createTestHarness } from "wrangler";
 
-const server = createTestHarness({
-	workers: [{ configPath: "./wrangler.jsonc" }],
-});
+const isCINonLinux = process.platform !== "linux" && process.env.CI === "true";
 
-describe("container app", () => {
+function isDockerRunning() {
+	try {
+		execSync("docker ps", { stdio: "ignore" });
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
+
+/** Indicates whether the test is being run locally (not in CI) AND docker is currently not running on the system */
+const isLocalWithoutDockerRunning =
+	process.env.CI !== "true" && !isDockerRunning();
+
+if (isLocalWithoutDockerRunning) {
+	console.warn(
+		"The tests are running locally but there is no docker instance running on the system, skipping containers tests"
+	);
+}
+
+describe.skipIf(
+	isCINonLinux ||
+		// If the tests are being run locally and docker is not running we just skip this test
+		isLocalWithoutDockerRunning
+)("container app", () => {
+	const server = createTestHarness({
+		workers: [{ configPath: "./wrangler.jsonc" }],
+	});
+
 	beforeAll(async () => {
 		await server.listen();
 	});
