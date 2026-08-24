5e59ab80f5a00e57e0e549913870103730b226b7
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2026-08-24 16:10:55 +0100

    Remove legacy local testing paths

--- packages/vite-plugin-cloudflare/playground/cron-triggers/__tests__/cron-triggers.spec.ts
+++ packages/vite-plugin-cloudflare/playground/cron-triggers/__tests__/cron-triggers.spec.ts
@@ -1,13 +1,8 @@
-import { describe, test } from "vitest";
+import { test } from "vitest";
 import { getTextResponse, serverLogs } from "../../__test-utils__";
 
-describe.each(["/cdn-cgi/local/scheduled", "/cdn-cgi/handler/scheduled"])(
-	"%s",
-	(path) => {
-		test("Supports testing Cron Triggers", async ({ expect }) => {
-			const cronResponse = await getTextResponse(path);
-			expect(cronResponse).toBe("ok");
-			expect(serverLogs.info.join()).toContain("Cron processed");
-		});
-	}
-);
+test("Supports testing Cron Triggers", async ({ expect }) => {
+	const cronResponse = await getTextResponse("/cdn-cgi/local/scheduled");
+	expect(cronResponse).toBe("ok");
+	expect(serverLogs.info.join()).toContain("Cron processed");
+});
