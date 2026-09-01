604c612858f69e568c13cdf621ee7149f6cad1be
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2026-09-01 21:27:47 +0100

    Support child environments

--- packages/vite-plugin-cloudflare/playground/build-output/src/index.ts
+++ packages/vite-plugin-cloudflare/playground/build-output/src/index.ts
@@ -1,10 +1,14 @@
 import { env } from "cloudflare:workers";
+import additionalModule from "./additional-module.txt";
 
 export default {
 	fetch(request) {
 		const url = new URL(request.url);
 		if (url.pathname === "/text-binding") {
 			return new Response(env.MY_TEXT);
+		}
+		if (url.pathname === "/additional-module") {
+			return new Response(additionalModule);
 		}
 		return new Response("hello from worker");
 	},
--- packages/vite-plugin-cloudflare/playground/child-environment/__tests__/child-environment.spec.ts
+++ packages/vite-plugin-cloudflare/playground/child-environment/__tests__/child-environment.spec.ts
@@ -1,10 +1,14 @@
 import { test } from "vitest";
-import { getTextResponse, isBuild } from "../../__test-utils__";
+import { getTextResponse } from "../../__test-utils__";
 
-test.runIf(!isBuild)(
-	"can import module from child environment",
-	async ({ expect }) => {
-		const response = await getTextResponse();
-		expect(response).toBe("Hello from the child environment");
-	}
-);
+test("can import module from child environment", async ({ expect }) => {
+	const response = await getTextResponse();
+	expect(response).toBe("Hello from the child environment");
+});
+
+test("can import additional module from child environment", async ({
+	expect,
+}) => {
+	const response = await getTextResponse("/additional-module");
+	expect(response).toBe("Hello from a child environment additional module\n");
+});
