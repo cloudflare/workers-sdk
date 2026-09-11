2eb98193d48a5d481af480bf2a0616b0496b725d
tmo <tennyson@cloudflare.com>
2026-09-11 17:05:06 +0100

    [wrangler] Pass cron triggers to Miniflare

--- packages/wrangler/src/dev/miniflare/index.ts
+++ packages/wrangler/src/dev/miniflare/index.ts
@@ -1203,6 +1203,7 @@ export async function buildMiniflareOptions(
 				...sitesOptions,
 				...assetOptions,
 				routes: config.routes,
+				cronTriggers: config.crons,
 				outboundService: config.outboundService,
 				zone: config.zone,
 				access: config.access?.dev,
